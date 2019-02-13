3be2b7e366ec69fcc68791cf99710d9deb999ad4
Ewan Dennis <ewan.dennis@sparkpost.com>
2019-02-13 11:10:44 +0000

    /src/stories -> /stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure } from '@storybook/react';
 import '../src/critical.scss';
 import '../src/index.scss';
 
-const req = require.context('../src/stories', true, /.stories.js$/);
+const req = require.context('../stories', true, /.stories.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
