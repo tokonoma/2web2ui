6930eca001d3ca1bca80fce77504f1cc6c1477f8
Ewan Dennis <ewan.dennis@sparkpost.com>
2019-02-20 15:41:34 +0000

    AC-560: remove unnecessary helpers

--- src/selectors/currentUser.js
+++ src/selectors/currentUser.js
@@ -1,7 +1,3 @@
-import { SUBACCOUNT_REPORTING_USER_ROLE } from 'src/constants';
-
 export const userCookieConsentFlagSelector = (state) => !!state.currentUser.cookie_consent;
 
 export const usernameSelector = (state) => state.currentUser.username || state.tfa.username || state.auth.username;
-
-export const isSubaccountReportingUser = (state) => state.currentUser.access_level === SUBACCOUNT_REPORTING_USER_ROLE;
