4dd932d4a4c1c0ab75736d15ec663ed546e544ad
Jimmy Burba <james.burba@sparkpost.com>
2019-02-21 12:21:43 -0500

    AC-563 update copy some more

--- src/pages/users/components/EditForm.js
+++ src/pages/users/components/EditForm.js
@@ -23,12 +23,9 @@ export const EditForm = ({
 
   const roleSection = subaccountReportingUser
     ? (<>
-        <p> This user is assigned to a subaccount. Its role can’t be changed </p>
-        <LabelledValue label="Subaccount name">
-          <PageLink to={`/account/subaccounts/${subaccount.id}`}>{subaccount.name}</PageLink>
-        </LabelledValue>
-        <LabelledValue label="Subaccount ID">
-          {subaccount.id}
+        <p> This user has access to reporting features and read-only template access, limited to a single subaccount. Its role can’t be changed. </p>
+        <LabelledValue label="Subaccount">
+          <PageLink to={`/account/subaccounts/${subaccount.id}`}>{subaccount.name}</PageLink> ({subaccount.id})
         </LabelledValue>
       </>)
     : (<Field
