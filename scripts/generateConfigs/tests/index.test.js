380cbaebf8de5ff2d5ecd84bae5a80689632fdeb
mhossain <mohammad.hossain@sparkpost.com>
2019-02-27 07:32:45 +1100

    full config snapshot

--- scripts/generateConfigs/tests/index.test.js
+++ scripts/generateConfigs/tests/index.test.js
@@ -1,24 +1,4 @@
 const generateConfigs = require("../index");
-const tenants = require("../tenants");
-const constructContent = require("../constructContent");
-
-jest.mock("../tenants", () => ({
-  production: {
-    "tenant1": {},
-    "tenant2": {nextHost: 'tenant2.next.sparkpost.com'},
-    "tenant3": {originHost: 'tenant3.origin.sparkpost.com'},
-    "tenantN": {}
-  }
-}));
-
-jest.mock("../constructContent", () => {
-  return function({ alias, host, nextHost, originHost, tenantId, ...config }) {
-    return {
-      host: `${tenantId}.sparkpost.com`,
-      apiBase: `http://${tenantId}.sparkpost.com/api`
-    };
-  };
-});
 
 describe("generateConfigs", () => {
   it("returns combined configs", () => {
