b135124e41fdc038aa76489d83beb03574e4a453
mhossain <mohammad.hossain@sparkpost.com>
2019-03-05 07:50:12 +1100

    Simplify ip edit page, split ip listing to own component

---
+++ src/pages/ipPools/components/IPList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { TableCollection } from 'src/components';
+
+
+const columns = ['Sending IP', 'Hostname'];
+
+export class IPList extends Component {
+  getRowData = (ip) => {
+    const { submitting, pool } = this.props;
+    const ipLink = <Link to={`/account/ip-pools/edit/${pool.id}/ip/${ip.external_ip}`}>{ip.external_ip}</Link>;
+
+    return [
+      ipLink,
+      ip.hostname
+    ];
+  };
+
+  render() {
+    const { pool, ips } = this.props;
+
+    if (!ips.length) {
+      return null;
+    }
+
+    return (
+      <TableCollection
+        columns={columns}
+        rows={ips}
+        getRowData={this.getRowData}
+        pagination={false}
+      />
+    );
+  }
+}
+
+export default IPList;
--- src/pages/ipPools/components/PoolForm.js
+++ src/pages/ipPools/components/PoolForm.js
@@ -2,18 +2,15 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
-import { Panel, Button, UnstyledLink } from '@sparkpost/matchbox';
+import { Button, Panel, UnstyledLink } from '@sparkpost/matchbox';
 import { SelectWrapper } from 'src/components/reduxFormWrappers';
-import { TableCollection } from 'src/components';
+import { SendingDomainTypeaheadWrapper, TextFieldWrapper } from 'src/components';
 import AccessControl from 'src/components/auth/AccessControl';
 import { required } from 'src/helpers/validation';
 import { configFlag } from 'src/helpers/conditions/config';
-import { TextFieldWrapper, SendingDomainTypeaheadWrapper } from 'src/components';
 import { selectIpPoolFormInitialValues, selectIpsForCurrentPool, shouldShowIpPurchaseCTA } from 'src/selectors/ipPools';
 import isDefaultPool from '../helpers/defaultPool';
-
-
-const columns = ['Sending IP', 'Hostname'];
+import IPList from './IPList';
 
 export class PoolForm extends Component {
   poolSelect = (ip, poolOptions, submitting) => (<Field
@@ -31,68 +28,55 @@ export class PoolForm extends Component {
       ipLink,
       ip.hostname
     ];
-  }
+  };
 
-  renderCollection() {
-    const { isNew, ips, list, pool: currentPool, showPurchaseCTA } = this.props;
-    const poolOptions = list.map((pool) => ({
-      value: pool.id,
-      label: (pool.id === currentPool.id) ? '-- Change Pool --' : `${pool.name} (${pool.id})`
-    }));
-    const getRowDataFunc = this.getRowData.bind(this, poolOptions);
+  renderPoolIps = () => {
+    const { isNew, ips, pool, showPurchaseCTA } = this.props;
 
-    // New pools have no IPs
     if (isNew) {
       return null;
     }
 
-    // Loading
-    if (!ips) {
-      return null;
-    }
-
     const purchaseCTA = showPurchaseCTA
-      ? <Fragment>, or by <UnstyledLink to="/account/billing" component={Link}>purchasing new IPs</UnstyledLink></Fragment>
+      ? <Fragment>, or by <UnstyledLink to="/account/billing" component={Link}>purchasing new
+        IPs</UnstyledLink></Fragment>
       : null;
 
-    return (
-      <Fragment>
-        <Panel.Section>
-          <p>
-            Add dedicated IPs to this pool by moving them from their current pool{purchaseCTA}.
-          </p>
-        </Panel.Section>
-        <TableCollection
-          columns={columns}
-          rows={ips}
-          getRowData={getRowDataFunc}
-          pagination={false}
-        />
-      </Fragment>
-    );
-  }
+
+    return (<Panel title='Sending IPs'>
+      <Panel.Section>
+        <p>
+          Add dedicated sending IPs to this pool by moving them from their current pool{purchaseCTA}.
+          {!!ips.length && <span> Click on existing Sending IP to modify.</span>}
+        </p>
+        <IPList ips={ips} pool={pool}/>
+      </Panel.Section>
+    </Panel>);
+  };
 
   render() {
-    const { isNew, pool, handleSubmit, submitting, pristine } = this.props;
+    const { isNew, ips, pool, handleSubmit, submitting, pristine } = this.props;
     const submitText = isNew ? 'Create IP Pool' : 'Update IP Pool';
     const editingDefault = !isNew && isDefaultPool(pool.id);
     const helpText = editingDefault ? 'You cannot change the default IP pool\'s name' : '';
 
-    return (
-      <Panel>
-        <form onSubmit={handleSubmit}>
-          <Panel.Section>
-            <Field
-              name='name'
-              component={TextFieldWrapper}
-              validate={required}
-              label='Pool Name'
-              placeholder='My IP Pool'
-              disabled={editingDefault || submitting}
-              helpText={helpText}
-            />
 
-            {!editingDefault &&
+    return (
+      <Fragment>
+        <Panel>
+          <form onSubmit={handleSubmit}>
+            <Panel.Section>
+              <Field
+                name='name'
+                component={TextFieldWrapper}
+                validate={required}
+                label='Pool Name'
+                placeholder='My IP Pool'
+                disabled={editingDefault || submitting}
+                helpText={helpText}
+              />
+
+              {!editingDefault &&
               <AccessControl condition={configFlag('featureFlags.allow_default_signing_domains_for_ip_pools')}>
                 <Field
                   name="signing_domain"
@@ -101,18 +85,17 @@ export class PoolForm extends Component {
                   disabled={submitting}
                 />
               </AccessControl>
-            }
-          </Panel.Section>
-
-          {this.renderCollection()}
-
-          <Panel.Section>
-            <Button submit primary disabled={submitting || pristine}>
-              {submitting ? 'Saving' : submitText}
-            </Button>
-          </Panel.Section>
-        </form>
-      </Panel>
+              }
+            </Panel.Section>
+            <Panel.Section>
+              <Button submit primary disabled={submitting || pristine}>
+                {submitting ? 'Saving' : submitText}
+              </Button>
+            </Panel.Section>
+          </form>
+        </Panel>
+        {this.renderPoolIps()}
+      </Fragment>
     );
   }
 }
