782d1fe0f894dc17b9e155e9a824d68680eb545a
Patrick Sison <patrick.m.sison@gmail.com>
2019-03-14 09:28:55 -0400

    AC-647: Changed action popover to be right align to be consistent with other popover

--- src/components/actionPopover/ActionPopover.js
+++ src/components/actionPopover/ActionPopover.js
@@ -3,9 +3,11 @@ import { Popover, Button, ActionList } from '@sparkpost/matchbox';
 import { MoreHoriz } from '@sparkpost/matchbox-icons';
 
 const ActionPopover = ({ actions }) => (
-  <Popover left trigger={<Button flat><MoreHoriz size={20}/></Button>}>
-    <ActionList actions={actions}/>
-  </Popover>
+  <div style={{ textAlign: 'right' }}>
+    <Popover left trigger={<Button flat><MoreHoriz size={20}/></Button>}>
+      <ActionList actions={actions}/>
+    </Popover>
+  </div>
 );
 
 export default ActionPopover;
