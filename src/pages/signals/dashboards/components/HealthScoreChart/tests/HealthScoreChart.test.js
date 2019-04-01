b3db2e34e6ab2e70d4041e065b926ace8cc67112
Jon Ambas <jon@jonambas.com>
2019-04-01 13:37:25 -0400

    EO-661 Add new current score action to time series chart

--- src/pages/signals/dashboards/components/HealthScoreChart/tests/HealthScoreChart.test.js
+++ src/pages/signals/dashboards/components/HealthScoreChart/tests/HealthScoreChart.test.js
@@ -26,27 +26,24 @@ describe('Signals Health Score Chart', () => {
           spam_hits: 1
         }]
       },
-      data: [{
-        sid: -1,
-        current_health_score: 88,
-        WoW: -5, current_DoD: 5,
-        history: [{
-          date: '2019-03-24',
-          health_score: 75,
-          ranking: 'warning'
-        },{
-          date: '2019-03-25',
-          health_score: 96,
-          ranking: 'good'
-        },{
-          date: '2019-03-26',
-          health_score: 23,
-          ranking: 'danger'
-        },{
-          date: '2019-03-27',
-          health_score: null,
-          ranking: null
-        }]
+      current_health_score: 88,
+      WoW: -5, current_DoD: 5,
+      history: [{
+        date: '2019-03-24',
+        health_score: 75,
+        ranking: 'warning'
+      },{
+        date: '2019-03-25',
+        health_score: 96,
+        ranking: 'good'
+      },{
+        date: '2019-03-26',
+        health_score: 23,
+        ranking: 'danger'
+      },{
+        date: '2019-03-27',
+        health_score: null,
+        ranking: null
       }],
       filters: {
         relativeRange: '90days'
@@ -73,7 +70,7 @@ describe('Signals Health Score Chart', () => {
   });
 
   it('renders no account level data callout correctly', () => {
-    props.data = [];
+    props.history = [];
     const wrapper = subject(props);
     expect(wrapper.find('Callout')).toMatchSnapshot();
   });
