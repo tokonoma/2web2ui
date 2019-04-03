854b7dc3830f6f0110185bcc2a0e4d1e998778dd
Brian Kemper <brian.kemper@sparkpost.com>
2019-04-03 17:18:51 -0400

    FE-635 Reworked and tested runtime environment configuration

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -1,25 +1,21 @@
-import _ from 'lodash';
 import devConfig from './dev-config';
 import productionConfig from './production-config';
 import stagingConfig from './staging-config';
 import testConfig from './test-config';
 import uatConfig from './uat-config';
 
-export default (hostname) => {
-  if (process.env.NODE_ENV === 'production') {
+export default (nodeEnv, environment) => {
+  if (nodeEnv === 'production') {
     const configByEnvironment = {
       production: productionConfig,
       staging: stagingConfig,
       uat: uatConfig
     };
-    // see, https://github.com/SparkPost/2web2ui/blob/ab8aea57a4a5a17038467454a3bd1dd4b825f818/config/webpack.config.js#L646
-    const tenantConfig = TENANT_CONFIGS[hostname]; // eslint-disable-line no-undef
-    const envConfig = configByEnvironment[tenantConfig.environment];
 
-    return _.merge(envConfig, tenantConfig);
+    return configByEnvironment[environment];
   }
 
-  if (process.env.NODE_ENV === 'test') {
+  if (nodeEnv === 'test') {
     return testConfig;
   }
 
--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,15 @@ import defaultConfig from './default';
 import envConfig from './env';
 
 const hostname = window.location.hostname;
-const mergedConfig = _.merge({}, defaultConfig(hostname), envConfig(hostname));
+const nodeEnv = process.env.NODE_ENV;
+const tenantConfig = TENANT_CONFIGS[hostname] || {}; // eslint-disable-line no-undef
+
+const mergedConfig = _.merge(
+  {},
+  defaultConfig(hostname),
+  envConfig(nodeEnv, tenantConfig.environment),
+  tenantConfig
+);
 
 window.SP = {
   productionConfig: mergedConfig
