91cb02450c6ff24852de6d4f724398ee5e41b3b9
Jon Ambas <jon@jonambas.com>
2019-04-10 16:20:35 -0400

    EO-623 Use date picker in signals date filter

--- src/pages/signals/components/filters/tests/DateFilter.test.js
+++ src/pages/signals/components/filters/tests/DateFilter.test.js
@@ -1,20 +1,31 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import React from 'react';
 import { DateFilter } from '../DateFilter';
 
 describe('DateFilter', () => {
-  const subject = (props = {}) => shallow(<DateFilter signalOptions={{}} {...props} />);
+  const now = '2018-01-01T05:00:00Z';
+  const subject = (props = {}) => mount(<DateFilter left signalOptions={{}} {...props} now={now} />);
 
-  it('renders a select', () => {
-    expect(subject()).toMatchSnapshot();
+  it('renders datepicker', () => {
+    expect(subject().find('AppDatePicker').props()).toMatchSnapshot();
   });
 
-  it('calls changeSignalOptions when select changes', () => {
+  it('calls changeSignalOptions when dates changes', () => {
     const changeSignalOptions = jest.fn();
     const wrapper = subject({ changeSignalOptions });
+    wrapper.find('AppDatePicker').prop('onChange')('update');
+    expect(changeSignalOptions).toHaveBeenCalledWith('update');
+  });
 
-    wrapper.find('Select').simulate('change', { currentTarget: { value: '14days' }});
+  it('sets dates with a relative range', () => {
+    const wrapper = subject({ signalOptions: { relativeRange: '7days' }});
+    expect(wrapper.find('AppDatePicker').prop('to').toString()).toMatch('Dec 31 2017');
+    expect(wrapper.find('AppDatePicker').prop('from').toString()).toMatch('Dec 24 2017');
+  });
 
-    expect(changeSignalOptions).toHaveBeenCalledWith({ relativeRange: '14days' });
+  it('sets dates with a custom date', () => {
+    const wrapper = subject({ signalOptions: { relativeRange: 'custom', from: new Date('2015-01-04T05:00:00Z'), to: new Date('2015-01-09T05:00:00Z') }});
+    expect(wrapper.find('AppDatePicker').prop('to').toString()).toMatch('Jan 09 2015');
+    expect(wrapper.find('AppDatePicker').prop('from').toString()).toMatch('Jan 04 2015');
   });
 });
