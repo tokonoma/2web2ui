4dfd7f02c4ce45285916cd432904bcc4cc451568
Patrick Sison <patrick.m.sison@gmail.com>
2019-04-17 11:01:11 -0400

    AC-592: Updated test styling

--- src/components/billing/tests/PlanPrice.test.js
+++ src/components/billing/tests/PlanPrice.test.js
@@ -60,18 +60,26 @@ describe('PlanPrice', () => {
   });
 
   it('renders correctly for 30 day free plan', () => {
-    plan.monthly = 0;
-    plan.isFree = true;
-    plan.code = 'free15K-banana';
-    wrapper.setProps({ plan });
+    const free15kPlan = {
+      ...plan,
+      monthly: 0,
+      isFree: true,
+      code: 'free15K-banana'
+    };
+
+    wrapper.setProps({ plan: free15kPlan });
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders correctly for eternal free plan', () => {
-    plan.monthly = 0;
-    plan.isFree = true;
-    plan.code = 'free500-banana';
-    wrapper.setProps({ plan });
+    const eternalFree = {
+      ...plan,
+      monthly: 0,
+      isFree: true,
+      code: 'free500-banana'
+    };
+
+    wrapper.setProps({ plan: eternalFree });
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -82,10 +90,14 @@ describe('PlanPrice', () => {
   });
 
   it('renders test account with current free plan', () => {
-    plan.monthly = 0;
-    plan.isFree = true;
-    plan.code = 'free500-0419';
-    wrapper.setProps({ plan });
+    const currentFreePlan = {
+      ...plan,
+      monthly: 0,
+      isFree: true,
+      code: 'free500-0419'
+    };
+
+    wrapper.setProps({ plan: currentFreePlan });
     expect(wrapper).toMatchSnapshot();
   });
 
