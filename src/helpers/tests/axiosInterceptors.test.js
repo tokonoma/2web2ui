773a9e6cc88384d83f8300d2e75b7237ccc3bbfb
Patrick Sison <patrick.m.sison@gmail.com>
2019-04-17 16:20:03 -0400

    FE-653: Made retry test explicit

--- src/helpers/tests/axiosInterceptors.test.js
+++ src/helpers/tests/axiosInterceptors.test.js
@@ -2,15 +2,22 @@ import {
   sparkpostErrorHandler
 } from '../axiosInterceptors';
 
+import {
+  sparkpostAxios
+} from '../axiosInstances';
+
 let err;
+let requestConfig;
 
 describe('Sparkpost Axios error interceptor', () => {
   beforeEach(() => {
+
+    requestConfig = {
+      method: 'GET',
+      url: '/test'
+    };
     err = {
-      config: {
-        method: 'GET',
-        url: '/test'
-      },
+      config: requestConfig,
       response: {
         status: 404,
         message: 'not found'
@@ -24,7 +31,8 @@ describe('Sparkpost Axios error interceptor', () => {
 
   it('should retry if it is a 5XX error', async () => {
     err.response.status = 500;
-    await expect(sparkpostErrorHandler(err)).resolves.toMatchSnapshot();
+    await sparkpostErrorHandler(err);
+    expect(sparkpostAxios).toHaveBeenCalledWith({ ...requestConfig, retries: 1 });
   });
 
   it('should not retry 5XX if it is at max retries', () => {
