38cafb253897647ec8f0475c1840fe11b13dc838
Brian Kemper <brian.kemper@sparkpost.com>
2019-04-18 10:36:12 -0400

    TR-1206 useTabs 🎣

---
+++ src/hooks/useTabs.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+// This hook is intended to be used with Matchbox's Tabs component
+// note, initialTabs must be defined outside the component or provided by redux
+const useTabs = (initialTabs) => {
+  const [tabIndex, setIndex] = useState(0);
+  const [tabs, setTabs] = useState(initialTabs);
+
+  // only on mount attach click handler to tabs
+  useEffect(() => {
+    const tabsWithClickHandling = initialTabs.map((tab, index) => ({
+      ...tab, onClick: () => { setIndex(index); }
+    }));
+
+    setTabs(tabsWithClickHandling);
+  }, [initialTabs]);
+
+  return [tabIndex, tabs];
+};
+
+export default useTabs;
