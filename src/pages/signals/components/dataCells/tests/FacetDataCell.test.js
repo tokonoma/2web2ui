7b04265b033002aa8e9f76e86239600726ddc96c
Kevin Chu <kevinchu1337@gmail.com>
2019-05-06 12:02:45 -0400

    EO-737 Added tests for friendly label

--- src/pages/signals/components/dataCells/tests/FacetDataCell.test.js
+++ src/pages/signals/components/dataCells/tests/FacetDataCell.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import FacetDataCell from '../FacetDataCell';
+import { MB_PROVIDERS } from 'src/constants';
 
 describe('FacetDataCell', () => {
   const subject = (props = {}) => shallow(
@@ -40,5 +41,19 @@ describe('FacetDataCell', () => {
 
   it('adds a class to truncate its contents', () => {
     expect(subject({ truncate: true })).toMatchSnapshot();
+  });
+
+  it('formats mailbox provider label to friendly value', () => {
+    const props = {
+      facet: 'mb_provider',
+      id: 'other'
+    };
+    const wrapper = subject(props);
+    expect(wrapper.find('PageLink').prop('children')).toEqual(MB_PROVIDERS.other);
+  });
+
+  it('formats default label to id value', () => {
+    const wrapper = subject();
+    expect(wrapper.find('PageLink').prop('children')).toEqual('example.com');
   });
 });
