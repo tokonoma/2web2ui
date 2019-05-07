44e1056659007b042e92fe23fd1aefa43f29d49d
Kevin Chu <kevinchu1337@gmail.com>
2019-05-07 14:47:12 -0400

    EO-737 Address feedback again

--- src/pages/signals/components/dataCells/FacetDataCell.js
+++ src/pages/signals/components/dataCells/FacetDataCell.js
@@ -7,22 +7,24 @@ import classnames from 'classnames';
 
 const FacetDataCell = ({ dimension, facet, id, name, subaccountId, truncate }) => {
 
+  if (facet === 'sid' && id === -1) {
+    return (
+      <div className={classnames(styles.PaddedCell, truncate && styles.OverflowCell)}>
+        Master & All Subaccounts
+      </div>
+    );
+  }
+
   //This is the default case for the label. The switch statements below are for special cases.
   let label = (name) ? `${name} (${id})` : id;
   switch (facet) {
     case 'sid':
-      if (id === -1) {
-        return (
-          <div className={classnames(styles.PaddedCell, truncate && styles.OverflowCell)}>
-            Master & All Subaccounts
-          </div>
-        );
-      } else if (id === 0) {
+      if (id === 0) {
         label = 'Master Account';
       }
       break;
     case 'mb_provider':
-      label = MAILBOX_PROVIDERS[id];
+      label = MAILBOX_PROVIDERS[id] || label;
   }
 
   const search = (subaccountId >= 0) ? setSubaccountQuery(subaccountId) : undefined;
