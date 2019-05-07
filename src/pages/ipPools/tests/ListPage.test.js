3485b65bb9c3911c22cdd6bd13bad45ff9765d91
Patrick Sison <patrick.m.sison@gmail.com>
2019-05-07 14:48:51 -0400

    SD-913: Add request IPs to List page

--- src/pages/ipPools/tests/ListPage.test.js
+++ src/pages/ipPools/tests/ListPage.test.js
@@ -2,6 +2,7 @@ import { shallow } from 'enzyme';
 import React from 'react';
 
 import { IpPoolsList, getRowData, IPWarmupReminderBanner } from '../ListPage';
+import SupportTicketLink from 'src/components/supportTicketLink/SupportTicketLink';
 
 describe('IP Pools List Page', () => {
   let props;
@@ -16,6 +17,7 @@ describe('IP Pools List Page', () => {
       ],
       listPools: jest.fn(() => []),
       showPurchaseCTA: true,
+      isManuallyBilled: false,
       isAdmin: true
     };
 
@@ -54,6 +56,11 @@ describe('IP Pools List Page', () => {
   it('does not render purchase action if showPurchaseCTA is false2', () => {
     wrapper.setProps({ ipPools: [{ name: 'Default', ips: []}], showPurchaseCTA: false });
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders request IP action if showPurchaseCTA is true and isManuallyBilled is true', () => {
+    wrapper.setProps({ isManuallyBilled: true });
+    expect(wrapper.prop('secondaryActions')).toEqual([{ content: 'Request IPs', Component: SupportTicketLink, issueId: 'request_new_ip' } ]);
   });
 
   it('does not render purchase action if isAdmin is false', () => {
