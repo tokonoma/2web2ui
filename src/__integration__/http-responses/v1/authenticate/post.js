230476472019fa4aec45d7f6000fce83e905f2c2
Ewan Dennis <ewan.dennis@sparkpost.com>
2019-05-16 10:50:39 +0100

    FE-678: update mock API responses

--- src/__integration__/http-responses/v1/authenticate/post.js
+++ src/__integration__/http-responses/v1/authenticate/post.js
@@ -1,5 +1,29 @@
-export default ({ params }) => ({
-  access_token: 'mock-access-token',
-  expires_in: 86399,
-  token_type: 'Bearer'
-});
+import qs from 'qs';
+
+const accounts = {
+  'test-username': 'test-password',
+  'tfa-username': 'tfa-password',
+  'tfa-required-username': 'tfa-required-password'
+};
+
+export default ({ data }) => {
+  const { username, password } = qs.parse(data);
+  const acctPassword = accounts[username];
+  if (!acctPassword || password !== acctPassword) {
+    throw {
+      response: {
+        status: 400,
+        data: {
+          error: 'invalid_grant',
+          error_description: 'User credentials are invalid'
+        }
+      }
+    };
+  }
+
+  return {
+    access_token: 'mock-access-token',
+    expires_in: 86399,
+    token_type: 'Bearer'
+  };
+};
