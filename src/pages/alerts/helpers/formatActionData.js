2fd8bbfadd38ea2a543f91c254be2181bd4f4f33
Kevin Chu <kevinchu1337@gmail.com>
2019-05-17 14:22:27 -0400

    EO-767 Fixed alerts formatter and its tests

--- src/pages/alerts/helpers/formatActionData.js
+++ src/pages/alerts/helpers/formatActionData.js
@@ -1,34 +1,35 @@
 import _ from 'lodash';
 
 export default function (values) {
+  const formattedValues = { ...values }; //So values stays immutable
   const { email_addresses, alert_metric, threshold, assignTo, subaccount = {}} = values;
   const splitAddresses = _.split(email_addresses,',');
-  values.email_addresses = _.map(splitAddresses, (splitAddress) => _.trim(splitAddress));
+  formattedValues.email_addresses = _.map(splitAddresses, (splitAddress) => _.trim(splitAddress));
 
   if (alert_metric === 'monthly_sending_limit') {
-    values.threshold.error = {
+    formattedValues.threshold.error = {
       target: parseInt(threshold.error.target),
       comparator: 'gt'
     };
-    delete values.facet_name;
-    delete values.facet_value;
-    return values;
+    delete formattedValues.facet_name;
+    delete formattedValues.facet_value;
+    return formattedValues;
   }
 
-  values.threshold.error.target = parseFloat(threshold.error.target);
+  formattedValues.threshold.error.target = parseFloat(threshold.error.target);
   switch (assignTo) {
     case 'master':
-      values.alert_subaccount = 0;
+      formattedValues.alert_subaccount = 0;
       break;
     case 'subaccount':
-      values.alert_subaccount = parseInt(subaccount.id);
+      formattedValues.alert_subaccount = parseInt(subaccount.id);
       break;
     case 'all':
-      delete values.facet_name;
-      delete values.facet_value;
+      formattedValues.facet_name = 'ALL';
+      delete formattedValues.facet_value;
+      formattedValues.alert_subaccount = -1;
       break;
     default:
-      values.alert_subaccount = -1;
   }
-  return values;
+  return formattedValues;
 }
--- src/pages/alerts/helpers/tests/formatActionData.test.js
+++ src/pages/alerts/helpers/tests/formatActionData.test.js
@@ -1,4 +1,5 @@
 import formatActionData from '../formatActionData';
+import cases from 'jest-in-case';
 
 const subaccount = {
   customer_id: 101,
@@ -9,24 +10,20 @@ const subaccount = {
   status: 'active'
 };
 
-test('formats subaccount with alert_metric signals_health_wow', () => {
-  const input = {
-    id: '19447300-3b8b-11e9-b749-6d43cdcd6095',
-    name: 'JimAlertSignalsThresh',
-    alert_metric: 'signals_health_wow',
-    assignTo: 'subaccount',
-    subaccount,
-    alert_subaccount: 623,
-    email_addresses: 'sparky@sparkpost.com, test@foo.com',
-    facet_name: 'ip_pool',
-    facet_value: 'abc',
-    threshold: { error: { comparator: 'gt', target: 54 }},
-    criteria_metric: 'wow',
-    enabled: true
-  };
+const input = {
+  id: '19447300-3b8b-11e9-b749-6d43cdcd6095',
+  name: 'JimAlertSignalsThresh',
+  alert_metric: 'signals_health_wow',
+  subaccount,
+  email_addresses: 'sparky@sparkpost.com, test@foo.com',
+  facet_name: 'ip_pool',
+  facet_value: 'abc',
+  threshold: { error: { comparator: 'gt', target: 54 }},
+  criteria_metric: 'wow',
+  enabled: true
+};
 
-  expect(formatActionData(input)).toMatchSnapshot();
-});
+const emailAsArray = ['sparky@sparkpost.com', 'test@foo.com'];
 
 describe('Formatter', () => {
   it('should format params for alert metric - Monthly Sending Limit', () => {
@@ -42,5 +39,28 @@ describe('Formatter', () => {
     expect(returnValue.facet_name).toBeUndefined();
     expect(returnValue.facet_value).toBeUndefined();
     expect(returnValue.threshold.error.comparator).toEqual('gt');
+  });
+
+  cases('should initialize alert_subaccount correctly for', ({ input, expected }) => {
+    expect(formatActionData(input)).toEqual(expected);
+  }, {
+    'master account': {
+      input: { ...input, assignTo: 'master' },
+      expected: { ...input, assignTo: 'master', alert_subaccount: 0, email_addresses: emailAsArray }
+    },
+    'a subaccount': {
+      input: { ...input, assignTo: 'subaccount' },
+      expected: { ...input, assignTo: 'subaccount', alert_subaccount: 623, email_addresses: emailAsArray }
+    },
+    'master and all subaccount': {
+      input: { ...input, assignTo: 'all' },
+      expected: {
+        ...input,
+        assignTo: 'all',
+        alert_subaccount: -1,
+        email_addresses: emailAsArray,
+        facet_name: 'ALL',
+        facet_value: undefined }
+    }
   });
 });
