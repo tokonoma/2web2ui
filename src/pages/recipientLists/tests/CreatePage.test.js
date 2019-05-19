cb65ac138c1b9d1d86887e9ed0fece246a5da25a
mhossain <mohammad.hossain@sparkpost.com>
2019-05-19 19:35:09 -0400

    Add tests for CreatePage

--- src/pages/recipientLists/tests/CreatePage.test.js
+++ src/pages/recipientLists/tests/CreatePage.test.js
@@ -2,25 +2,55 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { CreatePage } from '../CreatePage';
+import parseRecipientListCsv from '../helpers/csv';
+
+jest.mock('../helpers/csv');
 
 describe('CreatePage', () => {
   let props;
+  let wrapper;
 
   beforeEach(() => {
     props = {
-      createRecipientList: jest.fn(() => Promise.resolve()),
+      createRecipientList: jest.fn((args) => Promise.resolve({ id: args.id })),
       showAlert: jest.fn(),
-      history: { push: jest.fn() }
+      history: { push: jest.fn() },
+      handleSubmit: jest.fn()
     };
+
+    wrapper = shallow(<CreatePage {...props} />);
   });
 
   it('should render correctly', () => {
-    expect(shallow(<CreatePage />)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('should create a recipient list', async() => {
-    const wrapper = shallow(<CreatePage {...props} />);
-    await wrapper.instance().createRecipientList();
-    expect(props.createRecipientList).toHaveBeenCalled();
+
+  describe('parseCsv', () => {
+    it('saves recipients when valid', async () => {
+      const recipients = [{ address: { name: 'foo', email: 'foo@domain.com' }}];
+      parseRecipientListCsv.mockImplementationOnce(() => Promise.resolve(recipients));
+      await wrapper.instance().parseCsv();
+      expect(wrapper.state('recipients')).toEqual(recipients);
+    });
+
+    it('alerts errors on parsing error', async () => {
+      const csvErrors = [
+        'Line 73: Too many notes',
+        'Line 247: Vanilla is unacceptable'
+      ];
+
+      parseRecipientListCsv.mockImplementationOnce(() => Promise.reject(csvErrors));
+      await wrapper.instance().parseCsv();
+      expect(props.showAlert).toHaveBeenCalledWith({ type: 'error', message: csvErrors });
+    });
+  });
+
+  describe('createRecipientsList', () => {
+    it('should POST with recipients & without csv prop', async () => {
+      const data = { name: 'Foo', id: 'foo', description: 'foo bar', csv: 'hello' };
+      await wrapper.instance().createRecipientsList(data);
+      expect(props.createRecipientList).toHaveBeenCalledWith({ name: 'Foo', id: 'foo', description: 'foo bar', recipients: []});
+    });
   });
 });
