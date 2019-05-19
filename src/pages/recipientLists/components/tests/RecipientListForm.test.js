64b592ee655d503d07ff6402236f6452b4bdb4fd
mhossain <mohammad.hossain@sparkpost.com>
2019-05-19 19:34:03 -0400

    Add tests for RecipientListForm

--- src/pages/recipientLists/components/tests/RecipientListForm.test.js
+++ src/pages/recipientLists/components/tests/RecipientListForm.test.js
@@ -6,85 +6,33 @@ import { RecipientListForm } from '../RecipientListForm';
 
 describe('RecipientListForm', () => {
   let props;
-  let formValues;
-  let csv;
+  let wrapper;
 
   beforeEach(() => {
     props = {
-      handleSubmit: jest.fn()
-    };
-    formValues = {
-      name: 'Freddie II Jr',
-      id: 'fred-2-jr',
-      description: 'Royalty amongst the fredericks'
+      onSubmit: jest.fn()
     };
 
-    csv = 'email,metadata\nscratch@example.com,"{""flavor"":""vanilla""}"\n';
+    wrapper = shallow(<RecipientListForm {...props} />);
   });
 
   it('defaults to create mode', () => {
-    const wrapper = shallow(<RecipientListForm {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders correctly in create mode', () => {
-    const wrapper = shallow(<RecipientListForm editMode={false} {...props} />);
+    wrapper.setProps({ editMode: false });
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders correctly in edit mode', () => {
-    const wrapper = shallow(<RecipientListForm editMode={true} {...props} />);
+  it('renders disable id in edit mode', () => {
+    wrapper.setProps({ editMode: true });
+    expect(wrapper.find('Field[name="id"]').prop('disabled')).toBe(true);
+  });
+
+  it('should disable form elements during submit', () => {
+    wrapper.setProps({ submitting: true });
     expect(wrapper).toMatchSnapshot();
-  });
-
-  it('renders CSV errors', () => {
-    const wrapper = shallow(<RecipientListForm editMode={false} {...props} />);
-    const csvErrors = [
-      'Line 73: Too many notes',
-      'Line 247: Vanilla is unacceptable'
-    ];
-    wrapper.setProps({ error: csvErrors });
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should disable form elements on submit', () => {
-    const wrapper = shallow(<RecipientListForm submitting={true} {...props} />);
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should submit without recipients', async() => {
-    const onSubmit = jest.fn();
-    const wrapper = shallow(<RecipientListForm onSubmit={onSubmit} {...props} />);
-    await wrapper.instance().preSubmit(formValues);
-    expect(onSubmit).toHaveBeenCalledWith(formValues);
-  });
-
-  it('should submit with recipients', async() => {
-    const formValuesWithCsv = { csv, ...formValues };
-    const parsedFormValues = {
-      recipients: [
-        {
-          address: {
-            email: 'scratch@example.com'
-          },
-          metadata: { flavor: 'vanilla' }
-        }
-      ],
-      ...formValues
-    };
-    const onSubmit = jest.fn();
-    const wrapper = shallow(<RecipientListForm onSubmit={onSubmit} {...props} />);
-    await wrapper.instance().preSubmit(formValuesWithCsv);
-    expect(onSubmit).toHaveBeenCalledWith(parsedFormValues);
-  });
-
-  it('should throw on submit if CSV parsing fails', () => {
-    const csv = 'email,metadata\nscratchexample.com,"{""flavor"":""vanilla"""\n';
-    const formValuesWithCsv = { csv, ...formValues };
-    const onSubmit = jest.fn();
-    const wrapper = shallow(<RecipientListForm onSubmit={onSubmit} {...props} />);
-    expect(wrapper.instance().preSubmit(formValuesWithCsv)).rejects.toMatchSnapshot();
-    expect(onSubmit).not.toHaveBeenCalled();
   });
 });
 
