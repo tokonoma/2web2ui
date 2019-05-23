8089b1ed0f5ae2baf26f9140a03e6a1e141c745b
Ewan Dennis <ewan.dennis@sparkpost.com>
2019-05-23 10:51:19 +0100

    FE-662: fix setup(Rest|Smtp) func tests

--- src/__func__/signup/setupRest.test.js
+++ src/__func__/signup/setupRest.test.js
@@ -1,6 +1,8 @@
 import mountRoute from 'src/__testHelpers__/mountRoute';
+import config from 'src/config';
 
 test('Signup flow: REST setup', async () => {
+  config.featureFlags.has_signup = true;
   const page = await mountRoute('/onboarding/email/api');
   expect(page.mockApiCalls).toMatchSnapshot();
 });
--- src/__func__/signup/setupSmtp.test.js
+++ src/__func__/signup/setupSmtp.test.js
@@ -1,6 +1,8 @@
 import mountRoute from 'src/__testHelpers__/mountRoute';
+import config from 'src/config';
 
 test('Signup flow: SMTP setup', async () => {
+  config.featureFlags.has_signup = true;
   const page = await mountRoute('/onboarding/email/smtp');
   expect(page.mockApiCalls).toMatchSnapshot();
 });
