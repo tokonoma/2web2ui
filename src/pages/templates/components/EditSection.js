f86fe1080f8c865e57ab18c05c8397bce668dd7e
Brian Kemper <brian.kemper@sparkpost.com>
2019-05-24 11:32:48 -0400

    TR-1436 Tweaked style of editor and preview panels to match

--- src/pages/templates/components/EditSection.js
+++ src/pages/templates/components/EditSection.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Panel, Tabs } from '@sparkpost/matchbox';
 import tabs from '../constants/editTabs';
 import useEditorContext from '../hooks/useEditorContext';
+import styles from './EditSection.module.scss';
 
 const EditSection = () => {
   const { currentTabIndex, setTab } = useEditorContext();
   const TabComponent = tabs[currentTabIndex].render;
 
   return (
-    <Panel>
-      <Tabs
-        color="blue"
-        selected={currentTabIndex}
-        onSelect={(nextTabIndex) => { setTab(nextTabIndex); }}
-        tabs={tabs.map(({ render, ...tab }) => tab)}
-      />
+    <Panel className={styles.EditSection}>
+      <div className={styles.TabsWrapper}>
+        <Tabs
+          color="blue"
+          selected={currentTabIndex}
+          onSelect={(nextTabIndex) => { setTab(nextTabIndex); }}
+          tabs={tabs.map(({ render, ...tab }) => tab)}
+        />
+      </div>
       <TabComponent />
     </Panel>
   );
--- src/pages/templates/components/Editor.js
+++ src/pages/templates/components/Editor.js
@@ -3,33 +3,37 @@ import AceEditor from 'react-ace';
 import 'brace/ext/searchbox';
 import 'brace/mode/text';
 import 'brace/theme/chaos';
+import styles from './Editor.module.scss';
 
 const Editor = ({ editorProps = {}, setOptions = {}, mode = 'text', value, ...props }) => (
-  <AceEditor
-    {...props}
-    cursorStart={1}
-    editorProps={{
-      ...editorProps,
-      $blockScrolling: Infinity
-    }}
-    fontSize={12}
-    highlightActiveLine
-    // note, must global import modes
-    mode={mode}
-    setOptions={{
-      // note, disabling worker only disables linting annotations and does not affect syntax highlighting
-      // see, https://github.com/securingsincity/react-ace/issues/275
-      useWorker: false,
-      ...setOptions,
-      displayIndentGuides: false
-    }}
-    showPrintMargin={false}
-    tabSize={2}
-    theme="chaos"
-    // note, template endpoint allows null content, but Ace doesn't
-    value={value === null ? '' : value}
-    width="auto"
-  />
+  <div className={styles.EditorWrapper}>
+    <AceEditor
+      {...props}
+      cursorStart={1}
+      editorProps={{
+        ...editorProps,
+        $blockScrolling: Infinity
+      }}
+      fontSize={12}
+      height="90vh"
+      highlightActiveLine
+      // note, must global import modes
+      mode={mode}
+      setOptions={{
+        // note, disabling worker only disables linting annotations and does not affect syntax highlighting
+        // see, https://github.com/securingsincity/react-ace/issues/275
+        useWorker: false,
+        ...setOptions,
+        displayIndentGuides: false
+      }}
+      showPrintMargin={false}
+      tabSize={2}
+      theme="chaos"
+      // note, template endpoint allows null content, but Ace doesn't
+      value={value === null ? '' : value}
+      width="auto"
+    />
+  </div>
 );
 
 export default Editor;
