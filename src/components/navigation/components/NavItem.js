82f4fee1444856babbef8d4543c0162e43b7e11a
mhossain <mohammad.hossain@sparkpost.com>
2019-05-26 12:41:42 -0400

    fix snapshots

--- src/components/navigation/components/NavItem.js
+++ src/components/navigation/components/NavItem.js
@@ -3,12 +3,14 @@ import { WindowSizeContext } from 'src/context/WindowSize';
 import RenderItem from './Item';
 import RenderItemWithChildren from './ItemWithChildren';
 
-export default function (props) {
+const NavItem = (props) => {
   function renderNavItem() {
     return props.children ? <RenderItemWithChildren {...props} /> : <RenderItem {...props} />;
   }
 
   return <WindowSizeContext.Consumer children={renderNavItem}/>;
 
-}
+};
 
+NavItem.displayName = 'NavItem';
+export default NavItem;
