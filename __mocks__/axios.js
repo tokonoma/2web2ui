0c669c4fabd92115ef8ec40f0ad288eaa4df6adb
Ewan Dennis <ewan.dennis@sparkpost.com>
2019-05-28 13:44:19 +0100

    FE-664: move mock API responses out of __integration__

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,4 +1,4 @@
-import lookup from 'src/__integration__/http-responses';
+import lookup from 'src/__testHelpers__/http-responses';
 
 // lookup() returns an API response body object or throws on error to simulate axios exception behaviour
 export const singleton = jest.fn((request) => new Promise((resolve) => resolve(lookup(request))));
