4bebb774a573943039872246eccadae34fa16895
Brian Kemper <brian.kemper@sparkpost.com>
2019-05-29 18:06:19 -0400

    TR-1282 useEditor hook to manage Ace annotations

---
+++ src/pages/templates/hooks/useEditor.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+const useEditor = ({ inlineErrors = []}) => {
+  const [annotations, setAnnotations] = useState([]);
+  const [editor, setEditor] = useState(); // instance of the Ace editor
+
+  // Merge annotations from Ace modes and append our custom inline errors
+  const nextAnnotations = [
+    ...annotations.filter(({ custom }) => !custom),
+    ...inlineErrors.map(({ message, line }) => ({
+      column: 0,
+      custom: true, // tag custom annotations to be excluded during update
+      row: line - 1,
+      text: message,
+      type: 'error'
+    }))
+  ];
+
+  // note, this is a hack to work around this bug and poor behavior of annotations prop
+  // see, https://github.com/securingsincity/react-ace/issues/483
+  useEffect(() => {
+    if (editor) {
+      // note, this will trigger onValidate which should call our setAnnotations
+      editor.getSession().setAnnotations(nextAnnotations);
+    }
+  }, [editor, nextAnnotations]);
+
+  return {
+    annotations,
+    editor,
+    setAnnotations,
+    setEditor
+  };
+};
+
+export default useEditor;
