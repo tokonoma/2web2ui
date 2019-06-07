c82a08860efb318e22ddea004af87dbdfc7760ce
Kevin Chu <kevinchu1337@gmail.com>
2019-06-07 13:57:31 -0400

    EO-802 Changed divider line date

--- src/pages/signals/constants/healthScoreV2.js
+++ src/pages/signals/constants/healthScoreV2.js
@@ -1,15 +1,16 @@
 import styles from 'src/pages/signals/dashboards/components/HealthScoreChart/HealthScoreChart.module.scss';
 
+const V2Date = '2019-06-08';
+
 export const newModelLine = [
   {
-    x: '2019-06-07',
+    x: V2Date,
     stroke: '#d2d2d7',
     strokeWidth: 1,
     label: { position: 'top', value: 'New', offset: 14, className: styles.xDividerLabel }
   },
   {
-    x: '2019-06-07',
-    stroke: '#d2d2d7',
+    x: V2Date,
     strokeWidth: 0,
     label: { position: 'top', value: 'Model', className: styles.xDividerLabel }
   }
@@ -21,4 +22,3 @@ export const newModelMarginsHealthScore =
 
 export const newModelMarginsOther =
 { top: 12, left: 18, right: 8, bottom: 5 };
-
