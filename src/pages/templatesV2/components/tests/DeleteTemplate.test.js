76f41312652b4ce9299801c3474fe5a4ab75ce2b
mhossain <mohammad.hossain@sparkpost.com>
2019-06-17 15:13:47 -0400

    Add tests for DeleteTemplate

---
+++ src/pages/templatesV2/components/tests/DeleteTemplate.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import useEditorContext from '../../hooks/useEditorContext';
+import DeleteTemplate from '../DeleteTemplate';
+
+jest.mock('../../hooks/useEditorContext');
+
+describe('DeleteTemplate', () => {
+  const subject = (editorState = {}, props = {}) => {
+    useEditorContext.mockReturnValue({
+      draft: { id: 'foo' },
+      isDeletePending: false,
+      deleteTemplate: jest.fn(() => Promise.resolve()),
+      ...editorState
+    });
+
+    return shallow(<DeleteTemplate {...props}>Delete</DeleteTemplate>);
+  };
+
+  it('renders delete button', () => {
+    expect(subject()).toMatchSnapshot();
+  });
+
+  it('shows modal upon clicking delete button', () => {
+    const wrapper = subject();
+    wrapper.find('Button').simulate('click');
+    expect(wrapper.find('DeleteModal').prop('open')).toBe(true);
+  });
+
+  it('deletes template upon confirmation', async () => {
+    const mockDelete = jest.fn(() => Promise.resolve());
+    const wrapper = subject({ deleteTemplate: mockDelete });
+    wrapper.find('Button').simulate('click');
+    await wrapper.find('DeleteModal').prop('onDelete')();
+    expect(mockDelete).toHaveBeenCalledWith('foo', undefined);
+  });
+
+  it('deletes template with subaccount upon confirmation', async () => {
+    const mockDelete = jest.fn(() => Promise.resolve());
+    const wrapper = subject({ deleteTemplate: mockDelete, subaccountId: 101 });
+    wrapper.find('Button').simulate('click');
+    await wrapper.find('DeleteModal').prop('onDelete')();
+    expect(mockDelete).toHaveBeenCalledWith('foo', 101);
+  });
+
+  it('invokes afterDelete upon deletion', async () => {
+    const mockAfterDelete = jest.fn();
+    const wrapper = subject({}, { afterDelete: mockAfterDelete });
+    wrapper.find('Button').simulate('click');
+    await wrapper.find('DeleteModal').prop('onDelete')();
+    expect(mockAfterDelete).toHaveBeenCalled();
+  });
+
+
+});
