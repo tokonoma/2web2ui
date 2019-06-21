54251a1fb8e16dcd30556f3799071a8e4cd416bd
mhossain <mohammad.hossain@sparkpost.com>
2019-06-21 13:09:37 -0400

    Update tests for editor actions

--- src/pages/templatesV2/components/editorActions/tests/PublishedModeActions.test.js
+++ src/pages/templatesV2/components/editorActions/tests/PublishedModeActions.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import useEditorContext from '../../../hooks/useEditorContext';
 import PublishedModeActions from '../PublishedModeActions';
 
+jest.mock('../../../hooks/useEditorContext');
+
 describe('PublishedModeActions', () => {
+  const subject = (editorState) => {
+    useEditorContext.mockReturnValue({
+      hasDraft: true,
+      ...editorState
+    });
+
+    return shallow(<PublishedModeActions />);
+  };
+
   it('renders published actions', () => {
-    expect(shallow(<PublishedModeActions/>)).toMatchSnapshot();
+    expect(subject()).toMatchSnapshot();
   });
 });
