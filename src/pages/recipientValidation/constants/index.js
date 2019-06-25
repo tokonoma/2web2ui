5980826815358aa7b793b239548b372d707f62a2
Patrick Sison <patrick.m.sison@gmail.com>
2019-06-25 02:11:40 -0400

    EO-847: Included icons

---
+++ src/pages/recipientValidation/constants/index.js
@@ -0,0 +1,9 @@
+export const ROLE_TOOLTIP = 'Role email addresses represent a group or activity (sales, abuse) \
+  and not an individual. They\'re not considered opt-in and are abused by \
+  web-scraping tactics. They tend to have a lower engagement rate and a higher unsubscribe rate.';
+
+export const DISPOSABLE_TOOLTIP = 'Disposable email providers are services that create email \
+  addresses that are discarded after one use or a short time period. These addresses should be avoided.';
+
+export const FREE_TOOLTIP = 'Free email providers such as Gmail allow anyone to sign up \
+  for an email address. Businesses may consider blocking these to eliminate low-quality leads.';
