5c73358a176426e229ef7f110884040732f3f51a
Kevin Chu <kevinchu1337@gmail.com>
2019-07-23 15:53:36 -0400

    EO-894 - Remove alerts action/reducer/selector

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,6 @@ import accessControlReady from './accessControlReady';
 import account from './account';
 import accountSingleSignOn from './accountSingleSignOn';
 import abTesting from './abTesting';
-import alerts from './alerts';
 import alertsV1 from './alertsV1';
 import apiKeys from './api-keys';
 import auth from './auth';
@@ -53,7 +52,6 @@ const appReducer = combineReducers({
   accessControlReady,
   account,
   accountSingleSignOn,
-  alerts,
   alertsV1,
   auth,
   billing,
