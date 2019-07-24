e7ce8efd40b95f94ae07a34ae39073dc19cf6f2d
Patrick Sison <patrick.m.sison@gmail.com>
2019-07-24 11:47:41 -0400

    SE-118: Split helper functions for calculating cost

--- src/pages/billing/helpers/totalRecipientValidationCost.js
+++ src/pages/billing/helpers/totalRecipientValidationCost.js
@@ -1,13 +1,18 @@
 import { RECIPIENT_VALIDATION_TIERS } from 'src/constants';
 import { formatCurrency } from 'src/helpers/units';
 
-const totalRVCost = (volume = 0) => {
-
+export const calculateCost = (volume = 0) => {
   const totalCost = RECIPIENT_VALIDATION_TIERS.reduce((acc, { volumeMax, volumeMin, cost }) => (
     acc + Math.max(Math.min(volumeMax, volume) - volumeMin, 0) * cost
   ), 0);
-
-  return formatCurrency(totalCost);
+  return totalCost;
 };
+
+export const calculateNewCost = (existingVolume = 0, newVolume = 0) => {
+  const diff = calculateCost(newVolume + existingVolume) - calculateCost(existingVolume);
+  return formatCurrency(diff);
+};
+
+const totalRVCost = (volume = 0) => formatCurrency(calculateCost(volume));
 
 export default totalRVCost;
