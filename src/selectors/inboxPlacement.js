688ee8d51557e149ecf7f7d117e744b7ba30bd23
mhossain <mohammad.hossain@sparkpost.com>
2019-07-30 19:42:57 -0400

    add tests for selector

--- src/selectors/inboxPlacement.js
+++ src/selectors/inboxPlacement.js
@@ -4,4 +4,4 @@ import endsWith from 'lodash/endsWith';
 export const getSeeds = (state) => state.inboxPlacement.seeds;
 
 export const selectReferenceSeed = createSelector(
-  [getSeeds], (seeds) => seeds.find((seed) => endsWith(seed, 'sparkpost.com')));
+  [getSeeds], (seeds) => seeds.find((seed) => endsWith(seed, '@seed.sparkpost.com')));
