162b39ea88347ecf3fc60507ecac818b3aa5f277
mhossain <mohammad.hossain@sparkpost.com>
2019-07-30 12:17:47 -0400

    remove SeedList Page

--- src/components/collection/SaveCSVButton.js
+++ src/components/collection/SaveCSVButton.js
@@ -2,14 +2,14 @@ import { Button } from '@sparkpost/matchbox';
 import React from 'react';
 import { formatToCsv } from 'src/helpers/downloading';
 
-const SaveCSVButton = ({ data, saveCsv, caption = 'Save As CSV', ...props }) => {
+const SaveCSVButton = ({ data, saveCsv }) => {
   const now = Math.floor(Date.now() / 1000);
 
   if (!saveCsv || !data) {
     return null;
   }
 
-  return <Button download={`sparkpost-csv-${now}.csv`} to={formatToCsv({ data, returnBlob: false })} {...props}>{caption}</Button>;
+  return <Button download={`sparkpost-csv-${now}.csv`} to={formatToCsv({ data, returnBlob: false })}>Save As CSV</Button>;
 };
 
 export default SaveCSVButton;
--- src/pages/inboxPlacement/index.js
+++
@@ -1,5 +0,0 @@
-import SeedListPage from './SeedListPage';
-
-export default {
-  SeedListPage
-};
