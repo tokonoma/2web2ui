5340477d035013ee63a390b76823967c3ac76e29
Kevin Chu <kevinchu1337@gmail.com>
2019-07-31 13:11:31 -0400

    EO-894 - Remove unnecessary files

--- src/pages/alerts/constants/criteria.js
+++
@@ -1,5 +0,0 @@
-export const CRITERIA = {
-  wow: 'Week over week',
-  dod: 'Day over Day',
-  threshold: 'Threshold'
-};
--- src/pages/alerts/constants/facets.js
+++
@@ -1,7 +0,0 @@
-export const FACETS = {
-  ALL: 'None',
-  campaign_id: 'Campaign ID',
-  ip_pool: 'IP Pool',
-  mb_provider: 'Mailbox Provider',
-  sending_domain: 'Sending Domain'
-};
