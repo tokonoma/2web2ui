2f7c26fb9560a6839885150ad6862e64f35aa352
Kevin Chu <kevinchu1337@gmail.com>
2019-08-02 15:37:52 -0400

    Update useTabs to allow default tab

--- src/hooks/useTabs/useTabs.js
+++ src/hooks/useTabs/useTabs.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react';
 
 // This hook is intended to be used with Matchbox's Tabs component
 // note, initialTabs must be defined outside the component or provided by redux
-const useTabs = (initialTabs) => {
-  const [tabIndex, setIndex] = useState(0);
+const useTabs = (initialTabs, initialIndex = 0) => {
+  const [tabIndex, setIndex] = useState(initialIndex);
   const [tabs, setTabs] = useState(initialTabs);
 
   // only on mount attach click handler to tabs
--- src/hooks/useTabs/useTabs.test.js
+++ src/hooks/useTabs/useTabs.test.js
@@ -5,20 +5,25 @@ import useTabs from '../useTabs';
 
 describe('useTabs', () => {
   const HoodedComponent = (props) => {
-    const [tabIndex, tabs] = useTabs(props.tabs);
+    const [tabIndex, tabs] = useTabs(props.tabs, props.initialIndex);
 
     return (
       <div tabIndex={tabIndex} tabs={tabs} />
     );
   };
 
-  const subject = () => mount(
-    <HoodedComponent tabs={[{ content: 'A' }, { content: 'B' }]} />
+  const subject = (initialIndex) => mount(
+    <HoodedComponent tabs={[{ content: 'A' }, { content: 'B' }]} initialIndex={initialIndex} />
   );
 
   it('sets initial index to zero', () => {
     const wrapper = subject();
     expect(wrapper.children()).toHaveProp('tabIndex', 0);
+  });
+
+  it('sets initial index to initial index prop', () => {
+    const wrapper = subject(1);
+    expect(wrapper.children()).toHaveProp('tabIndex', 1);
   });
 
   it('changes index when clicked', () => {
