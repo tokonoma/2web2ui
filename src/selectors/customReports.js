9a989cac1182ef7b3bb5ce3f0d1ed582a6a5e286
Jon Ambas <jon@jonambas.com>
2019-08-08 14:39:03 -0400

    SA-737 Add a comment to explain a false array value

--- src/selectors/customReports.js
+++ src/selectors/customReports.js
@@ -5,6 +5,8 @@ import _ from 'lodash';
 export const selectReportsEnhancementsEnabled = (state) => selectCondition(isUserUiOptionSet('feature_reports_enhancements', true))(state);
 
 export const selectCustomReports = (state) => {
+  // A value of `false` represents zero saved reports
+  // `false` is used instead of `[]` because API does not correctly update with an empty array
   const reports = _.get(state, 'currentUser.options.ui.customReports') || [];
   return _.sortBy(reports, [({ name = '' }) => name.toLowerCase()]);
 };
