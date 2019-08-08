f9fcd67e4dd24c0649612585b43c0afb183c29cd
mhossain <mohammad.hossain@sparkpost.com>
2019-08-08 12:32:27 -0400

    remove top/bottom border

--- src/pages/inboxPlacement/components/ProvidersBreakdown.js
+++ src/pages/inboxPlacement/components/ProvidersBreakdown.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Grid, Table } from '@sparkpost/matchbox';
+
 import styles from './ProvidersBreakdown.module.scss';
 
 export const GroupPercentage = ({ value }) => <span>{parseFloat(value * 100).toFixed(2)}%</span>;
 
-const MailBoxGroup = (groupData, index) => (<Table.Row key={index}>
+const MailBoxGroup = (groupData, index) => (<Table.Row key={index} className={styles.TableRow}>
   <Table.Cell className={styles.ProviderCell}>
-    <span>{groupData.mailbox_provider}</span>
+    <Link to={`/inbox-placement/details/${groupData.id}`}>
+      <strong>{groupData.mailbox_provider}</strong>
+    </Link>
   </Table.Cell>
-  <Table.Cell className={styles.CellBorder}>
+  <Table.Cell className={styles.TableCell}>
     <Grid>
-      <Grid.Column className={styles.ValueCell}>
+      <Grid.Column className={styles.GridColumn}>
         <GroupPercentage value={groupData.placement.inbox_pct}/>
       </Grid.Column>
-      <Grid.Column className={styles.ValueCell}>
+      <Grid.Column className={styles.GridColumn}>
         <GroupPercentage value={groupData.placement.spam_pct}/>
       </Grid.Column>
-      <Grid.Column className={styles.ValueCell}>
+      <Grid.Column className={styles.GridColumn}>
         <GroupPercentage value={groupData.placement.missing_pct}/>
       </Grid.Column>
     </Grid>
@@ -25,13 +29,13 @@ const MailBoxGroup = (groupData, index) => (<Table.Row key={index}>
 
 const ProvidersBreakdown = ({ data = []}) => (<Table className={styles.ProvidersBreakDown}>
   <tbody>
-    <Table.Row>
+    <Table.Row className={styles.TableRowHeader}>
       <Table.HeaderCell>Mailbox Provider</Table.HeaderCell>
-      <Table.HeaderCell>
+      <Table.HeaderCell className={styles.TableCell}>
         <Grid>
-          <Grid.Column className={styles.ValueCell}>Inbox</Grid.Column>
-          <Grid.Column className={styles.ValueCell}>Spam</Grid.Column>
-          <Grid.Column className={styles.ValueCell}>Missing</Grid.Column>
+          <Grid.Column className={styles.GridColumn}>Inbox</Grid.Column>
+          <Grid.Column className={styles.GridColumn}>Spam</Grid.Column>
+          <Grid.Column className={styles.GridColumn}>Missing</Grid.Column>
         </Grid>
       </Table.HeaderCell>
     </Table.Row>
