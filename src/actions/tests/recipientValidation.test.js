69c26f4fe3d08ea9ec382c3a91aa924720f708fa
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-08-21 14:20:59 -0400

    SE-157 - Add missing test for uploadListNew

--- src/actions/tests/recipientValidation.test.js
+++ src/actions/tests/recipientValidation.test.js
@@ -1,6 +1,7 @@
 import { snapshotActionCases } from 'src/__testHelpers__/snapshotActionHelpers';
 import {
   uploadList,
+  uploadListNew,
   singleAddress,
   getLatestJob,
   getJobStatus,
@@ -16,6 +17,17 @@ describe('Recipient Verification Actions', () => {
       name: 'when uploading csv',
       action: () => (
         uploadList({
+          data: 'csv-file'
+        })
+      )
+    }
+  ]);
+
+  snapshotActionCases('.uploadListNew', [
+    {
+      name: 'when uploading csv',
+      action: () => (
+        uploadListNew({
           data: 'csv-file'
         })
       )
