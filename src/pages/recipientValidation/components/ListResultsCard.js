5e5612a38a70fdf443b888f49de5174d4fc95f5c
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-08-22 11:58:03 -0400

    Resolve ESLint issues

--- src/pages/recipientValidation/components/ListResultsCard.js
+++ src/pages/recipientValidation/components/ListResultsCard.js
@@ -1,130 +1,89 @@
 import React from 'react';
-import { Panel, Button, Tag, Table, UnstyledLink } from '@sparkpost/matchbox';
+import { Panel, Tag } from '@sparkpost/matchbox';
 import { Error, FileDownload, CheckCircle, Cached, CloudUpload } from '@sparkpost/matchbox-icons';
 import DownloadLink from 'src/components/downloadLink/DownloadLink';
 import TableCollection from 'src/components/collection/TableCollection';
 import PageLink from 'src/components/pageLink/PageLink';
 import { formatDateTime, toMilliseconds } from 'src/helpers/date';
+import styles from './ListResultsCard.module.scss';
 
 const statusProps = {
   error: {
-    className: styles.Failed
+    className: styles.Failed,
     icon: Error,
     message: 'Failed. Please try again.'
+  },
+  success: {
+    className: styles.Complete,
+    icon: CheckCircle,
+    message: 'Completed'
+  },
+  queued_for_batch: {
+    className: styles.Ready,
+    icon: CloudUpload,
+    message: 'Ready to validate'
+  },
+  loading: {
+    className: styles.Loading,
+    icon: Cached,
+    message: 'Processing'
   }
 };
 
 const ListResultsCard = ({ results = {}, newListUpload }) => {
-  // //TODO: Remove newListUpload in SE-156
-  // const renderRow = ({ complete = 'unknown', uploaded, rejectedUrl, status = 'unknown', filename, key }) => {
-  //
-  /
-  //
-  //
-  //   };
-  //
-  //   return (
-  //     <Table.Row className={styles.TableRow} key={`rvlist-${key}`}>
-  //       <Table.Cell className={styles.TableCell}>
-  //         {renderStatus()}
-  //       </Table.Cell>
-  //       <Table.Cell className={styles.TableCell}>
-  //         {ready && (
-  //           <div className={styles.DownloadButton}>
-  //             <DownloadLink
-  //               component={Button}
-  //               to={rejectedUrl}>
-  //               Download Results <FileDownload />
-  //             </DownloadLink>
-  //           </div>
-  //         )}
-  //       </Table.Cell>
-  //     </Table.Row>
-  //   );
-  // };
-
   const columns = [
     {
-      dataCellComponent: ({ filename, jobId }) => {
-        return (
-          <PageLink to={`/recipient-validation/list/${jobId}`}>
-            {filename}
-          </PageLink>
-        );
-      },
+      dataCellComponent: ({ filename, jobId }) => (
+        <PageLink to={`/recipient-validation/list/${jobId}`}>
+          {filename}
+        </PageLink>
+      ),
       header: {
-        label: 'File Name',
+        label: 'File Name'
       }
     },
     {
       dataCellComponent: ({ uploadedAt }) => formatDateTime(toMilliseconds(uploadedAt)),
       header: {
-        label: 'Date Uploaded',
+        label: 'Date Uploaded'
       }
     },
     {
       dataCellComponent: ({ status }) => {
-        const { className, icon:Icon, message } = statusProps[status];
+        if (statusProps[status]) {
+          const { className, icon: Icon, message } = statusProps[status];
 
-        return (
-          <Tag>
-            <span className={className}><Icon /> </span>
-            <span>{message}</span>
-          </Tag>
-        );
-      }
-        const loading = !complete;
-        const ready = status === 'success';
-          const failed = status === 'error';
-          const uploadedNotProcessing = status === 'queued_for_batch';
+          return (
+            <Tag>
+              <span className={className}>
+                <Icon />&nbsp;
+              </span>
 
-          const renderStatus = () => {
+              <span>{message}</span>
+            </Tag>
+          );
+        }
 
-            if (failed) {
-              return (<Tag>
-                <span className={styles.Failed}><Error /> </span>
-                <span>Failed. Please try again.</span>
-              </Tag>);
-            }
-
-            if (ready) {
-              return (<Tag>
-                <span className={styles.Complete}><CheckCircle /> </span>
-                <span>Completed</span>
-              </Tag>);
-            }
-
-            if (uploadedNotProcessing) {
-              return (
-                <Tag>
-                  <span className={styles.Ready}><CloudUpload /> </span>
-                  <span>Ready to validate</span>
-                </Tag>
-              );
-            }
-
-            if (loading) {
-              return (<Tag>
-                <span className={styles.Loading}><Cached /> </span>
-                <span>Processing</span>
-              </Tag>);
-            }
-      }
-      ,
+        return null;
+      },
       header: {
-        label: 'Status',
+        label: 'Status'
       }
     },
     {
       dataCellComponent: () => null,
       header: {
-        label: 'Total',
+        label: 'Total'
       }
     },
     {
-      dataCellComponent: () => null,
+      dataCellComponent: () => (
+        <DownloadLink>
+          <FileDownload/>
+        </DownloadLink>
+      ),
       header: {
-        label: 'Download',
+        label: 'Download'
       }
     }
   ];
