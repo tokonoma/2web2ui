b83864541be4613e8c416135158be32bba6c3912
Brian Kemper <brian.kemper@sparkpost.com>
2019-08-26 11:15:12 -0400

    SE-117 Pulled JobStatusTag and JobReportDownloadLink from JobsTableCollection

--- src/pages/recipientValidation/components/JobsTableCollection.js
+++ src/pages/recipientValidation/components/JobsTableCollection.js
@@ -1,43 +1,17 @@
 import React from 'react';
-import { Panel, Tag, Button } from '@sparkpost/matchbox';
-import { Error, FileDownload, CheckCircle, Cached, CloudUpload } from '@sparkpost/matchbox-icons';
-import DownloadLink from 'src/components/downloadLink/DownloadLink';
+import { Panel } from '@sparkpost/matchbox';
 import TableCollection from 'src/components/collection/TableCollection';
 import PageLink from 'src/components/pageLink/PageLink';
 import { formatDateTime } from 'src/helpers/date';
 import withPollingJobs from '../containers/withPollingJobs';
-import styles from './JobsTableCollection.module.scss';
-
-const statusProps = {
-  error: {
-    className: styles.Failed,
-    icon: Error,
-    message: 'Failed. Please try again.'
-  },
-  success: {
-    className: styles.Complete,
-    icon: CheckCircle,
-    message: 'Completed'
-  },
-  queued_for_batch: {
-    className: styles.Ready,
-    icon: CloudUpload,
-    message: 'Ready to validate'
-  },
-  loading: {
-    className: styles.Loading,
-    icon: Cached,
-    message: 'Processing'
-  }
-};
+import JobReportDownloadLink from './JobReportDownloadLink';
+import JobStatusTag from './JobStatusTag';
 
 export const JobsTableCollection = ({ jobs }) => {
   const columns = [
     {
       dataCellComponent: ({ filename, jobId }) => (
-        <PageLink to={`/recipient-validation/list/${jobId}`}>
-          {filename}
-        </PageLink>
+        <PageLink children={filename} to={`/recipient-validation/list/${jobId}`} />
       ),
       header: {
         label: 'File Name'
@@ -50,51 +24,20 @@ export const JobsTableCollection = ({ jobs }) => {
       }
     },
     {
-      dataCellComponent: ({ complete, status }) => {
-        let jobStatus;
-
-        if (!complete && status !== 'queued_for_batch') {
-          jobStatus = 'loading';
-        } else {
-          jobStatus = status;
-        }
-
-        const { className, icon: Icon, message } = statusProps[jobStatus];
-
-        return (
-          <Tag>
-            <span className={className}>
-              <Icon />&nbsp;
-            </span>
-
-            <span>{message}</span>
-          </Tag>
-        );
-      },
+      dataCellComponent: ({ status }) => <JobStatusTag status={status} />,
       header: {
         label: 'Status'
       }
     },
     {
-      dataCellComponent: ({ addressCount }) => <span>{addressCount}</span>,
+      dataCellComponent: ({ addressCount }) => addressCount,
       header: {
         label: 'Total'
       }
     },
     {
-      dataCellComponent: ({ complete, uploadedFile, rejectedUrl }) => (
-        <>
-          {complete &&
-            <DownloadLink
-              component={Button}
-              to={rejectedUrl ? rejectedUrl : uploadedFile}
-            >
-              <span>Download Results&nbsp;</span>
-
-              <FileDownload/>
-            </DownloadLink>
-          }
-        </>
+      dataCellComponent: ({ complete, rejectedUrl, uploadedFile }) => (
+        <JobReportDownloadLink complete={complete} rejectedUrl={rejectedUrl} uploadedFile={uploadedFile} />
       ),
       header: {
         label: 'Download'
