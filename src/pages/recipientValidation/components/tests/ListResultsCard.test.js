230e9eea24d28d0b6ae726cfdcdaf784b9376aa4
Brian Kemper <brian.kemper@sparkpost.com>
2019-08-26 11:37:16 -0400

    SE-117 Use uploadedAt in ListResultsCard

--- src/pages/recipientValidation/components/tests/ListResultsCard.test.js
+++ src/pages/recipientValidation/components/tests/ListResultsCard.test.js
@@ -8,8 +8,10 @@ describe('ListResultsCard', () => {
   it('renders correctly when not complete', () => {
     const wrapper = subject({
       complete: false,
-      uploaded: 1541092618
+      uploadedAt: '2013-02-04T22:44:30.652Z'
     });
+
+    expect(wrapper.find('.TableCell').at(0)).toHaveProp('children', 'Feb 4 2013, 5:44pm');
     expect(wrapper.find('Tag').childAt(1).text()).toEqual('Processing');
     expect(wrapper.find('Cached')).toExist();
   });
@@ -17,10 +19,12 @@ describe('ListResultsCard', () => {
   it('renders correctly when complete', () => {
     const wrapper = subject({
       complete: true,
-      uploaded: 1541092618,
+      uploadedAt: '2013-02-04T22:44:30.652Z',
       rejectedUrl: 'testfile.csv',
       status: 'success'
     });
+
+    expect(wrapper.find('.TableCell').at(0)).toHaveProp('children', 'Feb 4 2013, 5:44pm');
     expect(wrapper.find('Tag').childAt(1).text()).toEqual('Completed');
     expect(wrapper.find('CheckCircle')).toExist();
   });
@@ -28,10 +32,12 @@ describe('ListResultsCard', () => {
   it('renders correctly when batch job fails', () => {
     const wrapper = subject({
       complete: false,
-      uploaded: 1541092618,
+      uploadedAt: '2013-02-04T22:44:30.652Z',
       rejectedUrl: 'testfile.csv',
       status: 'error'
     });
+
+    expect(wrapper.find('.TableCell').at(0)).toHaveProp('children', 'Feb 4 2013, 5:44pm');
     expect(wrapper.find('Tag').childAt(1).text()).toEqual('Failed. Please try again.');
     expect(wrapper.find('Error')).toExist();
   });
