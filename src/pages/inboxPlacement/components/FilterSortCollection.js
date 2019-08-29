9738311ba4b46ad3e4bda9f193b1ffe2d960163d
Kevin Chu <kevinchu1337@gmail.com>
2019-08-29 15:32:02 -0400

    SA-874 - Change FilterSortCollection styling

--- src/pages/inboxPlacement/components/FilterSortCollection.js
+++ src/pages/inboxPlacement/components/FilterSortCollection.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Select, Panel, Table } from '@sparkpost/matchbox';
+import { Select, Panel, Table, Grid } from '@sparkpost/matchbox';
 import { Collection } from 'src/components';
 import _ from 'lodash';
 import styles from './FilterSortCollection.module.scss';
@@ -18,11 +18,9 @@ const FilterSortCollection = ({ wrapperComponent, title, selectOptions, filterBo
   const TableBody = (props) => <tbody>{props.children}</tbody>;
 
   const CollectionWrapperComponent = (props) => (
-    <>
-      <div className={styles.TableWrapper}>
-        <Table>{props.children}</Table>
-      </div>
-    </>
+    <div className={styles.TableWrapper}>
+      <Table>{props.children}</Table>
+    </div>
   );
 
   const WrapperComponent = wrapperComponent ? wrapperComponent : CollectionWrapperComponent;
@@ -46,17 +44,21 @@ const FilterSortCollection = ({ wrapperComponent, title, selectOptions, filterBo
           <Panel.Section className = {styles.Title}>
             <h3>{title}</h3>
           </Panel.Section>
-          <Panel.Section>
-            <div className = {styles.FilterPanel}>
-              {filterBox}
-              <Select
-                id='sortSelect'
-                value='Sort By'
-                options={selectOptions}
-                onChange={sortOnChange}
-              />
-            </div>
-          </Panel.Section>
+          <div className = {styles.FilterPanel}>
+            <Grid>
+              <Grid.Column xs={12} md={9}>
+                {filterBox}
+              </Grid.Column>
+              <Grid.Column xs={12} md={3}>
+                <Select
+                  id='sortSelect'
+                  defaultValue={defaultSortColumn}
+                  options={selectOptions}
+                  onChange={sortOnChange}
+                />
+              </Grid.Column>
+            </Grid>
+          </div>
           {collection}
         </Panel>
         {pagination}
