318b005c2d4fc6246d032b21a95dee7049bc3dbe
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-08-29 18:21:41 -0400

    modal wrapping up

--- src/pages/billing/components/FeatureComparisonModal.js
+++ src/pages/billing/components/FeatureComparisonModal.js
@@ -1,35 +1,60 @@
 import React from 'react';
 import { Modal, Table, Panel } from '@sparkpost/matchbox';
-import { FEATURE_COMPARISON } from './../constants';
+import { FEATURE_COMPARISON, PLANS } from './../constants';
 import _ from 'lodash';
+import styles from './FeatureComparisonModal.module.scss';
+import classNames from 'classnames';
+import { Check, Close } from '@sparkpost/matchbox-icons';
 function Row({ featureName, testAccount, starterPlans, premierPlans }) {
   return <Table.Row>
-    <Table.Cell> {featureName} </Table.Cell>
-    <Table.Cell> {testAccount} </Table.Cell>
-    <Table.Cell> {starterPlans} </Table.Cell>
-    <Table.Cell> {premierPlans} </Table.Cell>
+    <Table.Cell className={styles.FeatureCell}> {featureName} </Table.Cell>
+    <Table.Cell className={classNames(styles.FeatureComaprisonCell, styles.PlanOne)}> {renderCell(testAccount)} </Table.Cell>
+    <Table.Cell className={classNames(styles.FeatureComaprisonCell, styles.PlanTwo)}> {renderCell(starterPlans)} </Table.Cell>
+    <Table.Cell className={classNames(styles.FeatureComaprisonCell, styles.PlanThree)}> {renderCell(premierPlans)} </Table.Cell>
+  </Table.Row>;
+}
+function HeaderRow({ plans }) {
+  return <Table.Row>
+    <Table.Cell className={styles.FeatureCell}>  </Table.Cell>
+    <Table.Cell className={classNames(styles.FeatureComaprisonCell, styles.PlanOne, styles.TableCaption)}> {plans[0]} </Table.Cell>
+    <Table.Cell className={classNames(styles.FeatureComaprisonCell, styles.PlanTwo, styles.TableCaption)}> {plans[1]} </Table.Cell>
+    <Table.Cell className={classNames(styles.FeatureComaprisonCell, styles.PlanThree, styles.TableCaption)}> {plans[2]} </Table.Cell>
   </Table.Row>;
 }
 function GroupHeading({ groupName, colSpan }) {
   return <Table.Row>
-    <Table.HeaderCell colSpan={colSpan}>
+    <Table.HeaderCell colSpan={colSpan} className={styles.GroupHeading}>
       {groupName}
     </Table.HeaderCell>
   </Table.Row>;
 }
+function Icon({ value }) {
+  return value ? <Check/> : <Close/>;
+}
+function renderCell(cellValue) {
+  if (typeof cellValue === 'boolean') { return <Icon value={cellValue}/> ; } else {
+    if (cellValue.indexOf('\n') !== -1) {
+      return <> {cellValue.substring(0,cellValue.indexOf('\n'))} <br/>
+    <span style={{ fontSize: '12px' }}> {cellValue.substring(cellValue.indexOf('\n'),cellValue.length)}</span>
+      </>;
+    }
+    return cellValue;
+  }
+}
 function ComparisonModal({ open, handleClose }) {
-  return (<Modal open={open} showCloseButton={true} onClose={handleClose}>
+  return (<Modal open={open} showCloseButton={true} onClose={handleClose} >
     <Panel>
-
-      {_.map(FEATURE_COMPARISON, (featureArray, groupName) => <>
+      <div className={styles.FeatureComparisonTable}>
+        <Table>
+          <HeaderRow plans={PLANS}/>
+        </Table>
+        {_.map(FEATURE_COMPARISON, (featureArray, groupName) => <>
           <Table>
-            <GroupHeading groupName={groupName} colSpan={featureArray.length}/>
-            <Table>
-              {featureArray.map((f) => <Row featureName={f[0]} testAccount={f[1]} starterPlans={f[2]} premierPlans={f[3]}/>)}
-            </Table>
+            <GroupHeading groupName={groupName} colSpan={featureArray.length} />
+            {featureArray.map((f) => <Row featureName={f[0]} testAccount={f[1]} starterPlans={f[2]} premierPlans={f[3]}/>)}
           </Table>
       </>)}
-
+      </div>
     </Panel>
   </Modal>);
 }
--- src/pages/billing/constants.js
+++ src/pages/billing/constants.js
@@ -3,30 +3,30 @@ export const FEATURE_COMPARISON = {
     ['Signals Predictive Analytics', true, true,true],
     ['Spam Trap Monitoring',true, true,true ],
     ['Engagement Insights',	true	,true	,true],
-    ['Sending Domains',	true,	10,	50],
-    ['Bounce Domains',	true,	10,	50],
-    ['Tracking Domains',	true,	5,	25],
+    ['Sending Domains',	true,	'10',	'50'],
+    ['Bounce Domains',	true,	'10',	'50'],
+    ['Tracking Domains',	true,	'5',	'25'],
     ['Searchable Email Event Data',	'10 days',	'5 days',	'10 days']
   ],
   'Advanced Settings': [
     ['Scheduled Sending',	'30 days'	,'3 days',	'30 days'],
-    ['Custom Webhooks',	true	,2,	5],
+    ['Custom Webhooks',	true	,'2',	'5'],
     ['Inbound Email Webhooks',	true,	false	,true],
     ['Real Time Alerts',	true	,false,	true],
-    ['Dedicated IP Address',	false,	false	,'1 with the option to buy more'],
+    ['Dedicated IP Address',	false,	false	,'1 \n with the option to buy more'],
     ['IP Pools' ,	false,	false,	true ],
     ['Automated IP Warmup',	false,	false,	true]
   ],
   'Content and Recipients': [
-    ['Advanced Templates',	true,	10,	true],
-    ['Snippets',	true,	10,	true],
+    ['Advanced Templates',	true,	'10',	true],
+    ['Snippets',	true,	'10',	true],
     ['Dynamic AMP Emails',	true,	true,	true],
-    ['AB Testing',	true,	false,	10],
-    ['Recipient Lists', 	true,	10,	true]
+    ['AB Testing',	true,	false,	'10'],
+    ['Recipient Lists', 	true,	'10',	true]
   ],
   'Account Management and Support': [
     [ 'Subaccounts',	true,	false,	'15 Active'],
-    [ 'Online Support',	'for 30 days',	true,	true],
+    [ 'Online Support',	'30 days',	true,	true],
     [ 'Phone Support',	false,	false,	true],
     [ 'Customer support manager (CSM)',	false,	false,	'Above 5M plans only'],
     [ 'Single Sign-On',	true,	false	,true],
@@ -34,3 +34,9 @@ export const FEATURE_COMPARISON = {
   ]
 
 };
+
+export const PLANS = [
+  'Test Account',
+  'Starter Plans',
+  'Premier Plans'
+];
