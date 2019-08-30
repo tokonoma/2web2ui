d6965893f37bce57cfe0407470ddb330ecab9768
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-08-30 14:45:50 -0400

    Cypress - pass in alternate base URL for CI testing

--- cypress/integration/auth/auth.spec.js
+++ cypress/integration/auth/auth.spec.js
@@ -2,7 +2,7 @@
 
 /* eslint-disable no-undef */
 describe('The auth view', () => {
-  before(() => cy.visit('/auth'));
+  before(() => cy.visit('/'));
 
   it('Renders the content "Log In"', () => {
     cy.contains('Log In').should('be.visible');
