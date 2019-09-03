41c86efbfaea41be3ae190f05171b1782b7def8d
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-03 08:43:55 -0400

    FE-714 - add check for error message that is only present when the backend is down

--- cypress/integration/auth/auth.spec.js
+++ cypress/integration/auth/auth.spec.js
@@ -12,6 +12,7 @@ describe('The auth view', () => {
     cy.get('[name="username"]').type(Cypress.env('USERNAME'));
     cy.get('[name="password"]').type(Cypress.env('PASSWORD'));
     cy.get('button[type="submit"]').click();
+    cy.contains('An unknown error occurred').should('not.be.visible');
     cy.url().should('include', '/dashboard');
   });
 });
