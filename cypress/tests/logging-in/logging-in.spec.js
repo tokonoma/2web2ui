52d95f949080ccbdb7b34825aca50445fb453227
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-09 13:29:27 -0400

    FE-714 - refactor logging in as a command

--- cypress/tests/logging-in/logging-in.spec.js
+++ cypress/tests/logging-in/logging-in.spec.js
@@ -9,9 +9,7 @@ describe('The auth view', () => {
   });
 
   it('Logs in', () => {
-    cy.get('[name="username"]').type(Cypress.env('USERNAME'));
-    cy.get('[name="password"]').type(Cypress.env('PASSWORD'));
-    cy.get('button[type="submit"]').click();
+    cy.login();
     cy.contains('An unknown error occurred').should('not.be.visible');
     cy.url().should('include', '/dashboard');
   });
