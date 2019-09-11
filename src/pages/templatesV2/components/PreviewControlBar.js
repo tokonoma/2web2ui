9b427b5729b7293247a9f46e2395ccd3bc73a046
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-11 14:22:01 -0400

    TR-1345 - Incorporate screen reader ony content for control bar buttons

--- src/pages/templatesV2/components/PreviewControlBar.js
+++ src/pages/templatesV2/components/PreviewControlBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { UnstyledLink } from '@sparkpost/matchbox';
+import { UnstyledLink, ScreenReaderOnly } from '@sparkpost/matchbox';
 import { DesktopWindows, PhoneAndroid } from '@sparkpost/matchbox-icons';
 import useEditorContext from '../hooks/useEditorContext';
 import styles from './PreviewControlBar.module.scss';
@@ -18,7 +18,10 @@ const PreviewControlBar = () => {
           onClick={() => { setPreviewDevice('desktop'); }}
           to="javascript:void(0);"
           role="button"
-        />
+        >
+          <ScreenReaderOnly>Desktop Preview</ScreenReaderOnly>
+        </UnstyledLink>
+
         <UnstyledLink
           children={<PhoneAndroid size={24} />}
           className={classNames(styles.PreviewDeviceButton, previewDevice === 'mobile' && styles.active)}
@@ -26,7 +29,9 @@ const PreviewControlBar = () => {
           onClick={() => { setPreviewDevice('mobile'); }}
           to="javascript:void(0);"
           role="button"
-        />
+        >
+          <ScreenReaderOnly>Mobile Preview</ScreenReaderOnly>
+        </UnstyledLink>
       </div>
     </div>
   );
