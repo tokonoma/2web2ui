c3a87a244000794ce9972bf2db3d7d628ad82667
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-11 14:22:57 -0400

    TR-1345 - Fix issue with missing icons

--- src/pages/templatesV2/components/PreviewControlBar.js
+++ src/pages/templatesV2/components/PreviewControlBar.js
@@ -12,24 +12,26 @@ const PreviewControlBar = () => {
     <div className={styles.PreviewControlBar}>
       <div className={styles.PreviewDeviceToggle}>
         <UnstyledLink
-          children={<DesktopWindows size={24} />}
           className={classNames(styles.PreviewDeviceButton, previewDevice === 'desktop' && styles.active)}
           id="preview-content-desktop-button"
           onClick={() => { setPreviewDevice('desktop'); }}
           to="javascript:void(0);"
           role="button"
         >
+          <DesktopWindows size={24} />
+
           <ScreenReaderOnly>Desktop Preview</ScreenReaderOnly>
         </UnstyledLink>
 
         <UnstyledLink
-          children={<PhoneAndroid size={24} />}
           className={classNames(styles.PreviewDeviceButton, previewDevice === 'mobile' && styles.active)}
           id="preview-content-mobile-button"
           onClick={() => { setPreviewDevice('mobile'); }}
           to="javascript:void(0);"
           role="button"
         >
+          <PhoneAndroid size={24} />
+
           <ScreenReaderOnly>Mobile Preview</ScreenReaderOnly>
         </UnstyledLink>
       </div>
