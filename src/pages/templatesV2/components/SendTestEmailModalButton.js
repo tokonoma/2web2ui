c1702b5b20078b391254453d460a2aa2cd7c10fe
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-13 11:20:23 -0400

    TR-1345 - Basic implementation of multi email form field working

--- src/pages/templatesV2/components/SendTestEmailModalButton.js
+++ src/pages/templatesV2/components/SendTestEmailModalButton.js
@@ -1,11 +1,13 @@
+/* eslint-disable max-lines */
 import React, { useState } from 'react';
 import {
   Button,
   Panel,
   Modal,
-  TextField
+  TextField,
+  ComboBoxTextField
 } from '@sparkpost/matchbox';
-// import { isEmailAddress } from 'src/helpers/email';
+import { isEmailAddress } from 'src/helpers/email';
 import useEditorContext from '../hooks/useEditorContext';
 
 const SendTestEmailModalButton = (props) => {
@@ -20,9 +22,10 @@ const SendTestEmailModalButton = (props) => {
   const templateId = match.params.id;
   const [isModalOpen, setModalOpen] = useState(false);
   const [fromEmail, setFromEmail] = useState(undefined);
+  const [hasToEmailError, setToEmailError] = useState(false);
   const [subject, setSubject] = useState(undefined);
   const [toEmail, setToEmail] = useState('');
-  //const [toEmails, setToEmails] = useState([]);
+  const [toEmailList, setToEmailList] = useState([]);
 
   const handleToChange = (e) => {
     setToEmail(e.target.value);
@@ -34,27 +37,72 @@ const SendTestEmailModalButton = (props) => {
     setSubject(content.subject);
   };
 
-  // const handleToKeyDown = (e) => {
-  //   console.log('e.keyCode', e.keyCode);
+  const handleModalClose = () => {
+    setModalOpen(false);
+    setToEmailError(false);
+  };
 
-  //   if (e.keyCode === 13) {
-  //     e.preventDefault; // prevent form submission
-  //   }
+  const handleToKeyDownAndBlur = (e) => {
+    // prevent form submission when using the enter key
+    if (e.keyCode === 13) {
+      e.preventDefault;
+    }
 
-  //   if (e.keyCode === 32) {
-  //     if (isEmailAddress(toEmail)) {
-  //       setToEmails([...toEmails, { email: toEmail }]);
-  //       setToEmail('');
+    // Remove the last email from the list when the user deletes
+    // and no in progress value is present in the field
+    if (e.keyCode === 8 && !toEmail) {
+      setToEmailList(toEmailList.filter((email, index) => {
+        if (index + 1 !== toEmailList.length) {
+          return email;
+        }
+      }));
+    }
 
-  //       console.log('toEmail', toEmail);
-  //     }
-  //   }
-  // };
+    if (e.type === 'blur' || e.keyCode === 32) {
+      if (e.type === 'keydown') {
+        e.preventDefault(); // Prevents spaces from being written to the field
+      }
+
+      const isValidEmail = isEmailAddress(toEmail);
+      const emailAlreadyAdded = toEmailList.find((item) => item.email === toEmail);
+
+      // A valid email address is entered, and it is added to the array
+      if (isValidEmail && !emailAlreadyAdded) {
+        setToEmailList([...toEmailList, { email: toEmail }]);
+        setToEmail('');
+        setToEmailError(false);
+      }
+
+      // Throw an error on the field if:
+      // 1. There is some text entry in the field
+      // 2. The entered email is not valid or
+      // 3. The entered email already exists in the list
+      if (toEmail && (!isValidEmail || emailAlreadyAdded)) {
+        setToEmailError(true);
+      }
+
+      if (e.type === 'blur' && toEmailList.length === 0) {
+        setToEmailError(true);
+      }
+    }
+  };
+
+  const handleRemoveItem = (target) => {
+    setToEmailList(toEmailList.filter((item) => {
+      if (target.email !== item.email) {
+        return item;
+      }
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (templateId) {
+    if (toEmailList.length === 0) {
+      setToEmailError(true);
+    }
+
+    if (toEmailList.length && templateId) {
       sendPreview({
         id: templateId,
         subaccountId: subaccountId,
@@ -87,7 +135,7 @@ const SendTestEmailModalButton = (props) => {
       <Modal
         open={isModalOpen}
         showCloseButton={true}
-        onClose={() => setModalOpen(false)}
+        onClose={handleModalClose}
       >
         <Panel
           accent
@@ -97,24 +145,26 @@ const SendTestEmailModalButton = (props) => {
           <p>Verify your email renders as expected in the inbox by sending a quick test.</p>
 
           <form onSubmit={(e) => handleSubmit(e)}>
-            {/* TODO: Get this guy working! */}
-            {/* <ComboBoxTextField
+            <ComboBoxTextField
               id="text-field-test-email-to"
               label="To:"
-              value={toValue}
-              selectedItems={toEmails}
+              value={toEmail}
+              selectedItems={toEmailList}
               itemToString={({ email }) => email}
               onChange={handleToChange}
-              onKeyDown={handleToKeyDown}
-            /> */}
+              onKeyDown={handleToKeyDownAndBlur}
+              onBlur={handleToKeyDownAndBlur}
+              removeItem={handleRemoveItem}
+              error={hasToEmailError ? 'Please enter valid addresses without duplication' : null}
+            />
 
-            <TextField
+            {/* <TextField
               id="text-field-email-to"
               label="To:"
               type="email"
               onChange={handleToChange}
               value={toEmail}
-            />
+            /> */}
 
             <TextField
               id="text-field-test-email-from"
