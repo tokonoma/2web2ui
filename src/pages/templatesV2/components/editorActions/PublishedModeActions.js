ca35cea1512256b60608f027ccb52ef81b584967
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-16 11:10:48 -0400

    TR-1512 - Remove unnecessary EditDraft component and relevant tests, re-incorporate existing styles

--- src/pages/templatesV2/components/editorActions/PublishedModeActions.js
+++ src/pages/templatesV2/components/editorActions/PublishedModeActions.js
@@ -1,32 +1,44 @@
 import React from 'react';
-import { Button, Popover } from '@sparkpost/matchbox';
+import {
+  Button,
+  Popover,
+  UnstyledLink
+} from '@sparkpost/matchbox';
 import { ArrowDropDown, FileEdit } from '@sparkpost/matchbox-icons';
+import { setSubaccountQuery } from 'src/helpers/subaccounts';
+import { routeNamespace } from '../../constants/routes';
 import styles from './Actions.module.scss';
-import EditDraft from './EditDraft';
 import useEditorContext from '../../hooks/useEditorContext';
 
 const PublishedModeActions = () => {
-  const { hasDraft } = useEditorContext();
+  const { hasDraft, draft } = useEditorContext();
   const draftText = hasDraft ? 'Edit Draft' : 'Save as Draft';
+  const editDraftTo = `/${routeNamespace}/edit/${draft.id}/draft/content${setSubaccountQuery(draft.subaccount_id)}`;
 
-  return (<Button.Group>
-    <EditDraft className={styles.Actions}>
-      <Button><strong>{draftText}</strong></Button>
-    </EditDraft>
+  return (
+    <Button.Group>
+      <Button to={editDraftTo} className={styles.Actions}>
+        <strong>{draftText}</strong>
+      </Button>
 
-    <div className={styles.Actions}>
-      <Popover
-        left={true}
-        trigger={<Button><ArrowDropDown/></Button>}
-      >
-        <div className={styles.ActionsBody}>
-          <EditDraft className={styles.ActionItem}>
-            <FileEdit/>{draftText}
-          </EditDraft>
-        </div>
-      </Popover>
-    </div>
-  </Button.Group>);
+      <div className={styles.Actions}>
+        <Popover
+          left={true}
+          trigger={<Button><ArrowDropDown/></Button>}
+        >
+          <div className={styles.ActionsBody}>
+            <div className={styles.ActionItem}>
+              <UnstyledLink to={editDraftTo}>
+                <FileEdit/>
+
+                {draftText}
+              </UnstyledLink>
+            </div>
+          </div>
+        </Popover>
+      </div>
+    </Button.Group>
+  );
 };
 PublishedModeActions.displayName = 'PublishedModeActions';
 export default PublishedModeActions;
