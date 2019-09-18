b2dd3b848471aceefe54d0041e5cf1d85fd16cc1
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-18 10:48:52 -0400

    TR-1345 - Flesh out remaining unit tests

--- src/pages/templatesV2/components/tests/SendTestEmailButton.test.js
+++ src/pages/templatesV2/components/tests/SendTestEmailButton.test.js
@@ -22,18 +22,18 @@ jest.mock('../../hooks/useEditorContext');
 ** The 'to' form group **
 - [X] The submit event does not fire when the user hits 'enter' on the field
 - [X] If the user hits 'spacebar' on the keyboard, no spaces are written to the 'to' field
-- [] If the user enters an email address and hits 'spacebar' the email address is rendered as a tag, the text field clears, and any error is removed
-- [] If the user enters an email address and blurs the field, the email address is rendered as a tag, the text field clears, and any error is removed
-- [] If there are items in the email list and to `toEmail` text field is blank, when the user hits backspace the last item from the `emailList` is removed
-- [] If the user enters an email address, hits spacebar, and the email address is already in the list, an error is rendered
-- [] if the user enters an invalid email address and hits spacebar, and error is rendered
-- [] if the user clicks on the remove item button in an email list tag, then that particular instance is removed from the UI
+- [X] If the user enters an email address and hits 'spacebar' the email address is rendered as a tag, the text field clears, and any error is removed
+- [X] If the user enters an email address and blurs the field, the email address is rendered as a tag, the text field clears, and any error is removed
+- [X] If there are items in the email list and to `toEmail` text field is blank, when the user hits backspace the last item from the `emailList` is removed
+- [X] If the user enters an email address, hits spacebar, and the email address is already in the list, an error is rendered
+- [X] if the user enters an invalid email address and hits spacebar, and error is rendered
+- [X] if the user clicks on the remove item button in an email list tag, then that particular instance is removed from the UI
 
 ** Submitting **
-- [] If the `toEmailList` has no entries, the form renders an error
-- [] If the form is submitted and there are emails in the `toEmailList`, then the form in the loading state
-- [] If the `toEmailList` has a value, then the `<Loading/>` component is rendered and the `sendPreview()` method is invoked
-- [] If the `sendPreview()` method succeeds, the `<Modal/>` prop `open` is set to `false`
+- [X] If the `toEmailList` has no entries, the form renders an error
+- [X] If the form is submitted and there are emails in the `toEmailList`, then the form in the loading state
+- [X] If the `toEmailList` has a value, then the `<Loading/>` component is rendered and the `sendPreview()` method is invoked
+- [X] If the `sendPreview()` method succeeds, the `<Modal/>` prop `open` is set to `false`
 - [] If the `sendPreview()` method fails, the `<Modal/>` remains open but the `<Loading/>` component is no longer rendered
 */
 
@@ -56,18 +56,28 @@ describe('SendTestEmailButton', () => {
 
     return shallow(<SendTestEmailButton {...props}/>);
   };
+  const toEmailSelector = '[name="emailTo"]';
 
+  // Render the subject, open the modal, and return relevant variables for use in test cases
   const openModal = () => {
     const promise = Promise.resolve();
     const updateDraft = jest.fn(() => promise);
-    const wrapper = subject({ updateDraft });
+    const sendPreview = jest.fn(() => promise);
+    const showAlert = jest.fn(() => promise);
+    const wrapper = subject({
+      updateDraft,
+      sendPreview,
+      showAlert
+    });
 
     wrapper.find('[children="Send a Test"]').simulate('click');
 
     return {
       wrapper,
       promise,
-      updateDraft
+      updateDraft,
+      sendPreview,
+      showAlert
     };
   };
 
@@ -131,13 +141,128 @@ describe('SendTestEmailButton', () => {
       });
     });
 
-    // it('renders a tag when the user enters a valid email address and hits the space bar or tab key', () => {
-    //   const { promise, wrapper } = openModal();
+    it('updates the `selectedItems` prop, clears the `value` prop a valid email address and hits the space bar or tab key', () => {
+      const { promise, wrapper } = openModal();
+      const toEmailSelector = '[name="emailTo"]';
 
-    //   return promise.then(() => {
-    //     console.log(wrapper.debug());
-    //   });
-    // });
+      return promise.then(() => {
+        // Adding a selected item via the spacebar
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@me.com' }});
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('value', 'hello@me.com');
+
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).not.toHaveProp('value', 'hello@me.com');
+        expect(wrapper.find(toEmailSelector)).toHaveProp('selectedItems', [ { email: 'hello@me.com' } ]);
+      });
+    });
+
+    it('updates the `error` prop when the user enters an invalid email and hits the spacebar or blurs the field', () => {
+      const { promise, wrapper } = openModal();
+      const toEmailSelector = '[name="emailTo"]';
+
+      return promise.then(() => {
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'invalidEmail' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('error', 'Please enter valid email addresses without duplicates');
+
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: '' }}); // Clear the error
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('error', '');
+
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'invalidEmailAgain' }});
+
+        wrapper.find(toEmailSelector).simulate('blur', { type: 'blur' });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('error', 'Please enter valid email addresses without duplicates');
+      });
+
+    });
+
+    it('removes the last item in the `selectedItems` array when the user uses the backspace key', () => {
+      const { promise, wrapper } = openModal();
+
+      return promise.then(() => {
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@me.com' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('selectedItems', [ { email: 'hello@me.com' } ]);
+
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@you.com' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 8, preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('selectedItems', [ { email: 'hello@me.com' } ]);
+
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 8, preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('selectedItems', []);
+      });
+    });
+
+    it('renders an error if the user enters a duplicate email address', () => {
+      const { promise, wrapper } = openModal();
+
+      return promise.then(() => {
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@me.com' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@me.com' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('error', 'Please enter valid email addresses without duplicates');
+      });
+    });
+
+    it('removes an item from the `selectedItems` prop when `removeItem` is invoked', () => {
+      const { promise, wrapper } = openModal();
+
+      return promise.then(() => {
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@me.com' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+        wrapper.find(toEmailSelector).props().removeItem({ email: 'hello@me.com' });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('selectedItems', []);
+      });
+    });
+  });
+
+  describe('the form submission', () => {
+    it('renders an error on the to email field when the user submits the form with no values in the email list', () => {
+      const { promise, wrapper } = openModal();
+
+      return promise.then(() => {
+        wrapper.find('form').simulate('submit', { preventDefault: jest.fn() });
+
+        expect(wrapper.find(toEmailSelector)).toHaveProp('error', 'Please enter valid email addresses without duplicates');
+      });
+    });
+
+    it('invokes the sendPreview function with loading UI, followed by showAlert without the loading UI when there are values in the to email list', () => {
+      const {
+        promise,
+        wrapper,
+        sendPreview,
+        showAlert
+      } = openModal();
+
+      return promise.then(() => {
+        wrapper.find(toEmailSelector).simulate('change', { target: { value: 'hello@me.com' }});
+        wrapper.find(toEmailSelector).simulate('keydown', { keyCode: 32, preventDefault: jest.fn() });
+        wrapper.find('form').simulate('submit', { preventDefault: jest.fn() });
+
+        expect(wrapper.find('Loading')).toExist();
+        expect(sendPreview).toHaveBeenCalled();
+
+        return promise.then(() => {
+          expect(wrapper.find('Loading')).not.toExist();
+          expect(wrapper.find('Modal')).toHaveProp('open', false);
+          expect(showAlert).toHaveBeenCalled();
+        });
+      });
+    });
   });
 });
 
