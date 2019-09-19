5a1610d6cc9b42856e36f6815b154df901c3e8d5
Patrick Sison <patrick.m.sison@gmail.com>
2019-09-19 11:10:23 -0400

    SA-903: Added divider to Inbox Placement

--- src/config/navItems/inboxPlacement.js
+++ src/config/navItems/inboxPlacement.js
@@ -2,6 +2,7 @@ import { Inbox } from '@sparkpost/matchbox-icons/matchbox-icons';
 import { hasAccountOptionEnabled } from 'src/helpers/conditions/account';
 
 export default {
+  divider: true,
   label: 'Inbox Placement',
   to: '/inbox-placement',
   tag: 'labs',
