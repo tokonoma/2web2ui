da9ee5aaf8354b10ae0e15767ecec29c9bf58bbe
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-09-20 15:51:30 -0400

    wip test for IntegrationPage
    wip test for IntegrationPage

---
+++ src/pages/signals/tests/IntegrationPage.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+import { RouterContextProvider } from 'src/context/RouterContext';
+
+import IntegrationPage from '../IntegrationPage';
+
+jest.mock('src/actions/ingestBatchEvents');
+
+describe('BatchStatusPage', () => {
+  const App = ({ requestParams = 'statuses=system', children }) => <MemoryRouter initialEntries={[`/?${requestParams}`]}>
+    <RouterContextProvider>
+      {children}
+    </RouterContextProvider>
+  </MemoryRouter>;
+
+  const renderPage = (props) => {
+    const baseProps = {
+      loadingStatus: false,
+      eventsByPage: [],
+      getIngestBatchEvents: jest.fn(),
+      nextCursor: jest.fn(),
+      totalCount: undefined
+    };
+    return <IntegrationPage {...baseProps} {...props} />;
+  };
+
+  const subject = ({ props = {}, requestParams = '' } = {}) => mount(
+    <App requestParams={requestParams} updateRoute={jest.fn()}>{renderPage(props)}</App>
+  ).find('IntegrationPage');
+
+
+  it('renders a page', () => {
+    expect(subject().find('Page')).toExist();
+  });
+});
