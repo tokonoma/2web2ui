205f0888fd88d0367adeb2c3c93fddcbe203baec
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-20 13:19:32 -0400

    TR-1781 - Add clarifying comment to shapeContent helper

--- src/actions/helpers/templates.js
+++ src/actions/helpers/templates.js
@@ -18,6 +18,8 @@ export const getTestDataKey = ({ id, username, mode }) => ([ 'tpldata', username
 export const shapeContent = (content = {}) => {
   const shapedContent = {
     ...content,
+    // The API won't create a new template unless either `content.text` or `content.html` has content.
+    // This allows a new template to be created by passing an empty string as the template's text content.
     text: content.text,
     reply_to: _.isEmpty(content.reply_to) ? undefined : content.reply_to,
     html: _.isEmpty(content.html) ? undefined : content.html,
