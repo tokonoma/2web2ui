92a5888e1d6893801667df9372e5787075cb0225
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-09-24 14:51:43 -0400

    TR-1784 - Redirect and alert when publishing a template

--- src/pages/templatesV2/components/editorActions/SaveAndPublishConfirmationModal.js
+++ src/pages/templatesV2/components/editorActions/SaveAndPublishConfirmationModal.js
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import ConfirmationModal from 'src/components/modals/ConfirmationModal';
 import { routeNamespace } from '../../constants/routes';
 import { setSubaccountQuery } from 'src/helpers/subaccounts';
-import useRouter from 'src/hooks/useRouter';
+import { RedirectAndAlert } from 'src/components/globalAlert';
 import useEditorContext from '../../hooks/useEditorContext';
 
 const SaveAndPublishConfirmationModal = (props) => {
@@ -15,24 +15,36 @@ const SaveAndPublishConfirmationModal = (props) => {
     publishDraft,
     isDraftPublishing
   } = useEditorContext();
-  const { history } = useRouter();
+  const [hasSuccessRedirect, setSuccessRedirect] = useState(false);
 
   const handleConfirm = useCallback(() => publishDraft(draft, draft.subaccount_id)
     .then(() => {
-      history.push(`/${routeNamespace}/edit/${draft.id}/published/content${setSubaccountQuery(draft.subaccount_id)}`);
-    }), [draft, history, publishDraft]);
+      setSuccessRedirect(true);
+    }), [draft, publishDraft]);
 
   return (
-    <ConfirmationModal
-      title='Are you sure you want to publish your template?'
-      content={<p>Once published, your template will be available for use in email campaigns and A/B tests.</p>}
-      confirming={isDraftPublishing}
-      isPending={isDraftPublishing}
-      open={open}
-      confirmVerb='Save and Publish'
-      onCancel={onCancel}
-      onConfirm={handleConfirm}
-    />
+    <>
+      {hasSuccessRedirect &&
+        <RedirectAndAlert
+          to={`/${routeNamespace}/edit/${draft.id}/published/content${setSubaccountQuery(draft.subaccount_id)}`}
+          alert={{
+            type: 'success',
+            message: 'Template published'
+          }}
+        />
+      }
+
+      <ConfirmationModal
+        title='Are you sure you want to publish your template?'
+        content={<p>Once published, your template will be available for use in email campaigns and A/B tests.</p>}
+        confirming={isDraftPublishing}
+        isPending={isDraftPublishing}
+        open={open}
+        confirmVerb='Save and Publish'
+        onCancel={onCancel}
+        onConfirm={handleConfirm}
+      />
+    </>
   );
 };
 
