c066df699303f9c46aceb49304dbae0b2f312a77
Patrick Sison <patrick.m.sison@gmail.com>
2019-09-26 13:50:13 -0400

    AC-987: Fixed calculation of changed conditions

--- src/pages/billing/context/FeatureChangeContext.js
+++ src/pages/billing/context/FeatureChangeContext.js
@@ -1,58 +1,124 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect, useMemo, useCallback } from 'react';
 import _ from 'lodash';
-
+import { connect } from 'react-redux';
 import { Button } from '@sparkpost/matchbox';
+
+import { getSubscription } from 'src/actions/billing';
+import { selectPlansByKey } from 'src/selectors/accountBillingInfo';
 import SupportTicketLink from 'src/components/supportTicketLink/SupportTicketLink';
 
-const defaultContext = {};
+const FeatureChangeContext = createContext({});
 
-const FeatureChangeContext = createContext(defaultContext);
+const FeatureChangeProvider = ({
+  children,
+  plans,
+  subscription,
+  getSubscription,
+  selectedBundle,
+  loading
+}) => {
 
-export const FeatureChangeContextProvider = ({ children }) => {
-  /**
-   *
-   * 1. Generate default state
-   * 2. Generate value
-   */
+  const [index, setIndex] = useState(0);//TODO: Remove
 
-  const defaultAction = (key) => (
-    <Button onClick={() => onComplete(key)}>Got it</Button>
-  );
+  //Rechecks conditions on re-entering tab. Only initializes once
 
-  const DEFAULT_QUESTIONS = [
-    {
-      key: 'dedicated-ips',
-      label: 'Dedicated IPs',
-      description: (<div>
-        Your new plan doesn't include dedicated Ips.
-        Your current IPs will now be billed at $20/month each,
-        or you can <SupportTicketLink issueId="general_issue">
-          submit a support ticket
-        </SupportTicketLink> to delete them.
-      </div>),
-      action: (key) => defaultAction(key)
-    }
-  ];
+  //TODO: Remove useCallback to remove mock
+  const checkConditions = useCallback(() => { getSubscription(index); setIndex(index + 1); }, [index, getSubscription]);
+  useEffect(() => {
+    window.addEventListener('focus', checkConditions);
+    return () => {
+      window.removeEventListener('focus', checkConditions);
+    };
+  }, [checkConditions, index]);
 
-  const initialState = {};
-  DEFAULT_QUESTIONS.forEach(({ key }) => initialState[key] = false);
-  const [ keys, setState ] = useState(initialState);
+  const [ actions, updateActions ] = useState({});
 
-  const onComplete = (key) => {
-    setState({
-      ...keys,
-      [key]: true
-    });
+  const selectedPlansByProduct = useMemo(() => {
+    const { plans: selectedPlans } = selectedBundle;
+    return selectedPlans.reduce((res, planId) => {
+      const plan = plans[planId];
+      return {
+        ...res,
+        [plan.product]: plan
+      };
+    }, {});
+  }, [plans, selectedBundle]);
+
+  // //Used for finding the features that need to have a proper function
+  const calculateDifferences = () => {
+    const { products: currentProducts } = subscription;
+    const diffObject = currentProducts.reduce((resObject, { product, plan, quantity, override }) => {
+      const comparedPlan = selectedPlansByProduct[product];
+      switch (product) {
+        case 'dedicated_ip':
+          if (actions.dedicated_ip || !comparedPlan) {
+            resObject[product] = {
+              label: 'Dedicated IPs',
+              description: (
+                <div>
+                  <span>
+                    Your new plan doesn't include dedicated IPs.
+                    Your current IPs will now be billed at $20/month each, or you can
+                  </span>
+                  <SupportTicketLink issueId="general_issue">
+                    submit a ticket
+                  </SupportTicketLink>
+                  <span>
+                    to delete them.
+                  </span>
+                </div>
+              )
+            };
+          }
+          return resObject;
+        case 'sso':
+        case 'tfa_required':
+          if (actions.auth || !comparedPlan) {
+            resObject.auth = {
+              label: 'Authentication and Security',
+              description: 'Your new plan no longer allows for single sign-on and multifactor authentication.'
+            };
+          }
+          return resObject;
+        case 'subaccounts': {
+          const condition = () => Boolean(comparedPlan && quantity <= comparedPlan.limit);
+          if (actions.subaccounts || !condition()) {
+            resObject.subaccounts = {
+              label: 'Subaccounts',
+              description: `Subaccounts quantity needs to be updated. Have ${quantity} ${index}`,
+              condition,
+              action: <Button external to='/account/subaccounts'>Update Status</Button>
+            };
+          }
+          return resObject;
+        }
+        case 'messaging':
+        default:
+          return resObject;
+      }
+    }, {});
+    updateActions({ ...actions, ...diffObject });
   };
 
-  const mappedActions = DEFAULT_QUESTIONS.map((feature) => ({
-    ...feature,
-    value: keys[feature.key]
-  }));
+  useMemo(calculateDifferences, [subscription]);
 
+  //Confirmation state
+  const [ confirmations, setConfirm ] = useState({});
+  const onConfirm = (key) => { setConfirm({ ...confirmations, [key]: true }); };
+
+  //Evaluates condition and generates action if condition exists
+  const featuresWithActions = useMemo(() => (_.map(actions, ({ action, condition, ...rest }, key) => ({
+    ...rest,
+    key,
+    value: condition ? condition() : confirmations[key],
+    action: condition ? action : <Button onClick={() => onConfirm(key)}>Got it</Button>
+  }))), [actions, confirmations, onConfirm]);
+
+  //Checks if all provided conditions are good
   const value = {
-    isReady: _.every(keys),
-    features: mappedActions
+    isReady: _.every(featuresWithActions, 'value'),
+    features: featuresWithActions,
+    loading
   };
 
   return (
@@ -61,6 +127,14 @@ export const FeatureChangeContextProvider = ({ children }) => {
     </FeatureChangeContext.Provider>
   );
 };
+
+const mapStateToProps = (state) => ({
+  plans: selectPlansByKey(state),
+  subscription: state.billing.subscription,
+  loading: state.billing.loading
+});
+
+export const FeatureChangeContextProvider = connect(mapStateToProps, { getSubscription })(FeatureChangeProvider);
 
 export const useFeatureChangeContext = () => useContext(FeatureChangeContext);
 
