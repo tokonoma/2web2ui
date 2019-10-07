f997544b00fdd0b4dd63c35d9952e22bcdf50a49
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-07 09:25:02 -0400

    TR-1374-lemmon-flavor - Add tests for EditTestDataSection

---
+++ src/pages/templatesV2/components/tests/EditTestDataSection.test.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import useEditorContext from '../../hooks/useEditorContext';
+import EditTestDataSection from '../EditTestDataSection';
+
+jest.mock('../../hooks/useEditorContext');
+
+describe('EditTestDataSection', () => {
+  const subject = (editorState) => {
+    useEditorContext.mockReturnValue({
+      setTestData: jest.fn(),
+      testData: {
+        options: {},
+        metadata: {},
+        substitution_data: {}
+      },
+      setContent: () => {},
+      ...editorState
+    });
+
+    return shallow(<EditTestDataSection/>);
+  };
+
+  it('has an initial "value" that is derived from passed in "testData" editorContext', () => {
+    const exampleTestData = {
+      options: {
+        option: true
+      },
+      metadata: {
+        meta: 'data'
+      },
+      substitution_data: {
+        substitution: 'data'
+      }
+    };
+    const wrapper = subject({ testData: exampleTestData });
+
+    expect(wrapper).toHaveProp('value', exampleTestData);
+  });
+
+  it('invokes the "setTestData" function on change', () => {
+    const mockSetTestData = jest.fn();
+    const wrapper = subject({ setTestData: mockSetTestData });
+
+    wrapper.simulate('change');
+
+    expect(mockSetTestData).toHaveBeenCalled();
+  });
+
+  it('has a "readOnly" prop value that is controlled by the value of "isPublishedMode"', () => {
+    let wrapper = subject({ isPublishedMode: true });
+
+    expect(wrapper).toHaveProp('readOnly', true);
+
+    wrapper = subject({ isPublishedMode: false });
+
+    expect(wrapper).toHaveProp('readOnly', false);
+  });
+});
