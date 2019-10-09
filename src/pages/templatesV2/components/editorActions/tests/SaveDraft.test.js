514daf0aae0bf693d800e62f3dbb4591ff36e769
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-09 10:20:40 -0400

    TR-1374-lemmon-flavor - Update failing tests for SaveDraft

--- src/pages/templatesV2/components/editorActions/tests/SaveDraft.test.js
+++ src/pages/templatesV2/components/editorActions/tests/SaveDraft.test.js
@@ -6,6 +6,17 @@ import useEditorContext from '../../../hooks/useEditorContext';
 jest.mock('../../../hooks/useEditorContext');
 
 describe('SaveDraft', () => {
+  const testData = JSON.stringify({
+    options: {
+      optionOne: true
+    },
+    metadata: {
+      meta: 'data'
+    },
+    substitution_data: {
+      substitution: 'data'
+    }
+  });
   const mockUpdateSnippet = jest.fn();
   const subject = (editorState) => {
     useEditorContext.mockReturnValue({
@@ -13,14 +24,7 @@ describe('SaveDraft', () => {
       history: {},
       ...editorState,
       updateSnippet: mockUpdateSnippet,
-      getParsedTestData: jest.fn().mockReturnValue({
-        metadata: {
-          meta: 'data'
-        },
-        substitution_data: {
-          substitution: 'data'
-        }
-      })
+      testData
     });
 
     return shallow(<SaveDraft className={'Foo'}/>);
@@ -44,28 +48,11 @@ describe('SaveDraft', () => {
     });
 
     wrapper.find('UnstyledLink').simulate('click');
-<<<<<<< HEAD
 
     return promise.then(() => {
       expect(updateDraft).toHaveBeenCalledWith({ id: 'foo', content }, undefined);
       expect(setHasSaved).toHaveBeenCalled();
       expect(showAlert).toHaveBeenCalled();
-=======
-    expect(updateDraft).toHaveBeenCalledWith({ id: 'foo', content }, undefined);
-    expect(mockUpdateSnippet).toHaveBeenCalledWith({
-      id: 'foo',
-      recipients: [{
-        address: {
-          email: 'sparkpost_templates_placeholder@sparkpost.com'
-        },
-        metadata: {
-          meta: 'data'
-        },
-        substitution_data: {
-          substitution: 'data'
-        }
-      }]
->>>>>>> TR-1374-lemmon-flavor - Replace recipient list implementation with snippets implementation
     });
   });
 
