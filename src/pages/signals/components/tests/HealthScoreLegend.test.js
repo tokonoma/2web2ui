67b3a3733744b9e5db229e398f76559920f72bdb
Kevin Chu <kevinchu1337@gmail.com>
2019-10-11 16:05:45 -0400

    SA-897 - Removed unnecessary tests.

--- src/pages/signals/components/tests/HealthScoreLegend.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import HealthScoreLegend from '../HealthScoreLegend';
-
-describe('Signals Health Score legend Component', () => {
-  it('renders correctly', () => {
-    expect(shallow(<HealthScoreLegend />)).toMatchSnapshot();
-  });
-});
