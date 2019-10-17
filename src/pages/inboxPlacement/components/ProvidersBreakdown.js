23c56efb20f4950a6e8e8a611949b7c84d50a15d
Patrick Sison <patrick.m.sison@gmail.com>
2019-10-17 11:28:51 -0400

    SA-832: Add authentication to table

--- src/pages/inboxPlacement/components/ProvidersBreakdown.js
+++ src/pages/inboxPlacement/components/ProvidersBreakdown.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Table } from '@sparkpost/matchbox';
+import cx from 'classnames';
 import { TableCollection } from 'src/components/collection';
 import { formatPercent } from 'src/helpers/units';
 import styles from './ProvidersBreakdown.module.scss';
@@ -12,6 +13,9 @@ const HeaderComponent = () => (<thead>
     <Table.HeaderCell className={styles.Placement}>Inbox</Table.HeaderCell>
     <Table.HeaderCell className={styles.Placement}>Spam</Table.HeaderCell>
     <Table.HeaderCell className={styles.Placement}>Missing</Table.HeaderCell>
+    <Table.HeaderCell className={cx(styles.Authentication, styles.divider)}>SPF</Table.HeaderCell>
+    <Table.HeaderCell className={styles.Authentication}>DKIM</Table.HeaderCell>
+    <Table.HeaderCell className={styles.Authentication}>DMARC</Table.HeaderCell>
   </Table.Row>
 </thead>);
 
@@ -19,13 +23,16 @@ const WrapperComponent = ({ children }) => (<div className={styles.TableWrapper}
   <Table>{children}</Table>
 </div>);
 
-const RowComponent = ({ id, mailbox_provider, placement }) => (<Table.Row className={styles.DataRow}>
+const RowComponent = ({ mailbox_provider, placement, authentication }) => (<Table.Row className={styles.DataRow}>
   <Table.Cell className={styles.ProviderCell}>
     <strong>{mailbox_provider}</strong>
   </Table.Cell>
   <Table.Cell className={styles.Placement}><GroupPercentage value={placement.inbox_pct}/></Table.Cell>
   <Table.Cell className={styles.Placement}><GroupPercentage value={placement.spam_pct}/></Table.Cell>
   <Table.Cell className={styles.Placement}><GroupPercentage value={placement.missing_pct}/></Table.Cell>
+  <Table.Cell className={cx(styles.Authentication, styles.divider)}><GroupPercentage value={authentication.spf_pct}/></Table.Cell>
+  <Table.Cell className={styles.Authentication}><GroupPercentage value={authentication.dkim_pct}/></Table.Cell>
+  <Table.Cell className={styles.Authentication}><GroupPercentage value={authentication.dmarc_pct}/></Table.Cell>
 </Table.Row>);
 
 const ProvidersBreakdown = ({ data = []}) => (<TableCollection
