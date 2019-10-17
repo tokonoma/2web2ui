ab2c4febd9e85bcd3c6870fe5f856735b1cf2a0b
Kevin Chu <kevinchu1337@gmail.com>
2019-10-17 14:07:09 -0400

    SA-956 Updated Inbox Placement messages to show headers

--- src/pages/inboxPlacement/components/AllMessagesCollection.js
+++ src/pages/inboxPlacement/components/AllMessagesCollection.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { Table } from '@sparkpost/matchbox';
+import React, { useState, useCallback } from 'react';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+
+import { Table, CodeBlock, UnstyledLink } from '@sparkpost/matchbox';
 import { TableCollection } from 'src/components/collection';
 import styles from './AllMessagesCollection.module.scss';
 import startCase from 'lodash/startCase';
 import { CheckCircle } from '@sparkpost/matchbox-icons';
+import { LoadingSVG } from 'src/components';
+import { getInboxPlacementMessage } from 'src/actions/inboxPlacement';
 
 export const passFail = (value) => {
   switch (value) {
@@ -21,6 +26,7 @@ const HeaderComponent = () => (<thead>
     <Table.HeaderCell className={styles.HeaderCell}>SPF</Table.HeaderCell>
     <Table.HeaderCell className={styles.HeaderCell}>DKIM</Table.HeaderCell>
     <Table.HeaderCell className={styles.HeaderCell}>DMARC</Table.HeaderCell>
+    <Table.HeaderCell className={classnames(styles.HeaderCell, styles.LeftBorder)}>Headers</Table.HeaderCell>
   </Table.Row>
 </thead>);
 
@@ -28,25 +34,70 @@ const WrapperComponent = ({ children }) => (
   <Table>{children}</Table>
 );
 
-const RowComponent = ({ email_address, folder, tab, dkim, spf, dmarc }) => (
-  <Table.Row className={styles.DataRow}>
-    <Table.Cell className={styles.SeedCell}>
-      <h4>{email_address}</h4>
-      <strong>{startCase(folder)}</strong> {tab && (` | ${startCase(tab)} Folder`)}
-    </Table.Cell>
-    <Table.Cell className={styles.Authentication}>{passFail(spf)}</Table.Cell>
-    <Table.Cell className={styles.Authentication}>{passFail(dkim)}</Table.Cell>
-    <Table.Cell className={styles.Authentication}>{passFail(dmarc)}</Table.Cell>
-  </Table.Row>);
 
-const AllMessagesCollection = ({ data = []}) => (
-  <TableCollection
-    rows={data}
-    wrapperComponent={WrapperComponent}
-    headerComponent={HeaderComponent}
-    rowComponent={RowComponent}
-    pagination={true}
-    saveCsv={false}
-  />);
+export const AllMessagesCollection = ({ data = [], getInboxPlacementMessage, testId, loading }) => {
+  const [openHeaders, setOpenHeaders] = useState([]);
 
-export default AllMessagesCollection;
+  const handleClick = useCallback((messageId) => {
+    if (openHeaders.includes(messageId)) {
+      const updatedOpenHeaders = openHeaders.filter((id) => id !== messageId);
+      return setOpenHeaders(updatedOpenHeaders);
+    }
+
+    if (data.find(({ id, headers }) => id === messageId && !headers)) {
+      getInboxPlacementMessage(testId, messageId);
+    }
+
+    return setOpenHeaders([...openHeaders, messageId]);
+  });
+
+  const RowComponent = ({ email_address, folder, tab, dkim, spf, dmarc, id: messageId, headers }) => (
+  <>
+    <Table.Row className={classnames(styles.DataRow, openHeaders.includes(messageId) && styles.NoBottomBorder)}>
+      <Table.Cell className={styles.SeedCell}>
+        <h4>{email_address}</h4>
+        <strong>{startCase(folder)}</strong> {tab && (` | ${startCase(tab)} Folder`)}
+      </Table.Cell>
+      <Table.Cell className={styles.Authentication}>{passFail(spf)}</Table.Cell>
+      <Table.Cell className={styles.Authentication}>{passFail(dkim)}</Table.Cell>
+      <Table.Cell className={styles.Authentication}>{passFail(dmarc)}</Table.Cell>
+      <Table.Cell className={classnames(styles.Authentication, !openHeaders.includes(messageId) && styles.LeftBorder)}>
+        <UnstyledLink onClick={() => handleClick(messageId)}>
+          {loading === messageId
+            ? <LoadingSVG size='XSmall'/>
+            : openHeaders.includes(messageId)
+              ? 'Close'
+              : 'Open'
+          }
+        </UnstyledLink>
+      </Table.Cell>
+    </Table.Row>
+    {openHeaders.includes(messageId) && loading !== messageId &&
+    <Table.Row>
+      <Table.Cell className={styles.NoPadding}>
+        <CodeBlock className={styles.MessageHeaders} height={200} code={headers || ''}/>
+      </Table.Cell>
+    </Table.Row>}
+  </>
+  );
+
+  return (
+    <TableCollection
+      rows={data}
+      wrapperComponent={WrapperComponent}
+      headerComponent={HeaderComponent}
+      rowComponent={RowComponent}
+      pagination={true}
+      saveCsv={false}
+    />);
+};
+
+function mapStateToProps(state) {
+  return {
+    loading: state.inboxPlacement.getMessagePending
+  };
+}
+
+export default connect(mapStateToProps, { getInboxPlacementMessage })(AllMessagesCollection);
+
+
--- src/pages/inboxPlacement/components/tests/AllMessagesCollection.test.js
+++ src/pages/inboxPlacement/components/tests/AllMessagesCollection.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import cases from 'jest-in-case';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import AllMessagesCollection, { passFail } from '../AllMessagesCollection';
+
+import { AllMessagesCollection, passFail } from '../AllMessagesCollection';
 
 describe('Component: AllMessagesCollection', () => {
-  const subject = ({ ...props }) => shallow(<AllMessagesCollection data={[]} {...props}/>);
+
+  const mockGetMessage = jest.fn();
+  const defaultProps = {
+    data: [],
+    loading: false,
+    id: 101,
+    getInboxPlacementMessage: mockGetMessage
+  };
+  const subject = ({ ...props }) => shallow(<AllMessagesCollection {...defaultProps} {...props}/>);
 
   it('renders correctly with no data', () => {
     expect(subject()).toMatchSnapshot();
@@ -14,6 +24,7 @@ describe('Component: AllMessagesCollection', () => {
   it('renders correctly with data', () => {
     const data = [
       {
+        id: 0,
         email_address: 'fido@dogmail.com',
         folder: 'spam',
         tab: null,
@@ -22,6 +33,7 @@ describe('Component: AllMessagesCollection', () => {
         dmarc: 'pass'
       },
       {
+        id: 1,
         email_address: 'sparky@gmail.com',
         folder: 'inbox',
         tab: 'promotion',
@@ -47,4 +59,43 @@ describe('Component: AllMessagesCollection', () => {
     }, testCases);
   });
 
+  describe('message headers', () => {
+
+    const data = [
+      {
+        id: 0,
+        headers: 'foo'
+      },
+      {
+        id: 1
+      }
+    ];
+    const mountedSubject = ({ ...props }) => mount(<Router><AllMessagesCollection {...defaultProps} {...props}/></Router>);
+    const click = (id, wrapper) => {
+      const row = wrapper.find('Collection').find('TableBody').findWhere((n) => n.name() === 'RowComponent' && n.prop('id') === id);
+      row.find('UnstyledLink').simulate('click');
+    };
+
+    it('dispatches the getMessage action when the open link is clicked', () => {
+      const wrapper = mountedSubject({ data });
+      click(1, wrapper);
+      expect(mockGetMessage).toHaveBeenCalled();
+    });
+
+    it('renders the headers if the open link is clicked and headers exist', () => {
+      const wrapper = mountedSubject({ data });
+      expect(wrapper.find('CodeBlock')).not.toExist();
+      click(0, wrapper);
+      expect(wrapper.find('CodeBlock')).toExist();
+    });
+
+    it('un-renders the headers if the close link is clicked (when headers are already open)', () => {
+      const wrapper = mountedSubject({ data });
+      expect(wrapper.find('CodeBlock')).not.toExist();
+      click(0, wrapper);
+      expect(wrapper.find('CodeBlock')).toExist();
+      click(0, wrapper);
+      expect(wrapper.find('CodeBlock')).not.toExist();
+    });
+  });
 });
