29c09f21e0572131a461f3a370fed74d193aed2a
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-18 15:53:14 -0400

    TR-1850 - Add additional tests for passed in methods

--- src/pages/templatesV2/components/tests/RecentActivity.test.js
+++ src/pages/templatesV2/components/tests/RecentActivity.test.js
@@ -50,5 +50,46 @@ describe('RecentActivity', () => {
     });
 
     expect(wrapper).toHaveTextContent('Recent Activity');
+    expect(wrapper).toHaveTextContent(publishedWithDraftTemplate.name);
+    expect(wrapper).toHaveTextContent(publishedTemplate.name);
+    expect(wrapper).toHaveTextContent(draftTemplate.name);
+  });
+
+  it('renders a maximum of four templates when more than four are present', () => {
+    const wrapper = subject({
+      templates: [
+        publishedWithDraftTemplate,
+        publishedTemplate,
+        draftTemplate,
+        draftTemplate,
+        draftTemplate
+      ]
+    });
+
+    expect(wrapper.find('Panel').length).toBe(4);
+  });
+
+
+
+  it('invokes the `onToggleDuplicateModal` propr when `DuplicateAction` is clicked', () => {
+    const mockToggleDeleteModal = jest.fn();
+    const mockToggleDuplicateModal = jest.fn();
+
+    const wrapper = subject({
+      templates: [
+        publishedWithDraftTemplate,
+        publishedTemplate,
+        draftTemplate,
+        draftTemplate
+      ],
+      onToggleDeleteModal: mockToggleDeleteModal,
+      onToggleDuplicateModal: mockToggleDuplicateModal
+    });
+
+    wrapper.find('DuplicateAction').first().simulate('click');
+    wrapper.find('DeleteAction').first().simulate('click');
+
+    expect(mockToggleDeleteModal).toHaveBeenCalled();
+    expect(mockToggleDuplicateModal).toHaveBeenCalled();
   });
 });
