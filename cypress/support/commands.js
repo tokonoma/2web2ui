8c215c03e632641591d46b9f9819b0ceab4dab78
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-21 15:56:38 -0400

    TR-1374-lemmon-flavor - Add end-to-end tests for core functionality of new templates editor

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,8 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import '@testing-library/cypress/add-commands';
+
 // TODO: refactor to use `cy.request()` to log in programmatically instead of via UI interaction
 Cypress.Commands.add('login', () => {
   cy.visit('/');
