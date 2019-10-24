173e580d7d909f9eec3a4ef27ca5c75dc883e5e4
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-24 09:42:38 -0400

    TR-1347 - Popover with insert snippet button present

--- src/pages/templatesV2/components/EditSection.js
+++ src/pages/templatesV2/components/EditSection.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import { Panel, Tabs } from '@sparkpost/matchbox';
+import React, { useState } from 'react';
+import {
+  Panel,
+  Tabs,
+  Button,
+  Popover,
+  ActionList,
+  ScreenReaderOnly
+} from '@sparkpost/matchbox';
+import { MoreVert } from '@sparkpost/matchbox-icons';
 import tabs from '../constants/editTabs';
+import InsertSnippetModal from './InsertSnippetModal';
 import useEditorContext from '../hooks/useEditorContext';
 import styles from './EditSection.module.scss';
 
 const EditSection = () => {
   const { currentTabIndex, setTab } = useEditorContext();
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
+  const [isSnippetModalOpen, setSnippetModalOpen] = useState(false);
   const TabComponent = tabs[currentTabIndex].render;
+
+  const handleInsertSnippetClick = () => {
+    setPopoverOpen(false);
+    setSnippetModalOpen(true);
+  };
 
   return (
     <Panel className={styles.EditSection}>
       <div className={styles.TabsWrapper}>
+        {/* eslint-disable no-unused-vars */}
         <Tabs
           color="blue"
           selected={currentTabIndex}
           onSelect={(nextTabIndex) => { setTab(nextTabIndex); }}
           tabs={tabs.map(({ render, ...tab }) => tab)}
         />
+        {/* eslint-enable no-unused-vars */}
+
+        <Popover
+          left
+          open={isPopoverOpen}
+          trigger={
+            <Button
+              flat
+              className={styles.MoreButton}
+              onClick={() => setPopoverOpen(!isPopoverOpen)}
+            >
+              <MoreVert/>
+
+              <ScreenReaderOnly>More</ScreenReaderOnly>
+            </Button>
+          }
+        >
+          <ActionList
+            actions={
+              [
+                {
+                  content: 'Insert Snippet',
+                  onClick: () => handleInsertSnippetClick()
+                }
+              ]
+            }
+          />
+        </Popover>
       </div>
+
       <TabComponent />
+
+      <InsertSnippetModal
+        open={isSnippetModalOpen}
+        onClose={() => setSnippetModalOpen(false)}
+        loading={false}
+      />
     </Panel>
   );
 };
---
+++ src/pages/templatesV2/components/InsertSnippetModal.js
@@ -0,0 +1,79 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import {
+  Modal,
+  Panel,
+  Label,
+  Button
+} from '@sparkpost/matchbox';
+import PageLink from 'src/components/pageLink';
+import { Typeahead, TypeaheadItem } from 'src/components/typeahead/Typeahead';
+import ButtonWrapper from 'src/components/buttonWrapper';
+import PanelLoading from 'src/components/panelLoading';
+import CopyField from 'src/components/copyField';
+import { slugToFriendly } from 'src/helpers/string';
+
+const InsertSnippetModal = (props) => {
+  const { open, loading, onClose } = props;
+
+  if (loading) {
+    return <PanelLoading/>;
+  }
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      showCloseButton={true}
+    >
+      <Panel
+        accent
+        title="Add a snippet"
+        sectioned
+      >
+        <form>
+          <p>Snippets are a great way to manage sections like headers or footers that are used across multiple templates. Simply edit your snippet, and that change will populate across all your templates.</p>
+
+          {/* <Typeahead
+            disabled={snippets.length === 0}
+            helpText={
+              snippets.length === 0 ? (
+                <span>
+                  You have not created a snippet. {
+                    <PageLink to="/snippets/create">Create your first snippet</PageLink>
+                  }
+                </span>
+              ) : ''
+            }
+            itemToString={(snippet) => (
+              snippet ? `${snippet.name || slugToFriendly(snippet.id)} (${snippet.id})` : ''
+            )}
+            label="Snippet"
+            name="snippetTypeahead"
+            onChange={this.handleChange}
+            placeholder={snippets.length === 0 ? '' : 'Type to search...'}
+            renderItem={({ id, name }) => (
+              <TypeaheadItem id={id} label={name || slugToFriendly(id)} />
+            )}
+            results={snippets}
+          /> */}
+
+          <Label id="snippet-copy-field">Snippet Code</Label>
+
+          <CopyField
+            id="snippet-copy-field"
+            value="A string"
+          />
+
+          <ButtonWrapper>
+            <Button color="orange">
+              Copy Code
+            </Button>
+          </ButtonWrapper>
+        </form>
+      </Panel>
+    </Modal>
+  );
+};
+
+export default InsertSnippetModal;
