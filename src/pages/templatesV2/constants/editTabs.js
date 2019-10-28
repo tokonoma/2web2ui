882ab28e2b1ee87d62726d87415032ce210c7845
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-10-28 15:00:38 -0400

    TR-1900 - Add relevant data-id attributes to tabs UI

--- src/pages/templatesV2/constants/editTabs.js
+++ src/pages/templatesV2/constants/editTabs.js
@@ -7,22 +7,34 @@ const editTabs = [
   {
     content: 'HTML',
     key: 'html',
-    render: EditHtmlSection
+    render: EditHtmlSection,
+    role: 'button',
+    to: 'javascript:void(0);',
+    ['data-id']: 'tab-html'
   },
   {
     content: 'AMP HTML',
     key: 'amp_html',
-    render: EditAmpSection
+    render: EditAmpSection,
+    role: 'button',
+    to: 'javascript:void(0);',
+    ['data-id']: 'tab-amp-html'
   },
   {
     content: 'Text',
     key: 'text',
-    render: EditTextSection
+    render: EditTextSection,
+    role: 'button',
+    to: 'javascript:void(0);',
+    ['data-id']: 'tab-text'
   },
   {
     content: 'Test Data',
     key: 'test_data',
-    render: EditTestDataSection
+    render: EditTestDataSection,
+    role: 'button',
+    to: 'javascript:void(0);',
+    ['data-id']: 'tab-test-data'
   }
 ];
 
