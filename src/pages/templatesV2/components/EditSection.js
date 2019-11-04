7e18a04182e51052a5edb4c1b205512f46063d0f
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-11-04 09:55:55 -0500

    TR-1849 - Add insert AMP modal

--- src/pages/templatesV2/components/EditSection.js
+++ src/pages/templatesV2/components/EditSection.js
@@ -9,24 +9,81 @@ import {
   Tag
 } from '@sparkpost/matchbox';
 import { MoreHoriz } from '@sparkpost/matchbox-icons';
+import ConfirmationModal from 'src/components/modals/ConfirmationModal';
 import tabs from '../constants/editTabs';
 import InsertSnippetModal from './InsertSnippetModal';
 import useEditorContext from '../hooks/useEditorContext';
 import styles from './EditSection.module.scss';
 
 const EditSection = () => {
-  const { currentTabIndex, setTab, isPublishedMode } = useEditorContext();
+  const {
+    currentTabIndex,
+    currentTabKey,
+    setTab,
+    setContent,
+    content,
+    isPublishedMode
+  } = useEditorContext();
   const [isPopoverOpen, setPopoverOpen] = useState(false);
   const [isSnippetModalOpen, setSnippetModalOpen] = useState(false);
+  const [isAMPModalOpen, setAMPModalOpen] = useState(false);
 
   const currentTab = tabs[currentTabIndex];
   const TabComponent = currentTab.render;
   const hasReadOnlyTag = isPublishedMode && (currentTab.key === 'html' || currentTab.key === 'amp_html' || currentTab.key === 'text');
   const hasPopover = !isPublishedMode;
+  const AMPBoilerplate = `
+<!DOCTYPE html>
+<html ⚡4email>
+  <head>
+    <meta charset="utf-8" />
+    <style amp4email-boilerplate>
+      body {
+        visibility: hidden;
+      }
+    </style>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+  </head>
+  <body>
+    Hello, world.
+  </body>
+</html>
+  `.trim(); // Tabbing is weird here to ensure no extra tabs are rendered in the editor
+  const popoverActions = [
+    {
+      content: 'Insert Snippet',
+      onClick: () => handleInsertSnippetClick(),
+      role: 'button',
+      href: 'javascript:void(0);',
+      title: 'Opens a dialog',
+      'data-id': 'popover-action-insert-snippet'
+    },
+    currentTabKey === 'amp_html' && {
+      content: 'Insert AMP Boilerplate',
+      onClick: () => handleInsertAMPClick(),
+      role: 'button',
+      href: 'javascript:void(0);',
+      title: 'Opens a dialog',
+      'data-id': 'popover-action-insert-amp-boilerplate'
+    }
+  ].filter(Boolean); // Removes empty items from the array
 
   const handleInsertSnippetClick = () => {
     setPopoverOpen(false);
     setSnippetModalOpen(true);
+  };
+
+  const handleInsertAMPClick = () => {
+    setPopoverOpen(false);
+    setAMPModalOpen(true);
+  };
+
+  const handleAMPConfirmClick = () => {
+    setAMPModalOpen(false);
+    setContent({
+      ...content,
+      amp_html: AMPBoilerplate
+    });
   };
 
   return (
@@ -67,19 +124,7 @@ const EditSection = () => {
               </Button>
             }
           >
-            <ActionList
-              actions={
-                [
-                  {
-                    content: 'Insert Snippet',
-                    onClick: () => handleInsertSnippetClick(),
-                    role: 'button',
-                    href: 'javascript:void(0);',
-                    'data-id': 'popover-action-insert-snippet'
-                  }
-                ]
-              }
-            />
+            <ActionList actions={popoverActions}/>
           </Popover>
         }
       </div>
@@ -90,6 +135,15 @@ const EditSection = () => {
         open={isSnippetModalOpen}
         onClose={() => setSnippetModalOpen(false)}
         loading={false}
+      />
+
+      <ConfirmationModal
+        open={isAMPModalOpen}
+        title='Are you sure you want to insert the AMP boilerplate?'
+        content={<p>Any existing markup in the AMP tab will be lost.</p>}
+        confirmVerb='Insert AMP Boilerplate'
+        onCancel={() => setAMPModalOpen(false)}
+        onConfirm={handleAMPConfirmClick}
       />
     </Panel>
   );
