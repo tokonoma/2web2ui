bf1400f773731ccf75a0d33b8227d08ef6dc42a5
Greg Friedman <greg.friedman@sparkpost.com>
2019-11-05 14:56:20 -0500

    SA-966 Add tests for new selectors

--- src/pages/inboxPlacement/components/tests/PlacementBreakdown.test.js
+++ src/pages/inboxPlacement/components/tests/PlacementBreakdown.test.js
@@ -76,38 +76,6 @@ describe('Component: PlacementBreakdown', () => {
     expect(subject({ data })).toMatchSnapshot();
   });
 
-  it('matches snapshot with region data', () => {
-    const data = [
-      {
-        'region': 'north america',
-        'placement': {
-          'inbox_pct': 0,
-          'spam_pct': 0,
-          'missing_pct': 1
-        },
-        'authentication': {
-          'spf_pct': 0,
-          'dkim_pct': 0,
-          'dmarc_pct': 1
-        }
-      },
-      {
-        'region': 'europe',
-        'placement': {
-          'inbox_pct': 0.8,
-          'spam_pct': 0.1,
-          'missing_pct': 0.1
-        },
-        'authentication': {
-          'spf_pct': 0.8,
-          'dkim_pct': 0.1,
-          'dmarc_pct': 0.1
-        }
-      }
-    ];
-    expect(subject({ data, type: PLACEMENT_FILTER_TYPES.REGION })).toMatchSnapshot();
-  });
-
   describe('SubComponent: GroupPercentage', () => {
     it('renders percentage correctly', () => {
       expect(shallow(<GroupPercentage value={0.2}/>)).toMatchSnapshot();
@@ -120,7 +88,8 @@ describe('Component: PlacementBreakdown', () => {
       expect(shallow(
         <RowComponent
           id={1}
-          mailbox_provider={mailbox_provider}
+          // This is undefined because PLACEMENT_FILTER_TYPES.REGION doesn't have a mailbox_provider in its data
+          mailbox_provider={undefined}
           region={region}
           type={PLACEMENT_FILTER_TYPES.REGION}
           placement={placement}
--- src/selectors/tests/inboxPlacement.test.js
+++ src/selectors/tests/inboxPlacement.test.js
@@ -1,4 +1,4 @@
-import { selectReferenceSeed } from '../inboxPlacement';
+import { selectReferenceSeed, selectTestDetailsPageError, selectTestDetailsPageLoading } from '../inboxPlacement';
 
 describe('Selectors: Inbox Placement', () => {
   describe('selectReferenceSeed', () => {
@@ -22,6 +22,68 @@ describe('Selectors: Inbox Placement', () => {
     it('returns first undefined if no reference seed found', () => {
       state.inboxPlacement.seeds.pop();
       expect(selectReferenceSeed(state)).toBe(undefined);
+    });
+  });
+
+  describe('selectTestDetailsPageError', () => {
+    let state;
+    beforeEach(() => {
+      state = {
+        inboxPlacement: {
+          getTestError: false,
+          getByProviderError: false,
+          getByRegionError: false,
+          getTestContentError: false
+        }
+      };
+    });
+
+    it('returns false when there are no errors', () => {
+      expect(selectTestDetailsPageError(state)).toBe(false);
+    });
+
+    it('returns true when there is one error', () => {
+      state.inboxPlacement.getTestError = true;
+      expect(selectTestDetailsPageError(state)).toBe(true);
+    });
+
+    it('returns true when there is more than one error', () => {
+      state.inboxPlacement.getTestError = true;
+      state.inboxPlacement.getByProviderError = true;
+      state.inboxPlacement.getByRegionError = true;
+      state.inboxPlacement.getTestContentError = true;
+      expect(selectTestDetailsPageError(state)).toBe(true);
+    });
+  });
+
+  describe('selectTestDetailsPageLoading', () => {
+    let state;
+    beforeEach(() => {
+      state = {
+        inboxPlacement: {
+          getTestPending: false,
+          getByProviderPending: false,
+          getByRegionPending: false,
+          getTestContentPending: false
+        }
+      };
+    });
+
+    it('returns false when there is nothing pending', () => {
+      expect(selectTestDetailsPageLoading(state)).toBe(false);
+    });
+
+    it('returns true when there is one item pending', () => {
+      state.inboxPlacement.getTestPending = true;
+      expect(selectTestDetailsPageLoading(state)).toBe(true);
+    });
+
+    it('returns true when there is more than one item pending', () => {
+      state.inboxPlacement.getTestPending = true;
+      state.inboxPlacement.getByProviderPending = true;
+      state.inboxPlacement.getByRegionPending = true;
+      state.inboxPlacement.getTestContentPending = true;
+      expect(selectTestDetailsPageLoading(state)).toBe(true);
     });
   });
 });
