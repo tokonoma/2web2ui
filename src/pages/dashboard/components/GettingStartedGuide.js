dd718cebd87d7c7f710491caf06b846704e25b05
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-11-06 15:57:46 -0500

    [AC-1079] added stepname for future use

--- src/pages/dashboard/components/GettingStartedGuide.js
+++ src/pages/dashboard/components/GettingStartedGuide.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Panel, Button } from '@sparkpost/matchbox';
 import { ArrowDownward, Send } from '@sparkpost/matchbox-icons';
 import { Card, CardTitle, CardContent, CardActions } from 'src/components';
@@ -11,20 +11,29 @@ export const GettingStartedGuide = ({ isGuideAtBottom, moveGuideAtBottom }) => {
     color: 'blue',
     onClick: moveGuideAtBottom
   }];
+  //stepName could be Features,Sending,Show Me Sparkpost, Let's Code
+  const [stepName, setStepName] = useState('Features');
+  const renderStep = () => {
+    switch (stepName) {
+      case 'Features':
+      default:
+        return <CardGroup>
+          <Card>
+            <CardTitle><Send size='20' style={{ transform: 'rotate(-45deg)', verticalAlign: '5%' }}/>   &nbsp;Sending with Sparkpost</CardTitle>
+            <CardContent>Learn how to send emails, integrate our API into your code, and make the most of our powerful analytics.</CardContent>
+            <CardActions>
+              <ButtonWrapper>
+                <Button color='orange' onClick={() => setStepName('Start Sending')}>Start Sending</Button>
+              </ButtonWrapper>
+            </CardActions>
+          </Card>
+        </CardGroup>;
+    }
+  };
   return <>
         <Panel title='Getting Started' actions={actions} sectioned >
-          <h5>  Features </h5>
-          <CardGroup>
-            <Card>
-              <CardTitle><Send size='20' style={{ transform: 'rotate(-45deg)', verticalAlign: '5%' }}/>   &nbsp;Sending with Sparkpost</CardTitle>
-              <CardContent>Learn how to send emails, integrate our API into your code, and make the most of our powerful analytics.</CardContent>
-              <CardActions>
-                <ButtonWrapper>
-                  <Button color='orange'>Start Sending</Button>
-                </ButtonWrapper>
-              </CardActions>
-            </Card>
-          </CardGroup>
+          <p><strong> Features</strong> </p>
+          {renderStep()}
         </Panel>
         </>;
 };
