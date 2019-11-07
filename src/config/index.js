37ae7085ac4625035054a461e4a154d8f528849c
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-11-07 09:09:41 -0500

    FE-715 - Update handling of hostname

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 import defaultConfig from './default';
 import envConfig from './env';
 
-const hostname = window.location.hostname;
+// The Cypress test runner always uses the public staging endpoint instead of inferring from the window location
+const hostname = window.Cypress ? 'app-staging.sparkpost.com' : window.location.hostname;
 const nodeEnv = process.env.NODE_ENV;
 const tenantConfig = TENANT_CONFIGS[hostname] || {}; // eslint-disable-line no-undef
 
