24ffbb6ee6ddad10759842cea8614731fc74a30c
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-11-12 13:55:51 -0500

    TR-1455 - Update failing test

--- src/pages/templates/components/tests/ListComponents.test.js
+++ src/pages/templates/components/tests/ListComponents.test.js
@@ -23,7 +23,7 @@ describe('Template List Components', () => {
 
     it('should navigate to published page if template is published', () => {
       wrapper.setProps({ list_status: 'published' });
-      expect(wrapper.find('Link').props().to).toEqual('/templatesv2/edit/id-123/published/content?subaccount=123');
+      expect(wrapper.find('Link').props().to).toEqual('/templates/edit/id-123/published/content?subaccount=123');
     });
   });
 
