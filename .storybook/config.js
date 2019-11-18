89661fc109bf5e2d20c5ba5b1659e6acdecf414a
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-11-18 16:29:20 -0500

    FE-726 - Add some documentation for several components using Storybook docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,5 @@ import { configure } from '@storybook/react';
 import '../src/critical.scss';
 import '../src/index.scss';
 
-configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module)
+configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
 
-configure(loadStories, module);
