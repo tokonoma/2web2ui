e2fdcd7a48ba19a53ecd9838b947a51e79ffd787
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-11-18 16:40:02 -0500

    FE-726 - Add Callout docs

--- src/components/callout/Callout.js
+++ src/components/callout/Callout.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Callout.module.scss';
 
-const Callout = ({ children, height = '220px', title }) => (
+const Callout = ({ children, height, title }) => (
   <div className={styles.Callout} style={{ height }}>
     <div>
       <h3 className={styles.Title}>{title}</h3>
@@ -9,5 +10,15 @@ const Callout = ({ children, height = '220px', title }) => (
     </div>
   </div>
 );
+
+Callout.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+  height: PropTypes.string
+};
+
+Callout.defaultProps = {
+  height: '220px'
+};
 
 export default Callout;
