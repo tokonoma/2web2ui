4a752e40d8c3dc8592383bdd09b22b4564cdb738
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-11-19 09:23:11 -0500

    FE-726 - Add additional documentation and intro page

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,5 +3,8 @@ import { configure } from '@storybook/react';
 import '../src/critical.scss';
 import '../src/index.scss';
 
-configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
+configure([
+  require.context('../src', false, /Intro\.stories\.mdx/),
+  require.context('../src', true, /\.stories\.(js|mdx)$/)
+], module);
 
