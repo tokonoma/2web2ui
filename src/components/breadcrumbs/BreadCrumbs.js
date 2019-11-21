3ff7bea1b011be8e3d698ec660afe61c4a7020fb
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-11-21 11:02:58 -0500

    [AC-1081] Addressing feedback

--- src/components/breadcrumbs/BreadCrumbs.js
+++ src/components/breadcrumbs/BreadCrumbs.js
@@ -5,7 +5,7 @@ export const BreadCrumbs = ({ children }) => (
   <div className={styles.BreadCrumbContainer}>
     {React.Children.map(children, (child, index) => (
   <>
-  &nbsp; {child} &nbsp; {index + 1 !== React.Children.count(children) && <div className={styles.Arrow}>></div>}
+  {child} {index + 1 !== React.Children.count(children) && <div className={styles.Arrow}>&nbsp;>&nbsp;</div>}
   </>
     ))}
   </div>
