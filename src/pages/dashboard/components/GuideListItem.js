d29cea1bdb5955e2ea9467bd4e60a547942e46fd
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-11-21 10:46:54 -0500

    [AC-1082] wip

--- src/pages/dashboard/components/GuideListItem.js
+++ src/pages/dashboard/components/GuideListItem.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import ButtonWrapper from 'src/components/buttonWrapper';
 import { PanoramaFishEye, CheckCircleOutline } from '@sparkpost/matchbox-icons';
-import { Button } from '@sparkpost/matchbox';
+import { Button, Grid } from '@sparkpost/matchbox';
 
 export const GuideListItem = ({ itemCompleted, children, action: { name, onClick }}) => (
-  <div>
-    <div>
-      {itemCompleted ? <CheckCircleOutline size={50}/> : <PanoramaFishEye size={10}/>}
-    </div>
-    <div>
-      {children}
-    </div>
-    <ButtonWrapper>
-      <Button onClick={onClick} color={!itemCompleted && 'orange'}> {name} </Button>
-    </ButtonWrapper>
-  </div>
+  <Grid>
+    <Grid.Column xs={12} md={9}>
+      <div style={{ display: 'inline-block' }}>
+        {itemCompleted ? <CheckCircleOutline size={36}/> : <PanoramaFishEye size={36}/>}
+      </div>
+      <div style={{ display: 'inline-block' }}>
+        {children}
+      </div>
+    </Grid.Column>
+    <Grid.Column xs={12} md={3}>
+      <div style={{ float: 'right' }}>
+        <Button onClick={onClick} color={!itemCompleted && 'orange'}> {name} </Button>
+      </div>
+    </Grid.Column>
+  </Grid>
 );
 
 export const GuideListItemTitle = ({ children }) => (
