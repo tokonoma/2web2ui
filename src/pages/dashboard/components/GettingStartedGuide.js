c3667ab734a83e41b002242dd5158397b8406096
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-11-21 14:12:41 -0500

    [AC-1082] wip

--- src/pages/dashboard/components/GettingStartedGuide.js
+++ src/pages/dashboard/components/GettingStartedGuide.js
@@ -68,8 +68,10 @@ export const GettingStartedGuide = ({ isGuideAtBottom, moveGuideAtBottom }) => {
         </Grid>
          </>;
       case 'Show Me SparkPost':
-        return <GuideListItem action={{ name: 'Send Test Email', onClick: () => {} }}>
-          <GuideListItemTitle> Send a Test Email</GuideListItemTitle>
+        return <GuideListItem action={{ name: 'Send Test Email', onClick: () => {} }} >
+          <GuideListItemTitle>
+            Send a Test Email
+          </GuideListItemTitle>
           <GuideListItemDescription>Send a test email using our starter template.</GuideListItemDescription>
         </GuideListItem>;
       case 'Let\'s Code':
--- src/pages/dashboard/components/GuideListItem.js
+++ src/pages/dashboard/components/GuideListItem.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { PanoramaFishEye, CheckCircleOutline } from '@sparkpost/matchbox-icons';
+import { RadioButtonUnchecked, CheckCircleOutline } from '@sparkpost/matchbox-icons';
 import { Button, Grid } from '@sparkpost/matchbox';
 
 export const GuideListItem = ({ itemCompleted, children, action: { name, onClick }}) => (
   <Grid>
     <Grid.Column xs={12} md={9}>
-      <div style={{ display: 'inline-block' }}>
-        {itemCompleted ? <CheckCircleOutline size={36}/> : <PanoramaFishEye size={36}/>}
-      </div>
-      <div style={{ display: 'inline-block' }}>
-        {children}
-      </div>
+      <Grid>
+        <Grid.Column md={1} xs={12}>
+          {itemCompleted ? <CheckCircleOutline size={36} style={{ color: 'green' }}/> : <RadioButtonUnchecked size={36}/>}
+        </Grid.Column>
+        <Grid.Column md={11} xs={12}>
+          {children}
+        </Grid.Column>
+      </Grid>
     </Grid.Column>
     <Grid.Column xs={12} md={3}>
       <div style={{ float: 'right' }}>
@@ -21,13 +23,13 @@ export const GuideListItem = ({ itemCompleted, children, action: { name, onClick
 );
 
 export const GuideListItemTitle = ({ children }) => (
-  <div>
+  <div style={{ fontWeight: 600 }}>
     {children}
   </div>
 );
 
 export const GuideListItemDescription = ({ children }) => (
-  <div>
+  <div style={{ paddingTop: '10px' }}>
     {children}
   </div>
 );
