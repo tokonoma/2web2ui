91e139191e7569afb9a848ccfee2fa02d45d7ce5
Patrick Sison <patrick.m.sison@gmail.com>
2019-11-26 14:04:05 -0500

    AC-990: Promo code test

--- src/pages/billing/components/tests/PlanSelect.test.js
+++ src/pages/billing/components/tests/PlanSelect.test.js
@@ -18,7 +18,7 @@ describe('Plan Select:', () => {
         messaging: {
           code: '2',
           includesIp: false,
-          monthly: 0,
+          price: 0,
           name: 'Two',
           overage: 0.2,
           volume: 2,
@@ -30,7 +30,7 @@ describe('Plan Select:', () => {
         tier: 'starter',
         messaging: {
           code: '3',
-          monthly: 300,
+          price: 300,
           name: 'Three',
           overage: 0.3,
           volume: 3
@@ -42,7 +42,7 @@ describe('Plan Select:', () => {
         messaging: {
           code: '4',
           includesIp: true,
-          monthly: 400,
+          price: 400,
           name: 'Four',
           overage: 0.4,
           volume: 4
@@ -71,7 +71,7 @@ describe('Selected Plan:', () => {
       messaging: {
         code: '2',
         includesIp: false,
-        monthly: 0,
+        price: 0,
         name: 'Two',
         overage: 0.2,
         volume: 2,
@@ -98,6 +98,19 @@ describe('Selected Plan:', () => {
 
   it('should render plan price with the plan', () => {
     expect(subject()).toMatchSnapshot();
+  });
+
+  it('should render promo code if price is not 0', () => {
+    const wrapper = subject({
+      bundle: {
+        ...defaultProps.bundle,
+        messaging: {
+          ...defaultProps.bundle.messaging,
+          price: 3
+        }
+      }
+    });
+    expect(wrapper.find('PromoCodeNew')).toExist();
   });
 });
 
