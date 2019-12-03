1f12085fdd5ca80563e18c73c7c2bece4d9d4dbc
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-12-03 13:23:44 -0500

    [AC-1083] tests added and corrected

--- src/pages/dashboard/components/tests/GettingStartedGuide.test.js
+++ src/pages/dashboard/components/tests/GettingStartedGuide.test.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { GettingStartedGuide } from '../GettingStartedGuide';
-import { ArrowDownward } from '@sparkpost/matchbox-icons';
-
 
 describe('GettingStartedGuide', () => {
   const defaultProps = {
     isGuideAtBottom: false,
-    moveGuideAtBottom: jest.fn(),
-    storeStepName: jest.fn()
+    history: {
+      push: jest.fn(),
+    },
+    setAccountOption: jest.fn(),
   };
 
-  const subject = (props) => (shallow(<GettingStartedGuide {...defaultProps} {...props} />));
+  const subject = props => shallow(<GettingStartedGuide {...defaultProps} {...props} />);
 
   it('should render correctly when guide is at bottom or when guide is at top', () => {
-    expect(subject({ isGuideAtBottom: true }).find('Panel').prop('actions')).toBe(null);
-    expect(subject({ isGuideAtBottom: false }).find('Panel').prop('actions')).toEqual(expect.arrayContaining([{ 'color': 'blue', 'content': <span> Move to Bottom <ArrowDownward size="20" /> </span>, 'onClick': defaultProps.moveGuideAtBottom }]));
+    expect(
+      subject({ isGuideAtBottom: true })
+        .find('Panel')
+        .prop('actions'),
+    ).toBe(null);
+    expect(
+      subject({ isGuideAtBottom: false })
+        .find('Panel')
+        .prop('actions'),
+    ).not.toBe(null);
   });
 
   it('should render Sending Step when Start Sending Button is clicked', () => {
@@ -26,22 +34,26 @@ describe('GettingStartedGuide', () => {
     expect(instance).toHaveTextContent('Let&#39;s Code');
   });
 
-  it('should store the stepName when any CardAction Button is clicked' , () => {
+  it('should store the stepName when any CardAction Button is clicked', () => {
     const instance = subject();
-    instance.find('CardActions').find('Button').at(0).simulate('click');
-    expect(defaultProps.storeStepName).toHaveBeenCalled();
-
+    instance
+      .find('CardActions')
+      .find('Button')
+      .at(0)
+      .simulate('click');
+    expect(defaultProps.setAccountOption).toHaveBeenCalled();
   });
 
   it('should render the corresponding step when breadcrumb is clicked', () => {
-
     const instance = subject();
     instance.find('Button').simulate('click');
     instance.find({ children: 'Show Me SparkPost' }).simulate('click');
-    instance.find('BreadCrumbsItem').at(1).simulate('click');
+    instance
+      .find('BreadCrumbsItem')
+      .at(1)
+      .simulate('click');
     expect(instance).toHaveTextContent('Show Me SparkPost');
     expect(instance).toHaveTextContent('Let&#39;s Code');
-
   });
 
   it('should render the BreadCrumbItem as active corresponding to the Step', () => {
@@ -56,5 +68,17 @@ describe('GettingStartedGuide', () => {
     instance.find('Button').simulate('click');
     instance.find({ children: 'Show Me SparkPost' }).simulate('click');
     expect(instance.find('GuideListItem')).toHaveLength(3);
+  });
+
+  it('should navigate to templates page when Send a Test Email button is clicked', () => {
+    const instance = subject();
+    instance.find('Button').simulate('click');
+    instance.find({ children: 'Show Me SparkPost' }).simulate('click');
+    instance
+      .find('GuideListItem')
+      .at(0)
+      .prop('action')
+      .onClick();
+    expect(defaultProps.history.push).toHaveBeenCalledWith('/templates');
   });
 });
