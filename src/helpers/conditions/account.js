c6578275987db24dfc37788b108b3f39c318907d
Patrick Sison <patrick.m.sison@gmail.com>
2019-12-06 09:59:05 -0500

    AC-921: Updated condition test

--- src/helpers/conditions/account.js
+++ src/helpers/conditions/account.js
@@ -10,8 +10,9 @@ export const hasStatus = status => ({ account }) => account.status === status;
 export const hasStatusReasonCategory = category => ({ account }) =>
   account.status_reason_category === category;
 export const isSuspendedForBilling = all(hasStatus('suspended'), hasStatusReasonCategory('100.01'));
+const SELF_SERVE = ['active', 'inactive', 'none'];
 export const subscriptionSelfServeIsTrue = ({ subscription }) =>
-  _.get(subscription, 'type') !== 'manual';
+  _.includes(SELF_SERVE, _.get(subscription, 'type'));
 export const isAws = ({ account }) => _.get(account, 'subscription.type') === 'aws';
 export const isCustomBilling = ({ account }) => _.get(account, 'subscription.custom', false);
 export const isSelfServeBilling = any(subscriptionSelfServeIsTrue, isAws);
