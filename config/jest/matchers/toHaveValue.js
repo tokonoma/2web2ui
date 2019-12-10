9881f5c2bfc697580df8c14a39ef33c0a04f4b44
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-12-10 12:38:58 -0500

    FE-797 - Adjust import to ensure the correct module is imported, implement with existing component

--- config/jest/matchers/toHaveValue.js
+++ config/jest/matchers/toHaveValue.js
@@ -1,9 +1,14 @@
 import { toHaveValue as testingLibraryToHaveValue } from '@testing-library/jest-dom';
-import { toHaveValue as enzymeToHaveValue } from 'enzyme-matchers/lib/assertions/toHaveValue';
+import { toHaveValue as enzymeToHaveValue } from 'enzyme-matchers';
 
 export default function toHaveValue(element, expectedValue) {
   try {
-    return enzymeToHaveValue(element, expectedValue);
+    const { pass, message } = enzymeToHaveValue(element, expectedValue);
+
+    return {
+      pass,
+      message: () => message,
+    };
   } catch {
     // Using `.apply` necessary so that the Testing Library .toHaveValue had access to a valid value for `this`
     return testingLibraryToHaveValue.apply(this, [element, expectedValue]);
