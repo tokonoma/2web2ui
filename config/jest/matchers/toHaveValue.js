3263c726567828be791a1fc838399b4ecfb71916
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-12-10 12:23:35 -0500

    FE-797 - Move comment to correct location

--- config/jest/matchers/toHaveValue.js
+++ config/jest/matchers/toHaveValue.js
@@ -2,10 +2,10 @@ import { toHaveValue as testingLibraryToHaveValue } from '@testing-library/jest-
 import { toHaveValue as enzymeToHaveValue } from 'enzyme-matchers/lib/assertions/toHaveValue';
 
 export default function toHaveValue(element, expectedValue) {
-  // Using `.apply` necessary so that the Testing Library .toHaveValue had access to a valid value for `this`
   try {
     return enzymeToHaveValue(element, expectedValue);
   } catch {
+    // Using `.apply` necessary so that the Testing Library .toHaveValue had access to a valid value for `this`
     return testingLibraryToHaveValue.apply(this, [element, expectedValue]);
   }
 }
