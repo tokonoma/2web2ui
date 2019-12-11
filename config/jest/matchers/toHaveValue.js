c8ff7fabf13bd77b61dd91f3c03b4c985f69ddf5
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-12-11 09:34:51 -0500

    FE-797 - Update toHaveValue matcher to check for Enzyme element or HTML object

--- config/jest/matchers/toHaveValue.js
+++ config/jest/matchers/toHaveValue.js
@@ -2,15 +2,28 @@ import { toHaveValue as testingLibraryToHaveValue } from '@testing-library/jest-
 import { toHaveValue as enzymeToHaveValue } from 'enzyme-matchers';
 
 export default function toHaveValue(element, expectedValue) {
-  try {
+  const isEnzymeObject =
+    element &&
+    (element.constructor.name === 'ShallowWrapper' || element.constructor.name === 'ReactWrapper');
+  const isHTMLElement = element && element instanceof window.HTMLElement; // Based on testing library's implementation => https://github.com/testing-library/jest-dom/blob/master/src/utils.js#L53
+
+  if (isEnzymeObject) {
     const { pass, message } = enzymeToHaveValue(element, expectedValue);
 
     return {
       pass,
       message: () => message,
     };
-  } catch {
+  }
+
+  if (isHTMLElement) {
     // Using `.apply` necessary so that the Testing Library .toHaveValue had access to a valid value for `this`
     return testingLibraryToHaveValue.apply(this, [element, expectedValue]);
   }
+
+  return {
+    pass: false,
+    message: () =>
+      `A valid Enzyme object or HTML element is required when using the "toHaveValue" matcher.`,
+  };
 }
