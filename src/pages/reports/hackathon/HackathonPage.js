f391b87976515792e3b9889f879f138722abfb3f
Tarryn Schantell <tarryn@edatasource.com>
2019-12-11 18:11:47 -0500

    Hacking away

--- src/pages/reports/hackathon/HackathonPage.js
+++ src/pages/reports/hackathon/HackathonPage.js
@@ -1,23 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import { Page } from '@sparkpost/matchbox';
+import { Page, Slider } from '@sparkpost/matchbox';
 import { Users } from 'src/components/images';
 import GoogleMapReact from 'google-map-react';
+import { subHours, addHours, subDays, startOfDay, differenceInHours, format } from 'date-fns';
+
+const DISPLAY_TIME = 500;
+const END_DATE = startOfDay(new Date());
+const START_DATE = subDays(END_DATE, 7);
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function generateTwentyDataPoints(lastTime) {
+  const lat = getRandomArbitrary(26, 48);
+  const lng = getRandomArbitrary(-124, -67);
+  return [...Array(5)].map(() => {
+    return { lat, lng, time: lastTime };
+  });
+}
+function generateListOfDataFromMockServer() {
+  let serverData = [];
+  let currTime = START_DATE;
+
+  while (currTime <= END_DATE) {
+    const lastTime = currTime;
+    serverData = [...serverData, ...generateTwentyDataPoints(lastTime)];
+    currTime = addHours(currTime, 1);
+  }
+
+  return serverData;
+}
+const serverGeneratedData = generateListOfDataFromMockServer();
 
 export const HackathonPage = props => {
   let [mapPoints, setMapPoints] = useState([]);
-  useEffect(() => {
+
+  const sliderSlices = [...Array(7)].reduce((sliderTicks, tick, i) => {
+    const chunks = differenceInHours(END_DATE, START_DATE) / 7;
+    const curr = addHours(START_DATE, chunks * i);
+    sliderTicks[Number(curr) - Number(START_DATE)] = format(curr, 'YYYY-MM-DD');
+    return sliderTicks;
+  }, {});
+  sliderSlices[Number(END_DATE) - Number(START_DATE)] = format(END_DATE, 'YYYY-MM-DD');
+
+  const startMappingDataPoints = (allTheData = [], lastTime) => {
+    const addMapPoint = () => {
+      const newMapPoints = allTheData.filter(d => {
+        return d.time <= addHours(lastTime, 1) && d.time > subHours(lastTime, 6);
+      });
+      console.log(newMapPoints);
+      setMapPoints(newMapPoints);
+    };
     setTimeout(() => {
-      const lat = 50 + Math.random() * 10;
-      const lng = 30 + Math.random() * 10;
-      const newMapPoints = [...mapPoints, { lat, lng }];
-      if (mapPoints.length < 10) {
-        setMapPoints(newMapPoints);
-      }
-    }, 100);
+      requestAnimationFrame(addMapPoint);
+    }, DISPLAY_TIME);
+  };
+
+  useEffect(() => {
+    const lastTime = mapPoints.length ? mapPoints.slice(-1)[0].time : addHours(START_DATE, 6);
+    console.log(lastTime);
+    startMappingDataPoints(serverGeneratedData, lastTime);
   }, [mapPoints]);
 
   const apiKey = { key: '' };
 
+  const startDate = START_DATE;
+  const endDate = END_DATE;
   const heatMapData = {
     positions: mapPoints,
     options: {
@@ -26,11 +73,12 @@ export const HackathonPage = props => {
     },
   };
   const center = {
-    lat: 59.95,
-    lng: 30.33,
+    lat: 39.82821,
+    lng: -98.579527,
   };
   const zoom = 1;
-  console.log(mapPoints);
+  const currentTime = heatMapData.positions.length && heatMapData.positions.slice(-1)[0].time;
+
   return (
     <Page
       empty={{
@@ -47,10 +95,18 @@ export const HackathonPage = props => {
           heatmap={heatMapData}
           center={center}
           zoom={zoom}
+          onChildClick={props => console.log(props)}
           onChildMouseEnter={props => console.log(props)}
           onChildMouseLeave={() => {}}
         ></GoogleMapReact>
       </div>
+      <Slider
+        disabled
+        min={0}
+        max={Number(endDate) - Number(START_DATE)}
+        ticks={sliderSlices}
+        value={Number(currentTime || startDate) - Number(START_DATE)}
+      />
     </Page>
   );
 };
