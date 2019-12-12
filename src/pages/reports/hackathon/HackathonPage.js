dfbf61a7018fdcb219c11d6a13ce1e8005cdc4ca
Tarryn Schantell <tarryn@edatasource.com>
2019-12-11 19:59:15 -0500

    Update to have slider be representation of real data

--- src/pages/reports/hackathon/HackathonPage.js
+++ src/pages/reports/hackathon/HackathonPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Page, Slider, Tabs } from '@sparkpost/matchbox';
 import { Users } from 'src/components/images';
 import GoogleMapReact from 'google-map-react';
-import { subHours, addHours, subDays, startOfDay, differenceInHours, format, isBefore, isAfter } from 'date-fns';
+import { subHours, addHours, subDays, startOfDay, differenceInHours, format } from 'date-fns';
 import { getHackathonData } from 'src/actions/hackathon';
 import connect from 'react-redux/es/connect/connect';
 import useTabs from '../../../hooks/useTabs/useTabs';
@@ -10,29 +10,6 @@ import useTabs from '../../../hooks/useTabs/useTabs';
 const DISPLAY_TIME = 500;
 const END_DATE = startOfDay(new Date());
 const START_DATE = subDays(END_DATE, 4);
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-function generateTwentyDataPoints(lastTime) {
-  const lat = getRandomArbitrary(26, 48);
-  const lng = getRandomArbitrary(-124, -67);
-  return [...Array(5)].map(() => {
-    return { lat, lng, time: lastTime };
-  });
-}
-function generateListOfDataFromMockServer() {
-  let serverData = [];
-  let currTime = START_DATE;
-
-  while (currTime <= END_DATE) {
-    const lastTime = currTime;
-    serverData = [...serverData, ...generateTwentyDataPoints(lastTime)];
-    currTime = addHours(currTime, 1);
-  }
-
-  return serverData;
-}
-const serverGeneratedData = generateListOfDataFromMockServer();
 
 const TABS = [
   { content: 'Delivery', key: 'delivery' },
@@ -42,35 +19,33 @@ const TABS = [
   { content: 'Deliverability-Spam', key: 'd12-spam' },
 ];
 
-export const HackathonPage = ({ getHackathonData, data=[] }) => {
+export const HackathonPage = ({ getHackathonData, data = [] }) => {
   const [mapPoints, setMapPoints] = useState([]);
   const [selectedTabIndex, tabs] = useTabs(TABS, 0);
+  let startDate = START_DATE;
+  let endDate = END_DATE;
+  let sliderSlices = [];
+
   useEffect(() => {
     getHackathonData();
   }, [getHackathonData, selectedTabIndex]);
 
   useEffect(() => {
-    if (data.length>0) {
+    if (data.length > 0) {
+      console.log(data);
+
       setMapPoints([]);
     }
   }, [data]);
 
-  const sliderSlices = [...Array(7)].reduce((sliderTicks, tick, i) => {
-    const chunks = differenceInHours(END_DATE, START_DATE) / 7;
-    const curr = addHours(START_DATE, chunks * i);
-    sliderTicks[Number(curr) - Number(START_DATE)] = format(curr, 'YYYY-MM-DD');
-    return sliderTicks;
-  }, {});
-  sliderSlices[Number(END_DATE) - Number(START_DATE)] = format(END_DATE, 'YYYY-MM-DD');
-
   const startMappingDataPoints = (allTheData = [], lastTime) => {
     const addMapPoint = () => {
       const newMapPoints = allTheData.filter(d => {
-         //console.log(addHours(lastTime, 24));
-         console.log(isBefore(d.eventTime, addHours(lastTime, 1)) && isAfter(d.eventTime, subHours(lastTime, 6)));
-        return  new Date(d.eventTime) >= subHours(lastTime, 6) && new Date(d.eventTime) < addHours(lastTime, 1);
+        return (
+          new Date(d.eventTime) >= subHours(lastTime, 6) &&
+          new Date(d.eventTime) < addHours(lastTime, 1)
+        );
       });
-      console.log(newMapPoints);
       setMapPoints(newMapPoints);
     };
     setTimeout(() => {
@@ -79,19 +54,31 @@ export const HackathonPage = ({ getHackathonData, data=[] }) => {
   };
 
   useEffect(() => {
-    if(data.length>0) {
-      const lastTime = mapPoints.length ? mapPoints.slice(-1)[0].eventTime : addHours(data[0].eventTime, 6);
-      console.log('lastTime', lastTime, data);
+    if (data.length > 0) {
+      const lastTime = mapPoints.length ? mapPoints.slice(-1)[0].eventTime : addHours(startDate, 6);
       startMappingDataPoints(data, lastTime);
     }
-  }, [mapPoints, data]);
+  }, [mapPoints, data, startDate]);
+
+  if (data.length > 0) {
+    startDate = new Date(data[0].eventTime);
+    endDate = new Date(data.slice(-1)[0].eventTime);
+
+    sliderSlices = [...Array(7)].reduce((sliderTicks, tick, i) => {
+      const chunks = differenceInHours(endDate, startDate) / 7;
+      const curr = addHours(startDate, chunks * i);
+      sliderTicks[Number(curr) - Number(startDate)] = format(curr, 'YYYY-MM-DD');
+      return sliderTicks;
+    }, {});
+    sliderSlices[Number(endDate) - Number(startDate)] = format(endDate, 'YYYY-MM-DD');
+  }
 
   const apiKey = { key: '' };
 
-  const startDate = START_DATE;
-  const endDate = END_DATE;
   const heatMapData = {
-    positions: mapPoints.map(({longitude, latitude, eventTime}) => {return {lng: longitude, lat: latitude, eventTime}}),
+    positions: mapPoints.map(({ longitude, latitude, eventTime }) => {
+      return { lng: longitude, lat: latitude, eventTime };
+    }),
     options: {
       radius: 10,
       opacity: 0.6,
@@ -129,9 +116,9 @@ export const HackathonPage = ({ getHackathonData, data=[] }) => {
       <Slider
         disabled
         min={0}
-        max={Number(endDate) - Number(START_DATE)}
+        max={Number(endDate) - Number(startDate)}
         ticks={sliderSlices}
-        value={Number(currentTime || startDate) - Number(START_DATE)}
+        value={Number(new Date(currentTime || startDate)) - Number(startDate)}
       />
     </Page>
   );
