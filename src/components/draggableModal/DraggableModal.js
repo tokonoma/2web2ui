49a81c328c98a47873d0b534d577f36de2a01b9d
Greg Friedman <greg.friedman@sparkpost.com>
2019-12-12 17:33:02 -0500

    Updates from before presentation

--- src/components/draggableModal/DraggableModal.js
+++ src/components/draggableModal/DraggableModal.js
@@ -3,14 +3,14 @@ import Draggable from 'react-draggable';
 
 export const DraggableModal = ({ open, handleToggle, children }) => {
   return (
-    <Draggable enableUserSelectHack={false}>
+    <Draggable enableUserSelectHack={false} handle=".handle">
       {open ? (
         <div
           style={{
             position: 'fixed',
             background: 'white',
             minHeight: '900px',
-            width: '500px',
+            width: '700px',
             zIndex: 999,
             padding: '10px',
             boxShadow: '0 1px 6px 0 rgba(32, 33, 36, .28)',
@@ -18,13 +18,18 @@ export const DraggableModal = ({ open, handleToggle, children }) => {
             overflowY: 'scroll',
           }}
         >
-          <button
-            style={{ position: 'absolute', color: 'black', top: '0px', right: '0px' }}
-            onClick={handleToggle}
+          <div
+            className="handle"
+            style={{ height: 30, backgroundColor: 'gray', position: 'relative' }}
           >
-            {' '}
-            X{' '}
-          </button>
+            <button
+              style={{ position: 'absolute', color: 'black', top: '0px', right: '0px' }}
+              onClick={handleToggle}
+            >
+              {' '}
+              X{' '}
+            </button>
+          </div>
           {children}
         </div>
       ) : (
