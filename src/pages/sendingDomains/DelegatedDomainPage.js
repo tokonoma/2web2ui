64db071da032ee69b400b3b6bd08ecbbabf8604a
Neal <nstanard@gmail.com>
2019-12-12 10:24:41 -0500

    styled table with mock data

--- src/pages/sendingDomains/DelegatedDomainPage.js
+++ src/pages/sendingDomains/DelegatedDomainPage.js
@@ -1,16 +1,151 @@
 import React from 'react';
+import './hack.css';
+import { Button, TextField, Select, Panel } from '@sparkpost/matchbox';
 import { connect } from 'react-redux';
 import { list } from 'src/actions/delegatedDomains';
 import { Page } from '@sparkpost/matchbox';
+import { Banner } from '@sparkpost/matchbox';
+import useRouter from 'src/hooks/useRouter';
 
 const DelegatedDomainPage = ({ delegatedDomains, list }) => {
+  const { requestParams: { id } = {} } = useRouter();
   React.useEffect(() => {
-    list();
-  }, [list]);
+    list(id);
+  }, [id, list]);
+  console.log(delegatedDomains);
 
-  return <Page title="Delgated Sending Domains">My Body</Page>;
+  const mockData = {
+    results: [
+      {
+        domain: 'foo.com',
+        records: [
+          {
+            type: 'NS',
+            name: 'foo.com.',
+            ttl: 172800,
+            data: 'ns-1536.awsdns-00.co.uk',
+          },
+          {
+            type: 'NS',
+            name: 'foo.com.',
+            ttl: 172800,
+            data: 'ns-1536.awsdns-00.co.uk',
+          },
+          {
+            type: 'NS',
+            name: 'foo.com.',
+            ttl: 172800,
+            data: 'ns-1536.awsdns-00.co.uk',
+          },
+          {
+            type: 'NS',
+            name: 'foo.com.',
+            ttl: 172800,
+            data: 'ns-1536.awsdns-00.co.uk',
+          },
+          {
+            type: 'A',
+            name: 'www.foo.com.',
+            ttl: 3600,
+            data: '1.2.3.4',
+          },
+          {
+            type: 'A',
+            name: 'www.foo.com.',
+            ttl: 3600,
+            data: '5.6.7.8',
+          },
+          {
+            type: 'A',
+            name: 'www.foo.com.',
+            ttl: 3600,
+            data: '4.3.2.1',
+          },
+          {
+            type: 'A',
+            name: 'www.foo.com.',
+            ttl: 3600,
+            data: '8.7.6.5',
+          },
+        ],
+      },
+    ],
+  };
+
+  function getDataRows() {
+    return mockData.results[0].records.map((item, index) => {
+      return (
+        <tr key={item.name + item.type + index}>
+          <td>{item.name}</td>
+          <td>{item.type}</td>
+          <td>{item.ttl}</td>
+          <td style={{ width: 400 }}>{item.data}</td>
+          {/* conditionally show edit text based on type - NS doens't get it */}
+          <td style={{ textAlign: 'end', width: 170 }}>
+            <Button outline size="small">
+              delete
+            </Button>
+            &nbsp;
+            <Button outline size="small">
+              edit
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  // const action = {
+  //   content: 'Learn more.',
+  //   to: LINKS.SENDING_REQS,
+  //   external: true,
+  // };
+
+  return (
+    <Page title="Delgated Sending Domains">
+      {/* action={action} */}
+      <Banner status="info" title="We'll be your DNS daddy, but here's the deal...">
+        <h5 style={{ marginTop: 15, marginBottom: 0 }}>Records we manage:</h5>
+        <p style={{ margin: 0 }}>A IP</p>
+        <p style={{ margin: 0 }}>MX name</p>
+        <p style={{ margin: 0 }}>NS hostnames</p>
+        <p style={{ margin: 0 }}>TXT SPF</p>
+        <h5 style={{ marginTop: 15, marginBottom: 0 }}>Records you manage:</h5>
+        <p style={{ margin: 0 }}>A IP</p>
+        <p style={{ margin: 0 }}>TXT Not SPF</p>
+        <p style={{ margin: 0 }}>CNAME any but @</p>
+      </Banner>
+      <h1>Custom records</h1>
+      <div id="dns-container">
+        <div id="new">
+          <TextField name="" placeholder={'@'} />
+          <Select id="id" options={['A', 'TXT', 'CNAME']} />
+          <TextField name="" placeholder={'1H'} />
+          <TextField name="" placeholder={'IPv4 Address'} />
+          <Button style={{ marginBottom: 20 }} type="button">
+            Add
+          </Button>
+        </div>
+        <Panel>
+          <table id="dns" style={{ tableLayout: 'fixed' }}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>TTL</th>
+                <th style={{ width: 400 }}>Data</th>
+                <th style={{ width: 170 }}></th>
+              </tr>
+            </thead>
+            <tbody>{getDataRows()}</tbody>
+          </table>
+        </Panel>
+      </div>
+    </Page>
+  );
 };
 
+// export default DelegatedDomainPage;
 export default connect(state => ({ delegatedDomains: state.delegatedDomains }), { list })(
   DelegatedDomainPage,
 );
