13825563a410c9da8097e9b3fa3ccef76346139f
Nick Lemmon <nick.lemmon@sparkpost.com>
2019-12-17 09:18:18 -0500

    SD-1264 - Add caption to screen reader only table

--- src/pages/ipPools/components/DeliveryHistoryLineChart.js
+++ src/pages/ipPools/components/DeliveryHistoryLineChart.js
@@ -9,54 +9,81 @@ import {
   ResponsiveContainer,
   CartesianGrid,
 } from 'recharts';
+import { ScreenReaderOnly } from '@sparkpost/matchbox';
+import { formatDate } from 'src/helpers/date';
 import { getLineChartFormatters } from 'src/helpers/chart';
 import { formatNumber } from 'src/helpers/units';
 import styles from './DeliveryHistoryLineChart.module.scss';
 
-const DeliveryHistoryLineChart = props => {
-  const { data } = props;
+const DeliveryHistoryLineChart = ({ data }) => {
   const { xTickFormatter, tooltipLabelFormatter } = getLineChartFormatters('day');
 
   return (
-    <div className={styles.ChartWrapper} data-id="delivery-history-line-chart">
-      <ResponsiveContainer height={350} width={'100%'}>
-        <LineChart data={data}>
-          <YAxis
-            dataKey="deliveries"
-            tickLine={false}
-            scale="linear"
-            width={66}
-            tickFormatter={formatNumber}
-            stroke="#55555a"
-          >
-            <Label value="Volume" angle={-90} position="insideLeft" opacity={0.5} fontSize={16} />
-          </YAxis>
+    <>
+      <div className={styles.ChartWrapper} data-id="delivery-history-line-chart" aria-hidden="true">
+        <ResponsiveContainer height={350} width={'100%'}>
+          <LineChart data={data}>
+            <YAxis
+              dataKey="deliveries"
+              tickLine={false}
+              scale="linear"
+              width={66}
+              tickFormatter={formatNumber}
+              stroke="#55555a"
+            >
+              <Label value="Volume" angle={-90} position="insideLeft" opacity={0.5} fontSize={16} />
+            </YAxis>
 
-          <XAxis
-            dataKey="date"
-            tickLine={false}
-            padding={{ left: 25 }}
-            height={66}
-            stroke="#55555a"
-            tickFormatter={xTickFormatter}
-          >
-            <Label value="Date" offset={15} position="insideBottom" opacity={0.5} fontSize={16} />
-          </XAxis>
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              padding={{ left: 25 }}
+              height={66}
+              stroke="#55555a"
+              tickFormatter={xTickFormatter}
+            >
+              <Label value="Date" offset={15} position="insideBottom" opacity={0.5} fontSize={16} />
+            </XAxis>
 
-          <Tooltip labelFormatter={tooltipLabelFormatter} />
+            <Tooltip labelFormatter={tooltipLabelFormatter} />
 
-          <Line
-            type="monotone"
-            stroke="#2693C3"
-            strokeWidth={2}
-            dataKey="deliveries"
-            dot={{ r: 3, strokeWidth: 1 }}
-          />
+            <Line
+              type="monotone"
+              stroke="#2693C3"
+              strokeWidth={2}
+              dataKey="deliveries"
+              dot={{ r: 3, strokeWidth: 1 }}
+            />
 
-          <CartesianGrid vertical={false} opacity={0.5} />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
+            <CartesianGrid vertical={false} opacity={0.5} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+
+      <ScreenReaderOnly>
+        <table>
+          <caption>Deliveries Over the Last 10 Days</caption>
+
+          <thead>
+            <tr>
+              <th>Date</th>
+
+              <th>Number of Deliveries</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {data.map(row => (
+              <tr>
+                <td>{formatDate(row.date)}</td>
+
+                <td>{row.deliveries}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </ScreenReaderOnly>
+    </>
   );
 };
 
