e29a39070c4e4195c8162aa8136b4eaec7a72bb5
Brian Kemper <brian.kemper@sparkpost.com>
2019-12-17 13:26:11 -0500

    WIP

--- src/pages/alerts/constants/formConstants.js
+++ src/pages/alerts/constants/formConstants.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { EmailIcon, SlackIcon, WebhookIcon } from 'src/components/icons';
-import { emails, ifStringPresent } from 'src/helpers/validation';
+import { emails, ifStringPresent, maxItems, noDuplicateItems } from 'src/helpers/validation';
 import { UnstyledLink } from '@sparkpost/matchbox';
 export const FORM_NAME = 'alertForm';
 
@@ -9,7 +9,7 @@ export const METRICS = {
   health_score: 'Health Score',
   block_bounce_rate: 'Block Bounce Rate',
   hard_bounce_rate: 'Hard Bounce Rate',
-  soft_bounce_rate: 'Soft Bounce Rate'
+  soft_bounce_rate: 'Soft Bounce Rate',
 };
 
 export const FILTERS_FRIENDLY_NAMES = {
@@ -18,110 +18,111 @@ export const FILTERS_FRIENDLY_NAMES = {
   mailbox_provider: 'Mailbox Provider',
   sending_domain: 'Sending Domain',
   sending_ip: 'Sending IP',
-  subaccount_id: 'Subaccounts'
+  subaccount_id: 'Subaccounts',
 };
 
 export const RECOMMENDED_METRIC_VALUE = {
   monthly_sending_limit: {
     raw: {
-      gt: 80
-    }},
+      gt: 80,
+    },
+  },
   health_score: {
     raw: {
       lt: 80,
-      gt: 70
+      gt: 70,
     },
     week_over_week: {
-      gt: 10
+      gt: 10,
     },
     day_over_day: {
-      gt: 10
-    }
+      gt: 10,
+    },
   },
   block_bounce_rate: {
     raw: {
-      gt: 20
-    }
+      gt: 20,
+    },
   },
   hard_bounce_rate: {
     raw: {
-      gt: 20
-    }
+      gt: 20,
+    },
   },
   soft_bounce_rate: {
     raw: {
-      gt: 20
-    }
-  }
+      gt: 20,
+    },
+  },
 };
 
-const webhookSubtitle = <Fragment>
-  {'Create a webhook for this alert. '}
-  <UnstyledLink external to={'https://www.sparkpost.com/docs/user-guide/alerts/#webhook-payload-sample'}>Sample payload</UnstyledLink>
-</Fragment>;
+const webhookSubtitle = (
+  <Fragment>
+    {'Create a webhook for this alert. '}
+    <UnstyledLink
+      external
+      to={'https://www.sparkpost.com/docs/user-guide/alerts/#webhook-payload-sample'}
+    >
+      Sample payload
+    </UnstyledLink>
+  </Fragment>
+);
 
-const slackSubtitle = <Fragment>
-  {'Integrate this alert with Slack. '}
-  <UnstyledLink external to={'https://api.slack.com/incoming-webhooks'}>How to create a Slack Incoming webhook</UnstyledLink>
-</Fragment>;
-
+const slackSubtitle = (
+  <Fragment>
+    {'Integrate this alert with Slack. '}
+    <UnstyledLink external to={'https://api.slack.com/incoming-webhooks'}>
+      How to create a Slack Incoming webhook
+    </UnstyledLink>
+  </Fragment>
+);
 
 export const NOTIFICATION_CHANNEL_DATA = {
   emails: {
     icon: <EmailIcon />,
     subtitle: 'Receive notifications through email. One address per line. Up to 10 addresses.',
     fieldProps: {
-      validate: [ifStringPresent(emails)],
+      validate: [
+        ifStringPresent(emails),
+        ifStringPresent(noDuplicateItems),
+        ifStringPresent(maxItems(10)),
+      ],
       placeholder: 'example@email.com',
-      multiline: true
-    }
+      multiline: true,
+    },
   },
   slack: {
     icon: <SlackIcon />,
     subtitle: slackSubtitle,
     fieldProps: {
-      placeholder: 'https://hooks.slack.com/services/T00/B00/XX '
-    }
+      placeholder: 'https://hooks.slack.com/services/T00/B00/XX ',
+    },
   },
   webhook: {
     icon: <WebhookIcon />,
     subtitle: webhookSubtitle,
     fieldProps: {
-      placeholder: 'https://example.com/webhook-target'
-    }
-  }
+      placeholder: 'https://example.com/webhook-target',
+    },
+  },
 };
 
-export const SIGNALS_FILTERS = [
-  'none',
-  'ip_pool',
-  'mailbox_provider',
-  'sending_domain'
-];
+export const SIGNALS_FILTERS = ['none', 'ip_pool', 'mailbox_provider', 'sending_domain'];
 
-export const REALTIME_FILTERS = [
-  'sending_ip',
-  'mailbox_provider',
-  'sending_domain'
-];
+export const REALTIME_FILTERS = ['sending_ip', 'mailbox_provider', 'sending_domain'];
 
-export const SOURCE_FRIENDLY_NAMES =
-  {
-    raw: 'Score',
-    week_over_week: 'Week over Week',
-    day_over_day: 'Day over Day'
-  };
+export const SOURCE_FRIENDLY_NAMES = {
+  raw: 'Score',
+  week_over_week: 'Week over Week',
+  day_over_day: 'Day over Day',
+};
 
 export const OPERATOR_FRIENDLY_NAMES = {
   gt: 'Above',
-  lt: 'Below'
+  lt: 'Below',
 };
 
-export const NOTIFICATION_CHANNELS = [
-  'emails',
-  'slack',
-  'webhook'
-];
+export const NOTIFICATION_CHANNELS = ['emails', 'slack', 'webhook'];
 
 export const DEFAULT_FORM_VALUES = {
   name: '',
@@ -130,9 +131,9 @@ export const DEFAULT_FORM_VALUES = {
   sending_ip: [],
   mailbox_provider: [],
   sending_domain: [],
-  single_filter: { filter_type: 'none', filter_values: []},
+  single_filter: { filter_type: 'none', filter_values: [] },
   source: 'raw',
   operator: 'gt',
   value: 0,
-  muted: false
+  muted: false,
 };
