8a8380a600c7151289e258ec6f64dd5b2307a364
Kevin Chu <kevinchu1337@gmail.com>
2019-12-17 14:30:05 -0500

    SA-817: Update FilterSortCollection

--- src/pages/inboxPlacement/components/FilterSortCollection.js
+++ src/pages/inboxPlacement/components/FilterSortCollection.js
@@ -3,8 +3,21 @@ import { Select, Panel, Table, Grid } from '@sparkpost/matchbox';
 import { Collection } from 'src/components';
 import _ from 'lodash';
 import styles from './FilterSortCollection.module.scss';
-const FilterSortCollection = ({ wrapperComponent, title, selectOptions, filterBoxConfig, defaultSortColumn, defaultSortDirection = 'desc', rows = [], rowComponent }) => {
-
+import TableHeader from 'src/components/collection/TableHeader';
+const FilterSortCollection = ({
+  wrapperComponent,
+  title,
+  selectOptions,
+  filterBoxConfig,
+  defaultSortColumn,
+  defaultSortDirection = 'desc',
+  rows = [],
+  columns,
+  headerComponent,
+  rowComponent,
+  getRowData,
+  saveCsv = true,
+}) => {
   const [sortColumn, setSortColumn] = useState(defaultSortColumn);
   const [sortDirection, setSortDirection] = useState(defaultSortDirection);
 
@@ -13,56 +26,68 @@ const FilterSortCollection = ({ wrapperComponent, title, selectOptions, filterBo
     setSortDirection(sortDirection);
   };
 
-  const sortOnChange = ({ target: { value }}) => setSort({ sortColumn: value, sortDirection: defaultSortDirection });
+  const sortOnChange = ({ target: { value } }) =>
+    setSort({ sortColumn: value, sortDirection: defaultSortDirection });
 
-  const TableBody = (props) => <tbody>{props.children}</tbody>;
+  const TableBody = props => <tbody>{props.children}</tbody>;
 
-  const CollectionWrapperComponent = (props) => (
+  const CollectionWrapperComponent = props => (
     <div className={styles.TableWrapper}>
       <Table>{props.children}</Table>
     </div>
   );
 
   const WrapperComponent = wrapperComponent ? wrapperComponent : CollectionWrapperComponent;
-  const RowComponent = rowComponent ? rowComponent : () => (<div></div>) ;
-  const sortedRows = sortColumn ? _.orderBy(rows, sortColumn, sortDirection) : rows ;
+  const RowComponent = rowComponent
+    ? rowComponent
+    : props => <Table.Row rowData={getRowData(props)} />;
+  const HeaderComponent = headerComponent
+    ? headerComponent
+    : columns
+    ? () => <TableHeader columns={columns} />
+    : () => null;
+  const sortedRows = sortColumn ? _.orderBy(rows, sortColumn, sortDirection) : rows;
 
   return (
     <Collection
       outerWrapper={WrapperComponent}
       bodyWrapper={TableBody}
+      headerComponent={HeaderComponent}
       rowComponent={RowComponent}
       rows={sortedRows}
       sortColumn={sortColumn}
       filterBox={filterBoxConfig}
       pagination={true}
       defaultPerPage={10}
+      saveCsv={saveCsv}
     >
       {({ collection, filterBox, pagination }) => (
         <>
-        <Panel>
-          <Panel.Section className = {styles.Title}>
-            <h3>{title}</h3>
-          </Panel.Section>
-          <div className = {styles.FilterPanel}>
-            <Grid>
-              <Grid.Column xs={12} md={9}>
-                {filterBox}
-              </Grid.Column>
-              <Grid.Column xs={12} md={3}>
-                <Select
-                  id='sortSelect'
-                  defaultValue={defaultSortColumn}
-                  options={selectOptions}
-                  onChange={sortOnChange}
-                />
-              </Grid.Column>
-            </Grid>
-          </div>
-          {collection}
-        </Panel>
-        {pagination}
-      </>
+          <Panel>
+            {title && (
+              <Panel.Section className={styles.Title}>
+                <h3>{title}</h3>
+              </Panel.Section>
+            )}
+            <div className={styles.FilterPanel}>
+              <Grid>
+                <Grid.Column xs={12} md={9}>
+                  {filterBox}
+                </Grid.Column>
+                <Grid.Column xs={12} md={3}>
+                  <Select
+                    id="sortSelect"
+                    defaultValue={defaultSortColumn}
+                    options={selectOptions}
+                    onChange={sortOnChange}
+                  />
+                </Grid.Column>
+              </Grid>
+            </div>
+            {collection}
+          </Panel>
+          {pagination}
+        </>
       )}
     </Collection>
   );
