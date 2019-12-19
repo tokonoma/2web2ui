e60f1849581b59bc238350b052f84718b8cacc16
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-12-19 12:12:01 -0500

    [AC-1088] added story for breadcrumb and prettier stuff

--- src/components/breadcrumbs/BreadCrumbs.js
+++ src/components/breadcrumbs/BreadCrumbs.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import styles from './BreadCrumbs.module.scss';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 export const BreadCrumbs = ({ children }) => (
   <div className={styles.BreadCrumbContainer}>
     {React.Children.map(children, (child, index) => (
-  <>
-  {child} {index + 1 !== React.Children.count(children) && <div className={styles.Arrow}>&nbsp;>&nbsp;</div>}
-  </>
+      <>
+        {child}{' '}
+        {index + 1 !== React.Children.count(children) && (
+          <div className={styles.Arrow}>&nbsp;>&nbsp;</div>
+        )}
+      </>
     ))}
   </div>
 );
 
-
 export const BreadCrumbsItem = ({ children, active, onClick }) => (
-  <span onClick={onClick} className={classNames(!active && styles.InActiveItem, styles.BreadCrumbItem)}>{children} </span>
+  <span
+    onClick={onClick}
+    className={classNames(!active && styles.InActiveItem, styles.BreadCrumbItem)}
+  >
+    {children}{' '}
+  </span>
 );
+
+BreadCrumbs.propTypes = {
+  children: PropTypes.element,
+};
+
+BreadCrumbsItem.propTypes = {
+  children: PropTypes.element,
+  active: PropTypes.bool,
+  onClick: PropTypes.func,
+};
