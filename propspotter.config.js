e2988114d8d9a8741858cbe3fa8ce11f4022b47f
Jon Ambas <jon@jonambas.com>
2019-12-23 17:08:28 -0500

    Add location url

--- propspotter.config.js
+++ propspotter.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  title: '2web2ui components',
   include: "/src/**/!(__testHelpers__)/**/!(*.spec|*.test).js",
   port: 9000,
-  openBrowser: true
+  openBrowser: true,
+  locationUrl: 'https://github.com/SparkPost/2web2ui/tree/master'
 }
