a3ca3481b7741611f36eee3847f70288f6ba49ec
shrirupa <shrirupa.chowdhury@sparkpost.com>
2019-12-30 13:58:53 -0500

    [FE-784] wip - added tests for downgrading plans

--- cypress/tests/integration/accounts/change-billing-page.spec.js
+++ cypress/tests/integration/accounts/change-billing-page.spec.js
@@ -38,9 +38,6 @@ describe('Billing Page', () => {
     });
 
     cy.visit('/account/billing/plan');
-  });
-
-  it('on downgrading plan renders section with changes to features', () => {
     cy.server();
     cy.fixture('metrics/deliverability/200.get.json').as('deliverabilityGet');
     cy.route({
@@ -55,7 +52,67 @@ describe('Billing Page', () => {
       url: 'https://app.brightback.com/precancel',
       response: '@precancelPost',
     });
+  });
+
+  //downgrading, starter  => free plan
+  it('on changing plan renders section with changes to features', () => {
     cy.get('[data-id=select-plan-free500-0419]').click();
     cy.findAllByText('Changes to Features').should('exist');
   });
+
+  //downgrading, starter  => free plan
+  it('Change plan button is displayed only when all the "features have been updated" ', () => {
+    cy.get('[data-id=select-plan-free500-0419]').click();
+    cy.get('body').then($body => {
+      if ($body.text().includes('features have been updated')) {
+        cy.get('a[type=button]')
+          .contains('Change Plan')
+          .should('be.visible');
+      } else {
+        cy.get('a[type=button]')
+          .contains('Change Plan')
+          .should('not.be.visible');
+      }
+    });
+  });
+
+  //downgrading, starter  => free plan
+  //redirects to brightback page
+  it('redirects to brightback page on downgrading to free plan', () => {
+    cy.get('[data-id=select-plan-free500-0419]').click();
+    cy.get('a[type=button]')
+      .contains('Change Plan')
+      .click();
+    cy.url().should('include', 'brightback ');
+  });
 });
+
+// const fillBrightBackFormAndRedirect = () => {
+//   cy.get('[type="radio"]').check('other_reason');
+//   cy.get('[type="text"]')
+//     .first()
+//     .type('Some reason');
+//   cy.get('[id="competition-content"]').click();
+//   cy.get('div')
+//     .contains('None')
+//     .click();
+//   cy.get('[type="radio"]').check('1');
+//   cy.get('[type="checkbox"]').check();
+//   cy.get('button')
+//     .contains('Cancel my plan')
+//     .click();
+//   cy.get('input[name="delete"]').type('CANCEL');
+//   cy.get('button')
+//     .contains('Cancel my account')
+//     .click();
+// };
+
+// //downgrading, starter  => free plan
+// //after downgrading billing page has a pending plan change banner and Change Plan button is not visible
+// it('after downgrading billing page has a pending plan change banner and Change Plan button is not visible', () => {
+//   cy.get('[data-id=select-plan-free500-0419]').click();
+//   cy.get('a[type=button]')
+//     .contains('Change Plan')
+//     .click();
+//   fillBrightBackFormAndRedirect();
+// });
