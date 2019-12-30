796eeeff8760664375933fff157ae5c6b5e50f03
Patrick Sison <patrick.m.sison@gmail.com>
2019-12-30 12:17:58 -0500

    AC-1093: Removed references to increasing limits

--- src/components/usageReport/SendMoreCTA.js
+++ src/components/usageReport/SendMoreCTA.js
@@ -6,49 +6,41 @@ import { showAlert } from 'src/actions/globalAlert';
 import { PageLink } from 'src/components';
 import ConditionSwitch, { Case, defaultCase } from 'src/components/auth/ConditionSwitch';
 import { AccessControl } from 'src/components/auth';
-import { isAdmin, isEmailVerified } from 'src/helpers/conditions/user';
+import { isAdmin } from 'src/helpers/conditions/user';
 import { onPlanWithStatus } from 'src/helpers/conditions/account';
-import { not } from 'src/helpers/conditions';
 import { LINKS } from 'src/constants';
 
 export class SendMoreCTA extends Component {
-
   resendVerification = () => {
     const { verifyEmail, showAlert } = this.props;
-    return verifyEmail()
-      .then(() => showAlert({
+    return verifyEmail().then(() =>
+      showAlert({
         type: 'success',
-        message: 'Please click the link in the email we sent you to verify your email.'
-      }));
-  }
+        message: 'Please click the link in the email we sent you to verify your email.',
+      }),
+    );
+  };
 
   render() {
-    const { hasSendingLimits, verifyingEmail } = this.props;
+    const { hasSendingLimits } = this.props;
 
     return (
       <AccessControl condition={isAdmin}>
         <p>
-          Need to send more?
-          {' '}
+          Need to send more?{' '}
           <ConditionSwitch>
-
-            {/* email isn't verified */}
-            <Case condition={not(isEmailVerified)}>
-              {verifyingEmail ? (
-                <span>Resending a verification email... </span>
-              ) : (
-                <UnstyledLink onClick={this.resendVerification}> Verify your email. </UnstyledLink>
-              )}
-            </Case>
-
             {/* on a deprecated plan */}
-            <Case condition={onPlanWithStatus('deprecated')} children={<PageLink to="/account/billing">Switch to a new plan.</PageLink>} />
+            <Case
+              condition={onPlanWithStatus('deprecated')}
+              children={<PageLink to="/account/billing">Switch to a new plan.</PageLink>}
+            />
 
             {/* regardless of self serve billing */}
-            <Case condition={defaultCase} children={<PageLink to="/account/billing">Upgrade your account.</PageLink>} />
-
-          </ConditionSwitch>
-          {' '}
+            <Case
+              condition={defaultCase}
+              children={<PageLink to="/account/billing">Upgrade your account.</PageLink>}
+            />
+          </ConditionSwitch>{' '}
           {hasSendingLimits && (
             <UnstyledLink to={LINKS.DAILY_MONTHLY_QUOTA_LIMIT_DOC} external>
               Learn more about these limits.
@@ -60,10 +52,11 @@ export class SendMoreCTA extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  verifyingEmail: state.currentUser.verifyingEmail
+const mapStateToProps = state => ({
+  verifyingEmail: state.currentUser.verifyingEmail,
 });
 const mapDispatchToProps = {
-  verifyEmail, showAlert
+  verifyEmail,
+  showAlert,
 };
 export default connect(mapStateToProps, mapDispatchToProps)(SendMoreCTA);
--- src/components/usageReport/tests/SendMoreCTA.test.js
+++ src/components/usageReport/tests/SendMoreCTA.test.js
@@ -5,18 +5,15 @@ import { SendMoreCTA } from '../SendMoreCTA';
 describe('SendMoreCTA Component', () => {
   let props;
   let wrapper;
-  let instance;
 
   beforeEach(() => {
-
     props = {
       verifyEmail: jest.fn(() => Promise.resolve()),
       showAlert: jest.fn(() => Promise.resolve()),
-      openSupportTicketForm: jest.fn()
+      openSupportTicketForm: jest.fn(),
     };
 
     wrapper = shallow(<SendMoreCTA {...props} />);
-    instance = wrapper.instance();
   });
 
   it('renders correctly', () => {
@@ -26,21 +23,6 @@ describe('SendMoreCTA Component', () => {
   it('renders learn more about sending limits link', () => {
     wrapper.setProps({ hasSendingLimits: true });
 
-    expect(
-      wrapper.findWhere((node) => node.text() === 'Learn more about these limits.')
-    ).toExist();
-  });
-
-  describe('resendVerification', () => {
-    it('renders verifying state', () => {
-      wrapper.setProps({ verifyingEmail: true });
-      expect(wrapper.find('span').text()).toEqual('Resending a verification email... ');
-    });
-
-    it('re-sends verification email and shows alert', async () => {
-      await instance.resendVerification();
-      expect(props.verifyEmail).toHaveBeenCalled();
-      expect(props.showAlert).toHaveBeenCalled();
-    });
+    expect(wrapper.findWhere(node => node.text() === 'Learn more about these limits.')).toExist();
   });
 });
