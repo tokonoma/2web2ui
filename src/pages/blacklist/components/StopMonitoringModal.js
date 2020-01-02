a640a69d14074cc34e2c9d44154e0b421a817f64
Kevin Chu <kevinchu1337@gmail.com>
2020-01-02 10:56:36 -0500

    SA-1132: Created Stop Monitoring modal

---
+++ src/pages/blacklist/components/StopMonitoringModal.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button, Modal, Panel } from '@sparkpost/matchbox';
+import { connect } from 'react-redux';
+
+import { Loading } from 'src/components';
+import styles from './StopMonitoringModal.module.scss';
+import { deleteMonitor } from 'src/actions/blacklist';
+import { showAlert } from 'src/actions/globalAlert';
+
+export const StopMonitoringModal = ({
+  closeModal,
+  deleteMonitor,
+  isPending,
+  monitorToDelete,
+  showAlert,
+}) => {
+  const confirmAction = () => {
+    deleteMonitor(monitorToDelete).then(() => {
+      showAlert({
+        type: 'success',
+        message: `Stopped Monitoring ${monitorToDelete}.`,
+      });
+      closeModal();
+    });
+  };
+  const renderContent = () => {
+    return (
+      <>
+        <h4>Explanation Text</h4>
+        <Button className={styles.Confirm} disabled={isPending} onClick={confirmAction} primary>
+          Stop Monitoring
+        </Button>
+      </>
+    );
+  };
+
+  const title = monitorToDelete ? `Stop Monitoring ${monitorToDelete}` : '';
+
+  return (
+    <Modal open={Boolean(monitorToDelete)} onClose={closeModal} showCloseButton={true}>
+      <Panel title={title} accent sectioned>
+        {isPending ? (
+          <div className={styles.Loading}>
+            <Loading />
+          </div>
+        ) : (
+          renderContent()
+        )}
+      </Panel>
+    </Modal>
+  );
+};
+
+const mapStateToProps = state => ({
+  isPending: state.blacklist.deleteMonitorPending || state.blacklist.monitorsPending,
+});
+export default connect(mapStateToProps, { deleteMonitor, showAlert })(StopMonitoringModal);
---
+++ src/pages/blacklist/components/tests/StopMonitoringModal.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import { StopMonitoringModal } from '../StopMonitoringModal';
+
+describe('Stop Monitoring Modal', () => {
+  const closeModal = jest.fn();
+  const deleteMonitor = jest.fn().mockResolvedValue();
+  const showAlert = jest.fn();
+  const monitorToDelete = 'sparkpost.io';
+  const subject = ({ ...props }) => {
+    const defaults = { closeModal, monitorToDelete, deleteMonitor, showAlert };
+
+    return render(<StopMonitoringModal {...defaults} {...props} />);
+  };
+
+  it('renders the modal correctly', () => {
+    const { queryByText } = subject();
+
+    expect(queryByText('Stop Monitoring sparkpost.io')).toBeInTheDocument();
+    expect(queryByText('Stop Monitoring')).toBeInTheDocument();
+    expect(queryByText('Explanation Text')).toBeInTheDocument();
+  });
+
+  it('upon clicking the confirm button, deletes the resource, shows alert, and closes the modal', async () => {
+    const { queryByText } = subject();
+
+    await fireEvent.click(queryByText('Stop Monitoring'));
+    expect(deleteMonitor).toHaveBeenCalled();
+    expect(showAlert).toHaveBeenCalled();
+    expect(closeModal).toHaveBeenCalled();
+  });
+});
