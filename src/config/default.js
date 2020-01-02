2e1839a02d20db2ee21b3ed47f207fb0ce1a21ac
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-02 13:56:05 -0500

    TR-2038 - Update config to be 12 MB instead of 10 MB

--- src/config/default.js
+++ src/config/default.js
@@ -57,7 +57,7 @@ const config = identifier => ({
   },
   maxUploadSizeBytes: 20000000,
   maxRecipVerifUploadSizeBytes: 20971520, // NGNIX configures max upload size in megabytes, translating to this value in bytes
-  maxRecipListUploadSizeBytes: 10485760,
+  maxRecipListUploadSizeBytes: 12582912,
   metricsPrecisionMap: [
     { time: 60, value: '1min', format: 'ha' },
     { time: 60 * 2, value: '5min', format: 'ha' },
