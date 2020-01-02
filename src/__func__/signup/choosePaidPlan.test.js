e5b89ce0427514cd9d7d5f59f5c4261f51eb3a1d
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-01-02 17:44:56 -0500

    [AC-1161] updated snapshots

--- src/__func__/signup/choosePaidPlan.test.js
+++ src/__func__/signup/choosePaidPlan.test.js
@@ -3,10 +3,10 @@ import signupFlow, { freePlanPredicate } from './helpers';
 test('Signup flow: choose paid plan', async () => {
   const page = await signupFlow();
   await page.signup();
-  await page.choosePlan({ planFilter: (plan) => !freePlanPredicate(plan), submit: false });
+  await page.choosePlan({ planFilter: plan => !freePlanPredicate(plan), submit: false });
   await page.fillBillingForm();
   await page.simulate('form', 'submit');
 
-  expect(page.currentRoute()).toEqual('/onboarding/sending-domain');
+  expect(page.currentRoute()).toEqual('/dashboard');
   expect(page.mockApiCalls).toMatchSnapshot();
 });
