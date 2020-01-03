f4f72afdfd41a19e5cbac630ae5ee3eb84d472a2
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-03 10:50:02 -0500

    TR-2043 - Update icons to hide them from screen reader users

--- src/pages/recipientValidation/components/icons.js
+++ src/pages/recipientValidation/components/icons.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 export const WarningIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    aria-hidden="true"
+  >
     <path
       fill="#ffda00"
       d="M48.84 4.68A45.16 45.16 0 1 0 94 49.84 45.16 45.16 0 0 0 48.84 4.68zm-1.5 19.91a1.5 1.5 0 0 1 3 0V60a1.5 1.5 0 1 1-3 0zm3.26 51.22a2.29 2.29 0 0 1-4-1.6 2.29 2.29 0 0 1 4.4-.94 2.3 2.3 0 0 1-.4 2.54z"
@@ -10,7 +16,13 @@ export const WarningIcon = () => (
 );
 
 export const SuccessIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    aria-hidden="true"
+  >
     <path
       fill="#46d09e"
       d="M49.84 4.68A45.16 45.16 0 1 0 95 49.84 45.16 45.16 0 0 0 49.84 4.68zm23.63 32L46 64.06a5.26 5.26 0 0 1-7.44 0L26.22 51.67a1.49 1.49 0 0 1 0-2.12 1.51 1.51 0 0 1 2.12 0l12.38 12.39a2.27 2.27 0 0 0 3.2 0l27.43-27.43a1.49 1.49 0 0 1 2.12 0 1.51 1.51 0 0 1 0 2.12z"
@@ -19,7 +31,13 @@ export const SuccessIcon = () => (
 );
 
 export const InfoIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    aria-hidden="true"
+  >
     <path
       fill="#37aadc"
       d="M49.84 4.68A45.16 45.16 0 1 0 95 49.84 45.16 45.16 0 0 0 49.84
@@ -33,7 +51,13 @@ export const InfoIcon = () => (
 );
 
 export const ErrorIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="100"
+    height="100"
+    viewBox="0 0 100 100"
+    aria-hidden="true"
+  >
     <path
       fill="#d93b36"
       d="M49.84 4.68A45.16 45.16 0 1 0 95 49.84 45.16 45.16 0 0 0 49.84 4.68zM69 66.88A1.49 1.49 0 0 1 69 69a1.5 1.5 0 0 1-2.12 0l-17-17-17 17a1.53 1.53 0 0 1-1.06.44 1.5 1.5 0 0 1-1.06-2.56l17-17-17-17a1.5 1.5 0 0 1 2.12-2.12l17 17 17-17A1.5 1.5 0 0 1 69 32.8l-17 17z"
