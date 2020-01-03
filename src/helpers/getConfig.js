a84e64f94215d4dfb17391ae23e27f8b8372b130
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-03 09:33:07 -0500

    TR-2038 - Reformat file with Prettier

--- src/helpers/getConfig.js
+++ src/helpers/getConfig.js
@@ -2,6 +2,6 @@ import _ from 'lodash';
 import config from 'src/config';
 
 // This is much easier to stub than using the object directly
-const getConfig = (path) => _.get(config, path);
+const getConfig = path => _.get(config, path);
 
 export default getConfig;
