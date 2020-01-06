703118fb56df2b82862fcf803fb83a3a1f3bb241
Greg Friedman <greg.friedman@sparkpost.com>
2020-01-06 16:07:17 -0500

    SA-827 Added tests for new selectors

--- src/selectors/tests/blacklist.test.js
+++ src/selectors/tests/blacklist.test.js
@@ -52,4 +52,203 @@ describe('Blacklist Selectors: ', () => {
   it('selectBlacklistedCount returns total number of resources blacklisted', () => {
     expect(blacklistSelectors.selectBlacklistedCount({ blacklist: { monitors } })).toEqual(2);
   });
+
+  it('selectRelatedIncidentsForResource returns resources not including the current incident', () => {
+    const incident = {
+      id: 'id-1',
+      resource: 'foo',
+      blacklist_name: 'spamhaus-sbl',
+      occurred_at: '2019-07-23T12:48:00.000Z',
+    };
+
+    const incidentsForResource = [
+      {
+        id: 'id-1',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-2',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl 2',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-3',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl 3',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-4',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl 4',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-5',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl 5',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+    ];
+
+    expect(
+      blacklistSelectors.selectRelatedIncidentsForResource({
+        blacklist: { incidentsForResource, incident },
+      }),
+    ).toHaveLength(3);
+
+    // list of 3 where one is same id, so 2 total
+    expect(
+      blacklistSelectors.selectRelatedIncidentsForResource({
+        blacklist: { incidentsForResource: incidentsForResource.slice(0, 3), incident },
+      }),
+    ).toHaveLength(2);
+
+    // list of 3 where none have current id, so 3 total
+    expect(
+      blacklistSelectors.selectRelatedIncidentsForResource({
+        blacklist: { incidentsForResource: incidentsForResource.slice(1), incident },
+      }),
+    ).toHaveLength(3);
+  });
+
+  it('selectRelatedIncidentsForBlacklist returns resources not including the current incident', () => {
+    const incident = {
+      id: 'id-1',
+      resource: 'foo',
+      blacklist_name: 'spamhaus-sbl',
+      occurred_at: '2019-07-23T12:48:00.000Z',
+    };
+
+    const incidentsForBlacklist = [
+      {
+        id: 'id-1',
+        resource: 'foo 1',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-2',
+        resource: 'foo 2',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-3',
+        resource: 'foo 3',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-4',
+        resource: 'foo 4',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+      {
+        id: 'id-5',
+        resource: 'foo 5',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-23T12:48:00.000Z',
+      },
+    ];
+
+    expect(
+      blacklistSelectors.selectRelatedIncidentsForBlacklist({
+        blacklist: { incidentsForBlacklist, incident },
+      }),
+    ).toHaveLength(3);
+
+    // list of 3 where one is same id, so 2 total
+    expect(
+      blacklistSelectors.selectRelatedIncidentsForBlacklist({
+        blacklist: { incidentsForBlacklist: incidentsForBlacklist.slice(0, 3), incident },
+      }),
+    ).toHaveLength(2);
+
+    // list of 3 where none have current id, so 3 total
+    expect(
+      blacklistSelectors.selectRelatedIncidentsForBlacklist({
+        blacklist: { incidentsForBlacklist: incidentsForBlacklist.slice(1), incident },
+      }),
+    ).toHaveLength(3);
+  });
+
+  it('selectHistoricalIncidents returns 3 resources not including the current incident', () => {
+    const incident = {
+      id: 'id-1',
+      resource: 'foo',
+      blacklist_name: 'spamhaus-sbl',
+      occurred_at: '2019-07-23T12:48:00.000Z',
+    };
+
+    const historicalIncidents = [
+      {
+        id: 'id-1',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-22T12:48:00.000Z',
+      },
+      {
+        id: 'id-2',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-21T12:48:00.000Z',
+      },
+      {
+        id: 'id-3',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-20T12:48:00.000Z',
+      },
+      {
+        id: 'id-4',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-19T12:48:00.000Z',
+      },
+      {
+        id: 'id-5',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-18T12:48:00.000Z',
+      },
+      {
+        id: 'id-6',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-17T12:48:00.000Z',
+      },
+      {
+        id: 'id-7',
+        resource: 'foo',
+        blacklist_name: 'spamhaus-sbl',
+        occurred_at: '2019-07-16T12:48:00.000Z',
+      },
+    ];
+
+    // list of 7 where one is same id, so 6 total
+    expect(
+      blacklistSelectors.selectHistoricalIncidents({
+        blacklist: { historicalIncidents, incident },
+      }),
+    ).toHaveLength(6);
+
+    // list of 3 where one is same id, so 2 total
+    expect(
+      blacklistSelectors.selectHistoricalIncidents({
+        blacklist: { historicalIncidents: historicalIncidents.slice(0, 3), incident },
+      }),
+    ).toHaveLength(2);
+
+    // list of 6 where none have current id, so 6 total
+    expect(
+      blacklistSelectors.selectHistoricalIncidents({
+        blacklist: { historicalIncidents: historicalIncidents.slice(1), incident },
+      }),
+    ).toHaveLength(6);
+  });
 });
