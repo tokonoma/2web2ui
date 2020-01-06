5f8d66a7e0ecd69e83e8b09ff9ffad61401f1ec7
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-06 13:03:25 -0500

    FE-786 - Add fixtures and fleshed out tests for the list page

--- cypress/tests/integration/recipient-validation/list-page.spec.js
+++ cypress/tests/integration/recipient-validation/list-page.spec.js
@@ -21,6 +21,10 @@ describe('The recipient validation page', () => {
   beforeEach(() => {
     cy.stubAuth();
     cy.login({ isStubbed: true });
+    stubRequest({
+      method: 'GET',
+      fixture: 'recipient-validation/list/200.get.json',
+    });
   });
 
   it('has a relevant page title', () => {
@@ -29,11 +33,6 @@ describe('The recipient validation page', () => {
   });
 
   it('renders all relevant statuses based on the server response', () => {
-    stubRequest({
-      method: 'GET',
-      fixture: 'recipient-validation/list/200.get.json',
-    });
-
     cy.get('tbody tr')
       .eq(0)
       .should('contain', 'success.csv')
@@ -105,17 +104,49 @@ describe('The recipient validation page', () => {
     cy.findByText('How was this calculated?').should('be.visible');
   });
 
-  it('uploads stuff', () => {
+  it('accepts an uploaded CSV and re-directs the user to a view describing the volume and price of the validation', () => {
     stubRequest({
       method: 'POST',
       url: '/api/v1/recipient-validation/upload',
       fixture: 'recipient-validation/upload/200.post.json',
     });
 
+    stubRequest({
+      method: 'GET',
+      url: '/api/v1/recipient-validation/job/fake-list',
+      fixture: 'recipient-validation/job/fake-list/200.get.json',
+    });
+
+    stubRequest({
+      method: 'GET',
+      url: '/api/v1/usage',
+      fixture: 'usage/200.get.json',
+    });
+
     const exampleCSV = 'recipient-validation/example.csv';
 
     cy.fixture(exampleCSV).then(fileContent => {
       cy.get('[name="csv"]').upload({ fileContent, fileName: 'example.csv', mimeType: 'text/csv' });
+
+      cy.findByText('example.csv').should('be.visible');
+      cy.get('[data-id="recipient-list-address-count"]').should('contain', '2');
+      cy.findByText('$0.02').should('be.visible');
+      cy.findByText('Validate').should('be.visible');
+      cy.findByText('No, thanks').should('be.visible');
+    });
+  });
+
+  describe('the tabbed view that allows the user to choose different means of running validations', () => {
+    it('renders the API Integration section when the user clicks on "API Integration"', () => {
+      cy.findByText('API Integration').click();
+      cy.findByText('Integrate Now').should('be.visible');
+      cy.findByText('/api/v1/recipient-validation/single/{address}').should('be.visible');
+      cy.findByText('Create API Key').should('have.attr', 'href', '/account/api-keys/create');
+      cy.findByText('API Docs').should(
+        'have.attr',
+        'href',
+        'https://developers.sparkpost.com/api/recipient-validation/',
+      );
     });
   });
 });
