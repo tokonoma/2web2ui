145ab34a41fdc102b706c75d2e7576b173276227
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-06 10:23:00 -0500

    FE-786 - Add tests for different rendered statuses for the recipient validation list

--- cypress/tests/integration/recipient-validation/recipient-validation.spec.js
+++ cypress/tests/integration/recipient-validation/recipient-validation.spec.js
@@ -26,7 +26,6 @@ describe('The recipient validation page', () => {
   it('has a relevant page title', () => {
     cy.visit('/recipient-validation/list');
     cy.title().should('include', 'Recipient Validation');
-    cy.findByText('Recipient Validation').should('be.visible');
   });
 
   it('renders all relevant statuses based on the server response', () => {
@@ -34,5 +33,69 @@ describe('The recipient validation page', () => {
       method: 'GET',
       fixture: 'recipient-validation/list/200.get.json',
     });
+
+    cy.get('tbody tr')
+      .eq(0)
+      .should('contain', 'success.csv')
+      .should('contain', 'Complete');
+
+    cy.get('tbody tr')
+      .eq(1)
+      .should('contain', 'reading-upload-file.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(2)
+      .should('contain', 'checking-regex.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(3)
+      .should('contain', 'performing-mx-lookup.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(4)
+      .should('contain', 'performing-decay-scoring.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(5)
+      .should('contain', 'performing-role-based-lookup.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(6)
+      .should('contain', 'performing-disposable-domain.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(7)
+      .should('contain', 'performing-free-email.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(8)
+      .should('contain', 'performing-did-you-mean.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(9)
+      .should('contain', 'uploading-results-to-s3.csv')
+      .should('contain', 'See Progress')
+      .should('contain', 'Processing');
+
+    cy.get('tbody tr')
+      .eq(10)
+      .should('contain', 'error.csv')
+      .should('contain', 'Validation Error');
   });
 });
