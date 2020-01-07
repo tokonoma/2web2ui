ff013df245ad268436864aa55a5fed2c3fe6b3ac
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-07 15:30:57 -0500

    TR-2016 - Reformat file with Prettier

--- src/pages/templates/components/PreviewErrorFrame.js
+++ src/pages/templates/components/PreviewErrorFrame.js
@@ -7,10 +7,13 @@ const PreviewErrorFrame = ({ errors }) => (
   <div className={styles.PreviewErrorFrame}>
     <Warning size={72} />
     <h1>Oh no! An Error Occurred</h1>
-    <p>We are unable to load your template preview due to a {errors[0].message} on line {errors[0].line} of your {errors[0].part}.</p>
     <p>
-      If you notice this happens often, check your substitution data or code syntax as
-      these are frequent causes of preview errors.
+      We are unable to load your template preview due to a {errors[0].message} on line{' '}
+      {errors[0].line} of your {errors[0].part}.
+    </p>
+    <p>
+      If you notice this happens often, check your substitution data or code syntax as these are
+      frequent causes of preview errors.
     </p>
   </div>
 );
