89e3cf8131c6f1b087e05752e2c8b185b9a37ef2
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-07 09:00:14 -0500

    FE-786 - Add some tests for the single result view

--- cypress/tests/integration/recipient-validation/single-validation-page.spec.js
+++ cypress/tests/integration/recipient-validation/single-validation-page.spec.js
@@ -4,6 +4,9 @@ describe('The recipient validation single result page', () => {
   beforeEach(() => {
     cy.stubAuth();
     cy.login({ isStubbed: true });
+  });
+
+  it('renders with a relevant page title', () => {
     cy.stubRequest(
       {
         url: '/api/v1/recipient-validation/single/**',
@@ -13,9 +16,59 @@ describe('The recipient validation single result page', () => {
         cy.visit('/recipient-validation/single/fake-email@sparkpost.com');
       },
     );
+
+    cy.title().should('include', 'Results');
   });
 
-  it('renders with a relevant page title', () => {
-    cy.title().should('include', 'Results');
+  it('renders with the "Valid" status when the result returns as valid', () => {
+    cy.stubRequest(
+      {
+        url: '/api/v1/recipient-validation/single/**',
+        fixture: 'recipient-validation/single/fake-email/200.get.valid-result.json',
+      },
+      () => {
+        cy.visit('/recipient-validation/single/fake-email@sparkpost.com');
+      },
+    );
+
+    cy.get('[data-id="validation-result"]').within(() => {
+      cy.findByText('valid').should('be.visible'); // Though rendered as "Valid", the text is transformed to be capitalized using CSS
+    });
+
+    cy.findByText(
+      'A valid result means that everything checks out for email address and we have no data suggesting that is invalid for any reason.',
+    ).should('be.visible');
+  });
+
+  it('renders with the "Risky" status when the result returns as risky', () => {
+    cy.stubRequest(
+      {
+        url: '/api/v1/recipient-validation/single/**',
+        fixture: 'recipient-validation/single/fake-email/200.get.risky-result.json',
+      },
+      () => {
+        cy.visit('/recipient-validation/single/fake-email@sparkpost.com');
+      },
+    );
+
+    cy.get('[data-id="validation-result"]').within(() => {
+      cy.findByText('risky').should('be.visible'); // Though rendered as "Risky", the text is transformed to be capitalized using CSS
+    });
+  });
+
+  it('renders with the "Undeliverable" status when the result returns as undeliverable', () => {
+    cy.stubRequest(
+      {
+        url: '/api/v1/recipient-validation/single/**',
+        fixture: 'recipient-validation/single/fake-email/200.get.undeliverable-result.json',
+      },
+      () => {
+        cy.visit('/recipient-validation/single/fake-email@sparkpost.com');
+      },
+    );
+
+    cy.get('[data-id="validation-result"]').within(() => {
+      cy.findByText('undeliverable').should('be.visible'); // Though rendered as "undeliverable", the text is transformed to be capitalized using CSS
+    });
   });
 });
