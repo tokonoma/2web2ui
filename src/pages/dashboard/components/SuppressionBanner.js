1c1040552e040b549a76857ba5f119a3ead6c56c
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-01-07 14:18:15 -0500

    [AC-1161] Addressing Feedback

--- src/pages/dashboard/components/SuppressionBanner.js
+++ src/pages/dashboard/components/SuppressionBanner.js
@@ -9,7 +9,7 @@ const SuppressionBanner = ({ accountAgeInWeeks, hasSuppressions }) => {
         <p>
           {`Welcome! Make sure you import your suppression list from your previous provider to avoid
           sending to people who have previously opted out, consistently bounced, etc. Learn more
-          about migrating from Mailgun, Mandrill, or SendGrid.`}
+          about migrating from Mailgun, Mandrill, or SendGrid. `}
           <UnstyledLink to={LINKS.SUPPRESSION_MIGRATION} external>
             Import your suppressions
           </UnstyledLink>
