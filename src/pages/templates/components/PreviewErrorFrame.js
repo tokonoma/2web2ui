491073fd27da97baf36bb93a706209da78719778
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-07 17:03:02 -0500

    TR-2016 - Add conditional check for syntax errors

--- src/pages/templates/components/PreviewErrorFrame.js
+++ src/pages/templates/components/PreviewErrorFrame.js
@@ -3,20 +3,35 @@ import { Warning } from '@sparkpost/matchbox-icons';
 import styles from './PreviewErrorFrame.module.scss';
 
 /* eslint-disable no-restricted-syntax */
-const PreviewErrorFrame = ({ errors }) => (
-  <div className={styles.PreviewErrorFrame}>
-    <Warning size={72} />
-    <h1>Oh no! An Error Occurred</h1>
-    <p>
-      We are unable to load your template preview due to a {errors[0].message} on line{' '}
-      {errors[0].line} of your {errors[0].part}.
-    </p>
-    <p>
-      If you notice this happens often, check your substitution data or code syntax as these are
-      frequent causes of preview errors.
-    </p>
-  </div>
-);
+const PreviewErrorFrame = ({ errors }) => {
+  const error = errors[0];
+  const { message, description, line, part, code } = error;
+
+  return (
+    <div className={styles.PreviewErrorFrame}>
+      <Warning size={72} />
+
+      <h2>Oh no! An Error Occurred</h2>
+
+      {/* See https://www.sparkpost.com/docs/tech-resources/extended-error-codes/ */}
+      {code === '3000' ? (
+        <>
+          <p>
+            We are unable to load your template preview due to a {message} on line {line} of your{' '}
+            {part}.
+          </p>
+
+          <p>
+            If you notice this happens often, check your substitution data or code syntax as these
+            are frequent causes of preview errors.
+          </p>
+        </>
+      ) : (
+        <p>{description}</p>
+      )}
+    </div>
+  );
+};
 /* eslint-enable no-restricted-syntax */
 
 export default PreviewErrorFrame;
