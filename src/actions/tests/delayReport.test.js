3782b11f2e1d59acf8ce0934d3b7a228b7b051c5
Brian Kemper <brian.kemper@sparkpost.com>
2020-01-07 01:13:55 -0500

    SA-1145 Fixed delay report to handle special character filter values

--- src/actions/tests/delayReport.test.js
+++ src/actions/tests/delayReport.test.js
@@ -1,43 +1,27 @@
 import { refreshDelayReport } from '../delayReport';
 import * as metricsActions from 'src/actions/metrics';
-import * as metricsHelpers from 'src/helpers/metrics';
 
-jest.mock('src/helpers/metrics');
 jest.mock('src/actions/metrics');
 
 describe('Action Creator: Refresh Delay Report', () => {
-
   let dispatchMock;
-  let queryMock;
-  let updates;
-  let result;
 
   beforeEach(() => {
-    queryMock = {};
-    updates = { abc: 'cool' };
-    metricsHelpers.getQueryFromOptions = jest.fn(() => queryMock);
-    dispatchMock = jest.fn((a) => Promise.resolve(a));
-    result = refreshDelayReport(updates)(dispatchMock);
-  });
-
-  it('should return a promise', () => {
-    expect(result).toBeInstanceOf(Promise);
-  });
-
-  it('should get query options, passing in updates', () => {
-    expect(metricsHelpers.getQueryFromOptions).toHaveBeenCalledWith(updates);
+    dispatchMock = jest.fn(a => Promise.resolve(a));
+    refreshDelayReport({ filters: [{ type: 'Campaign', value: 'test-camp' }] })(dispatchMock);
   });
 
   it('should dispatch actions', () => {
     expect(dispatchMock).toHaveBeenCalledTimes(2);
     expect(metricsActions.fetchDeliverability).toHaveBeenCalledWith({
       type: 'GET_DELAY_REPORT_AGGREGATES',
-      params: {
-        ...queryMock,
-        metrics: 'count_accepted,count_delayed,count_delayed_first'
-      }
+      params: expect.objectContaining({
+        campaigns: 'test-camp',
+        metrics: 'count_accepted,count_delayed,count_delayed_first',
+      }),
     });
-    expect(metricsActions.fetchDelayReasonsByDomain).toHaveBeenCalledWith(queryMock);
+    expect(metricsActions.fetchDelayReasonsByDomain).toHaveBeenCalledWith(
+      expect.objectContaining({ campaigns: 'test-camp' }),
+    );
   });
-
 });
