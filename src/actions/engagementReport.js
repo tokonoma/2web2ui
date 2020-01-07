90f543bfccba60d87126895dab1dc883b44c8d12
Brian Kemper <brian.kemper@sparkpost.com>
2020-01-07 01:17:27 -0500

    SA-1145 Fixed engagement report to handle special chars in filter values

--- src/actions/engagementReport.js
+++ src/actions/engagementReport.js
@@ -1,27 +1,32 @@
 import { fetch as getMetrics } from 'src/actions/metrics';
-import { getQueryFromOptions } from 'src/helpers/metrics';
+import { getMetricsFromKeys, getQueryFromOptions } from 'src/helpers/metrics';
 
-export function refreshEngagementReport (updates = {}) {
-  return (dispatch) => {
-    const params = getQueryFromOptions(updates);
+const DELIVERABILITY_METRICS = getMetricsFromKeys([
+  'count_accepted',
+  'count_sent',
+  'count_unique_clicked_approx',
+  'count_unique_confirmed_opened_approx',
+]);
 
+const LINK_METRICS = getMetricsFromKeys(['count_clicked', 'count_raw_clicked_approx']);
+
+export function refreshEngagementReport(updates = {}) {
+  return dispatch => {
     return Promise.all([
-      dispatch(getMetrics({
-        params: {
-          ...params,
-          metrics: 'count_accepted,count_sent,count_unique_clicked_approx,count_unique_confirmed_opened_approx'
-        },
-        path: 'deliverability',
-        type: 'GET_ENGAGEMENT_AGGREGATE_METRICS'
-      })),
-      dispatch(getMetrics({
-        params: {
-          ...params,
-          metrics: 'count_clicked,count_raw_clicked_approx'
-        },
-        path: 'deliverability/link-name',
-        type: 'GET_ENGAGEMENT_LINK_METRICS'
-      }))
+      dispatch(
+        getMetrics({
+          params: getQueryFromOptions({ ...updates, metrics: DELIVERABILITY_METRICS }),
+          path: 'deliverability',
+          type: 'GET_ENGAGEMENT_AGGREGATE_METRICS',
+        }),
+      ),
+      dispatch(
+        getMetrics({
+          params: getQueryFromOptions({ ...updates, metrics: LINK_METRICS }),
+          path: 'deliverability/link-name',
+          type: 'GET_ENGAGEMENT_LINK_METRICS',
+        }),
+      ),
     ]);
   };
 }
--- src/actions/tests/engagementReport.test.js
+++ src/actions/tests/engagementReport.test.js
@@ -1,51 +1,34 @@
 import { refreshEngagementReport } from '../engagementReport';
 import * as metricsActions from 'src/actions/metrics';
-import * as metricsHelpers from 'src/helpers/metrics';
 
-jest.mock('src/helpers/metrics');
 jest.mock('src/actions/metrics');
 
 describe('Action Creator: Refresh Engagement Report', () => {
-
   let dispatchMock;
-  let queryMock;
-  let updates;
-  let result;
 
   beforeEach(() => {
-    queryMock = {};
-    updates = { abc: 'cool' };
-    metricsHelpers.getQueryFromOptions = jest.fn(() => queryMock);
-    dispatchMock = jest.fn((a) => Promise.resolve(a));
-    result = refreshEngagementReport(updates)(dispatchMock);
-  });
-
-  it('should return a promise', () => {
-    expect(result).toBeInstanceOf(Promise);
-  });
-
-  it('should get query options, passing in updates', () => {
-    expect(metricsHelpers.getQueryFromOptions).toHaveBeenCalledWith(updates);
+    dispatchMock = jest.fn(a => Promise.resolve(a));
+    refreshEngagementReport({ filters: [{ type: 'Campaign', value: 'test-camp' }] })(dispatchMock);
   });
 
   it('should dispatch actions', () => {
     expect(dispatchMock).toHaveBeenCalledTimes(2);
     expect(metricsActions.fetch).toHaveBeenCalledWith({
-      params: {
-        ...queryMock,
-        metrics: 'count_accepted,count_sent,count_unique_clicked_approx,count_unique_confirmed_opened_approx'
-      },
+      params: expect.objectContaining({
+        campaigns: 'test-camp',
+        metrics:
+          'count_accepted,count_sent,count_unique_clicked_approx,count_unique_confirmed_opened_approx',
+      }),
       path: 'deliverability',
-      type: 'GET_ENGAGEMENT_AGGREGATE_METRICS'
+      type: 'GET_ENGAGEMENT_AGGREGATE_METRICS',
     });
     expect(metricsActions.fetch).toHaveBeenCalledWith({
-      params: {
-        ...queryMock,
-        metrics: 'count_clicked,count_raw_clicked_approx'
-      },
+      params: expect.objectContaining({
+        campaigns: 'test-camp',
+        metrics: 'count_clicked,count_raw_clicked_approx',
+      }),
       path: 'deliverability/link-name',
-      type: 'GET_ENGAGEMENT_LINK_METRICS'
+      type: 'GET_ENGAGEMENT_LINK_METRICS',
     });
   });
-
 });
