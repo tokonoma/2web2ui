e421ad6e99baefc39f997ff46dbca0fd3438efe1
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-10 09:48:07 -0500

    FE-786 - Update file name to actually match what exists...duh

--- cypress/tests/integration/recipient-validation/list-page-routes/list-route.spec.js
+++ cypress/tests/integration/recipient-validation/list-page-routes/list-route.spec.js
@@ -145,7 +145,7 @@ describe('The recipient validation /list route', () => {
 
     cy.visit(ROUTE_URL);
 
-    const exampleCSV = 'recipient-validation/fake-file.csv';
+    const exampleCSV = 'recipient-validation/fake-list.csv';
 
     cy.fixture(exampleCSV).then(fileContent => {
       cy.get('[name="csv"]').upload({ fileContent, fileName: 'example.csv', mimeType: 'text/csv' });
