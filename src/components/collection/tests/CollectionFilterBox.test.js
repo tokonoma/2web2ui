74ed2edf46763d58d80ca0e960e2f0df1da04059
Brian Kemper <brian.kemper@sparkpost.com>
2020-01-15 17:57:04 -0500

    SA-1153 Tests for initializing FilterBox value

---
+++ src/components/collection/tests/CollectionFilterBox.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import CollectionFilterBox from '../Filterbox';
+
+describe('CollectionFilterBox', () => {
+  const subject = (props = {}) => render(<CollectionFilterBox {...props} />);
+
+  it('sets initial value', () => {
+    const { queryByDisplayValue } = subject({ initialValue: 'testing' });
+    expect(queryByDisplayValue('testing')).toBeVisible();
+  });
+
+  it('triggers onBlur event handler', () => {
+    const onBlur = jest.fn();
+    const { getByPlaceholderText } = subject({ onBlur });
+
+    fireEvent.blur(getByPlaceholderText(/filter results/i), { target: { value: 'testing' } });
+
+    expect(onBlur).toHaveBeenCalledWith('testing');
+  });
+});
