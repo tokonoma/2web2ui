33d4e7b53aa401be8a53e2151f5d37cb0a4f4e09
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-16 13:07:56 -0500

    TR-2140 - Stub API keys and Sending Domains requests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -69,6 +69,8 @@ Cypress.Commands.add('stubAuth', () => {
   cy.fixture('account/plans/200.get.json').as('plansGet');
   cy.fixture('authenticate/grants/200.get.json').as('grantsGet');
   cy.fixture('suppression-list/200.get.json').as('suppressionsGet');
+  cy.fixture('api-keys/200.get.json').as('apiKeysGet');
+  cy.fixture('sending-domains/200.get.json').as('sendingDomainsGet');
 
   cy.route({
     method: 'POST',
@@ -112,4 +114,16 @@ Cypress.Commands.add('stubAuth', () => {
     status: 200,
     response: '@suppressionsGet',
   }).as('stubbedSuppressionsGet');
+  cy.route({
+    method: 'GET',
+    url: '/api/v1/api-keys',
+    status: 200,
+    response: '@apiKeysGet',
+  }).as('stubbedApiKeysGet');
+  cy.route({
+    method: 'GET',
+    url: '/api/v1/sending-domains',
+    status: 200,
+    response: '@sendingDomainsGet',
+  }).as('stubbedSendingDomains');
 });
