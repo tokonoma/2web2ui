68134235b2247b9e9ea96b9d4518e3d644efe239
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-16 13:02:29 -0500

    TR-2140 - Add stub for suppression lists

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -68,6 +68,7 @@ Cypress.Commands.add('stubAuth', () => {
   cy.fixture('account/200.get.json').as('accountGet');
   cy.fixture('account/plans/200.get.json').as('plansGet');
   cy.fixture('authenticate/grants/200.get.json').as('grantsGet');
+  cy.fixture('suppression-list/200.get.json').as('suppressionsGet');
 
   cy.route({
     method: 'POST',
@@ -105,4 +106,10 @@ Cypress.Commands.add('stubAuth', () => {
     status: 200,
     response: '@grantsGet',
   }).as('stubbedGrantsRequest');
+  cy.route({
+    method: 'GET',
+    url: '/api/v1/suppression-list*',
+    status: 200,
+    response: '@suppressionsGet',
+  }).as('stubbedSuppressionsGet');
 });
