c7b2f6c6bbb85b9641c587173f644e631c8a14f8
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-24 12:55:54 -0500

    TR-2133 - Add test and adjust title content to match page implementation

--- cypress/tests/integration/recipients/suppressions/listPage.spec.js
+++ cypress/tests/integration/recipients/suppressions/listPage.spec.js
@@ -1,6 +1,24 @@
+const PAGE_URL = '/lists/suppressions';
+
 describe('The recipients suppressions list page', () => {
   beforeEach(() => {
     cy.stubAuth();
-    cy.logIn({ isStubbed: true });
+    cy.login({ isStubbed: true });
+
+    cy.stubRequest({
+      url: '/api/v1/subaccounts',
+      fixture: 'subaccounts/200.get.json',
+    });
+
+    cy.stubRequest({
+      url: '/api/v1/suppression-list*',
+      fixture: 'suppression-list/200.get.json',
+    });
+  });
+
+  it('has a relevant page title', () => {
+    cy.visit(PAGE_URL);
+
+    cy.title().should('include', 'Suppressions');
   });
 });
