c43877beea323fbce37ef73bd15f0e9cabc713ae
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-30 16:02:28 -0500

    FE-787 - Add screen reader only label to field, add additional events page tests

--- cypress/tests/integration/events/listPage.spec.js
+++ cypress/tests/integration/events/listPage.spec.js
@@ -71,15 +71,111 @@ describe('The events page', () => {
     cy.queryByText('An error occurred').should('not.be.visible');
   });
 
-  it('re-requests events data when changing the filter date', () => {
+  // TODO: Left off here
+  it('renders the "Share this report" modal when clicking on the "Share" button', () => {
     cy.visit(PAGE_URL);
+
+    cy.findByText('Share').click();
+
+    cy.findByText('Share this report').should('be.visible');
   });
 
-  it('re-requests events data when the user filters by a recipient email address', () => {});
+  describe('filtering', () => {
+    // TODO: Work on this test once `TR-2133` is merged
+    // it('re-requests events data when changing the filter date', () => {
+    //   cy.visit(PAGE_URL);
+    // });
 
-  it('re-requests data when removing an active filter and when clicking "Clear All Filters"', () => {});
+    it('re-requests events data when the user filters by a recipient email address', () => {
+      cy.visit(PAGE_URL);
 
-  it('renders the "Share this report" modal when clicking on the "Share" button', () => {});
+      cy.stubRequest({
+        url: '/api/v1/events/message*',
+        fixture: 'events/message/200.get.different-results.json',
+      });
+
+      cy.findByLabelText('Filter by Recipient Email Address')
+        .type('hello@gmail.com')
+        .blur();
+
+      cy.findByText('Recipients: hello@gmail.com').should('be.visible');
+      cy.findByText('Clear All Filters').should('be.visible');
+      cy.findByText('different-recipient@hotmail.com').should('be.visible');
+      cy.queryByText('fake-recipient@hotmail.com').should('not.be.visible');
+    });
+
+    it('renders an invalid email address in the recipient email address filter when an invalid email is entered', () => {
+      cy.visit(PAGE_URL);
+
+      cy.findByLabelText('Filter by Recipient Email Address')
+        .type('hello')
+        .blur();
+
+      cy.findByText('hello is not a valid email address').should('be.visible');
+
+      cy.findByLabelText('Filter by Recipient Email Address')
+        .clear()
+        .type('@gmail.com')
+        .blur();
+
+      cy.findByText('@gmail.com is not a valid email address').should('be.visible');
+
+      cy.findByLabelText('Filter by Recipient Email Address')
+        .clear()
+        .type('a@b')
+        .blur();
+
+      cy.findByText('a@b is not a valid email address').should('be.visible');
+
+      cy.findByLabelText('Filter by Recipient Email Address')
+        .clear()
+        .type('a@b.c')
+        .blur();
+
+      cy.queryByText('a@b.c is not a valid email address').should('not.be.visible');
+    });
+
+    it('re-requests data when removing an active filter and when clicking "Clear All Filters"', () => {
+      cy.visit(PAGE_URL);
+
+      cy.stubRequest({
+        url: '/api/v1/events/message*',
+        fixture: 'events/message/200.get.different-results.json',
+      });
+
+      cy.findByLabelText('Filter by Recipient Email Address')
+        .type('hello@gmail.com')
+        .blur();
+
+      cy.findByText('different-recipient@hotmail.com').should('be.visible');
+      cy.queryByText('fake-recipient@hotmail.com').should('not.be.visible');
+
+      cy.stubRequest({
+        url: '/api/v1/events/message*',
+        fixture: 'events/message/200.get.json',
+      });
+
+      cy.findByText('Clear All Filters').click();
+
+      cy.queryByText('fake-recipient@hotmail.com').should('be.visible');
+      cy.findByText('different-recipient@hotmail.com').should('not.be.visible');
+    });
+
+    describe('the "Advanced Filters" modal', () => {
+      beforeEach(() => {
+        cy.visit(PAGE_URL);
+        cy.findByText('Add Filters').click();
+      });
+
+      it('renders documentation about each event type when hovering over a check box label', () => {});
+
+      it('allows the addition and removal of other filters via the "Add Filter" and "Remove" buttons', () => {});
+
+      it('closes the modal when clicking "Cancel"', () => {});
+
+      it('applies filters when clicking "Apply Filters" by re-requesting events data', () => {});
+    });
+  });
 
   describe('the events table', () => {
     it('renders with a "View Details" button that links to the detail page for this event', () => {});
@@ -87,20 +183,5 @@ describe('The events page', () => {
     it('renders the "Event", "Subject", "Recipient", "From Address", and event "Time" for each event', () => {});
 
     it('renders the empty state when no events are returned', () => {});
-  });
-
-  describe('the "Advanced Filters" modal', () => {
-    beforeEach(() => {
-      cy.visit(PAGE_URL);
-      cy.findByText('Add Filters').click();
-    });
-
-    it('renders documentation about each event type when hovering over a check box label', () => {});
-
-    it('allows the addition and removal of other filters via the "Add Filter" and "Remove" buttons', () => {});
-
-    it('closes the modal when clicking "Cancel"', () => {});
-
-    it('applies filters when clicking "Apply Filters" by re-requesting events data', () => {});
   });
 });
--- src/pages/reports/messageEvents/components/MessageEventsSearch.js
+++ src/pages/reports/messageEvents/components/MessageEventsSearch.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import moment from 'moment';
 import _ from 'lodash';
-import { getMessageEvents, refreshMessageEventsDateRange, updateMessageEventsSearchOptions, addFilters } from 'src/actions/messageEvents';
+import {
+  getMessageEvents,
+  refreshMessageEventsDateRange,
+  updateMessageEventsSearchOptions,
+  addFilters,
+} from 'src/actions/messageEvents';
 import { selectMessageEventsSearchOptions } from 'src/selectors/messageEvents';
 import { Panel, TextField } from '@sparkpost/matchbox';
 import AdvancedFiltersModal from './AdvancedFiltersModal';
@@ -19,44 +24,58 @@ import config from 'src/config';
 import styles from './MessageEventsSearch.module.scss';
 
 export class MessageEventsSearch extends Component {
-
   state = {
-    recipientError: null
-  }
+    recipientError: null,
+  };
 
   componentDidMount() {
-    const { updateMessageEventsSearchOptions, refreshMessageEventsDateRange, location, search } = this.props;
+    const {
+      updateMessageEventsSearchOptions,
+      refreshMessageEventsDateRange,
+      location,
+      search,
+    } = this.props;
     refreshMessageEventsDateRange({ relativeRange: search.dateOptions.relativeRange }); // Sets default dateoptions from initial state
     updateMessageEventsSearchOptions(parseSearch(location.search));
   }
 
-  getInvalidAddresses = (addresses) => {
-    const invalids = _.filter(addresses, (address) => {
+  getInvalidAddresses = addresses => {
+    const invalids = _.filter(addresses, address => {
       address = _.trim(address);
 
       return address && recipientEmailValidator(address) !== undefined;
     });
 
     return invalids;
-  }
+  };
 
-  handleRecipientsChange = (event) => {
+  handleRecipientsChange = event => {
     const value = event.target.value;
     const recipients = stringToArray(value);
     const invalids = this.getInvalidAddresses(recipients);
 
     if (invalids.length) {
-      this.setState({ recipientError: `${invalids.join(', ')} ${invalids.length > 1 ? 'are not' : 'is not a'} valid email ${invalids.length > 1 ? 'addresses' : 'address'}` });
+      this.setState({
+        recipientError: `${invalids.join(', ')} ${
+          invalids.length > 1 ? 'are not' : 'is not a'
+        } valid email ${invalids.length > 1 ? 'addresses' : 'address'}`,
+      });
       return;
     }
 
     event.target.value = '';
     this.setState({ recipientError: null });
     this.props.addFilters({ recipients });
-  }
+  };
 
   render() {
-    const { search, refreshMessageEventsDateRange, loading, now = new Date(), searchOptions } = this.props;
+    const {
+      search,
+      refreshMessageEventsDateRange,
+      loading,
+      now = new Date(),
+      searchOptions,
+    } = this.props;
     const { retentionPeriodDays } = config.messageEvents;
     const { recipients } = ALL_EVENTS_FILTERS;
 
@@ -74,15 +93,21 @@ export class MessageEventsSearch extends Component {
                 datePickerProps={{
                   disabledDays: {
                     after: now,
-                    before: moment(now).subtract(retentionPeriodDays, 'days').toDate()
+                    before: moment(now)
+                      .subtract(retentionPeriodDays, 'days')
+                      .toDate(),
                   },
-                  canChangeMonth: false
+                  canChangeMonth: false,
                 }}
               />
             </div>
             <div className={styles.RecipientFilter}>
+              <label className={styles.ScreenReaderOnlyLabel} htmlFor="email-recipients-filter">
+                Filter by Recipient Email Address
+              </label>
+
               <TextField
-                labelHidden
+                id="email-recipients-filter"
                 placeholder={recipients.placeholder}
                 onBlur={this.handleRecipientsChange}
                 onKeyDown={onEnter(this.handleRecipientsChange)}
@@ -91,7 +116,7 @@ export class MessageEventsSearch extends Component {
               />
             </div>
             <div>
-              <AdvancedFiltersModal/>
+              <AdvancedFiltersModal />
             </div>
             <div>
               <ShareModal disabled={loading} searchOptions={searchOptions} />
@@ -104,10 +129,15 @@ export class MessageEventsSearch extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   search: state.messageEvents.search,
   loading: state.messageEvents.loading,
-  searchOptions: selectMessageEventsSearchOptions(state)
+  searchOptions: selectMessageEventsSearchOptions(state),
 });
-const mapDispatchToProps = { getMessageEvents, refreshMessageEventsDateRange, updateMessageEventsSearchOptions, addFilters };
+const mapDispatchToProps = {
+  getMessageEvents,
+  refreshMessageEventsDateRange,
+  updateMessageEventsSearchOptions,
+  addFilters,
+};
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MessageEventsSearch));
