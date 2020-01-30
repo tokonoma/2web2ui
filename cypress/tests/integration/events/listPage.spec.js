4ecf5d1c8044750848d5bf7f1b8a69d5d878f1d0
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-01-30 10:54:35 -0500

    FE-787 - Add structure for tests

--- cypress/tests/integration/events/listPage.spec.js
+++ cypress/tests/integration/events/listPage.spec.js
@@ -12,4 +12,32 @@ describe('The events page', () => {
     cy.title().should('include', 'Events Search');
     cy.findByText('Events Search').should('be.visible');
   });
+
+  it('renders the empty state when no events are returned', () => {});
+
+  it('renders an error when the request for events documentation data fails', () => {});
+
+  it('renders an error when the request for events data fails', () => {});
+
+  it('re-requests events data when changing the filter date', () => {});
+
+  it('re-requests events data when the user filters by a recipient email address', () => {});
+
+  it('re-requests data when removing an active filter and when clicking "Clear All Filters"', () => {});
+
+  it('renders the "Share this report" modal when clicking on the "Share" button', () => {});
+
+  describe('the "Advanced Filters" modal', () => {
+    beforeEach(() => {
+      cy.findByText('Add Filters').click();
+    });
+
+    it('renders documentation about each event type when hovering over a check box label', () => {});
+
+    it('allows the addition and removal of other filters via the "Add Filter" and "Remove" buttons', () => {});
+
+    it('closes the modal when clicking "Cancel"', () => {});
+
+    it('applies filters when clicking "Apply Filters" by re-requesting events data', () => {});
+  });
 });
