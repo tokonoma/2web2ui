8b2d1fc2dd105b6a379c00c52e51bf36d1b3ce19
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-02-03 16:36:14 -0500

    FE-788 - Add empty tests for health score subaccount page

---
+++ cypress/tests/integration/signals-analytics/healthScoreBySubaccountPage.spec.js
@@ -0,0 +1,8 @@
+describe('The health score by subaccount page', () => {
+  beforeEach(() => {
+    cy.stubAuth();
+    cy.login({ isStubbed: true });
+  });
+
+  it('renders with a relevant page title', () => {});
+});
