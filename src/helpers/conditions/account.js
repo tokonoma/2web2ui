074aedba5bc973280ce94d676b460c173fbdbb79
Patrick Sison <patrick.m.sison@gmail.com>
2020-02-04 16:36:46 -0500

    AC-1218: Added comment to function

--- src/helpers/conditions/account.js
+++ src/helpers/conditions/account.js
@@ -13,9 +13,11 @@ export const isSuspendedForBilling = all(hasStatus('suspended'), hasStatusReason
 export const subscriptionSelfServeIsTrue = ({ account }) =>
   _.get(account, 'subscription.self_serve', false);
 const SELF_SERVE_ACCOUNT_TYPES = ['active', 'inactive', 'none'];
-export const isBillingSubscriptionSelfServe = ({ subscription }) => {
-  return _.includes(SELF_SERVE_ACCOUNT_TYPES, _.get(subscription, 'type'));
-};
+
+// Does not get the subscription by default as part of access control.
+export const isBillingSubscriptionSelfServe = ({ subscription }) =>
+  _.includes(SELF_SERVE_ACCOUNT_TYPES, _.get(subscription, 'type'));
+
 export const isAws = ({ account }) => _.get(account, 'subscription.type') === 'aws';
 export const isManuallyBilled = ({ account }) => _.get(account, 'subscription.type') === 'manual';
 export const isCustomBilling = ({ account }) => _.get(account, 'subscription.custom', false);
