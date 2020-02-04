03d0e5d491e86e769a92543de1ae2cea4c149ed4
Kevin Chu <kevinchu1337@gmail.com>
2020-02-04 14:38:55 -0500

    FE-789: Add data-id tags

--- src/pages/reports/components/ReportOptions.js
+++ src/pages/reports/components/ReportOptions.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { addFilters, removeFilter, refreshReportOptions, refreshTypeaheadCache, initTypeaheadCache } from 'src/actions/reportOptions';
+import {
+  addFilters,
+  removeFilter,
+  refreshReportOptions,
+  refreshTypeaheadCache,
+  initTypeaheadCache,
+} from 'src/actions/reportOptions';
 import ShareModal from './ShareModal';
 import { parseSearch } from 'src/helpers/reports';
 import { Grid, Panel, Tag } from '@sparkpost/matchbox';
@@ -11,18 +17,11 @@ import typeaheadCacheSelector from 'src/selectors/reportFilterTypeaheadCache';
 import CustomReports from './CustomReports';
 import styles from './ReportOptions.module.scss';
 
-const RELATIVE_DATE_OPTIONS = [
-  'hour',
-  'day',
-  '7days',
-  '30days',
-  '90days',
-  'custom'
-];
+const RELATIVE_DATE_OPTIONS = ['hour', 'day', '7days', '30days', '90days', 'custom'];
 
 export class ReportOptions extends Component {
   componentDidMount() {
-    const { options, filters = []} = parseSearch(this.props.location.search);
+    const { options, filters = [] } = parseSearch(this.props.location.search);
     this.props.addFilters(filters);
     this.props.refreshReportOptions(options);
 
@@ -32,64 +31,81 @@ export class ReportOptions extends Component {
 
   renderActiveFilters = () => {
     const { reportOptions } = this.props;
-    return reportOptions.filters.length
-      ? <Panel.Section>
+    return reportOptions.filters.length ? (
+      <Panel.Section>
         <small>Filters:</small>
-        {reportOptions.filters.map((item, index) => <Tag key={index} onRemove={() => this.handleFilterRemove(index)} className={styles.TagWrapper}>{item.type}: {item.value}</Tag>)}
+        {reportOptions.filters.map((item, index) => (
+          <Tag
+            key={index}
+            onRemove={() => this.handleFilterRemove(index)}
+            className={styles.TagWrapper}
+          >
+            {item.type}: {item.value}
+          </Tag>
+        ))}
       </Panel.Section>
-      : null;
+    ) : null;
   };
 
-  handleFilterRemove = (index) => {
+  handleFilterRemove = index => {
     this.props.removeFilter(index);
   };
 
-  handleTypeaheadSelect = (item) => {
+  handleTypeaheadSelect = item => {
     this.props.addFilters([item]);
   };
 
   render() {
-    const { customReportsEnabled, typeaheadCache, reportOptions, reportLoading, refreshReportOptions, searchOptions } = this.props;
+    const {
+      customReportsEnabled,
+      typeaheadCache,
+      reportOptions,
+      reportLoading,
+      refreshReportOptions,
+      searchOptions,
+    } = this.props;
 
     return (
-      <Panel>
-        {customReportsEnabled && <CustomReports searchOptions={searchOptions} />}
-        <Panel.Section >
-          <Grid>
-            <Grid.Column xs={12} md={6}>
-              <div className={styles.FieldWrapper}>
-                <DatePicker
-                  {...reportOptions}
-                  relativeDateOptions={RELATIVE_DATE_OPTIONS}
-                  disabled={reportLoading}
-                  onChange={refreshReportOptions}
-                  roundToPrecision={true}
+      <div data-id="report-options">
+        <Panel>
+          {customReportsEnabled && <CustomReports searchOptions={searchOptions} />}
+          <Panel.Section>
+            <Grid>
+              <Grid.Column xs={12} md={6}>
+                <div className={styles.FieldWrapper}>
+                  <DatePicker
+                    {...reportOptions}
+                    relativeDateOptions={RELATIVE_DATE_OPTIONS}
+                    disabled={reportLoading}
+                    onChange={refreshReportOptions}
+                    roundToPrecision={true}
+                  />
+                </div>
+              </Grid.Column>
+              <Grid.Column xs={8} md={4} xl={5}>
+                <Typeahead
+                  reportOptions={reportOptions}
+                  placeholder="Filter by domain, campaign, etc"
+                  onSelect={this.handleTypeaheadSelect}
+                  items={typeaheadCache}
+                  selected={reportOptions.filters}
                 />
-              </div>
-            </Grid.Column>
-            <Grid.Column xs={8} md={4} xl={5}>
-              <Typeahead
-                reportOptions={reportOptions}
-                placeholder='Filter by domain, campaign, etc'
-                onSelect={this.handleTypeaheadSelect}
-                items={typeaheadCache}
-                selected={reportOptions.filters}
-              />
-            </Grid.Column>
-            <Grid.Column xs={4} md={2} xl={1}>
-              <ShareModal disabled={reportLoading} searchOptions={searchOptions} />
-            </Grid.Column>
-          </Grid>
-        </Panel.Section>
-        {this.renderActiveFilters()}
-      </Panel>
+              </Grid.Column>
+              <Grid.Column xs={4} md={2} xl={1}>
+                <ShareModal disabled={reportLoading} searchOptions={searchOptions} />
+              </Grid.Column>
+            </Grid>
+          </Panel.Section>
+          {this.renderActiveFilters()}
+        </Panel>
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   reportOptions: state.reportOptions,
-  typeaheadCache: typeaheadCacheSelector(state)
+  typeaheadCache: typeaheadCacheSelector(state),
 });
 
 const mapDispatchToProps = {
@@ -97,6 +113,6 @@ const mapDispatchToProps = {
   removeFilter,
   refreshReportOptions,
   initTypeaheadCache,
-  refreshTypeaheadCache
+  refreshTypeaheadCache,
 };
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ReportOptions));
