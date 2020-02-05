74db5ff9c65fd123ea8ce1e6b814f5d227ec8b92
Greg Friedman <greg.friedman@sparkpost.com>
2020-02-05 12:13:04 -0500

    SA-1195 Add test to hasSubaccounts selector to include when the user itself is a subaccount user

--- src/selectors/tests/subaccounts.test.js
+++ src/selectors/tests/subaccounts.test.js
@@ -1,17 +1,25 @@
 import * as selector from '../subaccounts';
 
 describe('Subaccount selectors', () => {
-
   describe('selectSubaccount', () => {
     it('should return subaccount unformatted', () => {
-      const state = { subaccounts: { subaccount: { status: 'foo', key: 'value' }}};
-      expect(selector.selectSubaccount(state)).toEqual({ compliance: false, status: 'foo', key: 'value' });
+      const state = { subaccounts: { subaccount: { status: 'foo', key: 'value' } } };
+      expect(selector.selectSubaccount(state)).toEqual({
+        compliance: false,
+        status: 'foo',
+        key: 'value',
+      });
     });
 
     it('should return compliance_status as status when it is not active', () => {
-      const state = { subaccounts: { subaccount: { compliance_status: 'terminated', key: 'value' }}};
-      expect(selector.selectSubaccount(state)).toEqual({ compliance: true, status: 'terminated', key: 'value' });
-
+      const state = {
+        subaccounts: { subaccount: { compliance_status: 'terminated', key: 'value' } },
+      };
+      expect(selector.selectSubaccount(state)).toEqual({
+        compliance: true,
+        status: 'terminated',
+        key: 'value',
+      });
     });
   });
 
@@ -23,34 +31,41 @@ describe('Subaccount selectors', () => {
             {
               compliance_status: 'active',
               status: 'valid',
-              name: 'Subby 1'
+              name: 'Subby 1',
             },
             {
               compliance_status: 'not-active',
               status: 'status',
-              name: 'Subby 2'
-            }
-          ]
+              name: 'Subby 2',
+            },
+          ],
         },
-        account: { options: {}}
+        account: { options: {} },
       };
 
       expect(selector.selectSubaccounts(state)).toEqual([
         {
           compliance: false,
           status: 'valid',
-          name: 'Subby 1'
+          name: 'Subby 1',
         },
         {
           compliance: true,
           status: 'not-active',
-          name: 'Subby 2'
-        }
+          name: 'Subby 2',
+        },
       ]);
     });
 
     it('should return if user has subaccounts', () => {
-      const state = { currentUser: { has_subaccounts: false }};
+      const state = { currentUser: { has_subaccounts: false } };
+      expect(selector.hasSubaccounts(state)).toEqual(false);
+    });
+
+    it('should return false if user is a subaccount user', () => {
+      const state = {
+        currentUser: { has_subaccounts: true, access_level: 'subaccount_reporting' },
+      };
       expect(selector.hasSubaccounts(state)).toEqual(false);
     });
 
@@ -61,28 +76,29 @@ describe('Subaccount selectors', () => {
             {
               compliance_status: 'active',
               status: 'valid',
-              name: 'Subby 1'
+              name: 'Subby 1',
             },
             {
               status: 'terminated',
-              name: 'Subby 2'
+              name: 'Subby 2',
             },
             {
               compliance_status: 'terminated',
               status: 'active',
-              name: 'Subby 3'
-            }
-          ]
+              name: 'Subby 3',
+            },
+          ],
         },
-        account: { options: { ui: { 'hideTerminatedSubaccounts': true }}}
+        account: { options: { ui: { hideTerminatedSubaccounts: true } } },
       };
-      expect(selector.selectSubaccounts(state)).toEqual([{
-        compliance: false,
-        status: 'valid',
-        name: 'Subby 1'
-      }]);
+      expect(selector.selectSubaccounts(state)).toEqual([
+        {
+          compliance: false,
+          status: 'valid',
+          name: 'Subby 1',
+        },
+      ]);
     });
-
   });
 
   describe('getSubaccountIdFromProps', () => {
@@ -101,46 +117,55 @@ describe('Subaccount selectors', () => {
 
   describe('getSubaccountIdFromParams', () => {
     it('should return subaccount router params', () => {
-      const props = { match: { params: { id: 101 }}};
+      const props = { match: { params: { id: 101 } } };
       expect(selector.getSubaccountIdFromParams({}, props)).toEqual(101);
     });
   });
 
   describe('selectSubaccountIdFromQuery', () => {
     it('should return subaccount id from query params', () => {
-      const props = { location: { search: '?subaccount=101' }};
+      const props = { location: { search: '?subaccount=101' } };
       expect(selector.selectSubaccountIdFromQuery({}, props)).toEqual('101');
     });
   });
 
   describe('getSubaccounts', () => {
     it('should return subaccount list', () => {
-      const state = { subaccounts: { list: ['sub', 'list']}};
+      const state = { subaccounts: { list: ['sub', 'list'] } };
       expect(selector.getSubaccounts(state, {})).toEqual(['sub', 'list']);
     });
   });
 
   describe('selectSubaccountFromQuery', () => {
     it('should return subaccount object', () => {
-      const state = { subaccounts: { list: [
-        { name: 'sub 1', id: 101 },
-        { name: 'sub 2', id: 501 }
-      ]}};
-      const props = { location: { search: '?subaccount=501' }};
+      const state = {
+        subaccounts: {
+          list: [
+            { name: 'sub 1', id: 101 },
+            { name: 'sub 2', id: 501 },
+          ],
+        },
+      };
+      const props = { location: { search: '?subaccount=501' } };
       expect(selector.selectSubaccountFromQuery(state, props)).toEqual({ name: 'sub 2', id: 501 });
     });
   });
 
-  describe('getSubaccountsIndexedById' , () => {
+  describe('getSubaccountsIndexedById', () => {
     it('should return the subaccounts indexed by id', () => {
-      const state = { subaccounts: { list: [
-        { name: 'sub 1', id: 101 },
-        { name: 'sub 2', id: 501 }
-      ]}};
+      const state = {
+        subaccounts: {
+          list: [
+            { name: 'sub 1', id: 101 },
+            { name: 'sub 2', id: 501 },
+          ],
+        },
+      };
 
-      expect(selector.getSubaccountsIndexedById(state)).toEqual({ 101: { name: 'sub 1', id: 101 },
-        501: { name: 'sub 2', id: 501 }});
-
+      expect(selector.getSubaccountsIndexedById(state)).toEqual({
+        101: { name: 'sub 1', id: 101 },
+        501: { name: 'sub 2', id: 501 },
+      });
     });
   });
 });
