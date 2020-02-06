06b0c34702d4dc091a28f9c54f649f180d8e8915
Kevin Chu <kevinchu1337@gmail.com>
2020-02-06 16:55:03 -0500

    SA-1196: Address feedback

--- src/selectors/currentUser.js
+++ src/selectors/currentUser.js
@@ -1,7 +1,9 @@
+import { isSubaccountUser } from 'src/helpers/conditions/user';
+
 export const userCookieConsentFlagSelector = state => !!state.currentUser.cookie_consent;
 
 export const usernameSelector = state =>
   state.currentUser.username || state.tfa.username || state.auth.username;
 
 export const subaccountReportingIdSelector = ({ currentUser = {} }) =>
-  currentUser.access_level === 'subaccount_reporting' ? currentUser.subaccount_id : -1;
+  isSubaccountUser({ currentUser }) ? currentUser.subaccount_id : -1;
