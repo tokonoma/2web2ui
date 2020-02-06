f1932448168503e4b70c528eafe1337a1512168a
Greg Friedman <greg.friedman@sparkpost.com>
2020-02-06 10:20:03 -0500

    FE-854 Add Un-tested component

---
+++ src/toDelete/ToDelete.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ToDelete = ({ a, b }) => {
+  let x = 1;
+
+  if (b === 3) {
+    x *= 2;
+  } else if (b === 1) {
+    x *= 3;
+  }
+
+  switch (a) {
+    case 5:
+      return <span>{`Hello ${x}`}</span>;
+    case 6:
+      return <span>{`Goodbye ${x}`}</span>;
+    case 7:
+      return <span>{`Okay ${x}`}</span>;
+    default:
+      return null;
+  }
+};
+
+export default ToDelete;
