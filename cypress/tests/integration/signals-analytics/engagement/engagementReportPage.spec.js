4832efb82af99d7060e57e907e2a4f3bd63347c5
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-02-07 15:38:04 -0500

    FE-790 - Add tests for engagement report data

--- cypress/tests/integration/signals-analytics/engagement/engagementReportPage.spec.js
+++ cypress/tests/integration/signals-analytics/engagement/engagementReportPage.spec.js
@@ -1,5 +1,6 @@
 const PAGE_URL = '/reports/engagement';
 const DELIVERABILITY_API_URL = '/api/v1/metrics/deliverability**/**';
+const STABLE_UNIX_DATE = 1581087062000; // Stable unix timestamp (2/6/2020)
 
 describe('The engagement report page', () => {
   beforeEach(() => {
@@ -52,11 +53,60 @@ describe('The engagement report page', () => {
   //   cy.findByText('No clicks to report').should('be.visible');
   // });
 
-  it('re-requests data when filtering by "Broad Date Range"', () => {});
+  it('re-requests data when filtering by "Broad Date Range" and updates query params accordingly', () => {
+    const broadDateRangeLabel = 'Broad Date Range';
 
-  it('re-requests data when filtering by "Narrow Date Range"', () => {});
+    cy.visit(PAGE_URL);
 
-  it('renders engagement metrics as text', () => {});
+    cy.stubRequest({
+      url: DELIVERABILITY_API_URL,
+      fixture: 'metrics/deliverability/200.get.different-results.json',
+    });
 
-  it('renders "Sent", "Accepted", "Unique Confirmed Opens", and "Unique Clicks" data within a chart', () => {});
+    cy.findByLabelText(broadDateRangeLabel).select('Last Hour');
+    cy.url().should('include', 'range=hour');
+
+    cy.findByLabelText(broadDateRangeLabel).select('Last 24 Hours');
+    cy.url().should('include', 'range=day');
+
+    cy.findByLabelText(broadDateRangeLabel).select('Last 7 Days');
+    cy.url().should('include', 'range=7days');
+
+    cy.findByLabelText(broadDateRangeLabel).select('Last 30 Days');
+    cy.url().should('include', 'range=30days');
+
+    cy.findByLabelText(broadDateRangeLabel).select('Last 90 Days');
+    cy.url().should('include', 'range=90days');
+
+    cy.get('[data-id="summary-panel"]').within(() => {
+      // This may seem like an odd way to check for different data,
+      // but this is a check for unusual numbers that aren't as realistic
+      // as the default `200.get.json` fixture
+      cy.findByText('123');
+      cy.findByText('789');
+      cy.findByText('456');
+    });
+  });
+
+  it('renders engagement metrics as text', () => {
+    cy.visit(PAGE_URL);
+
+    cy.get('[data-id="summary-panel"]').within(() => {
+      cy.findByText('150,000'); // `count_sent`
+      cy.findByText('200,000'); // `count_unique_confirmed_opened_approx`
+      cy.findByText('250,000'); // `count_accepted`
+      cy.findByText('325,000'); // `count_unique_clicked_approx`
+    });
+  });
+
+  it('renders "Sent", "Accepted", "Unique Confirmed Opens", and "Unique Clicks" data within a chart', () => {
+    cy.visit(PAGE_URL);
+
+    cy.get('.recharts-wrapper').within(() => {
+      cy.findByText('150K').should('be.visible'); // `count_sent`
+      cy.findByText('200K').should('be.visible'); // `count_unique_confirmed_opened_approx`
+      cy.findByText('250K').should('be.visible'); // `count_accepted`
+      cy.findByText('325K').should('be.visible'); // `count_unique_clicked_approx`
+    });
+  });
 });
--- src/pages/reports/engagement/components/EngagementSummary.js
+++ src/pages/reports/engagement/components/EngagementSummary.js
@@ -9,7 +9,14 @@ import { formatFullNumber } from 'src/helpers/units';
 import { safeRate } from 'src/helpers/math';
 import styles from './EngagementSummary.module.scss';
 
-export function EngagementSummary ({ accepted = 0, clicks = 0, filters, loading, opens = 0, sent = 0 }) {
+export function EngagementSummary({
+  accepted = 0,
+  clicks = 0,
+  filters,
+  loading,
+  opens = 0,
+  sent = 0,
+}) {
   if (loading) {
     return <PanelLoading minHeight="115px" />;
   }
@@ -19,27 +26,39 @@ export function EngagementSummary ({ accepted = 0, clicks = 0, filters, loading,
     return null;
   }
 
-  const timeRange = filters.relativeRange === 'custom'
-    ? <span>from <strong>{formatDateTime(filters.from)}</strong> to <strong>{formatDateTime(filters.to)}</strong></span>
-    : <span>in the <strong>{relativeDateOptionsIndexed[filters.relativeRange].toLowerCase()}</strong></span>;
+  const timeRange =
+    filters.relativeRange === 'custom' ? (
+      <span>
+        from <strong>{formatDateTime(filters.from)}</strong> to{' '}
+        <strong>{formatDateTime(filters.to)}</strong>
+      </span>
+    ) : (
+      <span>
+        in the <strong>{relativeDateOptionsIndexed[filters.relativeRange].toLowerCase()}</strong>
+      </span>
+    );
 
   return (
-    <Panel className={styles.EngagementSummary}>
+    <Panel className={styles.EngagementSummary} data-id="summary-panel">
       <Grid>
         <Grid.Column xs={12} md={3} xl={2}>
-          <h1>< Percent value={safeRate(opens, accepted)} /></h1>
+          <h1>
+            <Percent value={safeRate(opens, accepted)} />
+          </h1>
           <h6>Unique Open Rate</h6>
         </Grid.Column>
         <Grid.Column xs={12} md={3} xl={2}>
-          <h1>< Percent value={safeRate(clicks, accepted)} /></h1>
+          <h1>
+            <Percent value={safeRate(clicks, accepted)} />
+          </h1>
           <h6>Unique Click Rate</h6>
         </Grid.Column>
         <Grid.Column xs={12} md={6} xl={8}>
           <p>
-            Of <strong>{formatFullNumber(sent)}</strong> sent
-            recipients, <strong>{formatFullNumber(accepted)}</strong> messages were
-            accepted, <strong>{formatFullNumber(opens)}</strong> were opened
-            and <strong>{formatFullNumber(clicks)}</strong> were clicked {timeRange}.
+            Of <strong>{formatFullNumber(sent)}</strong> sent recipients,{' '}
+            <strong>{formatFullNumber(accepted)}</strong> messages were accepted,{' '}
+            <strong>{formatFullNumber(opens)}</strong> were opened and{' '}
+            <strong>{formatFullNumber(clicks)}</strong> were clicked {timeRange}.
           </p>
         </Grid.Column>
       </Grid>
