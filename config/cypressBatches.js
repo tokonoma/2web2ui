091feeb1b9c220d38ddb860a25398d51666fee90
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-02-12 13:34:21 -0500

    FE-858 - Update batches after merge

--- config/cypressBatches.js
+++ config/cypressBatches.js
@@ -3,7 +3,10 @@ module.exports = {
     'cypress/tests/integration/accounts/**/*',
     'cypress/tests/integration/events/**/*',
     'cypress/tests/integration/navigation/**/*',
-    'cypress/tests/integration/reports/**/*',
   ],
-  second: ['cypress/tests/integration/templates/**/*', 'cypress/tests/integration/recipients/**/*'],
+  second: [
+    'cypress/tests/integration/recipients/**/*',
+    'cypress/tests/integration/signals-analytics/**/*',
+    'cypress/tests/integration/templates/**/*',
+  ],
 };
