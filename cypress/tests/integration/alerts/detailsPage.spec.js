b21e4f3fcd04fdc3c8a9b5e875d0ea9bcf31049e
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-02-13 13:42:32 -0500

    FE-858 - Flesh out tests further

--- cypress/tests/integration/alerts/detailsPage.spec.js
+++ cypress/tests/integration/alerts/detailsPage.spec.js
@@ -10,6 +10,16 @@ describe('The alerts details pages', () => {
       url: API_URL,
       fixture: 'alerts/2/200.get.json',
     });
+
+    cy.stubRequest({
+      url: `${API_URL}/incidents`,
+      fixture: 'alerts/2/incidents/200.get.json',
+    });
+
+    cy.stubRequest({
+      url: '/api/v1/subaccounts',
+      fixture: 'subaccounts/200.get.json',
+    });
   });
 
   it('renders with a relevant page title', () => {
@@ -17,5 +27,102 @@ describe('The alerts details pages', () => {
 
     cy.title().should('include', 'Alert Details | Alerts');
     cy.findByText('Alert Details').should('be.visible');
+  });
+
+  it('renders with the alert name', () => {
+    cy.visit(PAGE_URL);
+
+    cy.findByText('This is an alert').should('be.visible');
+  });
+
+  it('renders with relevant alert details data', () => {
+    cy.visit(PAGE_URL);
+
+    cy.get('[data-id="alert-details-panel"]').within(() => {
+      cy.findByText('Health Score').should('be.visible');
+      cy.findByText('Fake Subaccount 1 (101)').should('be.visible');
+      cy.findByText('Fake Subaccount 2 (102)').should('be.visible');
+      cy.findByText('Fake Subaccount 3 (103)').should('be.visible');
+      cy.findByText('fake-email@sparkpost.com');
+      cy.get('[data-id="alert-toggle"]')
+        .find('input')
+        .should('not.be.checked');
+    });
+  });
+
+  it('renders with a link to the edit alert page', () => {
+    cy.visit(PAGE_URL);
+
+    cy.findByText('Edit').should('have.attr', 'href', '/alerts/edit/2');
+  });
+
+  it('renders with a link back to the alerts list page', () => {
+    cy.visit(PAGE_URL);
+
+    cy.findByText('Back to Alerts').should('have.attr', 'href', '/alerts');
+  });
+
+  it('renders as muted by default when the server returns a muted alert', () => {
+    cy.stubRequest({
+      url: API_URL,
+      fixture: 'alerts/2/200.get.muted.json',
+    });
+
+    cy.visit(PAGE_URL);
+
+    cy.get('[data-id="alert-toggle"]')
+      .find('input')
+      .should('be.checked');
+  });
+
+  describe.only('the duplicate and delete buttons', () => {
+    beforeEach(() => cy.visit(PAGE_URL));
+
+    it('routes to the create page when clicking on "Duplicate"', () => {
+      cy.findByText('Duplicate').click();
+
+      cy.url().should('include', '/alerts/create/2');
+      cy.findByLabelText('Alert Name').should('have.value', 'This is an alert Copy');
+    });
+
+    it('opens a delete confirmation modal when clicking "Delete"', () => {
+      cy.findByText('Delete').click();
+
+      cy.findByText('Are you sure you want to delete this alert?').should('be.visible');
+
+      cy.findByText('Cancel').click();
+
+      cy.queryByText('Are you sure you want to delete this alert?').should('not.be.visible');
+    });
+
+    it('renders a success message and re-routes to the list page when an alert is successfully deleted', () => {
+      cy.stubRequest({
+        method: 'DELETE',
+        url: API_URL,
+        fixture: 'alerts/2/200.delete.json',
+      });
+
+      cy.findByText('Delete').click();
+      cy.get('#modal-portal').within(() => cy.findByText('Delete').click());
+
+      cy.findByText('Alert: This is an alert Deleted').should('be.visible');
+      cy.url().should('include', '/alerts');
+      cy.title().should('not.include', 'Alert Details');
+    });
+
+    // TODO: Left off here
+    it('renders an error message when deletion fails', () => {});
+  });
+
+  describe('the alert incidents table', () => {
+    it('renders the empty state when no results are returned', () => {});
+
+    it('renders "Active" and already-resolved incidents', () => {});
+
+    it('is sortable by "Triggered"', () => {});
+
+    it('is sortable by "Resolved"', () => {});
+
+    it('it sortable by "Score"', () => {});
   });
 });
