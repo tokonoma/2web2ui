4854dbfe0163369f58a59aadf0597687a1b536fd
Kevin Chu <kevinchu1337@gmail.com>
2020-02-20 18:13:28 -0500

    FE-860: moved blacklist tests to faster queue

--- config/cypressBatches.js
+++ config/cypressBatches.js
@@ -4,11 +4,11 @@ module.exports = {
     'cypress/tests/integration/alerts/**/*',
     'cypress/tests/integration/events/**/*',
     'cypress/tests/integration/navigation/**/*',
+    'cypress/tests/integration/blacklists/**/*',
   ],
   'Vin Diesel': [
     'cypress/tests/integration/recipients/**/*',
     'cypress/tests/integration/signals-analytics/**/*',
     'cypress/tests/integration/templates/**/*',
-    'cypress/tests/integration/blacklists/**/*',
   ],
 };
