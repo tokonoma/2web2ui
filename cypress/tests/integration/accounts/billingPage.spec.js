263db5345f5b0493515c75248dfd6bdf44ee324e
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-02-24 14:54:15 -0500

    FE-783 - Add additional billing page tests

--- cypress/tests/integration/accounts/billingPage.spec.js
+++ cypress/tests/integration/accounts/billingPage.spec.js
@@ -60,13 +60,27 @@ describe('Billing Page', () => {
     cy.title().should('include', 'Billing');
   });
 
-  it("renders with the user's currently selected plan", () => {
-    cy.visit(PAGE_URL);
+  it("renders with the user's currently selected plan", () => {});
 
-    cy.findByText('50,000 emails for $20 per month').should('be.visible');
-    cy.findByText('$1.00 per thousand extra emails').should('be.visible');
+  describe("rendering different plans based on the user's subscription", () => {
+    it('renders with the free plan', () => {
+      cy.stubRequest({
+        url: '/api/v1/billing/subscription',
+        fixture: 'billing/subscription/200.get.premier-plan.json',
+        fixtureAlias: 'billingSubscriptionGet',
+      });
 
-    // TODO: Check for other plan types
+      cy.visit(PAGE_URL);
+    });
+
+    it('renders with the 50k/month plan', () => {
+      cy.visit(PAGE_URL);
+
+      cy.findByText('50,000 emails for $20 per month').should('be.visible');
+      cy.findByText('$1.00 per thousand extra emails').should('be.visible');
+    });
+
+    it('renders the premier plan', () => {});
   });
 
   it('displays a pending plan change banner whenever a plan is downgraded', () => {
@@ -102,5 +116,89 @@ describe('Billing Page', () => {
 
     cy.visit(PAGE_URL);
     cy.findByText('Change Plan').should('have.attr', 'href', '/account/billing/plan');
+  });
+
+  describe('the invoice history table', () => {
+    function assertTableRow({ rowIndex, date, amount, invoice }) {
+      cy.get('tbody tr')
+        .eq(rowIndex)
+        .within(() => {
+          cy.get('td')
+            .eq(0)
+            .should('contain', date);
+
+          cy.get('td')
+            .eq(1)
+            .should('contain', amount);
+
+          cy.get('td')
+            .eq(2)
+            .should('contain', invoice);
+        });
+    }
+
+    it("renders the user's prior invoices in table rows", () => {
+      cy.visit(PAGE_URL);
+
+      cy.findByText('Invoice History')
+        .scrollIntoView()
+        .should('be.visible');
+
+      assertTableRow({
+        rowIndex: 0,
+        date: 'Jan 3 2020',
+        amount: '$123.00',
+        invoice: 'INV123',
+      });
+
+      assertTableRow({
+        rowIndex: 1,
+        date: 'Jan 2 2020',
+        amount: '$456.00',
+        invoice: 'INV456',
+      });
+
+      assertTableRow({
+        rowIndex: 2,
+        date: 'Jan 1 2020',
+        amount: '$789.00',
+        invoice: 'INV789',
+      });
+    });
+
+    it('does not render the "Invoice History" table when no results are returned', () => {
+      cy.stubRequest({
+        url: '/api/v1/account/invoices',
+        fixture: '200.get.no-results',
+        fixtureAlias: 'invoicesGet',
+      });
+
+      cy.visit(PAGE_URL);
+
+      cy.queryByText('Invoice History').should('not.be.visible');
+    });
+
+    it('has a download button in each table row that requests an individual invoice', () => {
+      cy.visit(PAGE_URL);
+
+      cy.findByText('Invoice History').scrollIntoView();
+
+      // File download assertion not currently supported, but should be
+      // in a future release: https://github.com/cypress-io/cypress/issues/949
+      // In the meantime, just checking the request goes through.
+
+      cy.stubRequest({
+        url: '/api/v1/account/invoices/abc',
+        fixture: 'account/invoices/abc/200.get.pdf',
+      });
+
+      cy.get('tbody tr')
+        .first()
+        .within(() => {
+          cy.findByText('Download').click();
+        });
+
+      cy.findByText('Downloaded invoice: INV123').should('be.visible');
+    });
   });
 });
