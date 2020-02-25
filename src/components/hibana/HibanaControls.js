46fd99011c067f1c500e6387fe4f113852c3b88b
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-02-25 15:00:40 -0500

    FE-904 - Implementing Hibana controls account toggle

---
+++ src/components/hibana/HibanaControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { isAccountUiOptionSet } from 'src/helpers/conditions/account';
+import { selectCondition } from 'src/selectors/accessConditionState';
+import styles from './HibanaComponents.module.scss';
+
+function HibanaControls({ children, hasThemeControls }) {
+  if (hasThemeControls) {
+    return <div className={styles.HibanaControls}>{children}</div>;
+  }
+
+  return null;
+}
+
+const mapStateToProps = state => {
+  return {
+    hasThemeControls: selectCondition(isAccountUiOptionSet('hibana.hasThemeControls'))(state),
+  };
+};
+
+export default connect(mapStateToProps, {})(HibanaControls);
---
+++ src/components/hibana/index.js
@@ -0,0 +1,2 @@
+export { default as HibanaControls } from './HibanaControls';
+export { default as HibanaToggle } from './HibanaToggle';
