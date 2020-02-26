d51f7b2305aa613bdade69dd7a058ff4f289ea64
Patrick Sison <patrick.m.sison@gmail.com>
2020-02-26 13:30:11 -0500

    AC-1177: Update unit test

--- src/pages/billing/components/tests/SubmitSection.test.js
+++ src/pages/billing/components/tests/SubmitSection.test.js
@@ -8,14 +8,9 @@ jest.mock('../../context/FeatureChangeContext');
 
 describe('SubmitSection: ', () => {
   describe('SubmitSection: ', () => {
-    const defaultContextState = {
-      isReady: true
-    };
-
     const subject = (props = {}, contextState = {}) => {
       useFeatureChangeContext.mockReturnValue({
-        ...defaultContextState,
-        ...contextState
+        ...contextState,
       });
       return shallow(<SubmitSection {...props} />);
     };
@@ -23,11 +18,6 @@ describe('SubmitSection: ', () => {
     it('should render if context state isReady', () => {
       const wrapper = subject();
       expect(wrapper.find(Brightback)).toExist();
-    });
-
-    it('should render nothing if context state is not ready', () => {
-      const wrapper = subject({}, { isReady: false });
-      expect(wrapper.find(Brightback)).not.toExist();
     });
 
     it('should render nothing if context state is loading', () => {
@@ -45,7 +35,6 @@ describe('SubmitSection: ', () => {
       const button = getButton(wrapper, { enabled: true, to: 'url' });
       expect(button.prop('to')).toEqual('url');
       expect(button.prop('type')).toEqual('button');
-
     });
 
     it('should not use url if not enabled', () => {
