d7c56f397fca089d686fb462043184a1b5777478
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-03-02 14:19:15 -0500

    FE-901 - Add additional comments to help with future efforts

--- cypress/tests/integration/alerts/createPage.spec.js
+++ cypress/tests/integration/alerts/createPage.spec.js
@@ -176,6 +176,8 @@ describe('Alerts Page - Create', () => {
           channels: { emails: ['sparkky@sparkpost.io'] },
         };
 
+        // TODO: Remove these logs if test is no longer showing flakiness
+        cy.log('Keeping these logs in here due to test flakiness!');
         cy.log(requestBody);
         cy.log(expectedRequestBody);
 
