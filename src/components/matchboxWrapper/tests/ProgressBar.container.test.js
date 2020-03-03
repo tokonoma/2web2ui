c6da969549de528d0018022e2e85a3f1ef630352
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-03 17:24:57 -0500

    [FE-884] Addressing feedback

--- src/components/matchboxWrapper/tests/ProgressBar.container.test.js
+++ src/components/matchboxWrapper/tests/ProgressBar.container.test.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ProgressBar from '../ProgressBar.container.js';
 import { mount } from 'enzyme';
-import { HibanaProvider } from 'src/context/HibanaContext';
-
+import { render } from '@testing-library/react';
+import { useHibana } from 'src/context/HibanaContext';
+jest.mock('src/context/HibanaContext');
+jest.mock('@sparkpost/matchbox', () => ({
+  ProgressBar: () => <div>default progress bar</div>,
+}));
+jest.mock('@sparkpost/matchbox-hibana', () => ({
+  ProgressBar: () => <div>hibana progress bar</div>,
+}));
 describe('ProgressBar', () => {
-  const subject = (props, context) => {
-    return mount(
-      <HibanaProvider>
-        <ProgressBar {...props} />
-      </HibanaProvider>,
-      context,
-    );
+  const subject = (method, props) => {
+    return method(<ProgressBar {...props} />);
   };
   it('should only pass pre-hibana props when hibana is not enabled', () => {
+    useHibana.mockImplementationOnce(() => [{ isHibanaEnabled: false }]);
     const props = { completed: 50, color: 'orange', size: 'normal', my: 50 };
-    const instance = subject(<ProgressBar {...props} />, { isHibanaEnabled: false });
+    const instance = subject(mount, props);
     expect(
       instance
         .find('ProgressBar')
@@ -39,5 +42,17 @@ describe('ProgressBar', () => {
         .last()
         .props(),
     ).not.toHaveProperty('my');
+  });
+  it('should only render hibana component when hibana is enabled', () => {
+    useHibana.mockImplementationOnce(() => [{ isHibanaEnabled: true }]);
+    const { queryByText } = render(<ProgressBar />);
+    expect(queryByText('hibana progress bar')).toBeInTheDocument();
+    expect(queryByText('default progress bar')).not.toBeInTheDocument();
+  });
+  it('should only render matchbox component when hibana is not enabled', () => {
+    useHibana.mockImplementationOnce(() => [{ isHibanaEnabled: false }]);
+    const { queryByText } = render(<ProgressBar />);
+    expect(queryByText('hibana progress bar')).not.toBeInTheDocument();
+    expect(queryByText('default progress bar')).toBeInTheDocument();
   });
 });
