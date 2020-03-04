74f7139af549e9419a88a0b09dddca13f04649de
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-04 13:46:00 -0500

    [FE-865] adding in other changes

---
+++ src/components/matchbox/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Banner as PreHibanaBanner } from '@sparkpost/matchbox';
+import { Banner as HibanaBanner } from '@sparkpost/matchbox';
+import { useHibana } from 'src/context/HibanaContext';
+import _ from 'lodash';
+
+export default function Banner(props) {
+  const [state, _dispatch] = useHibana();
+  const { isHibanaEnabled } = state;
+  const preHibanaProps = ['status', 'title', 'onDismiss', 'actions', 'action', 'children'];
+
+  if (isHibanaEnabled) {
+    return <HibanaBanner {...props} />;
+  }
+  return <PreHibanaBanner {..._.pick(props, preHibanaProps)} />;
+}
