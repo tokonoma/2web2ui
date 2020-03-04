cc9d48d3d427d921c18c6dbe41cb5bad61430e59
Kevin Chu <kevinchu1337@gmail.com>
2020-03-04 16:49:46 -0500

    FE-866: Update styling and syntax

--- src/components/matchbox/Box.js
+++ src/components/matchbox/Box.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Box as HibanaBox } from '@sparkpost/matchbox-hibana';
 import { useHibana } from 'src/context/HibanaContext';
 
-const Box = props => {
+export default function Box(props) {
   const [state] = useHibana();
   const { isHibanaEnabled } = state;
 
   return isHibanaEnabled ? <HibanaBox {...props} /> : <>{props.children}</>;
-};
-
-export default Box;
+}
