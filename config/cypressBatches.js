51ae15132126d48efbe90b48acfeac8594dcaca4
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-03-04 13:25:30 -0500

    FE-785-debugging - Debugging changeBillingPlanPage

--- config/cypressBatches.js
+++ config/cypressBatches.js
@@ -1,12 +1,5 @@
 module.exports = {
-  'Paul Walker': [
-    'cypress/tests/integration/accounts/**/*',
-    'cypress/tests/integration/alerts/**/*',
-    'cypress/tests/integration/events/**/*',
-    'cypress/tests/integration/navigation/**/*',
-    'cypress/tests/integration/blacklists/**/*',
-    'cypress/tests/integration/hibana/**/*',
-  ],
+  'Paul Walker': ['cypress/tests/integration/accounts/**/*'],
   'Vin Diesel': [
     'cypress/tests/integration/recipients/**/*',
     'cypress/tests/integration/signals-analytics/**/*',
