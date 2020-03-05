0db71f8ad333ccfdb37ee229952b781c8e9fa727
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-05 10:43:42 -0500

    [FE-884] Addressing Feedback

--- src/components/matchbox/ProgressBar.js
+++ src/components/matchbox/ProgressBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ProgressBar as PreHibanaProgressBar } from '@sparkpost/matchbox';
+import { ProgressBar as OGProgressBar } from '@sparkpost/matchbox';
 import { ProgressBar as HibanaProgressBar } from '@sparkpost/matchbox-hibana';
 import { useHibana } from 'src/context/HibanaContext';
 import _ from 'lodash';
@@ -12,5 +12,5 @@ export default function ProgressBar(props) {
   if (isHibanaEnabled) {
     return <HibanaProgressBar {...props} />;
   }
-  return <PreHibanaProgressBar {..._.pick(props, preHibanaProps)} />;
+  return <OGProgressBar {..._.pick(props, preHibanaProps)} />;
 }
