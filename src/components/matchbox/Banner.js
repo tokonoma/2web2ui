5daa4c7f040653128e3e8d83957b757d83aa0503
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-06 16:42:37 -0500

    [FE-865] added test for Banner

--- src/components/matchbox/Banner.js
+++ src/components/matchbox/Banner.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Banner as PreHibanaBanner } from '@sparkpost/matchbox';
+import { Banner as OGBanner } from '@sparkpost/matchbox';
 import { Banner as HibanaBanner } from '@sparkpost/matchbox-hibana';
 import { useHibana } from 'src/context/HibanaContext';
 import _ from 'lodash';
 
 export default function Banner(props) {
-  const [state, _dispatch] = useHibana();
+  const [state] = useHibana();
   const { isHibanaEnabled } = state;
   const preHibanaProps = ['status', 'title', 'onDismiss', 'actions', 'action', 'children'];
 
   if (isHibanaEnabled) {
     return <HibanaBanner {...props} />;
   }
-  return <PreHibanaBanner {..._.pick(props, preHibanaProps)} />;
+  return <OGBanner {..._.pick(props, preHibanaProps)} />;
 }
