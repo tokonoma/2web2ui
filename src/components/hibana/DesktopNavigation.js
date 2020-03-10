429ff92e044e47ebadff61f0620a1c98e5ab10ae
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-03-10 16:53:43 -0400

    FE-922 - Move comment to more logical location

--- src/components/hibana/DesktopNavigation.js
+++ src/components/hibana/DesktopNavigation.js
@@ -7,6 +7,7 @@ import { SparkPost } from 'src/components';
 import styles from './DesktopNavigation.module.scss';
 
 export default function DesktopNavigation({ navItems, location }) {
+  // TODO: This can be cleaned up post OG theme removal - by updating the navItems config
   const isActive = navItem => {
     if (
       location.pathname.includes('/reports/message-events') &&
@@ -30,83 +31,86 @@ export default function DesktopNavigation({ navItems, location }) {
 
   return (
     <div className={styles.DesktopNavigation} data-id="desktop-navigation">
-      <div className={styles.Wrapper}>
-        <div className={styles.SubWrapper}>
-          <SkipLink />
+      <NavWrapper>
+        <SkipLink />
 
-          <div className={styles.PrimaryNavLayout}>
-            <div className={styles.LogoWrapper}>
-              <Link to="/dashboard">
-                <SparkPost.Logo className={styles.Logo} />
-              </Link>
-            </div>
-
-            <nav className={styles.PrimaryNav}>
-              {/* Visually hidden headings to help guide screen reader users */}
-              <ScreenReaderOnly>
-                <h2>Main Navigation</h2>
-              </ScreenReaderOnly>
-
-              <Inline>
-                {navItems.map((item, index) => {
-                  // TODO: This can be cleaned up post OG theme removal - by updating the navItems config
-                  return (
-                    <NavLink
-                      variant="primary"
-                      to={item.to}
-                      key={`nav-item-${index}`}
-                      isActive={isActive(item)}
-                    >
-                      {item.label}
-                    </NavLink>
-                  );
-                })}
-              </Inline>
-            </nav>
-
-            {/* TODO: Implement in FE-924 */}
-            <AccountDropdown>
-              <button className={styles.AccountDropdownButton}>
-                <span aria-hidden="true">SP</span>
-
-                <ScreenReaderOnly>Account Menu</ScreenReaderOnly>
-              </button>
-            </AccountDropdown>
+        <div className={styles.PrimaryNavLayout}>
+          <div className={styles.LogoWrapper}>
+            <Link to="/dashboard">
+              <SparkPost.Logo className={styles.Logo} />
+            </Link>
           </div>
+
+          <nav className={styles.PrimaryNav}>
+            {/* Visually hidden headings to help guide screen reader users */}
+            <ScreenReaderOnly>
+              <h2>Main Navigation</h2>
+            </ScreenReaderOnly>
+
+            <Inline>
+              {navItems.map((item, index) => {
+                return (
+                  <NavLink
+                    variant="primary"
+                    to={item.to}
+                    key={`nav-item-${index}`}
+                    isActive={isActive(item)}
+                  >
+                    {item.label}
+                  </NavLink>
+                );
+              })}
+            </Inline>
+          </nav>
+
+          {/* TODO: Implement in FE-924 */}
+          <AccountDropdown>
+            <button className={styles.AccountDropdownButton}>
+              <span aria-hidden="true">SP</span>
+
+              <ScreenReaderOnly>Account Menu</ScreenReaderOnly>
+            </button>
+          </AccountDropdown>
         </div>
-      </div>
+      </NavWrapper>
 
       {navItems.map(item => {
         if (isActive(item) && item.children) {
           return (
             <nav className={styles.SecondaryNav} key="secondary-nav" data-id="secondary-navigation">
-              <div className={styles.Wrapper}>
-                <div className={styles.SubWrapper}>
-                  {/* Visually hidden headings to help guide screen reader users */}
-                  <ScreenReaderOnly>
-                    <h3>Category Navigation</h3>
-                  </ScreenReaderOnly>
+              <NavWrapper>
+                {/* Visually hidden headings to help guide screen reader users */}
+                <ScreenReaderOnly>
+                  <h3>Category Navigation</h3>
+                </ScreenReaderOnly>
 
-                  <Inline>
-                    {item.children.map((childItem, index) => {
-                      return (
-                        <NavLink
-                          variant="secondary"
-                          to={childItem.to}
-                          key={`subnav-item-${index}`}
-                          isActive={location.pathname.includes(childItem.to)}
-                        >
-                          {childItem.label}
-                        </NavLink>
-                      );
-                    })}
-                  </Inline>
-                </div>
-              </div>
+                <Inline>
+                  {item.children.map((childItem, index) => {
+                    return (
+                      <NavLink
+                        variant="secondary"
+                        to={childItem.to}
+                        key={`subnav-item-${index}`}
+                        isActive={location.pathname.includes(childItem.to)}
+                      >
+                        {childItem.label}
+                      </NavLink>
+                    );
+                  })}
+                </Inline>
+              </NavWrapper>
             </nav>
           );
         }
       })}
+    </div>
+  );
+}
+
+function NavWrapper({ children }) {
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.SubWrapper}>{children}</div>
     </div>
   );
 }
