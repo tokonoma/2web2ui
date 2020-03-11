311337d95314d8efaf0b6df7719a95eeaf094505
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-11 17:09:56 -0400

    [AC-1268] Changed UploadedListForm to functional component

--- src/pages/recipientValidation/components/UploadedListForm.js
+++ src/pages/recipientValidation/components/UploadedListForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Grid, UnstyledLink } from '@sparkpost/matchbox';
 import { getUsage } from 'src/actions/account';
@@ -9,61 +9,54 @@ import styles from './UploadedListForm.module.scss';
 import { formatFullNumber } from 'src/helpers/units';
 import RVPriceModal from './RVPriceModal';
 
-export class UploadedListForm extends React.Component {
-  state = {
-    isModalOpen: false,
-  };
+export function UploadedListForm({
+  getUsage,
+  currentUsage,
+  job: { addressCount, filename },
+  loading,
+}) {
+  const [isModalOpen, setModalisOpen] = useState(false);
+  useEffect(() => {
+    getUsage();
+  }, [getUsage]);
 
-  componentDidMount() {
-    this.props.getUsage();
-  }
-
-  handleModal = isModalOpen => {
-    this.setState({ isModalOpen });
-  };
-
-  render() {
-    const { currentUsage, job, loading } = this.props;
-    const { addressCount, filename } = job;
-
-    return (
-      <div className={styles.formContainer}>
-        <h2>{filename}</h2>
-        <p className={styles.descriptionParagraph}>
-          <span>Your list has been uploaded successfully and the cost to validate is below. </span>
-          <strong>Would you like to proceed?</strong>
-        </p>
-        <Grid>
-          <Grid.Column xs={12} md={4}>
-            <div>Your list has</div>
-            <div className={styles.CountBox}>
-              <span className={styles.Number} data-id="recipient-list-address-count">
-                {formatFullNumber(addressCount)}
-              </span>{' '}
-              emails
+  return (
+    <div className={styles.formContainer}>
+      <h2>{filename}</h2>
+      <p className={styles.descriptionParagraph}>
+        <span>Your list has been uploaded successfully and the cost to validate is below. </span>
+        <strong>Would you like to proceed?</strong>
+      </p>
+      <Grid>
+        <Grid.Column xs={12} md={4}>
+          <div>Your list has</div>
+          <div className={styles.CountBox}>
+            <span className={styles.Number} data-id="recipient-list-address-count">
+              {formatFullNumber(addressCount)}
+            </span>{' '}
+            emails
+          </div>
+        </Grid.Column>
+        <Grid.Column xs={12} mdOffset={1} md={7}>
+          <div>and will cost</div>
+          {loading ? (
+            <div className={styles.LoadingCostContainer}>
+              <LoadingSVG size="Small" />
             </div>
-          </Grid.Column>
-          <Grid.Column xs={12} mdOffset={1} md={7}>
-            <div>and will cost</div>
-            {loading ? (
-              <div className={styles.LoadingCostContainer}>
-                <LoadingSVG size="Small" />
-              </div>
-            ) : (
-              <>
-                <div className={styles.Cost}>{calculateNewCost(currentUsage, addressCount)}</div>
-                <UnstyledLink onClick={() => this.handleModal(true)}>
-                  How was this calculated?
-                </UnstyledLink>
-              </>
-            )}
-          </Grid.Column>
-        </Grid>
+          ) : (
+            <>
+              <div className={styles.Cost}>{calculateNewCost(currentUsage, addressCount)}</div>
+              <UnstyledLink onClick={() => setModalisOpen(true)}>
+                How was this calculated?
+              </UnstyledLink>
+            </>
+          )}
+        </Grid.Column>
+      </Grid>
 
-        {<RVPriceModal isOpen={this.state.isModalOpen} handleOpen={this.handleModal} />}
-      </div>
-    );
-  }
+      {<RVPriceModal isOpen={isModalOpen} handleOpen={setModalisOpen} />}
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
--- src/pages/recipientValidation/components/tests/UploadedListForm.test.js
+++ src/pages/recipientValidation/components/tests/UploadedListForm.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { UploadedListForm } from '../UploadedListForm';
 
 describe('UploadedListForm', () => {
-  const subject = (props = {}) =>
-    shallow(
+  const subject = (props = {}, method = shallow) =>
+    method(
       <UploadedListForm
         currentUsage={12345}
-        getUsage={() => {}}
+        getUsage={jest.fn()}
         job={{
           filename: 'big-test.csv',
           addressCount: 123,
@@ -23,7 +23,7 @@ describe('UploadedListForm', () => {
 
   it('should call getUsage on mount', () => {
     const getUsage = jest.fn();
-    subject({ getUsage });
+    subject({ getUsage }, mount);
     expect(getUsage).toHaveBeenCalled();
   });
 
