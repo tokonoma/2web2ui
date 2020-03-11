ee31ea8f79fd19f7e0af88449687049361aaf8f4
Kevin Chu <kevinchu1337@gmail.com>
2020-03-11 14:48:39 -0400

    FE-881: Added Hibana Panel wrapper

---
+++ src/components/matchbox/Panel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Panel as OGPanel } from '@sparkpost/matchbox';
+import { Panel as HibanaPanel } from '@sparkpost/matchbox-hibana';
+import { useHibana } from 'src/context/HibanaContext';
+import { omitSystemProps } from 'src/helpers/hibana';
+
+const Panel = props => {
+  const [state] = useHibana();
+  const { isHibanaEnabled } = state;
+
+  if (!isHibanaEnabled) {
+    return <OGPanel {...omitSystemProps(props)} />;
+  }
+  return <HibanaPanel {...props} />;
+};
+
+const Section = props => {
+  const [state] = useHibana();
+  const { isHibanaEnabled } = state;
+  if (!isHibanaEnabled) {
+    return <OGPanel.Section {...omitSystemProps(props)} />;
+  }
+  return <HibanaPanel.Section {...props} />;
+};
+
+Panel.Section = Section;
+
+export default Panel;
