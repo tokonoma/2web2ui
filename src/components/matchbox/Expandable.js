4f058fae6ac8e9abfdc46029ca7608b437b6ff4e
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-12 09:16:33 -0400

    [FE-873] Address Feedback

--- src/components/matchbox/Expandable.js
+++ src/components/matchbox/Expandable.js
@@ -3,15 +3,12 @@ import { Expandable as OGExpandable } from '@sparkpost/matchbox';
 import { Expandable as HibanaExpandable } from '@sparkpost/matchbox-hibana';
 import { useHibana } from 'src/context/HibanaContext';
 import { omitSystemProps } from 'src/helpers/hibana';
-import _ from 'lodash';
 
 export default function Expandable(props) {
   const [state] = useHibana();
   const { isHibanaEnabled } = state;
-  const whiteListedProps = ['id'];
-
   if (isHibanaEnabled) {
     return <HibanaExpandable {...props} />;
   }
-  return <OGExpandable {...omitSystemProps(props, whiteListedProps)} />;
+  return <OGExpandable {...omitSystemProps(props)} />;
 }
