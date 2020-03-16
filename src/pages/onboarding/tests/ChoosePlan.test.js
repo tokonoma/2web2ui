afd936147f9a08488a233cba2899e4fbe544520c
Patrick Sison <patrick.m.sison@gmail.com>
2020-03-16 16:41:30 -0400

    AC-1230: Removed tests covered in integration tests

--- src/pages/onboarding/tests/ChoosePlan.test.js
+++ src/pages/onboarding/tests/ChoosePlan.test.js
@@ -7,10 +7,7 @@ import PromoCodeNew from '../../../components/billing/PromoCodeNew';
 jest.mock('src/helpers/billing');
 
 describe('ChoosePlan page tests', () => {
-  let wrapper;
-  let instance;
-
-  const props = {
+  const defaultProps = {
     hasError: false,
     getPlans: jest.fn(),
     getBundles: jest.fn(),
@@ -35,138 +32,37 @@ describe('ChoosePlan page tests', () => {
     clearPromoCode: jest.fn(),
   };
 
+  const subject = (props = {}, render = shallow) =>
+    render(<ChoosePlan {...defaultProps} {...props} />);
+
   beforeEach(() => {
-    wrapper = shallow(<ChoosePlan {...props} />);
-    instance = wrapper.instance();
     billingHelpers.prepareCardInfo = jest.fn(a => a);
   });
 
   it('should render correctly', () => {
+    const wrapper = subject();
     expect(wrapper).toMatchSnapshot();
-    expect(props.history.push).not.toHaveBeenCalled();
+    expect(defaultProps.history.push).not.toHaveBeenCalled();
   });
 
   it('should render component', () => {
+    const wrapper = subject({ loading: true });
     wrapper.setProps({ loading: true });
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should redirect to next step when api calls fail', () => {
-    wrapper.setProps({ hasError: true });
-    expect(instance.props.history.push).toHaveBeenCalledWith('/dashboard');
+    expect(wrapper.find('Loading')).toExist();
   });
 
   it('should show free bullets when isFree is selected', () => {
-    wrapper.setProps({ selectedPlan: { isFree: true } });
+    const wrapper = subject({ selectedPlan: { isFree: true } });
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should not PromoCode field if plan isFree', () => {
-    wrapper.setProps({ selectedPlan: { isFree: true } });
+    const wrapper = subject({ selectedPlan: { messaging: { price: 0 } } });
     expect(wrapper.find(PromoCodeNew)).toHaveLength(0);
   });
 
   it('should disable submit and plan picker when submitting', () => {
-    wrapper.setProps({ submitting: true });
+    const wrapper = subject({ submitting: true });
     expect(wrapper).toMatchSnapshot();
-  });
-
-  describe('onSubmit tests', () => {
-    it('should not need valid cc info for a free plan selection', async () => {
-      await instance.onSubmit({ planpicker: { code: 'free', isFree: true } });
-      expect(billingHelpers.prepareCardInfo).not.toHaveBeenCalled();
-    });
-
-    it('should prepare cc info for a paid plan selection', async () => {
-      await instance.onSubmit({
-        planpicker: { code: 'paid' },
-        card: { number: '411', name: 'Captain Moneybags' },
-      });
-      expect(billingHelpers.prepareCardInfo).toHaveBeenCalledWith({
-        number: '411',
-        name: 'Captain Moneybags',
-      });
-    });
-
-    it('should go to next page if plan is free, no-op', async () => {
-      await instance.onSubmit({ planpicker: { isFree: true } });
-      expect(instance.props.history.push).toHaveBeenCalledWith('/dashboard');
-      expect(instance.props.billingCreate).not.toHaveBeenCalled();
-    });
-
-    it('should create billing record on submission', async () => {
-      const values = { planpicker: { isFree: false }, key: 'value', card: 'card info' };
-      await instance.onSubmit(values);
-      expect(instance.props.billingCreate).toHaveBeenCalledWith(values);
-      expect(instance.props.history.push).toHaveBeenCalledWith('/dashboard');
-      expect(instance.props.showAlert).toHaveBeenCalledWith({
-        type: 'success',
-        message: 'Added your plan',
-      });
-    });
-
-    it('should call verify of promo code before rest of submission flow if available', async () => {
-      const values = {
-        planpicker: { isFree: false, billingId: 'test-id' },
-        key: 'value',
-        card: 'card info',
-        promoCode: 'test-promo-code',
-      };
-      wrapper.setProps({
-        billing: { ...props.billing, selectedPromo: { promoCode: 'test-promo-code' } },
-        handleSubmit: jest.fn(fn => fn(values)),
-      });
-      await wrapper.simulate('form', 'submit');
-      expect(props.verifyPromoCode).toHaveBeenCalledWith({
-        promoCode: 'test-promo-code',
-        billingId: 'test-id',
-        meta: { promoCode: 'test-promo-code', showErrorAlert: false },
-      });
-      expect(props.billingCreate).toHaveBeenCalledWith({
-        ...values,
-        discountId: 'test-discount',
-      });
-      expect(props.history.push).toHaveBeenCalledWith('/dashboard');
-      expect(props.showAlert).toHaveBeenCalledWith({
-        type: 'success',
-        message: 'Added your plan',
-      });
-    });
-
-    it('should throw error if promo code validation fails', async () => {
-      expect.assertions(2);
-      const values = {
-        planpicker: { isFree: false, billingId: 'test-id' },
-        key: 'value',
-        card: 'card info',
-      };
-      const verifyPromoCode = jest.fn(() => Promise.reject());
-      wrapper.setProps({
-        billing: { ...props.billing, selectedPromo: { promoCode: 'test-promo-code' } },
-        verifyPromoCode,
-      });
-      return instance.onSubmit(values).catch(() => {
-        expect(props.billingCreate).not.toHaveBeenCalled();
-        expect(props.history.push).not.toHaveBeenCalled();
-      });
-    });
-
-    it('should throw error if billing creation fails', async () => {
-      expect.assertions(2);
-      const values = {
-        planpicker: { isFree: false, billingId: 'test-id' },
-        key: 'value',
-        card: 'card info',
-      };
-      const billingCreate = jest.fn(() => Promise.reject());
-      wrapper.setProps({
-        billing: { ...props.billing, selectedPromo: { promoCode: 'test-promo-code' } },
-        billingCreate,
-      });
-      return instance.onSubmit(values).catch(() => {
-        expect(props.billingCreate).not.toHaveBeenCalled();
-        expect(props.history.push).not.toHaveBeenCalled();
-      });
-    });
   });
 });
