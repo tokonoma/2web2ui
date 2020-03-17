dcb76de1091bdb5ad87e6df8c127ff9b7232d34b
Brian Kemper <brian.kemper@sparkpost.com>
2020-03-17 00:43:33 -0400

    FE-872 Index entry for Error

--- src/components/matchbox/index.js
+++ src/components/matchbox/index.js
@@ -1,5 +1,6 @@
 export { default as Banner } from './Banner';
 export { default as Box } from './Box';
+export { default as Error } from './Error';
 export { default as Expandable } from './Expandable';
 export { default as Inline } from './Inline';
 export { default as Label } from './Label';
