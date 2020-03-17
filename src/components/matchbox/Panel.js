faade3eb7665541a84a8475bc5356a48dbc3d7b3
Kevin Chu <kevinchu1337@gmail.com>
2020-03-17 10:37:07 -0400

    FE-881: Add default bottom padding and add footer

--- src/components/matchbox/Panel.js
+++ src/components/matchbox/Panel.js
@@ -14,7 +14,7 @@ const Panel = props => {
   if (!isHibanaEnabled) {
     return <OGPanel {...omitSystemProps(props)} />;
   }
-  return <HibanaPanel {...props} />;
+  return <HibanaPanel mb={'400'} {...props} />;
 };
 
 const Section = props => {
@@ -26,6 +26,19 @@ const Section = props => {
   return <HibanaPanel.Section {...props} />;
 };
 
+const Footer = props => {
+  const [state] = useHibana();
+  const { isHibanaEnabled } = state;
+  if (!isHibanaEnabled) {
+    return <OGPanel.Footer {...omitSystemProps(props)} />;
+  }
+  return <HibanaPanel.Footer {...props} />;
+};
+
+Section.displayName = 'Panel.Section';
+Footer.displayName = 'Panel.Footer';
+
 Panel.Section = Section;
+Panel.Footer = Footer;
 
 export default Panel;
