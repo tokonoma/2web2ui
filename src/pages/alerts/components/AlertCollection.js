36f0224b96fba67d89f0d2ff54619eecda950132
Brian Kemper <brian.kemper@sparkpost.com>
2020-03-20 15:46:33 -0400

    FE-881 Fixed imports for AlertCollection

--- src/pages/alerts/components/AlertCollection.js
+++ src/pages/alerts/components/AlertCollection.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Table, Tooltip, ScreenReaderOnly } from '@sparkpost/matchbox';
 import { TableCollection, PageLink, DisplayDate } from 'src/components';
-import { Button, Panel, Tag } from 'src/components/matchbox';
+import { NewCollectionBody } from 'src/components/collection';
+import { Button, Tag } from 'src/components/matchbox';
 import AlertToggle from './AlertToggle';
 import { Delete } from '@sparkpost/matchbox-icons';
 import { METRICS } from '../constants/formConstants';
