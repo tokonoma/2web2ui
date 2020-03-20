d1ff7b0e004346fc3dcd6521976561a7282c2ce2
Brian Kemper <brian.kemper@sparkpost.com>
2020-03-20 15:45:04 -0400

    FE-881 Hibana support for PanelLoading's minHeight prop

--- src/components/panelLoading/PanelLoading.js
+++ src/components/panelLoading/PanelLoading.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Panel } from 'src/components/matchbox';
+import { Box, Panel } from 'src/components/matchbox';
 import { Loading } from 'src/components/loading/Loading';
 import styles from './PanelLoading.module.scss';
 
-const PanelLoading = (props) => {
+const PanelLoading = props => {
   const { minHeight, accent } = props;
 
   return (
-    <Panel
-      className={styles.Loading}
-      style={{ minHeight }}
-      accent={accent}
-      data-id="panel-loading"
-    >
-      <Loading />
+    <Panel className={styles.Loading} style={{ minHeight }} accent={accent} data-id="panel-loading">
+      <Box minHeight={minHeight}>
+        <Loading />
+      </Box>
     </Panel>
   );
 };
 
 PanelLoading.propTypes = {
   minHeight: PropTypes.string,
-  accent: PropTypes.bool
+  accent: PropTypes.bool,
 };
 
 PanelLoading.defaultProps = {
   minHeight: '400px',
-  accent: false
+  accent: false,
 };
 
 export default PanelLoading;
