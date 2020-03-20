d6ea5c9e1f533935902ab0d272c5ea476b987bf2
Brian Kemper <brian.kemper@sparkpost.com>
2020-03-20 18:18:57 -0400

    FE-881 Tests for NewCollectionBody to test without filter box and pagination

--- src/components/collection/tests/NewCollectionBody.test.js
+++ src/components/collection/tests/NewCollectionBody.test.js
@@ -3,15 +3,22 @@ import { shallow } from 'enzyme';
 import NewCollectionBody from '../NewCollectionBody';
 
 describe('NewCollectionBody', () => {
-  const subject = (props = {}) => shallow(<NewCollectionBody {...props} />);
+  const subject = (props = {}) =>
+    shallow(<NewCollectionBody {...props} collection="My Collection" heading="My Header" />);
 
-  it('renders table collection', () => {
-    const props = {
-      heading: 'Header',
-      filterBox: '<FilterBox />',
-      collection: '<Collection />',
-      pagination: '<Pagination />',
-    };
-    expect(subject(props)).toMatchSnapshot();
+  it('renders a table collection', () => {
+    const wrapper = subject();
+    expect(wrapper).toHaveTextContent('My Header');
+    expect(wrapper).toHaveTextContent('My Collection');
+  });
+
+  it('renders a table collection with a filterBox', () => {
+    const wrapper = subject({ filterBox: 'My Filterbox' });
+    expect(wrapper).toHaveTextContent('My Filterbox');
+  });
+
+  it('renders a table collection with pagination', () => {
+    const wrapper = subject({ pagination: 'My Pagination' });
+    expect(wrapper).toHaveTextContent('My Pagination');
   });
 });
