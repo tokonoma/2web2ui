76c307f33743eea3fb353e3f471f66040a1d3f32
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-03-20 20:24:29 -0400

    FE-894 - Remove duplicate TestApp import

--- src/pages/templates/components/tests/SendTestEmailButton.test.js
+++ src/pages/templates/components/tests/SendTestEmailButton.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import TestApp from 'src/__testHelpers__/TestApp';
 import useEditorContext from '../../hooks/useEditorContext';
 import SendTestEmailButton from '../SendTestEmailButton';
 import TestApp from 'src/__testHelpers__/TestApp';
