fa8e2f46176254bbf25f3f5c312070f2be397802
Brian Kemper <brian.kemper@sparkpost.com>
2020-03-25 23:48:40 -0400

    FE-898 Added Button support for ExternalLink

--- src/components/links/ExternalLink.js
+++ src/components/links/ExternalLink.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { OpenInNew } from '@sparkpost/matchbox-icons';
-import { UnstyledLink } from 'src/components/matchbox';
+import { Button, UnstyledLink } from 'src/components/matchbox';
 
 const ExternalLink = ({
+  as: Component = UnstyledLink,
   children,
   component: _component, // ignore, won't apply external props correctly if set
   onClick: _onClick, // ignore
   ...props
 }) => (
-  <UnstyledLink {...props} external>
+  <Component {...props} external>
     {children} <OpenInNew size={13} style={{ marginTop: '-0.1em' }} />
-  </UnstyledLink>
+  </Component>
 );
 
 ExternalLink.propTypes = {
+  as: PropTypes.oneOf([Button, UnstyledLink]),
   children: PropTypes.node.isRequired,
   to: PropTypes.string.isRequired,
 };
