da5686259727980ac35feb3107699c445f578b51
Brian Kemper <brian.kemper@sparkpost.com>
2020-03-27 02:29:26 -0400

    Tweaked SignUpTabs to fix tests

--- src/pages/join/components/SignUpTabs.js
+++ src/pages/join/components/SignUpTabs.js
@@ -2,22 +2,27 @@ import React from 'react';
 import { Tabs } from '@sparkpost/matchbox';
 import styles from './SignUpTabs.module.scss';
 
-const SignUpTabs = ({ location, brand, isSPCEU }) => (
-  <div className={styles.Tabs}>
-    <Tabs
-      tabs={[
-        {
-          content: 'SparkPost',
-          to: isSPCEU && `${brand.url}${location.pathname}${location.search}`,
-        },
-        {
-          content: 'SparkPost EU',
-          to: !isSPCEU && `${brand.url}${location.pathname}${location.search}`,
-        },
-      ]}
-      selected={isSPCEU ? 1 : 0}
-    />
-  </div>
-);
+const SignUpTabs = ({ location, brand, isSPCEU }) => {
+  const href = `${brand.url}${location.pathname}${location.search}`;
+  const noop = 'javascript:void(0);';
+
+  return (
+    <div className={styles.Tabs}>
+      <Tabs
+        tabs={[
+          {
+            content: 'SparkPost',
+            to: isSPCEU ? href : noop,
+          },
+          {
+            content: 'SparkPost EU',
+            to: !isSPCEU ? href : noop,
+          },
+        ]}
+        selected={isSPCEU ? 1 : 0}
+      />
+    </div>
+  );
+};
 
 export default SignUpTabs;
