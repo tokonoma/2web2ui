1656ffe2e89cd10f2f46e3f28302621ab87eb74b
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-31 12:54:34 -0400

    [FE-928] Hibana enabled Card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Card.module.scss';
 import classNames from 'classnames';
 import { useHibana } from 'src/context/HibanaContext';
-import { Box } from 'src/components/matchbox';
+import { Box, Text } from 'src/components/matchbox';
 
 export const Card = ({ children, textAlign }) => {
   const [state] = useHibana();
@@ -10,7 +10,7 @@ export const Card = ({ children, textAlign }) => {
   if (!isHibanaEnabled)
     return <div className={classNames(styles.CardContainer, styles[textAlign])}>{children}</div>;
   return (
-    <Box m="400" px="300" py="200" border={400} padding="300">
+    <Box border="400" padding="400" textAlign={textAlign}>
       {children}
     </Box>
   );
@@ -20,8 +20,19 @@ export const CardActions = ({ children }) => <>{children}</>;
 
 export const CardContent = ({ children }) => <div className={styles.CardContent}>{children}</div>;
 
-export const CardTitle = ({ children }) => (
-  <div className={styles.CardTitle} role="heading" aria-level="3">
-    {children}
-  </div>
-);
+export const CardTitle = ({ children }) => {
+  const [state] = useHibana();
+  const { isHibanaEnabled } = state;
+  if (!isHibanaEnabled)
+    return (
+      <div className={styles.CardTitle} role="heading" aria-level="3">
+        {children}
+      </div>
+    );
+
+  return (
+    <Text as="h3" fontSize="400">
+      {children}
+    </Text>
+  );
+};
