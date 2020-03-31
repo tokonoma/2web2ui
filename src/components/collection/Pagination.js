d0521116ba00e37aca117e64207bb4a89482f4dd
Patrick Sison <patrick.sison@sparkpost.com>
2020-03-31 13:10:06 -0400

    FE-880: Pagination component wrapper (#1534)

--- src/components/collection/Pagination.js
+++ src/components/collection/Pagination.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { Pagination } from '@sparkpost/matchbox';
+import { Pagination } from 'src/components/matchbox';
 import styles from './Pagination.module.scss';
 import { DEFAULT_PER_PAGE_BUTTONS } from 'src/constants';
 import PerPageButtons from './PerPageButtons';
 import SaveCSVButton from './SaveCSVButton';
 
-
 class CollectionPagination extends Component {
-
   renderPageButtons() {
     const { data, perPage, currentPage, pageRange, onPageChange } = this.props;
 
@@ -27,22 +25,22 @@ class CollectionPagination extends Component {
 
   render() {
     const { data, perPage, perPageButtons, onPerPageChange, saveCsv, currentPage } = this.props;
-    if (!currentPage) { return null; }
+    if (!currentPage) {
+      return null;
+    }
 
     return (
       <div>
-        <div className={styles.PageButtons}>
-          {this.renderPageButtons()}
-        </div>
+        <div className={styles.PageButtons}>{this.renderPageButtons()}</div>
         <div className={styles.PerPageButtons}>
           <PerPageButtons
             totalCount={data.length}
-            data = {data}
-            perPage= {perPage}
+            data={data}
+            perPage={perPage}
             perPageButtons={perPageButtons}
             onPerPageChange={onPerPageChange}
           />
-          <SaveCSVButton data = {data} saveCsv = {saveCsv}/>
+          <SaveCSVButton data={data} saveCsv={saveCsv} />
         </div>
       </div>
     );
@@ -52,7 +50,7 @@ class CollectionPagination extends Component {
 CollectionPagination.defaultProps = {
   pageRange: 5,
   perPageButtons: DEFAULT_PER_PAGE_BUTTONS,
-  saveCsv: true
+  saveCsv: true,
 };
 
 export default CollectionPagination;
