c81a60257e1e5a231d85531dd009079b6cd7c61c
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-03-31 16:49:31 -0400

    [FE-928] <Callout/>  is hibanaEnabled

--- src/components/callout/Callout.test.js
+++ src/components/callout/Callout.test.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Callout from './Callout';
+import TestApp from 'src/__testHelpers__/TestApp';
 
 describe('Callout', () => {
   it('renders title', () => {
-    const wrapper = shallow(<Callout title="Example" />);
+    const wrapper = mount(
+      <TestApp>
+        <Callout title="Example" />
+      </TestApp>,
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders title and details', () => {
-    const wrapper = shallow(
-      <Callout title="Example">
-        Here is the deal...
-      </Callout>
+    const wrapper = mount(
+      <TestApp>
+        <Callout title="Example">Here is the deal...</Callout>
+      </TestApp>,
     );
 
     expect(wrapper).toMatchSnapshot();
