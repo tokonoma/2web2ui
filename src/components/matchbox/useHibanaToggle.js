24e0dd1a3ef45b3f3d496c6dad5055e6826ee9fa
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-04-02 14:18:33 -0400

    [FE-928] updated useHibanaToggle

--- src/components/matchbox/useHibanaToggle.js
+++ src/components/matchbox/useHibanaToggle.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { useHibana } from 'src/context/HibanaContext';
-import { omitSystemProps, omitDeprecatedProps } from 'src/helpers/hibana';
 
 export default function useHibanaToggle(OGComponent, HibanaComponent) {
   const [state] = useHibana();
   const { isHibanaEnabled } = state;
-  return props => (whiteListedProps = [], deprecatedProps = []) => {
-    return isHibanaEnabled ? (
-      <HibanaComponent {...omitDeprecatedProps(props, whiteListedProps)} />
-    ) : (
-      <OGComponent {...omitSystemProps(props, deprecatedProps)} />
-    );
-  };
+  return props => (isHibanaEnabled ? <HibanaComponent {...props} /> : <OGComponent {...props} />);
 }
