0f972cc3281f99d49b41ac80aa5795563b6f6c5d
Brian Kemper <brian.kemper@sparkpost.com>
2020-04-06 23:10:03 -0400

    FE-885 Removed title prop from RadioGroup

--- src/components/reduxFormWrappers/tests/RadioGroup.test.js
+++ src/components/reduxFormWrappers/tests/RadioGroup.test.js
@@ -4,54 +4,59 @@ import { shallow } from 'enzyme';
 
 import RadioGroup from '../RadioGroup';
 
-cases('RadioGroup', ({ name, ...props }) => { // ignore test name
-  const wrapper = shallow(<RadioGroup {...props} />);
-  expect(wrapper).toMatchSnapshot();
-}, {
-  'renders radio buttons': {
-    input: { name: 'example' },
-    meta: { },
-    options: [
-      { label: 'Option One', value: 'one' },
-      { label: 'Option Two', value: 'two' }
-    ],
-    title: 'Choose one'
+cases(
+  'RadioGroup',
+  ({ name, ...props }) => {
+    // ignore test name
+    const wrapper = shallow(<RadioGroup {...props} />);
+    expect(wrapper).toMatchSnapshot();
   },
-  'renders radio buttons with first option checked': {
-    input: { name: 'example', value: 'one' },
-    meta: { },
-    options: [
-      { label: 'Option One', value: 'one' },
-      { label: 'Option Two', value: 'two' }
-    ],
-    title: 'Choose one'
+  {
+    'renders radio buttons': {
+      input: { name: 'example' },
+      meta: {},
+      options: [
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two' },
+      ],
+      label: 'Choose one',
+    },
+    'renders radio buttons with first option checked': {
+      input: { name: 'example', value: 'one' },
+      meta: {},
+      options: [
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two' },
+      ],
+      label: 'Choose one',
+    },
+    'renders radio buttons with second option disabled': {
+      input: { name: 'example' },
+      meta: {},
+      options: [
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two', disabled: true },
+      ],
+      label: 'Choose one',
+    },
+    'renders error state (top)': {
+      input: { name: 'example' },
+      meta: { touched: true, error: 'error message' },
+      options: [
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two' },
+      ],
+      label: 'Choose one',
+    },
+    'renders error state (bottom)': {
+      input: { name: 'example' },
+      meta: { touched: true, error: 'error message' },
+      options: [
+        { label: 'Option One', value: 'one' },
+        { label: 'Option Two', value: 'two' },
+      ],
+      label: 'Choose one',
+      bottomError: true,
+    },
   },
-  'renders radio buttons with second option disabled': {
-    input: { name: 'example' },
-    meta: { },
-    options: [
-      { label: 'Option One', value: 'one' },
-      { label: 'Option Two', value: 'two', disabled: true }
-    ],
-    title: 'Choose one'
-  },
-  'renders error state (top)': {
-    input: { name: 'example' },
-    meta: { touched: true, error: 'error message' },
-    options: [
-      { label: 'Option One', value: 'one' },
-      { label: 'Option Two', value: 'two' }
-    ],
-    title: 'Choose one'
-  },
-  'renders error state (bottom)': {
-    input: { name: 'example' },
-    meta: { touched: true, error: 'error message' },
-    options: [
-      { label: 'Option One', value: 'one' },
-      { label: 'Option Two', value: 'two' }
-    ],
-    title: 'Choose one',
-    bottomError: true
-  }
-});
+);
