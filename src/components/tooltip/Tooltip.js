4df1e80fb259d782a1cb476643ef02a7bdc270ad
Brian Kemper <brian.kemper@sparkpost.com>
2020-04-08 17:45:44 -0400

    FE-882 Updated Tooltip to use useUniqueId

--- src/components/tooltip/Tooltip.js
+++ src/components/tooltip/Tooltip.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import { Popover } from 'src/components/matchbox';
+import useUniqueId from 'src/hooks/useUniqueId';
 import style from './Tooltip.module.scss';
 
 // note, Matchbox's Tooltip has static dimensions that prevent us from doing fun things
 // todo, should use Matchbox's Tooltip
 const Tooltip = ({ children, content, initialOpen = false }) => {
   const [isOpen, setOpen] = useState(initialOpen);
+  const uniqueId = useUniqueId('tooltip-');
 
   return (
     <Popover
+      id={uniqueId}
       children={content}
       className={style.Content}
       open={isOpen}
       sectioned={false}
       trigger={
         <span
+          aria-controls={uniqueId}
           onMouseEnter={() => {
             setOpen(true);
           }}
