c3ebab4ea6579094598e01573bba8de0af60c4fc
Greg Friedman <greg.friedman@sparkpost.com>
2020-04-08 13:44:05 -0400

    FE-935 FileFieldWrapper updated for Hibana

--- src/components/reduxFormWrappers/FileFieldWrapper.js
+++ src/components/reduxFormWrappers/FileFieldWrapper.js
@@ -1,92 +1,181 @@
 import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import Dropzone from 'react-dropzone';
-import { Error, Label } from 'src/components/matchbox';
+import { Error, Label, Box, Text } from 'src/components/matchbox';
+import useHibanaToggle from 'src/hooks/useHibanaToggle';
 import { FileUpload } from '@sparkpost/matchbox-icons';
 import { shrinkToFit } from 'src/helpers/string';
 import styles from './FileFieldWrapper.module.scss';
 
-// TODO: Integrate in Matchbox if Dropzone isn't too big of a dependency
-// TODO: Need a clear button
-export default class FileFieldWrapper extends Component {
-  handleCancel = () => {
-    this.props.input.onBlur(); // run validation
+const getSharedHandlers = input => {
+  const handleCancel = () => {
+    input.onBlur(); // run validation
   };
 
   // Always set value to dropped file even if rejected for validate functions to set error
-  handleDrop = (acceptedFiles, rejectedFiles) => {
+  const handleDrop = (acceptedFiles, rejectedFiles) => {
     const files = [...acceptedFiles, ...rejectedFiles];
-    this.props.input.onChange(files[0]);
-    this.props.input.onBlur(); // run validation
+    input.onChange(files[0]);
+    input.onBlur(); // run validation
   };
 
-  handleOpen = () => {
-    this.dropzoneRef.open();
-  };
+  return { handleDrop, handleCancel };
+};
 
-  setDropzoneRef = ref => {
-    this.dropzoneRef = ref;
-  };
-
-  render() {
-    const {
-      disabled,
-      fileType,
-      fileTypes = [],
-      helpText,
-      input,
-      label,
-      meta,
-      required,
-      style = {},
-      labelHidden,
-      placeholder = 'Drag a file here, or click to browse',
-    } = this.props;
-    const filename = _.get(input, 'value.name');
-    let acceptedTypes = fileType ? `.${fileType}` : '';
-    if (fileTypes.length) {
-      acceptedTypes = fileTypes;
-    }
-
-    return (
-      <fieldset className={styles.Field}>
-        <Label id={input.id}>
-          {!labelHidden && label}
-          {!labelHidden && required && ' *'}
-          {meta.touched && meta.error ? (
-            <span>
-              {' '}
-              <Error error={meta.error} wrapper="span" />
-            </span>
-          ) : null}
-        </Label>
-        <div className={styles.InputWrapper}>
-          <Dropzone
-            accept={acceptedTypes}
-            activeClassName={styles.DropzoneActive}
-            className={styles.Dropzone}
-            style={style}
-            disabledClassName={styles.DropzoneDisabled}
-            disabled={disabled}
-            id={input.id}
-            multiple={false}
-            name={input.name}
-            onDrop={this.handleDrop}
-            onFileDialogCancel={this.handleCancel}
-            ref={this.setDropzoneRef}
-          >
-            {filename && !meta.error ? (
-              <span>{shrinkToFit(filename, 50)}</span>
-            ) : (
-              <span className={styles.Placeholder}>
-                <FileUpload />
-                <span>{placeholder}</span>
-              </span>
-            )}
-          </Dropzone>
-        </div>
-        {helpText ? <div className={styles.Help}>{helpText}</div> : null}
-      </fieldset>
-    );
+// TODO: Integrate in Matchbox if Dropzone isn't too big of a dependency
+// TODO: Need a clear button
+function OGFileFieldWrapper({
+  disabled,
+  fileType,
+  fileTypes = [],
+  helpText,
+  input,
+  label,
+  meta,
+  required,
+  style = {},
+  labelHidden,
+  placeholder = 'Drag a file here, or click to browse',
+}) {
+  const filename = _.get(input, 'value.name');
+  let acceptedTypes = fileType ? `.${fileType}` : '';
+  if (fileTypes.length) {
+    acceptedTypes = fileTypes;
   }
+
+  const { handleCancel, handleDrop } = getSharedHandlers(input);
+
+  return (
+    <fieldset className={styles.Field}>
+      <Label id={input.id}>
+        {!labelHidden && label}
+        {!labelHidden && required && ' *'}
+        {meta.touched && meta.error ? (
+          <span>
+            {' '}
+            <Error error={meta.error} wrapper="span" />
+          </span>
+        ) : null}
+      </Label>
+      <div className={styles.InputWrapper}>
+        <Dropzone
+          accept={acceptedTypes}
+          activeClassName={styles.DropzoneActive}
+          className={styles.Dropzone}
+          style={style}
+          disabledClassName={styles.DropzoneDisabled}
+          disabled={disabled}
+          id={input.id}
+          multiple={false}
+          name={input.name}
+          onDrop={handleDrop}
+          onFileDialogCancel={handleCancel}
+        >
+          {filename && !meta.error ? (
+            <span>{shrinkToFit(filename, 50)}</span>
+          ) : (
+            <span className={styles.Placeholder}>
+              <FileUpload />
+              <span>{placeholder}</span>
+            </span>
+          )}
+        </Dropzone>
+      </div>
+      {helpText ? <div className={styles.Help}>{helpText}</div> : null}
+    </fieldset>
+  );
+}
+
+function HibanaFileFieldWrapper({
+  disabled,
+  fileType,
+  fileTypes = [],
+  helpText,
+  input,
+  label,
+  meta,
+  required,
+  style = {},
+  labelHidden,
+}) {
+  const filename = _.get(input, 'value.name');
+  let acceptedTypes = fileType ? `.${fileType}` : '';
+  if (fileTypes.length) {
+    acceptedTypes = fileTypes;
+  }
+
+  const { handleCancel, handleDrop } = getSharedHandlers(input);
+
+  const getLabel = () => {
+    let l = '';
+    if (!labelHidden) {
+      if (label) {
+        l = label;
+      }
+      if (required) {
+        l += ' *';
+      }
+    }
+    if (meta.touched && meta.error) {
+      l += (
+        <span>
+          {' '}
+          <Error error={meta.error} wrapper="span" />
+        </span>
+      );
+    }
+    return l;
+  };
+
+  return (
+    <Box as="fieldset" border="none" ml="0" mr="0" p="0">
+      <Label id={input.id} label={getLabel()}></Label>
+      <Box display="flex" justifyContent="space-between">
+        <Dropzone
+          accept={acceptedTypes}
+          activeClassName={styles.DropzoneActive}
+          className={styles.Dropzone}
+          style={style}
+          disabledClassName={styles.DropzoneDisabled}
+          disabled={disabled}
+          id={input.id}
+          multiple={false}
+          name={input.name}
+          onDrop={handleDrop}
+          onFileDialogCancel={handleCancel}
+        >
+          <Box py="500">
+            {filename && !meta.error ? (
+              <Text>{shrinkToFit(filename, 50)}</Text>
+            ) : (
+              <Box as="span" display="flex" justifyContent="center">
+                <Text>
+                  <Text as="span" fontWeight="medium" children="Drag and drop" />
+                  <Text as="span" children=" a file, or " />
+                  <Text as="u" fontWeight="medium" color="blue.700" children="select a file" />
+                  <Text as="span" children=" to upload" />
+                </Text>
+              </Box>
+            )}
+          </Box>
+        </Dropzone>
+      </Box>
+      {helpText ? (
+        <Text pt="200" lineHeight="100" fontSize="100" color="gray.700">
+          {helpText}
+        </Text>
+      ) : null}
+    </Box>
+  );
+}
+
+// padding-top: 0.33333rem;
+//     font-size: 0.83333rem;
+//     line-height: 1.2em;
+//     color: #828287;
+//     word-break: normal;
+//     word-wrap: normal;
+
+export default function FileFieldWrapper(props) {
+  return useHibanaToggle(OGFileFieldWrapper, HibanaFileFieldWrapper)(props);
 }
