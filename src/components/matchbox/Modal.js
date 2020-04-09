8471e38c787686dd1b52060cb0ed480295a12287
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-04-09 10:13:55 -0400

    [FE-877] corrected Modal component wrapper

--- src/components/matchbox/Modal.js
+++ src/components/matchbox/Modal.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Modal as OGModal } from '@sparkpost/matchbox';
 import { useHibana } from 'src/context/HibanaContext';
 import { omitSystemProps } from 'src/helpers/hibana';
-import { Modal as MBModal } from 'src/components/matchbox';
-import { Portal } from 'src/components/matchbox';
+import { Modal as MBModal } from '@sparkpost/matchbox';
+import { Modal as HibanaModal } from '@sparkpost/matchbox-hibana';
+import Portal from './Portal';
 
-const HibanaModal = ({ children, ...rest }) => (
+const OGModal = ({ children, ...rest }) => (
   <Portal containerId="modal-portal">
     <MBModal {...rest}>{children}</MBModal>
   </Portal>
 );
 export default function Modal(props) {
   const [{ isHibanaEnabled }] = useHibana();
-  if (isHibanaEnabled) {
-    return <HibanaModal {...props} />;
+  if (!isHibanaEnabled) {
+    return <OGModal {...omitSystemProps(props)} />;
   }
-  return <OGModal {...omitSystemProps(props)} />;
+  return <HibanaModal {...props} />;
 }
