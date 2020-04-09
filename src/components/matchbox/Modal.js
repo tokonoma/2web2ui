1717eedcd7db0c256103e98dcea2a55b3903addb
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-04-09 09:46:35 -0400

    [FE-877] added modal component wrapper

--- src/components/matchbox/Modal.js
+++ src/components/matchbox/Modal.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Modal as OGModal } from '@sparkpost/matchbox';
-import { Modal as HibanaModal } from '@sparkpost/matchbox-hibana';
 import { useHibana } from 'src/context/HibanaContext';
 import { omitSystemProps } from 'src/helpers/hibana';
+import { Modal as MBModal } from 'src/components/matchbox';
+import { Portal } from 'src/components/matchbox';
 
+const HibanaModal = ({ children, ...rest }) => (
+  <Portal containerId="modal-portal">
+    <MBModal {...rest}>{children}</MBModal>
+  </Portal>
+);
 export default function Modal(props) {
   const [{ isHibanaEnabled }] = useHibana();
   if (isHibanaEnabled) {
