e7595a312198c1d76a128d439e914c2ca3b16b59
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-04-13 16:49:02 -0400

    [FE-934] updated styles for CenteredLogo

--- src/components/sparkPost/CenteredLogo.js
+++ src/components/sparkPost/CenteredLogo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SparkPost from './SparkPost';
-import { UnstyledLink } from 'src/components/matchbox';
+import { Box, UnstyledLink } from 'src/components/matchbox';
 import { LINKS } from 'src/constants';
 import styles from './CenteredLogo.module.scss';
 import PartnerLogos from './partners/PartnerLogos';
+import useHibanaToggle from 'src/hooks/useHibanaToggle';
 
-const CenteredLogo = ({ showAwsLogo }) => (
+const OGCenteredLogo = ({ showAwsLogo }) => (
   <div className={styles.CenteredLogo}>
     {/* note, ExternalLink should be used instead of PageLink because the logo is linking to a
           different domain, our corp website, however, ExternalLink doesn't look good with the
@@ -18,4 +19,17 @@ const CenteredLogo = ({ showAwsLogo }) => (
   </div>
 );
 
+const HibanaCenteredLogo = ({ showAwsLogo }) => (
+  <Box textAlign="center" marginBottom="400">
+    {/* note, ExternalLink should be used instead of PageLink because the logo is linking to a
+          different domain, our corp website, however, ExternalLink doesn't look good with the
+          warning icon */}
+    <UnstyledLink to={LINKS.SP_HOME_PAGE} title="SparkPost">
+      <SparkPost.Logo />
+    </UnstyledLink>
+
+    {showAwsLogo && <PartnerLogos.AwsMP height={37} width={222} />}
+  </Box>
+);
+const CenteredLogo = props => useHibanaToggle(OGCenteredLogo, HibanaCenteredLogo)(props);
 export default CenteredLogo;
