f4c9e6e3e0a8677f72ee456ca598b843ab41c861
Brian Kemper <brian.kemper@sparkpost.com>
2020-04-13 13:56:14 -0400

    Revert "Merge branch 'rollback/FE-870'"
    This reverts commit 4ea22623b74b79e1c8c40442053d7bd23880075d, reversing
    changes made to df9d09075211e4a81f5a6c2adf0410ecb58e28cb.

---
+++ src/components/matchbox/ComboBoxTextField.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComboBoxTextField as OGComboBoxTextField } from '@sparkpost/matchbox';
+import { ComboBoxTextField as HibanaComboBoxTextField } from '@sparkpost/matchbox-hibana';
+import { useHibana } from 'src/context/HibanaContext';
+import { omitSystemProps } from 'src/helpers/hibana';
+
+function ComboBoxTextField(props) {
+  const [state] = useHibana();
+  const { isHibanaEnabled } = state;
+
+  if (!isHibanaEnabled) {
+    return <OGComboBoxTextField {...omitSystemProps(props)} />;
+  }
+
+  return <HibanaComboBoxTextField {...props} />;
+}
+
+HibanaComboBoxTextField.displayName = 'HibanaComboBoxTextField';
+
+export default ComboBoxTextField;
