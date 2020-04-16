f99b92b587e8548ad9dd22b72e5e10bcbb1542e3
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-04-16 18:18:33 -0400

    [FE-929] updated Legend component

--- src/components/charts/Legend.js
+++ src/components/charts/Legend.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import styles from './Legend.module.scss';
-import { Tooltip } from 'src/components/matchbox';
+import { Box, Text, Tooltip } from 'src/components/matchbox';
 import cx from 'classnames';
+import useHibanaToggle from 'src/hooks/useHibanaToggle';
 
-const Item = ({ label, fill = 'whitesmoke', tooltipContent, hasBorder }) => {
-  const content = (
+const Item = props => {
+  const { label, fill = 'whitesmoke', tooltipContent, hasBorder } = props;
+  const OGContent = (
     <div className={styles.Item}>
       <span className={cx(styles.Fill, hasBorder && styles.Border)} style={{ background: fill }} />
       <span className={styles.Label}>{label}</span>
     </div>
   );
+
+  const HibanaContent = (
+    <Box display="inline-block" whiteSpace="nowrap">
+      <Box
+        as="span"
+        display="inline-block"
+        verticalAlign="middle"
+        marginRight="10"
+        marginTop={-10}
+        size={12}
+        borderRadius={6}
+        borderStyle={hasBorder ? 'solid' : undefined}
+        borderColor={hasBorder ? '#00000' : undefined}
+        borderWidth={hasBorder ? '100' : undefined}
+      />
+      <Text
+        as="span"
+        display="inline-block"
+        verticalAlign="middle"
+        marginRight="300"
+        fontWeight="400"
+      >
+        {label}
+      </Text>
+    </Box>
+  );
+
+  const content = useHibanaToggle(OGContent, HibanaContent)(props);
 
   if (tooltipContent) {
     return (
