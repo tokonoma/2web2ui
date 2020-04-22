a89de19a6b6136e2bb83a9497b5c04c32ac9a0e5
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-04-22 00:14:34 -0400

    [FE-970] Updates to Left side with ContentText

--- src/pages/abTesting/components/content/SettingsContent.js
+++ src/pages/abTesting/components/content/SettingsContent.js
@@ -1,24 +1,25 @@
 import React, { Fragment } from 'react';
 import { ExternalLink } from 'src/components/links';
+import ContentText from './ContentText';
 
 const SettingsContent = ({ test }) => (
   <div>
     {(test.status === 'draft' || test.status === 'scheduled') && (
       <Fragment>
-        <p>
+        <ContentText fontSize="200" color="gray.600">
           <span>
             You may continue to adjust these settings and template variants while this test is in{' '}
           </span>
           {test.status}
           <span> mode.</span>
-        </p>
+        </ContentText>
 
-        <p>
+        <ContentText fontSize="200" color="gray.600">
           <ExternalLink to="https://www.sparkpost.com/docs/tech-resources/a-b-testing-sparkpost/">
             Learn more about configuring AB tests
           </ExternalLink>
           .
-        </p>
+        </ContentText>
       </Fragment>
     )}
   </div>
