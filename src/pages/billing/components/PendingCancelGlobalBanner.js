c48b502ea0ba780ab46168da4f96b1b8bbdb440b
Patrick Sison <patrick.m.sison@gmail.com>
2020-04-23 10:35:06 -0400

    FE-927: Updated global banner component

--- src/pages/billing/components/PendingCancelGlobalBanner.js
+++ src/pages/billing/components/PendingCancelGlobalBanner.js
@@ -1,55 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { formatDate } from 'src/helpers/date';
 import { Button } from 'src/components/matchbox';
 import { Close, AccessTime } from '@sparkpost/matchbox-icons';
-import styles from './PendingCancelGlobalBanner.module.scss';
+import OGStyles from './PendingCancelGlobalBanner.module.scss';
+import HibanaStyles from './PendingCancelGlobalBannerHibana.module.scss';
+import useHibanaOverride from 'src/hooks/useHibanaOverride';
+
 import { fetch as fetchAccount, renewAccount } from 'src/actions/account';
 import { hideGlobalBanner, showAlert } from 'src/actions/globalAlert';
 
-export class PendingCancelGlobalBanner extends Component {
-  handleClose = () => {
-    const { hideGlobalBanner } = this.props;
+export function PendingCancelGlobalBanner(props) {
+  const {
+    account: { pending_cancellation },
+    style,
+    hideGlobalBanner,
+    renewAccount,
+    fetchAccount,
+    showAlert,
+  } = props;
+  const styles = useHibanaOverride(OGStyles, HibanaStyles);
+
+  const handleClose = () => {
     hideGlobalBanner();
   };
 
-  handleRenewAccount = () => {
-    const { renewAccount, fetchAccount, showAlert } = this.props;
+  const handleRenewAccount = () => {
     return renewAccount().then(() => {
       showAlert({ type: 'success', message: 'Your account will not be cancelled.' });
       return fetchAccount();
     });
   };
 
-  render() {
-    const {
-      account: { pending_cancellation },
-      style,
-    } = this.props;
-
-    if (!pending_cancellation) {
-      return null;
-    }
-
-    return (
-      <div className={styles.banner} style={style}>
-        <span className={styles.CenterContainer}>
-          <AccessTime />
-          <span> Your account will be cancelled on </span>
-          {formatDate(pending_cancellation.effective_date)}
-          <span>, and you will no longer be able to send email or login. Changed your mind? </span>
-          <Button className={styles.RenewButton} flat onClick={this.handleRenewAccount}>
-            Don't Cancel
-          </Button>
-        </span>
-        <span className={styles.RightContainer}>
-          <Button className={styles.Close} flat onClick={this.handleClose}>
-            <Close />
-          </Button>
-        </span>
-      </div>
-    );
+  if (!pending_cancellation) {
+    return null;
   }
+
+  return (
+    <div className={styles.banner} style={style}>
+      <span className={styles.CenterContainer}>
+        <AccessTime />
+        <span> Your account will be cancelled on </span>
+        {formatDate(pending_cancellation.effective_date)}
+        <span>, and you will no longer be able to send email or login. Changed your mind? </span>
+        <Button className={styles.RenewButton} flat onClick={handleRenewAccount}>
+          Don't Cancel
+        </Button>
+      </span>
+      <span className={styles.RightContainer}>
+        <Button className={styles.Close} flat onClick={handleClose}>
+          <Close />
+        </Button>
+      </span>
+    </div>
+  );
 }
 
 export default connect(({ account }) => ({ account }), {
--- src/pages/billing/components/tests/PendingCancelGlobalBanner.test.js
+++ src/pages/billing/components/tests/PendingCancelGlobalBanner.test.js
@@ -1,43 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { PendingCancelGlobalBanner } from '../PendingCancelGlobalBanner';
-
+import TestApp from 'src/__testHelpers__/TestApp';
 
 describe('PendingCancelGlobalBanner', () => {
-
   const mockFunctions = {
     renewAccount: jest.fn(() => Promise.resolve()),
     showAlert: jest.fn(),
     hideGlobalBanner: jest.fn(),
-    fetchAccount: jest.fn()
+    fetchAccount: jest.fn(),
   };
 
-  const subject = (props = {}) => shallow(
-    <PendingCancelGlobalBanner
-      account={{ pending_cancellation: { effective_date: '2019-09-07T08:00:00.000Z' }}}
-      {...mockFunctions}
-      {...props}
-    />
-  );
-
-  it('should render', () => {
-    expect(subject()).toMatchSnapshot();
-  });
+  const subject = (props = {}) =>
+    render(
+      <TestApp>
+        <PendingCancelGlobalBanner
+          account={{ pending_cancellation: { effective_date: '2019-09-07T08:00:00.000Z' } }}
+          {...mockFunctions}
+          {...props}
+        />
+      </TestApp>,
+    );
 
   it('should call renewAccount, showAlert, and fetchAccount on renewAccount', async () => {
-    const wrapper = subject();
-    await wrapper.find('Button').at(0).simulate('click');
+    const { queryAllByRole } = subject();
+    await userEvent.click(queryAllByRole('button')[0]);
     expect(mockFunctions.renewAccount).toHaveBeenCalled();
     expect(mockFunctions.showAlert).toHaveBeenCalled();
     expect(mockFunctions.fetchAccount).toHaveBeenCalled();
   });
 
   it('should call hideGlobalBanner on clicking close', () => {
-    const wrapper = subject();
-    wrapper.find('Button').at(1).simulate('click');
+    const { queryAllByRole } = subject();
+    userEvent.click(queryAllByRole('button')[1]);
     expect(mockFunctions.hideGlobalBanner).toHaveBeenCalled();
   });
-
-
-
 });
