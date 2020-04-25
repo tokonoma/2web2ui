f8658e933f89bb8e43edf4bee746b0ae874a767a
Brian Kemper <brian.kemper@sparkpost.com>
2020-04-24 23:12:40 -0400

    FE-954 Swap style for Form layout

--- src/components/layout/Form.js
+++ src/components/layout/Form.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import useHibanaOverride from 'src/hooks/useHibanaOverride';
 import ScrollToTop from './ScrollToTop';
-import styles from './Layout.module.scss';
+import OGStyles from './Layout.module.scss';
+import HibanaStyles from './LayoutHibana.module.scss';
 
-const Form = ({ children }) => (
-  <div className={`${styles.wrapper} ${styles.form}`}>
-    <main role="main" className={styles.content}>
-      <div className={styles.container}>
-        { children }
-      </div>
-    </main>
-    <ScrollToTop/>
-  </div>
-);
+const Form = ({ children }) => {
+  const styles = useHibanaOverride(OGStyles, HibanaStyles);
+
+  return (
+    <div className={`${styles.wrapper} ${styles.form}`}>
+      <main role="main" className={styles.content}>
+        <div className={styles.container}>{children}</div>
+      </main>
+      <ScrollToTop />
+    </div>
+  );
+};
 
 export default Form;
--- src/components/layout/tests/Form.test.js
+++ src/components/layout/tests/Form.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Form from '../Form';
 import { shallow } from 'enzyme';
 
+jest.mock('src/hooks/useHibanaOverride', () => style => style);
+
 describe('Component: Form Layout', () => {
-
   it('should render correctly', () => {
-    expect(shallow(<Form><h1>My cool af children</h1></Form>)).toMatchSnapshot();
+    expect(
+      shallow(
+        <Form>
+          <h1>My cool af children</h1>
+        </Form>,
+      ),
+    ).toMatchSnapshot();
   });
-
 });
