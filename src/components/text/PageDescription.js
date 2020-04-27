3ff386db3ae893008b8eb0cdc17a9d48c9b667ad
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-04-27 13:53:26 -0400

    FE-976 - incorporate PageDescription in to incidents page

--- src/components/text/PageDescription.js
+++ src/components/text/PageDescription.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Text } from 'src/components/matchbox';
 import useHibanaToggle from 'src/hooks/useHibanaToggle';
+import styles from './PageDescription.module.scss';
 
 export function OGPageDescription(props) {
   const { children, className } = props;
 
   return (
-    <p className={className} data-id={props['data-id']}>
+    <p className={classNames(styles.PageDescription, className)} data-id={props['data-id']}>
       {children}
     </p>
   );
 }
 
 export function HibanaPageDescription(props) {
-  const { children, className, maxWidth } = props;
+  const { children, className, maxWidth, mb = '500' } = props;
 
   return (
     <Text
       color="gray.700"
       fontSize="300"
       lineHeight="300"
+      mb={mb}
       maxWidth={maxWidth}
-      className={className}
+      className={classNames(styles.PageDescription, className)}
       data-id={props['data-id']}
     >
       {children}
@@ -38,5 +41,7 @@ PageDescription.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   maxWidth: PropTypes.string,
+  mb: PropTypes.string,
+  marginBottom: PropTypes.string,
   'data-id': PropTypes.string,
 };
