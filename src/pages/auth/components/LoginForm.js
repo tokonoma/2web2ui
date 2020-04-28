b9c929c4e665ed41acfd763f9786c870e6f531e0
Brian Kemper <brian.kemper@sparkpost.com>
2020-04-28 16:25:28 -0400

    FE-954 Removed inline style from LoginForm

--- src/pages/auth/components/LoginForm.js
+++ src/pages/auth/components/LoginForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { TextFieldWrapper, CheckboxWrapper } from 'src/components';
+import { BottomPad } from 'src/components/hibana';
 import { PageLink } from 'src/components/links';
 import { Box, Button, Error, Stack } from 'src/components/matchbox';
 import { FORMS } from 'src/constants';
@@ -9,47 +10,42 @@ import { required } from 'src/helpers/validation';
 import { trimWhitespaces } from 'src/helpers/string';
 
 export const LoginForm = ({ loginPending, loginError, handleSubmit }) => (
-  <React.Fragment>
-    {loginError && (
-      <div style={{ marginBottom: '12px' }}>
-        <Error error={loginError} />
-      </div>
-    )}
-    <form onSubmit={handleSubmit}>
-      <Stack>
-        <Field
-          autoFocus
-          name="username"
-          id="username"
-          label="Email or Username"
-          normalize={trimWhitespaces}
-          component={TextFieldWrapper}
-          validate={required}
-        />
-
-        <Field
-          type="password"
-          name="password"
-          id="password"
-          label="Password"
-          component={TextFieldWrapper}
-          helpText={<PageLink to="/forgot-password">Forgot your password?</PageLink>}
-        />
-        <Field
-          name="rememberMe"
-          id="rememberMe"
-          label="Keep me logged in"
-          component={CheckboxWrapper}
-        />
-
-        <Box>
-          <Button variant="primary" submit disabled={loginPending} data-id="button-log-in">
-            {loginPending ? 'Logging In' : 'Log In'}
-          </Button>
-        </Box>
-      </Stack>
-    </form>
-  </React.Fragment>
+  <form onSubmit={handleSubmit}>
+    <Stack>
+      {loginError && (
+        <BottomPad>
+          <Error error={loginError} />
+        </BottomPad>
+      )}
+      <Field
+        name="username"
+        id="username"
+        label="Email or Username"
+        normalize={trimWhitespaces}
+        component={TextFieldWrapper}
+        validate={required}
+      />
+      <Field
+        type="password"
+        name="password"
+        id="password"
+        label="Password"
+        component={TextFieldWrapper}
+        helpText={<PageLink to="/forgot-password">Forgot your password?</PageLink>}
+      />
+      <Field
+        name="rememberMe"
+        id="rememberMe"
+        label="Keep me logged in"
+        component={CheckboxWrapper}
+      />
+      <Box>
+        <Button variant="primary" submit disabled={loginPending} data-id="button-log-in">
+          {loginPending ? 'Logging In' : 'Log In'}
+        </Button>
+      </Box>
+    </Stack>
+  </form>
 );
 
 function mapStateToProps({ auth }) {
