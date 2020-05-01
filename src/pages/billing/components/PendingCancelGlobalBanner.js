bd8c9f9af31b93981164ac12e7d6897a9c77ba5d
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-05-01 15:20:31 -0400

    FE-965 - Clean up styles for PendingCancelGlobalBanner

--- src/pages/billing/components/PendingCancelGlobalBanner.js
+++ src/pages/billing/components/PendingCancelGlobalBanner.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { formatDate } from 'src/helpers/date';
-import { Button, UnstyledLink } from 'src/components/matchbox';
+import { Box, Button, UnstyledLink } from 'src/components/matchbox';
 import { Close, AccessTime } from '@sparkpost/matchbox-icons';
 import OGStyles from './PendingCancelGlobalBanner.module.scss';
 import HibanaStyles from './PendingCancelGlobalBannerHibana.module.scss';
 import useHibanaOverride from 'src/hooks/useHibanaOverride';
-
 import { fetch as fetchAccount, renewAccount } from 'src/actions/account';
 import { hideGlobalBanner, showAlert } from 'src/actions/globalAlert';
 
 export function PendingCancelGlobalBanner(props) {
   const {
     account: { pending_cancellation },
-    style,
     hideGlobalBanner,
     renewAccount,
     fetchAccount,
@@ -25,7 +23,9 @@ export function PendingCancelGlobalBanner(props) {
     hideGlobalBanner();
   };
 
-  const handleRenewAccount = () => {
+  const handleRenewAccount = e => {
+    e.preventDefault(); // Prevents page scrolling from `#` href value
+
     return renewAccount().then(() => {
       showAlert({ type: 'success', message: 'Your account will not be cancelled.' });
       return fetchAccount();
@@ -37,13 +37,23 @@ export function PendingCancelGlobalBanner(props) {
   }
 
   return (
-    <div className={styles.banner} style={style}>
+    <div className={styles.banner}>
       <span className={styles.CenterContainer}>
-        <AccessTime />
-        <span> Your account will be cancelled on </span>
-        {formatDate(pending_cancellation.effective_date)}
-        <span>, and you will no longer be able to send email or login. Changed your mind? </span>
-        <UnstyledLink className={styles.RenewButton} onClick={handleRenewAccount}>
+        <Box display="flex" alignItems="center" mr="200">
+          <AccessTime />
+        </Box>
+
+        <span>
+          Your account will be cancelled on {formatDate(pending_cancellation.effective_date)} and
+          you will no longer be able to send email or login. Changed your mind?
+        </span>
+
+        <UnstyledLink
+          role="button"
+          to="#"
+          className={styles.RenewButton}
+          onClick={handleRenewAccount}
+        >
           Don't Cancel
         </UnstyledLink>
       </span>
