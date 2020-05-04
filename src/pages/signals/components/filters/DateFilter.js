1ab64975e79d243ad07f6fd93074d7b4c39917db
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-05-04 17:56:12 -0400

    [FE-958] Label Added to DateFilter

--- src/pages/signals/components/filters/DateFilter.js
+++ src/pages/signals/components/filters/DateFilter.js
@@ -4,43 +4,54 @@ import { getDates } from 'src/helpers/signals';
 import DatePicker from 'src/components/datePicker/DatePicker';
 import withSignalOptions from '../../containers/withSignalOptions';
 import { minDays } from 'src/helpers/validation';
+import { Label } from 'src/components/matchbox';
 import moment from 'moment';
 
-const OPTIONS = [
-  '7days',
-  '14days',
-  '30days',
-  '90days',
-  'custom'
-];
+const OPTIONS = ['7days', '14days', '30days', '90days', 'custom'];
 
-export function DateFilter({ signalOptions, changeSignalOptions, left, now = new Date() }) {
-
+export function DateFilter({
+  signalOptions,
+  changeSignalOptions,
+  left,
+  now = new Date(),
+  label = '',
+}) {
   // Constructs dates from relative range or custom dates
   function handleChange(updates) {
     changeSignalOptions(getDates({ ...updates, now }));
   }
 
   return (
-    <DatePicker
-      now={moment(now).subtract(1, 'day').toDate()}
-      from={signalOptions.from}
-      to={signalOptions.to}
-      relativeDateOptions={OPTIONS}
-      relativeRange={signalOptions.relativeRange}
-      onChange={handleChange}
-      dateFieldFormat={FORMATS.DATE}
-      datePickerProps={{
-        disabledDays: {
-          after: moment(now).subtract(1, 'day').toDate(),
-          before: moment(now).subtract(91, 'day').toDate()
-        },
-        fromMonth: moment(now).subtract(91, 'day').toDate()
-      }}
-      hideManualEntry
-      left={left}
-      validate={minDays(7)}
-    />
+    <>
+      <Label label={label} />
+      <DatePicker
+        now={moment(now)
+          .subtract(1, 'day')
+          .toDate()}
+        from={signalOptions.from}
+        to={signalOptions.to}
+        relativeDateOptions={OPTIONS}
+        relativeRange={signalOptions.relativeRange}
+        onChange={handleChange}
+        dateFieldFormat={FORMATS.DATE}
+        datePickerProps={{
+          disabledDays: {
+            after: moment(now)
+              .subtract(1, 'day')
+              .toDate(),
+            before: moment(now)
+              .subtract(91, 'day')
+              .toDate(),
+          },
+          fromMonth: moment(now)
+            .subtract(91, 'day')
+            .toDate(),
+        }}
+        hideManualEntry
+        left={left}
+        validate={minDays(7)}
+      />
+    </>
   );
 }
 
