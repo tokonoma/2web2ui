4e7e8a21ff5a91304e3807406282bfd2da4a04dc
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-05-11 19:57:53 -0400

    FE-958 - Clean up filter styles, fix tooltip styles and incorporate unique ID

--- src/pages/signals/components/dataCells/WoWHeaderCell.js
+++ src/pages/signals/components/dataCells/WoWHeaderCell.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { InfoOutline } from '@sparkpost/matchbox-icons';
 import { Tooltip } from 'src/components/matchbox';
+import useUniqueId from 'src/hooks/useUniqueId';
 import useHibanaOverride from 'src/hooks/useHibanaOverride';
 import OGStyles from './DataCell.module.scss';
 import hibanaStyles from './DataCellHibana.module.scss';
 
 const WoWHeaderCell = () => {
   const styles = useHibanaOverride(OGStyles, hibanaStyles);
+  const id = useUniqueId('wow-header-cell');
 
   return (
-    <Tooltip content="Week Over Week Change" dark>
+    <Tooltip id={id} content="Week Over Week Change" dark>
       WoW <InfoOutline size={15} className={styles.InfoIcon} />
     </Tooltip>
   );
