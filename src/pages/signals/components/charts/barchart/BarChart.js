131e1e39a1871e5cc821ded0d299e9eac0f1d81f
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-05-12 14:00:05 -0400

    FE-958 - Fix coloring, sizing issues with health score charts

--- src/pages/signals/components/charts/barchart/BarChart.js
+++ src/pages/signals/components/charts/barchart/BarChart.js
@@ -1,7 +1,18 @@
-/*eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ResponsiveContainer, ComposedChart, ReferenceLine, Bar, Tooltip, XAxis, YAxis, CartesianGrid, Rectangle } from 'recharts';
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  ReferenceLine,
+  Bar,
+  Tooltip,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Rectangle,
+} from 'recharts';
+import { tokens } from '@sparkpost/design-tokens-hibana';
+import { useHibana } from 'src/context/HibanaContext';
 import TooltipWrapper from 'src/components/charts/Tooltip';
 import './BarChart.scss';
 import _ from 'lodash';
@@ -21,22 +32,22 @@ import healthScoreThresholds from '../../../constants/healthScoreThresholds';
  *   xAxisProps={{ interval: 88, tickFormatter: (tick) => moment(tick).format('M/D') }}
  * />
  */
-class BarChart extends Component {
-
-  getSelectedStyle = _.memoize((key, isActiveDate, ranking, activeFill) =>
-    {
-      const selectedFill = (key === 'health_score' && ranking) ? healthScoreThresholds[ranking].barColor : activeFill;
-      if (key === 'health_score' || key === 'injections' || key === 'weight_value')  {
-       return isActiveDate ? {fill: selectedFill} : {};
+class BarChartClassComponent extends Component {
+  getSelectedStyle = _.memoize(
+    (key, isActiveDate, ranking, activeFill) => {
+      const selectedFill =
+        key === 'health_score' && ranking ? healthScoreThresholds[ranking].barColor : activeFill;
+      if (key === 'health_score' || key === 'injections' || key === 'weight_value') {
+        return isActiveDate ? { fill: selectedFill } : {};
       }
-      return isActiveDate ? {opacity : 1} : {opacity : .5}
+      return isActiveDate ? { opacity: 1 } : { opacity: 0.5 };
     },
-    (key, isActiveDate, ranking, activeFill) =>`${key}${isActiveDate}${ranking}${activeFill}`
+    (key, isActiveDate, ranking, activeFill) => `${key}${isActiveDate}${ranking}${activeFill}`,
   );
 
   renderBar = ({ key, selected, hovered, fill, activeFill, shouldHighlightSelected }) => (
     <Bar
-      stackId='stack'
+      stackId="stack"
       key={key}
       dataKey={key}
       onClick={this.props.onClick}
@@ -45,26 +56,44 @@ class BarChart extends Component {
       isAnimationActive={false}
       minPointSize={1}
       activeFill={activeFill}
-      cursor='pointer'
-      shape={(props) => {
-        const isActiveDate= (props.date === hovered) || (shouldHighlightSelected && props.date === selected);
+      cursor="pointer"
+      shape={props => {
+        const isActiveDate =
+          props.date === hovered || (shouldHighlightSelected && props.date === selected);
         const selectedStyle =
-          ((selected && shouldHighlightSelected)|| hovered) ?
-          this.getSelectedStyle(key, isActiveDate, props.ranking, activeFill) :
-          {};
-        return <Rectangle {...props} style={{ transition: '0s' }} {...selectedStyle}/>
+          (selected && shouldHighlightSelected) || hovered
+            ? this.getSelectedStyle(key, isActiveDate, props.ranking, activeFill)
+            : {};
+        return <Rectangle {...props} style={{ transition: '0s' }} {...selectedStyle} />;
       }}
     />
-  )
+  );
 
   renderBars = () => {
-    const { yKeys, yKey, selected, hovered, fill, activeFill, shouldHighlightSelected } = this.props;
+    const {
+      yKeys,
+      yKey,
+      selected,
+      hovered,
+      fill,
+      activeFill,
+      shouldHighlightSelected,
+    } = this.props;
 
     if (yKeys) {
-      return yKeys.map((key) => this.renderBar({...key, selected, hovered, shouldHighlightSelected}));
+      return yKeys.map(key =>
+        this.renderBar({ ...key, selected, hovered, shouldHighlightSelected }),
+      );
     }
 
-    return this.renderBar({ key: yKey, selected, hovered, fill, activeFill, shouldHighlightSelected });
+    return this.renderBar({
+      key: yKey,
+      selected,
+      hovered,
+      fill,
+      activeFill,
+      shouldHighlightSelected,
+    });
   };
 
   renderBackgrounds = () => {
@@ -72,20 +101,18 @@ class BarChart extends Component {
 
     return (
       <Bar
-        cursor='pointer'
-        dataKey='noKey'
-        stackId='stack'
+        cursor="pointer"
+        dataKey="noKey"
+        stackId="stack"
         isAnimationActive={false}
         onClick={onClick}
         onMouseOver={onMouseOver}
-        shape={
-          ({ payload, background, ...rest }) => (
-            <Rectangle {...rest} {...background} opacity={0} />
-          )
-        }
+        shape={({ payload, background, ...rest }) => (
+          <Rectangle {...rest} {...background} opacity={0} />
+        )}
       />
     );
-  }
+  };
 
   render() {
     const {
@@ -106,22 +133,23 @@ class BarChart extends Component {
       xAxisProps,
       yDomain,
       yAxisProps,
-     } = this.props;
+      gridStroke,
+    } = this.props;
 
     return (
-      <div className='LiftTooltip' onMouseOut={this.props.onMouseOut}>
-        <ResponsiveContainer height={height} width={width} className='SignalsBarChart'>
+      <div className="LiftTooltip" onMouseOut={this.props.onMouseOut}>
+        <ResponsiveContainer height={height} width={width} className="SignalsBarChart">
           <ComposedChart
             barCategoryGap={gap}
             data={timeSeries}
             margin={margin}
-            cursor={(isLink) ? 'pointer' : 'default'}
+            cursor={isLink ? 'pointer' : 'default'}
           >
             {this.renderBackgrounds()}
             <CartesianGrid
               vertical={false}
-              stroke='#e1e1e6'
-              shapeRendering='crispEdges'
+              stroke={gridStroke}
+              shapeRendering="crispEdges"
               {...cartesianGridProps}
             />
             <YAxis
@@ -136,9 +164,9 @@ class BarChart extends Component {
               axisLine={false}
               tickLine={false}
               dataKey={xKey}
-              type='category'
+              type="category"
               padding={{ left: 12, right: 12 }}
-              shapeRendering='crispEdges'
+              shapeRendering="crispEdges"
               {...xAxisProps}
             />
             {!disableHover && hovered && (
@@ -152,36 +180,48 @@ class BarChart extends Component {
               />
             )}
             {this.renderBars()}
-            {xAxisRefLines.length && (
-              _.map(xAxisRefLines, (xAxisRefLine, index) =>
+            {xAxisRefLines.length &&
+              _.map(xAxisRefLines, (xAxisRefLine, index) => (
                 <ReferenceLine
                   key={`x-${index}`}
                   style={{ pointerEvents: 'none' }}
                   x={xAxisRefLine.x}
-                  shapeRendering='crispEdges'
+                  shapeRendering="crispEdges"
                   stroke={xAxisRefLine.stroke}
                   strokeWidth={xAxisRefLine.strokeWidth}
                   label={xAxisRefLine.label}
                 />
-              )
-            )}
-            {yAxisRefLines.length && (
-              _.map(yAxisRefLines, (yAxisRefLine, index) =>
+              ))}
+            {yAxisRefLines.length &&
+              _.map(yAxisRefLines, (yAxisRefLine, index) => (
                 <ReferenceLine
                   key={`y-${index}`}
                   style={{ pointerEvents: 'none' }}
                   y={yAxisRefLine.y}
-                  shapeRendering='crispEdges'
+                  shapeRendering="crispEdges"
                   stroke={yAxisRefLine.stroke}
                   strokeWidth={yAxisRefLine.strokeWidth}
                 />
-              )
-            )}
+              ))}
           </ComposedChart>
         </ResponsiveContainer>
       </div>
     );
   }
+}
+
+function BarChart(props) {
+  const [state] = useHibana();
+  const { isHibanaEnabled } = state;
+
+  return (
+    <BarChartClassComponent
+      {...props}
+      fill={isHibanaEnabled ? tokens.color_blue_500 : '#B3ECEF'}
+      activeFill={isHibanaEnabled ? tokens.color_blue_600 : '#22838A'}
+      gridStroke={isHibanaEnabled ? tokens.color_gray_400 : '#E1E1E6'}
+    />
+  );
 }
 
 BarChart.propTypes = {
@@ -192,12 +232,10 @@ BarChart.propTypes = {
   shouldHighlightSelected: PropTypes.bool,
   tooltipContent: PropTypes.func,
   tooltipWidth: PropTypes.string,
-  yKeys: PropTypes.arrayOf(PropTypes.object)
+  yKeys: PropTypes.arrayOf(PropTypes.object),
 };
 
 BarChart.defaultProps = {
-  activeFill: '#22838A',
-  fill: '#B3ECEF',
   gap: 1,
   height: 250,
   margin: { top: 12, left: 18, right: 0, bottom: 5 },
@@ -207,7 +245,7 @@ BarChart.defaultProps = {
   yAxisRefLines: [],
   xKey: 'date',
   yKey: 'value',
-  yRange: ['auto', 'auto']
+  yRange: ['auto', 'auto'],
 };
 
 export default BarChart;
