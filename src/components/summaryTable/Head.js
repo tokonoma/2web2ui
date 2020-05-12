5c86acdbc5732f9513a409e3486ef12ac0fe2150
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-05-12 19:57:05 -0400

    FE-958 - Fix invalid text-align inline style

--- src/components/summaryTable/Head.js
+++ src/components/summaryTable/Head.js
@@ -37,7 +37,7 @@ function HibanaHead({ columns, onSort, order }) {
     <thead>
       <Table.Row header>
         {columns.map(({ align = 'left', dataKey, label, sortable, width }) => (
-          <Table.HeaderCell style={{ 'text-align': align }} key={`header-${dataKey}`} width={width}>
+          <Table.HeaderCell style={{ textAlign: align }} key={`header-${dataKey}`} width={width}>
             <HeaderLabel
               dataKey={dataKey}
               label={label}
