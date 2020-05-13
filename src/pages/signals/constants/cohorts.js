e53f252303a52d14cd67b31c77a0715e9eac9cd3
Brian Kemper <brian.kemper@sparkpost.com>
2020-05-13 16:23:43 -0400

    FE-958 Setup structure for engagement recency chart

--- src/pages/signals/constants/cohorts.js
+++ src/pages/signals/constants/cohorts.js
@@ -1,32 +1,42 @@
 export default {
-  'new': {
+  new: {
     fill: '#88E2E7',
+    hibanaFill: '#88E2E7',
+    OGFill: '#88E2E7',
     stroke: '#88E2E7',
     label: 'New',
-    description: 'Never engaged, first email in last 7 days'
+    description: 'Never engaged, first email in last 7 days',
   },
-  'uneng': {
+  uneng: {
     fill: '#50CFDA',
+    hibanaFill: '#50CFDA',
+    OGFill: '#50CFDA',
     stroke: '#50CFDA',
     label: 'Never Engaged',
-    description: '365+ days since last engagement'
+    description: '365+ days since last engagement',
   },
   '365d': {
     fill: '#29B9C7',
+    hibanaFill: '#29B9C7',
+    OGFill: '#29B9C7',
     stroke: '#29B9C7',
     label: 'Not Recently Engaged',
-    description: '90-365 days since last engagement'
+    description: '90-365 days since last engagement',
   },
   '90d': {
     fill: '#219EA8',
+    hibanaFill: '#219EA8',
+    OGFill: '#219EA8',
     stroke: '#219EA8',
     label: 'Semi Recently Engaged',
-    description: '14-90 days since last engagement'
+    description: '14-90 days since last engagement',
   },
   '14d': {
     fill: '#1A838B',
+    hibanaFill: '#1A838B',
+    OGFill: '#1A838B',
     stroke: '#1A838B',
     label: 'Recently Engaged',
-    description: '0-14 days since last engagement'
-  }
+    description: '0-14 days since last engagement',
+  },
 };
