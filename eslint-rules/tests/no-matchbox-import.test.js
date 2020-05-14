8f3e1b3b8b2bfa79ae33d1d0ca11ce7f6d60876f
Neal Stanard <nstanard@gmail.com>
2020-05-14 14:04:04 -0500

    FE-1074 - debugging RuleTester/Jest

--- eslint-rules/tests/no-matchbox-import.test.js
+++ eslint-rules/tests/no-matchbox-import.test.js
@@ -1,13 +1,6 @@
-// import rule from '../no-matchbox-import';
-import { describe, it, expect } from 'jest-without-globals';
+import rule from '../no-matchbox-import';
+import { it } from 'jest-without-globals'; // https://stackoverflow.com/questions/41324636/how-to-import-jest
 import { RuleTester } from 'eslint';
-
-// const ruleTester = new RuleTester({
-//     parserOptions: {
-//         ecmaVersion: 7,
-//         sourceType: 'module',
-//     },
-// });
 
 // https://eslint.org/docs/developer-guide/nodejs-api#customizing-ruletester
 RuleTester.describe = function(text, method) {
@@ -19,37 +12,38 @@ RuleTester.it = function(text, method) {
   it(RuleTester.it.title + ': ' + text, method);
 };
 
-describe('testing', () => {
-  it('it should do something', () => {
-    expect('').toBe('wat');
-  });
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+  },
 });
 
-// ruleTester.run('no-matchbox-import', rule, {
-//     valid: [
-//         {
-//             code: "import { OpenInNew } from '@sparkpost/matchbox-icons'",
-//             filename: '2web2ui/src/components/example',
-//         },
-//         {
-//             code: "import { ComboBox as OGComboBox } from '@sparkpost/matchbox'",
-//             filename: '2web2ui/src/components/matchbox',
-//         },
-//         {
-//             code: "import { ComboBox as HibanaComboBox } from '@sparkpost/matchbox-hibana'",
-//             filename: '2web2ui/src/components/matchbox',
-//         },
-//     ],
-//     invalid: [
-//         {
-//             code: "import { ComboBox as OGComboBox } from '@sparkpost/matchbox'",
-//             filename: '2web2ui/src/components/not-in-matchbox-wrapper-directory',
-//             errors: [{ message: 'Do not allow direct matchbox imports.' }],
-//         },
-//         {
-//             code: "import { ComboBox as HibanaComboBox } from '@sparkpost/matchbox-hibana'",
-//             filename: '2web2ui/src/components/not-in-matchbox-wrapper-directory',
-//             errors: [{ message: 'Do not allow direct matchbox imports.' }],
-//         },
-//     ],
-// });
+ruleTester.run('no-matchbox-import', rule, {
+  valid: [
+    {
+      code: "import { OpenInNew } from '@sparkpost/matchbox-icons'",
+      filename: '2web2ui/src/components/example',
+    },
+    {
+      code: "import { ComboBox as OGComboBox } from '@sparkpost/matchbox'",
+      filename: '2web2ui/src/components/matchbox',
+    },
+    {
+      code: "import { ComboBox as HibanaComboBox } from '@sparkpost/matchbox-hibana'",
+      filename: '2web2ui/src/components/matchbox',
+    },
+  ],
+  invalid: [
+    {
+      code: "import { ComboBox as OGComboBox } from '@sparkpost/matchbox'",
+      filename: '2web2ui/src/components/not-in-matchbox-wrapper-directory',
+      errors: [{ message: 'Do not allow direct matchbox imports.' }],
+    },
+    {
+      code: "import { ComboBox as HibanaComboBox } from '@sparkpost/matchbox-hibana'",
+      filename: '2web2ui/src/components/not-in-matchbox-wrapper-directory',
+      errors: [{ message: 'Do not allow direct matchbox imports.' }],
+    },
+  ],
+});
