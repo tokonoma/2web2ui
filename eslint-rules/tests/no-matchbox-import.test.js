ef8aceda4544190c57a3307999a6173e319ca71b
Brian Kemper <brian.kemper@sparkpost.com>
2020-05-14 18:34:04 -0400

    WIP

--- eslint-rules/tests/no-matchbox-import.test.js
+++ eslint-rules/tests/no-matchbox-import.test.js
@@ -1,47 +1,37 @@
 import runner from '../utils/eslint-test-runner';
-import caseList from './cases/no-matchbox-import';
 import rule from '../no-matchbox-import';
 
-// code: node.source.value in the real rule
-// it: custom friendly name for cli output
-
 const caseList = {
-  valid: [
-    {
-      it: 'allows matchbox-icons import from outside the matchbox component folder',
+  valid: {
+    'allows matchbox-icons import from outside the matchbox component folder': {
       code: "import { OpenInNew } from '@sparkpost/matchbox-icons'",
       filename: '2web2ui/src/components/example',
     },
-    {
-      it: 'allows matchbox-icons import from within the matchbox component folder',
+    'allows matchbox-icons import from within the matchbox component folder': {
       code: "import { OpenInNew } from '@sparkpost/matchbox-icons'",
       filename: '2web2ui/src/components/matchbox',
     },
-    {
-      it: 'Allows the matchbox import from inside the matchbox component folder',
+    'allows the matchbox import from inside the matchbox component folder': {
       code: "import { ComboBox as OGComboBox } from '@sparkpost/matchbox'",
       filename: '2web2ui/src/components/matchbox',
     },
-    {
-      it: 'Allows the matchbox-hibana import from inside the matchbox component folder',
+    'allows the matchbox-hibana import from inside the matchbox component folder': {
       code: "import { ComboBox as HibanaComboBox } from '@sparkpost/matchbox-hibana'",
       filename: '2web2ui/src/components/matchbox',
     },
-  ],
-  invalid: [
-    {
-      it: 'Does not allow matchbox import outside the matchbox component folder',
+  },
+  invalid: {
+    'does not allow matchbox import outside the matchbox component folder': {
       code: "import { ComboBox as OGComboBox } from '@sparkpost/matchbox'",
       filename: '2web2ui/src/components/not-in-matchbox-wrapper-directory',
       errors: [{ message: 'Do not allow direct matchbox imports.' }],
     },
-    {
-      it: 'Does not allow matchbox-hibana import outside the matchbox component folder',
+    'does not allow matchbox-hibana import outside the matchbox component folder': {
       code: "import { ComboBox as HibanaComboBox } from '@sparkpost/matchbox-hibana'",
       filename: '2web2ui/src/components/not-in-matchbox-wrapper-directory',
       errors: [{ message: 'Do not allow direct matchbox imports.' }],
     },
-  ],
+  },
 };
 
 runner('no-matchbox-import', rule, caseList);
--- eslint-rules/utils/eslint-test-runner.js
+++ eslint-rules/utils/eslint-test-runner.js
@@ -1,46 +1,79 @@
-const { RuleTester } = require('eslint');
+import { Linter } from 'eslint';
 
-const mapCodeToIt = function(cases) {
-  let titleByCode = {};
+const runner = (title, rule, { validCases = {}, invalidCases = {} } = {}) => {
+  describe(title, () => {
+    const linter = new Linter();
 
-  cases['valid'] = cases['valid'].map(({ it, ...item }) => {
-    titleByCode[item.code] = it;
-    return item;
-  });
+    linter.defineRules({ [title]: rule });
 
-  cases['invalid'] = cases['invalid'].map(({ it, ...item }) => {
-    titleByCode[item.code] = it;
-    return item;
-  });
+    Object.keys(validCases).forEach(description => {
+      it(description, () => {
+        const result = linter.verify(
+          validCases[description].code,
+          {
+            rules: { [title]: 'error' },
+            parserOptions: {
+              ecmaVersion: 7,
+              sourceType: 'module',
+            },
+          },
+          {
+            filename: '2web2ui/src/components',
+          },
+        );
 
-  return [cases, titleByCode];
-};
-
-const runner = (title, rule, originalCases) => {
-  const [cases, titleByCode] = mapCodeToIt(originalCases);
-
-  // wire up to Jest expect to assert
-  RuleTester.describe = describe;
-  RuleTester.it = (title, callback) => {
-    // ToDo: Fix map so it doesn't override when 'code' property is the same
-    // eslint-disable-next-line no-unused-vars
-    const itText = titleByCode[title];
-    it('', () => {
-      // ToDo: Catch the error and make it friendly
-      expect(callback()).toBeUndefined();
+        expect(result.length === 0).toEqual(true);
+      });
     });
-  };
-
-  const tester = new RuleTester({
-    parserOptions: {
-      ecmaVersion: 7,
-      sourceType: 'module',
-    },
   });
-
-  return tester.run(title, rule, cases);
 };
 
-// This file is a wrapper for our 2web2ui custom eslint rules. We need to export this into the .test.js files.
-// eslint-disable-next-line jest/no-export
 module.exports = runner;
+
+//
+// const { RuleTester } = require('eslint');
+//
+// const mapCodeToIt = function(cases) {
+//   let titleByCode = {};
+//
+//   cases['valid'] = cases['valid'].map(({ it, ...item }) => {
+//     titleByCode[item.code] = it;
+//     return item;
+//   });
+//
+//   cases['invalid'] = cases['invalid'].map(({ it, ...item }) => {
+//     titleByCode[item.code] = it;
+//     return item;
+//   });
+//
+//   return [cases, titleByCode];
+// };
+//
+// const runner = (title, rule, originalCases) => {
+//   const [cases, titleByCode] = mapCodeToIt(originalCases);
+//
+//   // wire up to Jest expect to assert
+//   RuleTester.describe = describe;
+//   RuleTester.it = (title, callback) => {
+//     // ToDo: Fix map so it doesn't override when 'code' property is the same
+//     // eslint-disable-next-line no-unused-vars
+//     const itText = titleByCode[title];
+//     it('', () => {
+//       // ToDo: Catch the error and make it friendly
+//       expect(callback()).toBeUndefined();
+//     });
+//   };
+//
+//   const tester = new RuleTester({
+//     parserOptions: {
+//       ecmaVersion: 7,
+//       sourceType: 'module',
+//     },
+//   });
+//
+//   return tester.run(title, rule, cases);
+// };
+//
+// // This file is a wrapper for our 2web2ui custom eslint rules. We need to export this into the .test.js files.
+// // eslint-disable-next-line jest/no-export
+// module.exports = runner;
