a3bd6a8e3d994cc015a9d87562725dcc6ecd7e04
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-06-03 15:45:39 -0400

    FE-1085 - Summary Report tests skipped for Hibana tests runs

--- cypress/tests/integration/signals-analytics/summaryReport.spec.js
+++ cypress/tests/integration/signals-analytics/summaryReport.spec.js
@@ -40,98 +40,92 @@ describe('Summary Report page', () => {
     });
   });
 
-  it('handles changing the date range correctly', () => {
-    const timestamp = 1580392800000; //01/30/2020 @ 2:00pm (UTC)
-    const momentDateTime = Cypress.moment(timestamp);
-    const now = momentDateTime.local().format('MMM Do YYYY h:mma');
-    const getDatePickerText = startDateTime => `${startDateTime} – ${now}`; //Use this dash: '–' ¯\_(ツ)_/¯
-    cy.clock(timestamp);
-    cy.visit(PAGE_URL);
+  // Skip these tests since the feature is entirely different for Hibana.
+  // A separate spec file would probably do the trick for that feature.
+  if (Cypress.env('DEFAULT_TO_HIBANA') !== true) {
+    it('handles changing the date range correctly', () => {
+      const timestamp = 1580392800000; //01/30/2020 @ 2:00pm (UTC)
+      const momentDateTime = Cypress.moment(timestamp);
+      const now = momentDateTime.local().format('MMM Do YYYY h:mma');
+      const getDatePickerText = startDateTime => `${startDateTime} – ${now}`; //Use this dash: '–' ¯\_(ツ)_/¯
+      cy.clock(timestamp);
+      cy.visit(PAGE_URL);
 
-    const dayAgo = momentDateTime
-      .subtract(1, 'day')
-      .local()
-      .format('MMM Do YYYY h:mma');
-    cy.url().should('not.include', 'range=7days');
-    cy.findByDataId('report-options').within(() => {
-      cy.findByLabelText('Narrow Date Range').should('have.value', getDatePickerText(dayAgo));
-      cy.get('select').select('7days');
-    });
-
-    const sevenDaysAgo = momentDateTime
-      .subtract(6, 'day')
-      .local()
-      .startOf('day')
-      .format('MMM Do YYYY h:mma');
-    cy.url().should('include', 'range=7days');
-    cy.findByDataId('report-options').within(() => {
-      cy.findByLabelText('Narrow Date Range').should('have.value', getDatePickerText(sevenDaysAgo));
-    });
-  });
-
-  it('handles applying filters correctly', () => {
-    cy.visit(PAGE_URL);
-    cy.findByDataId('report-options').within(() => {
-      cy.findAllByLabelText('Reports Filter Typeahead')
-        .first()
-        .type('sparkpost');
-      cy.wait('@getMetricsFilterOptions');
-      cy.findByDataId('report-filters-dropdown').within(() => {
-        cy.findByText('sparkpost-test')
-          .should('be.visible')
-          .closest('a')
-          .click({ force: true });
-      });
-      cy.findByText('Campaign').should('be.visible');
-      cy.findByText('sparkpost-test').should('be.visible');
-    });
-  });
-
-  it('handles changing metrics correctly', () => {
-    cy.visit(PAGE_URL);
-
-    // Selected filters render in different spots based on Hibana vs. OG
-    if (Cypress.env('DEFAULT_TO_HIBANA') === true) {
+      const dayAgo = momentDateTime
+        .subtract(1, 'day')
+        .local()
+        .format('MMM Do YYYY h:mma');
+      cy.url().should('not.include', 'range=7days');
       cy.findByDataId('report-options').within(() => {
-        cy.findByText('Accepted').should('be.visible');
+        cy.findByLabelText('Narrow Date Range').should('have.value', getDatePickerText(dayAgo));
+        cy.get('select').select('7days');
       });
-    } else {
+
+      const sevenDaysAgo = momentDateTime
+        .subtract(6, 'day')
+        .local()
+        .startOf('day')
+        .format('MMM Do YYYY h:mma');
+      cy.url().should('include', 'range=7days');
+      cy.findByDataId('report-options').within(() => {
+        cy.findByLabelText('Narrow Date Range').should(
+          'have.value',
+          getDatePickerText(sevenDaysAgo),
+        );
+      });
+    });
+
+    it('handles applying filters correctly', () => {
+      cy.visit(PAGE_URL);
+      cy.findByDataId('report-options').within(() => {
+        cy.findAllByLabelText('Reports Filter Typeahead')
+          .first()
+          .type('sparkpost');
+        cy.wait('@getMetricsFilterOptions');
+        cy.findByDataId('report-filters-dropdown').within(() => {
+          cy.findByText('sparkpost-test')
+            .should('be.visible')
+            .closest('a')
+            .click({ force: true });
+        });
+        cy.findByText('Campaign').should('be.visible');
+        cy.findByText('sparkpost-test').should('be.visible');
+      });
+    });
+
+    it('handles changing metrics correctly', () => {
+      cy.visit(PAGE_URL);
+
+      // Selected filters render in different spots based on Hibana vs. OG
       cy.findByDataId('summary-chart').within(() => {
         cy.findByText('Accepted').should('be.visible');
       });
-    }
 
-    cy.findByText('Select Metrics').click();
+      cy.findByText('Select Metrics').click();
 
-    cy.withinModal(() => {
-      cy.findByText('Select up to 5 metrics').should('be.visible');
-      cy.findByLabelText('Accepted').uncheck({ force: true });
-      cy.findByLabelText('Injected').check({ force: true });
-      cy.findByText('Apply Metrics').click();
-    });
-
-    if (Cypress.env('DEFAULT_TO_HIBANA') === true) {
-      cy.findByDataId('report-options').within(() => {
-        cy.queryByText('Accepted').should('not.be.visible');
-        cy.findByText('Injected').should('be.visible');
+      cy.withinModal(() => {
+        cy.findByText('Select up to 5 metrics').should('be.visible');
+        cy.findByLabelText('Accepted').uncheck({ force: true });
+        cy.findByLabelText('Injected').check({ force: true });
+        cy.findByText('Apply Metrics').click();
       });
-    } else {
+
       cy.findByDataId('summary-chart').within(() => {
         cy.queryByText('Accepted').should('not.be.visible');
         cy.findByText('Injected').should('be.visible');
       });
-    }
-  });
-
-  it('Opens share modal correctly', () => {
-    cy.visit(PAGE_URL);
-    cy.findByText('Share').click();
-    cy.findByText('Share this report').should('be.visible');
-    cy.url().then(url => {
-      cy.findByLabelText('Pin dates for this link').should('be.checked');
-      //replace the current range with the word custom since the dates are pinned
-      const editedUrl = url.replace(/range=.*?&/, 'range=custom&');
-      cy.get('[name="copy-field"]').should('have.value', editedUrl);
     });
-  });
+
+    it('Opens share modal correctly', () => {
+      cy.visit(PAGE_URL);
+      cy.findByText('Share').click();
+      cy.findByText('Share this report').should('be.visible');
+      cy.url().then(url => {
+        cy.findByLabelText('Pin dates for this link').should('be.checked');
+        //replace the current range with the word custom since the dates are pinned
+        const editedUrl = url.replace(/range=.*?&/, 'range=custom&');
+        cy.get('[name="copy-field"]').should('have.value', editedUrl);
+      });
+    });
+  }
 });
