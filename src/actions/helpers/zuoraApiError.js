9c31b7a5e0b444b779fb60e6b6046a57039905b5
Brian Kemper <brian.kemper@sparkpost.com>
2020-06-03 11:48:05 -0400

    WIP

---
+++ src/actions/helpers/zuoraApiError.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+
+// TODO: Return an Error object as if we were to create it with class and extends Error
+function ZuoraApiError(error) {
+  const message = _.get(
+    error.response,
+    'data.reasons[0].message',
+    'An error occurred while contacting the billing service',
+  );
+
+  this.name = 'ZuoraApiError';
+  this.stack = error.stack; // must manually assign prototype value
+  this.message = stripTags(message); // some messages include html tags
+
+  // Intentionally assigning additional properties
+  Object.assign(this, error);
+}
+
+ZuoraApiError.prototype = Object.create(Error.prototype);
+
+export default ZuoraApiError;
