8955669268aea052c06feaac8a8f2b49d528ef17
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-06-08 12:57:52 -0400

    [AC-1473] snapshots updated

--- src/actions/tests/scimToken.test.js
+++ src/actions/tests/scimToken.test.js
@@ -2,13 +2,13 @@ import * as scimTokenRequests from '../scimToken';
 jest.mock('src/helpers/axiosInstances');
 jest.mock('../helpers/sparkpostApiRequest', () => jest.fn(a => a));
 
-describe('Action Creator: dataPrivacy', () => {
-  it('submitRTBFRequest should make the appropriate request', () => {
+describe('Action Creator: scimToken', () => {
+  it('generateScimToken should make the appropriate request', () => {
     const thunk = scimTokenRequests.generateScimToken();
     expect(thunk).toMatchSnapshot();
   });
 
-  it('submitOptOutRequest should make the appropriate request', () => {
+  it('listScimToken should make the appropriate request', () => {
     const thunk = scimTokenRequests.listScimToken();
     expect(thunk).toMatchSnapshot();
   });
