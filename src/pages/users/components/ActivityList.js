48ceba469ed3949ec481096127fb475c807859f0
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-06-10 10:54:36 -0400

    hackathon-whatsinstore - Incorporate modal

--- src/pages/users/components/ActivityList.js
+++ src/pages/users/components/ActivityList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   CallMade,
   ChangeHistory,
@@ -6,111 +6,129 @@ import {
   OpenInBrowser,
   Streetview,
 } from '@sparkpost/matchbox-icons';
-import { Text } from 'src/components/matchbox';
+import { Button, Modal, Panel, Text } from 'src/components/matchbox';
 import { Activity } from 'src/components/activity';
 
 export default function ActivityList() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   return (
-    <Activity>
-      <Activity.Date>June 10</Activity.Date>
+    <>
+      <Activity>
+        <Activity.Date>June 10</Activity.Date>
 
-      {/* `click` event */}
-      <Activity.Item>
-        <Activity.Content>
-          <Activity.Avatar>
-            <Mouse />
-          </Activity.Avatar>
+        {/* `click` event */}
+        <Activity.Item>
+          <Activity.Content>
+            <Activity.Avatar>
+              <Mouse />
+            </Activity.Avatar>
 
-          <Text>
-            <strong>nlemmon86</strong> clicked on the <strong>Events</strong> link.
-            <Activity.ViewMore />
-          </Text>
+            <Text>
+              <strong>nlemmon86</strong> clicked on the <strong>Events</strong> link.
+              <Activity.ViewMore onClick={() => setModalOpen(true)} />
+            </Text>
 
-          <Activity.Time>12:22 PM</Activity.Time>
-        </Activity.Content>
-      </Activity.Item>
+            <Activity.Time>12:22 PM</Activity.Time>
+          </Activity.Content>
+        </Activity.Item>
 
-      {/* `request` event */}
-      <Activity.Item>
-        <Activity.Content>
-          <Activity.Avatar>
-            <CallMade />
-          </Activity.Avatar>
+        {/* `request` event */}
+        <Activity.Item>
+          <Activity.Content>
+            <Activity.Avatar>
+              <CallMade />
+            </Activity.Avatar>
 
-          <Text>
-            <strong>nlemmon86</strong> made a request to the <strong>Templates</strong> service.
-            <Activity.ViewMore />
-          </Text>
+            <Text>
+              <strong>nlemmon86</strong> made a request to the <strong>Templates</strong> service.
+              <Activity.ViewMore />
+            </Text>
 
-          <Activity.Time>12:22 PM</Activity.Time>
-        </Activity.Content>
-      </Activity.Item>
+            <Activity.Time>12:22 PM</Activity.Time>
+          </Activity.Content>
+        </Activity.Item>
 
-      {/* `pageview` event */}
-      <Activity.Item>
-        <Activity.Content>
-          <Activity.Avatar>
-            <Streetview />
-          </Activity.Avatar>
+        {/* `pageview` event */}
+        <Activity.Item>
+          <Activity.Content>
+            <Activity.Avatar>
+              <Streetview />
+            </Activity.Avatar>
 
-          <Text>
-            <strong>nlemmon86</strong> viewed the <strong>Events</strong> page.
-            <Activity.ViewMore />
-          </Text>
+            <Text>
+              <strong>nlemmon86</strong> viewed the <strong>Events</strong> page.
+              <Activity.ViewMore />
+            </Text>
 
-          <Activity.Time>12:22 PM</Activity.Time>
-        </Activity.Content>
-      </Activity.Item>
+            <Activity.Time>12:22 PM</Activity.Time>
+          </Activity.Content>
+        </Activity.Item>
 
-      {/* `change` event */}
-      <Activity.Item>
-        <Activity.Content>
-          <Activity.Avatar>
-            <ChangeHistory />
-          </Activity.Avatar>
+        {/* `change` event */}
+        <Activity.Item>
+          <Activity.Content>
+            <Activity.Avatar>
+              <ChangeHistory />
+            </Activity.Avatar>
 
-          <Text>
-            <strong>nlemmon86</strong> changed their <strong>password</strong>.
-            <Activity.ViewMore />
-          </Text>
+            <Text>
+              <strong>nlemmon86</strong> changed their <strong>password</strong>.
+              <Activity.ViewMore />
+            </Text>
 
-          <Activity.Time>12:22 PM</Activity.Time>
-        </Activity.Content>
-      </Activity.Item>
+            <Activity.Time>12:22 PM</Activity.Time>
+          </Activity.Content>
+        </Activity.Item>
 
-      {/* `login` event */}
-      <Activity.Item>
-        <Activity.Content>
-          <Activity.Avatar>
-            <OpenInBrowser />
-          </Activity.Avatar>
+        {/* `login` event */}
+        <Activity.Item>
+          <Activity.Content>
+            <Activity.Avatar>
+              <OpenInBrowser />
+            </Activity.Avatar>
 
-          <Text>
-            <strong>nlemmon86</strong> logged in.
-            <Activity.ViewMore />
-          </Text>
+            <Text>
+              <strong>nlemmon86</strong> logged in.
+              <Activity.ViewMore />
+            </Text>
 
-          <Activity.Time>12:22 PM</Activity.Time>
-        </Activity.Content>
-      </Activity.Item>
+            <Activity.Time>12:22 PM</Activity.Time>
+          </Activity.Content>
+        </Activity.Item>
 
-      <Activity.Date>June 09</Activity.Date>
+        <Activity.Date>June 09</Activity.Date>
 
-      {/* `login` event */}
-      <Activity.Item>
-        <Activity.Content>
-          <Activity.Avatar>
-            <OpenInBrowser />
-          </Activity.Avatar>
+        {/* `login` event */}
+        <Activity.Item>
+          <Activity.Content>
+            <Activity.Avatar>
+              <OpenInBrowser />
+            </Activity.Avatar>
 
-          <Text>
-            <strong>nlemmon86</strong> logged in.
-            <Activity.ViewMore />
-          </Text>
+            <Text>
+              <strong>nlemmon86</strong> logged in.
+              <Activity.ViewMore />
+            </Text>
 
-          <Activity.Time>12:22 PM</Activity.Time>
-        </Activity.Content>
-      </Activity.Item>
-    </Activity>
+            <Activity.Time>12:22 PM</Activity.Time>
+          </Activity.Content>
+        </Activity.Item>
+      </Activity>
+
+      <Modal open={isModalOpen}>
+        <Panel title="Activity Event Details">
+          <Panel.Section>
+            <p>Here are some event details</p>
+          </Panel.Section>
+
+          <Panel.Section>
+            <Button variant="secondary" onClick={() => setModalOpen(false)}>
+              Cool!
+            </Button>
+          </Panel.Section>
+        </Panel>
+      </Modal>
+    </>
   );
 }
