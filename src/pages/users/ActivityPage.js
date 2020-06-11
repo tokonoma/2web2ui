983b5d69f364e857916e1aaa3208c40f1e0acaff
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-06-10 22:19:34 -0400

    hackathon-whatsinstore - Totally abandoned infinite scrolling hook - this works

--- src/pages/users/ActivityPage.js
+++ src/pages/users/ActivityPage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import _ from 'lodash';
 import moment from 'moment';
 import { useQuery } from 'react-query';
-import { Page, Panel } from 'src/components/matchbox';
+import { Button, Page, Panel } from 'src/components/matchbox';
 import { PageDescription } from 'src/components/text';
 import { Empty } from 'src/components';
 import DatePicker from 'src/components/datePicker/DatePicker';
@@ -12,19 +12,28 @@ import ActivityList from './components/ActivityList';
 
 export default function ActivityPage() {
   const now = new Date();
+  const [limit, setLimit] = useState(50);
   const [datePicker, setDatePicker] = useState({
     relativeRange: 'custom',
     from: moment(now - 7 * 24 * 3600 * 1000),
     to: now,
     precision: 'hour',
   });
-  const { status, data } = useQuery(['activities', datePicker], getActivities);
+  const { status, data, refetch } = useQuery(
+    ['activities', { ...datePicker, limit }],
+    getActivities,
+  );
 
   const handleChange = e => {
     setDatePicker({
       ...datePicker,
       ...e,
     });
+  };
+
+  const handleLoadMore = () => {
+    setLimit(limit + 50);
+    refetch();
   };
 
   return (
@@ -52,13 +61,17 @@ export default function ActivityPage() {
           )}
 
           {status === 'success' && data.length > 0 && <ActivityList activities={data} />}
+
+          <Button variant="secondary" onClick={handleLoadMore} disabled={status === 'loading'}>
+            Load More
+          </Button>
         </Panel.Section>
       </Panel>
     </Page>
   );
 }
 
-async function getActivities(key, { from, to, limit = 500, tenant_id = 'spc', customer_id = 107 }) {
+async function getActivities(key, { from, to, limit, tenant_id = 'spc', customer_id = 107 }) {
   const { data } = await axios.get(
     'https://drbgqnye23.execute-api.us-east-1.amazonaws.com/tst/activity',
     {
