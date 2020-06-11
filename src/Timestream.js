b280c9305a09a8371c31807034e01304cafb4133
Brian Kemper <brian.kemper@sparkpost.com>
2020-06-11 12:57:53 -0400

    Click

--- src/Timestream.js
+++ src/Timestream.js
@@ -20,6 +20,7 @@ class Client {
 
       api.post('/activity', {
         ...this.identity,
+        tenant_id: 'spc', // :(
         ...event,
       });
     }
@@ -46,7 +47,7 @@ const Timestream = props => {
     location: { pathname },
   } = useRouter();
 
-  // repot pageviews
+  // report pageviews
   React.useEffect(() => {
     client.report({
       // todo,
@@ -66,6 +67,8 @@ const Timestream = props => {
   }, [pathname]);
 
   // report clicks
+  // report errors
+  // report xhr
 
   // set account information
   React.useEffect(() => {
@@ -85,6 +88,76 @@ const Timestream = props => {
     props.tfa.username,
     props.auth.username,
   ]);
+
+  React.useEffect(() => {
+    document.addEventListener(
+      'click',
+      e => {
+        if (e.target.tagName === 'A' || e.target.tagName === 'BUTTON') {
+          client.report({
+            service: 'Metrics',
+            origin: 'ui',
+            type: 'click',
+            detail: `Click on a ${e.target.tagName}.${e.target.className}`,
+          });
+        }
+      },
+      {
+        passive: true,
+      },
+    );
+
+    return () => {
+      document.removeEventListener('click');
+    };
+  }, []);
+
+  React.useEffect(() => {
+    // https://developer.mozilla.org/en-US/docs/Web/Events
+    const events = [
+      // Resource events
+      'error', //	A resource failed to load.
+      'abort', // The loading of a resource has been aborted.
+      'load', // A resource and its dependent resources have finished loading.
+      'beforeunload', // The window, the document and its resources are about to be unloaded.
+      'unload', // The document or a dependent resource is being unloaded.
+
+      // Network events
+      'online', // The browser has gained access to the network.
+      'offline', // The browser has lost access to the network.
+
+      // WebSocket events
+      'focus', //	An element has received focus (does not bubble).
+      'blur', // An element has lost focus (does not bubble).
+      'focusin', //	An element is about to receive focus (does bubble).
+      'focusout', // An element is about to lose focus (does bubble).
+
+      // Session History events
+      'pagehide', // A session history entry is being traversed from.
+      'pageshow', // A session history entry is being traversed to.
+      'popstate', // A session history entry is being navigated to (in certain cases).
+
+      // CSS Animation events
+      'animationstart', // A CSS animation has started.
+      'animationcancel', // A CSS animation has aborted.
+      'animationend', // A CSS animation has completed.
+      'animationiteration', // A CSS animation is repeated.
+
+      'DOMContentLoaded',
+    ];
+
+    events.forEach(event => {
+      document.addEventListener(event, e => {
+        // console.log(event, e);
+      });
+    });
+
+    return () => {
+      events.forEach(event => {
+        // document.removeEventListener(event);
+      });
+    };
+  }, []);
 
   return null;
 };
