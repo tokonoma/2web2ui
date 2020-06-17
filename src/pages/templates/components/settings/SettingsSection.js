c804e82a42f476e63df519633ecaee10ab92c9f3
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-06-17 09:23:04 -0400

    FE-1096 - Incorporate 'Template Settings' heading

--- src/pages/templates/components/settings/SettingsSection.js
+++ src/pages/templates/components/settings/SettingsSection.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Panel } from 'src/components/matchbox';
+import { ScreenReaderOnly, Panel } from 'src/components/matchbox';
 import SettingsForm from './SettingsForm.Container';
 import styles from './SettingsSection.module.scss';
 
 const SettingsSection = () => (
-  <Panel className={styles.SettingsSection} title="Template Settings">
+  <Panel className={styles.SettingsSection}>
+    <ScreenReaderOnly>
+      <h3>Template Settings</h3>
+    </ScreenReaderOnly>
+
     <SettingsForm />
   </Panel>
 );
